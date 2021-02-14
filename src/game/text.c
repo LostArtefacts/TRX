e0ace57d7600cb155c7b80e28a14f4ee2f083a87
rr- <rr-@sakuya.pl>
2021-02-14 16:46:20 +0100

    port T_RightAlign

--- src/game/text.c
+++ src/game/text.c
@@ -20,8 +20,18 @@ void __cdecl T_CentreV(TEXTSTRING* textstring, int16_t b)
     }
 }
 
+void __cdecl T_RightAlign(TEXTSTRING* textstring, int16_t b)
+{
+    if (b) {
+        textstring->flags |= TF_RIGHT;
+    } else {
+        textstring->flags &= ~TF_RIGHT;
+    }
+}
+
 void TR1MInjectText()
 {
     INJECT(0x004399A0, T_CentreH);
     INJECT(0x004399C0, T_CentreV);
+    INJECT(0x004399E0, T_RightAlign);
 }
--- src/game/text.h
+++ src/game/text.h
@@ -8,12 +8,12 @@
 #define T_InitPrint             ((void          __cdecl(*)())0x00439750)
 #define T_Print                 ((TEXTSTRING*   __cdecl(*)(int16_t xpos, int16_t ypos, int16_t zpos, const char *string))0x00439780)
 #define T_RemovePrint           ((void          __cdecl(*)(TEXTSTRING *text_string))0x00439AD0)
-#define T_RightAlign            ((void          __cdecl(*)(TEXTSTRING *text_string, int16_t enable))0x004399E0)
 #define T_ChangeText            ((void          __cdecl(*)(TEXTSTRING *text_string, const char *string))0x00439860)
 // clang-format on
 
 void __cdecl T_CentreH(TEXTSTRING* textstring, int16_t b);
 void __cdecl T_CentreV(TEXTSTRING* textstring, int16_t b);
+void __cdecl T_RightAlign(TEXTSTRING* textstring, int16_t b);
 
 void TR1MInjectText();
 
