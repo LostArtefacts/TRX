da3ee26f8c30c62a1f87185bf27207c5a51d8d63
rr- <rr-@sakuya.pl>
2021-02-20 23:22:47 +0100

    fix missing Demo Mode text

--- src/game/demo.c
+++ src/game/demo.c
@@ -39,10 +39,17 @@ int32_t __cdecl StartDemo()
         SeedRandomDraw(0xD371F947);
         SeedRandomControl(0xD371F947);
 
+#ifdef TOMB1M_FEAT_UI
+        txt = T_Print(0, -16, 0, "Demo Mode");
+        T_FlashText(txt, 1, 20);
+        T_BottomAlign(txt, 1);
+        T_CentreH(txt, 1);
+#else
         txt = T_Print(0, DumpHeight / 2 - 16, 0, "Demo Mode");
         T_FlashText(txt, 1, 20);
         T_CentreV(txt, 1);
         T_CentreH(txt, 1);
+#endif
 
         GameLoop(1);
 
