1abfbfdb2a7d293b006724501664475e9dffe4f9
rr- <rr-@sakuya.pl>
2021-02-24 13:03:12 +0100

    port CreatureTurn

--- src/game/box.c
+++ src/game/box.c
@@ -975,6 +975,40 @@ int32_t CreatureAnimation(int16_t item_num, int16_t angle, int16_t tilt)
     return 1;
 }
 
+int16_t CreatureTurn(ITEM_INFO* item, int16_t maximum_turn)
+{
+    CREATURE_INFO* creature = item->data;
+
+    if (!creature) {
+        return 0;
+    }
+
+    if (!item->speed || !maximum_turn) {
+        return 0;
+    }
+
+    int32_t x = creature->target.x - item->pos.x;
+    int32_t z = creature->target.z - item->pos.z;
+    int16_t angle = phd_atan(z, x) - item->pos.y_rot;
+    int32_t range = (item->speed << 14) / maximum_turn;
+
+    if (angle > FRONT_ARC || angle < -FRONT_ARC) {
+        if (SQUARE(x) + SQUARE(z) < SQUARE(range)) {
+            maximum_turn >>= 1;
+        }
+    }
+
+    if (angle > maximum_turn) {
+        angle = maximum_turn;
+    } else if (angle < -maximum_turn) {
+        angle = -maximum_turn;
+    }
+
+    item->pos.y_rot += angle;
+
+    return angle;
+}
+
 void CreatureTilt(ITEM_INFO* item, int16_t angle)
 {
     angle = angle * 4 - item->pos.z_rot;
@@ -998,4 +1032,5 @@ void T1MInjectGameBox()
     INJECT(0x0040ED30, CreatureCreature);
     INJECT(0x0040EE40, BadFloor);
     INJECT(0x0040EEE0, CreatureAnimation);
+    INJECT(0x0040F750, CreatureTurn);
 }
--- src/game/box.h
+++ src/game/box.h
@@ -5,7 +5,6 @@
 #include <stdint.h>
 
 // clang-format off
-#define CreatureTurn            ((PHD_ANGLE    (*)(ITEM_INFO *item, PHD_ANGLE maximum_turn))0x0040F750)
 #define CreatureHead            ((void         (*)(ITEM_INFO* item, int16_t required))0x0040F870)
 #define CreatureEffect          ((int16_t      (*)(ITEM_INFO* item, BITE_INFO* bite, int16_t(*generate)(int32_t x, int32_t y, int32_t z, int16_t speed, PHD_ANGLE yrot, int16_t room_number)))0x0040F8C0)
 // clang-format on
@@ -25,6 +24,7 @@ int32_t BadFloor(
     int32_t x, int32_t y, int32_t z, int16_t box_height, int16_t next_height,
     int16_t room_number, LOT_INFO* LOT);
 int32_t CreatureAnimation(int16_t item_num, int16_t angle, int16_t tilt);
+int16_t CreatureTurn(ITEM_INFO* item, int16_t maximum_turn);
 void CreatureTilt(ITEM_INFO* item, int16_t angle);
 
 void T1MInjectGameBox();
