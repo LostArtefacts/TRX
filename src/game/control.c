0ea5bab8eea1c01321c1fe9c70eafcf9f6744179
rr- <rr-@sakuya.pl>
2021-02-25 17:38:48 +0100

    port AnimateItem

--- src/game/control.c
+++ src/game/control.c
@@ -150,6 +150,113 @@ int32_t ControlPhase(int32_t nframes, int demo_mode)
     return 0;
 }
 
+void AnimateItem(ITEM_INFO* item)
+{
+    item->touch_bits = 0;
+    item->hit_status = 0;
+
+    ANIM_STRUCT* anim = &Anims[item->anim_number];
+
+    item->frame_number++;
+
+    if (anim->number_changes > 0) {
+        if (GetChange(item, anim)) {
+            anim = &Anims[item->anim_number];
+            item->current_anim_state = anim->current_anim_state;
+
+            if (item->required_anim_state == item->current_anim_state) {
+                item->required_anim_state = 0;
+            }
+        }
+    }
+
+    if (item->frame_number > anim->frame_end) {
+        if (anim->number_commands > 0) {
+            int16_t* command = &AnimCommands[anim->command_index];
+            for (int i = 0; i < anim->number_commands; i++) {
+                switch (*command++) {
+                case AC_MOVE_ORIGIN:
+                    TranslateItem(item, command[0], command[1], command[2]);
+                    command += 3;
+                    break;
+
+                case AC_JUMP_VELOCITY:
+                    item->fall_speed = command[0];
+                    item->speed = command[1];
+                    item->gravity_status = 1;
+                    command += 2;
+                    break;
+
+                case AC_DEACTIVATE:
+                    item->status = IS_DEACTIVATED;
+                    break;
+
+                case AC_SOUND_FX:
+                case AC_EFFECT:
+                    command += 2;
+                    break;
+                }
+            }
+        }
+
+        item->anim_number = anim->jump_anim_num;
+        item->frame_number = anim->jump_frame_num;
+
+        anim = &Anims[item->anim_number];
+        item->current_anim_state = anim->current_anim_state;
+        item->goal_anim_state = item->current_anim_state;
+        if (item->required_anim_state == item->current_anim_state) {
+            item->required_anim_state = 0;
+        }
+    }
+
+    if (anim->number_commands > 0) {
+        int16_t* command = &AnimCommands[anim->command_index];
+        for (int i = 0; i < anim->number_commands; i++) {
+            switch (*command++) {
+            case AC_MOVE_ORIGIN:
+                command += 3;
+                break;
+
+            case AC_JUMP_VELOCITY:
+                command += 2;
+                break;
+
+            case AC_SOUND_FX:
+                if (item->frame_number == command[0]) {
+                    SoundEffect(
+                        command[1], &item->pos,
+                        RoomInfo[item->room_number].flags);
+                }
+                command += 2;
+                break;
+
+            case AC_EFFECT:
+                if (item->frame_number == command[0]) {
+                    EffectRoutines[command[1]](item);
+                }
+                command += 2;
+                break;
+            }
+        }
+    }
+
+    if (!item->gravity_status) {
+        int32_t speed = anim->velocity;
+        if (anim->acceleration) {
+            speed +=
+                anim->acceleration * (item->frame_number - anim->frame_base);
+        }
+        item->speed = speed >> 16;
+    } else {
+        item->fall_speed += (item->fall_speed < FASTFALL_SPEED) ? GRAVITY : 1;
+        item->pos.y += item->fall_speed;
+    }
+
+    item->pos.x += (phd_sin(item->pos.y_rot) * item->speed) >> W2V_SHIFT;
+    item->pos.z += (phd_cos(item->pos.y_rot) * item->speed) >> W2V_SHIFT;
+}
+
 void TranslateItem(ITEM_INFO* item, int32_t x, int32_t y, int32_t z)
 {
     int32_t c = phd_cos(item->pos.y_rot);
@@ -163,5 +270,6 @@ void TranslateItem(ITEM_INFO* item, int32_t x, int32_t y, int32_t z)
 void T1MInjectGameControl()
 {
     INJECT(0x004133B0, ControlPhase);
+    INJECT(0x00413660, AnimateItem);
     INJECT(0x00413A10, TranslateItem);
 }
