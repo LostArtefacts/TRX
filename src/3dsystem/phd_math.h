9d0fb98f24b32e55a3d524ab6a11fc62fec44e40
rr- <rr-@sakuya.pl>
2021-03-03 15:53:57 +0100

    port phd_atan

--- src/3dsystem/phd_math.h
+++ src/3dsystem/phd_math.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 
 // clang-format off
-#define phd_atan                ((int32_t      (*)(int32_t x, int32_t y))0x0042A8A0)
 #define phd_sqrt                ((int32_t      (*)(int32_t angle))0x0042A900)
 // clang-format on
 
 int32_t phd_cos(int32_t angle);
 int32_t phd_sin(int32_t angle);
+int32_t phd_atan(int32_t x, int32_t y);
 
 void T1MInject3DSystemPHDMath();
 
