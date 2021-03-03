a725bc0758857ea3e58dde70f4d44bb1cbebe5e5
rr- <rr-@sakuya.pl>
2021-03-03 14:11:46 +0100

    port EnableBaddieAI

--- src/game/lot.c
+++ src/game/lot.c
@@ -1,6 +1,7 @@
 #include "game/lot.h"
 #include "game/vars.h"
 #include "specific/init.h"
+#include "specific/shed.h"
 #include "util.h"
 
 void InitialiseLOTArray()
@@ -28,8 +29,59 @@ void DisableBaddieAI(int16_t item_num)
     }
 }
 
+int32_t EnableBaddieAI(int16_t item_num, int32_t always)
+{
+    if (Items[item_num].data) {
+        return 1;
+    }
+
+    if (SlotsUsed < NUM_SLOTS) {
+        for (int32_t slot = 0; slot < NUM_SLOTS; slot++) {
+            CREATURE_INFO* creature = &BaddieSlots[slot];
+            if (creature->item_num == NO_ITEM) {
+                InitialiseSlot(item_num, slot);
+                return 1;
+            }
+        }
+        S_ExitSystem("UnpauseBaddie() grimmer!");
+    }
+
+    int32_t worst_dist = 0;
+    if (!always) {
+        ITEM_INFO* item = &Items[item_num];
+        int32_t x = (item->pos.x - Camera.pos.x) >> 8;
+        int32_t y = (item->pos.y - Camera.pos.y) >> 8;
+        int32_t z = (item->pos.z - Camera.pos.z) >> 8;
+        worst_dist = SQUARE(x) + SQUARE(y) + SQUARE(z);
+    }
+
+    int32_t worst_slot = -1;
+    for (int32_t slot = 0; slot < NUM_SLOTS; slot++) {
+        CREATURE_INFO* creature = &BaddieSlots[slot];
+        ITEM_INFO* item = &Items[creature->item_num];
+        int32_t x = (item->pos.x - Camera.pos.x) >> 8;
+        int32_t y = (item->pos.y - Camera.pos.y) >> 8;
+        int32_t z = (item->pos.z - Camera.pos.z) >> 8;
+        int32_t dist = SQUARE(x) + SQUARE(y) + SQUARE(z);
+        if (dist > worst_dist) {
+            worst_dist = dist;
+            worst_slot = slot;
+        }
+    }
+
+    if (worst_slot < 0) {
+        return 0;
+    }
+
+    Items[BaddieSlots[worst_slot].item_num].status = IS_INVISIBLE;
+    DisableBaddieAI(BaddieSlots[worst_slot].item_num);
+    InitialiseSlot(item_num, worst_slot);
+    return 1;
+}
+
 void T1MInjectGameLOT()
 {
     INJECT(0x0042A300, InitialiseLOTArray);
     INJECT(0x0042A360, DisableBaddieAI);
+    INJECT(0x0042A3A0, EnableBaddieAI);
 }
--- src/game/lot.h
+++ src/game/lot.h
@@ -5,11 +5,12 @@
 
 // clang-format off
 #define InitialiseLOT           ((void          (*)())0x0042A780)
-#define EnableBaddieAI          ((int32_t       (*)(int16_t item_num, int32_t))0x0042A3A0)
+#define InitialiseSlot          ((void          (*)(int16_t item_num, int32_t slot))0x0042A570)
 // clang-format on
 
 void InitialiseLOTArray();
 void DisableBaddieAI(int16_t item_num);
+int32_t EnableBaddieAI(int16_t item_num, int32_t always);
 
 void T1MInjectGameLOT();
 
