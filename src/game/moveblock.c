d46d9219835b2c073fead5767654419cc79f9688
rr- <rr-@sakuya.pl>
2021-03-04 16:02:47 +0100

    port MovableBlockControl

--- src/game/moveblock.c
+++ src/game/moveblock.c
@@ -1,3 +1,6 @@
+#include "game/control.h"
+#include "game/effects.h"
+#include "game/items.h"
 #include "game/moveblock.h"
 #include "game/types.h"
 #include "game/vars.h"
@@ -12,7 +15,52 @@ void InitialiseMovableBlock(int16_t item_num)
     }
 }
 
+// original name: MovableBlock
+void MovableBlockControl(int16_t item_num)
+{
+    ITEM_INFO* item = &Items[item_num];
+
+    if (item->flags & IF_ONESHOT) {
+        AlterFloorHeight(item, 1024);
+        KillItem(item_num);
+        return;
+    }
+
+    AnimateItem(item);
+
+    int16_t room_num = item->room_number;
+    FLOOR_INFO* floor =
+        GetFloor(item->pos.x, item->pos.y, item->pos.z, &room_num);
+    int32_t height = GetHeight(floor, item->pos.x, item->pos.y, item->pos.z);
+
+    if (item->pos.y < height) {
+        item->gravity_status = 1;
+    } else if (item->gravity_status) {
+        item->gravity_status = 0;
+        item->pos.y = height;
+        item->status = IS_DEACTIVATED;
+        FxDinoStomp(item);
+        SoundEffect(70, &item->pos, 0);
+    }
+
+    if (item->room_number != room_num) {
+        ItemNewRoom(item_num, room_num);
+    }
+
+    if (item->status == IS_DEACTIVATED) {
+        item->status = IS_NOT_ACTIVE;
+        RemoveActiveItem(item_num);
+        AlterFloorHeight(item, -1024);
+
+        room_num = item->room_number;
+        floor = GetFloor(item->pos.x, item->pos.y, item->pos.z, &room_num);
+        GetHeight(floor, item->pos.x, item->pos.y, item->pos.z);
+        TestTriggers(TriggerIndex, 1);
+    }
+}
+
 void T1MInjectGameMoveBlock()
 {
     INJECT(0x0042B430, InitialiseMovableBlock);
+    INJECT(0x0042B460, MovableBlockControl);
 }
--- src/game/moveblock.h
+++ src/game/moveblock.h
@@ -5,7 +5,6 @@
 
 // clang-format off
 #define MovableBlockCollision   ((void          (*)(int16_t item_num, ITEM_INFO* litem, COLL_INFO* coll))0x0042B5B0)
-#define MovableBlockControl     ((void          (*)(int16_t item_num))0x0042B460)
 #define InitialiseRollingBlock  ((void          (*)(int16_t item_num))0x0042BB90)
 #define RollingBlockControl     ((void          (*)(int16_t item_num))0x0042BBC0)
 #define AlterFloorHeight        ((void          (*)(ITEM_INFO* item, int32_t height))0x0042BCA0)
@@ -13,6 +12,7 @@
 // clang-format on
 
 void InitialiseMovableBlock(int16_t item_num);
+void MovableBlockControl(int16_t item_num);
 
 void T1MInjectGameMoveBlock();
 
