49b8f632a7db92304cd663cd995c57789b2d83d8
rr- <rr-@sakuya.pl>
2021-03-05 22:07:04 +0100

    port VoleControl

--- src/game/rat.h
+++ src/game/rat.h
@@ -3,11 +3,8 @@
 
 #include <stdint.h>
 
-// clang-format off
-#define VoleControl             ((void          (*)(int16_t item_num))0x00434210)
-// clang-format on
-
 void RatControl(int16_t item_num);
+void VoleControl(int16_t item_num);
 
 void T1MInjectGameRat();
 
