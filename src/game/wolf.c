4513b31b882605014aacbe245e27df4c714c93c2
rr- <rr-@sakuya.pl>
2021-03-05 23:09:58 +0100

    port LionControl

--- src/game/wolf.c
+++ src/game/wolf.c
@@ -22,6 +22,17 @@
 #define WOLF_HOWL_CHANCE 384
 #define WOLF_TOUCH 0x774F
 
+#define LION_BITE_DAMAGE 250
+#define LION_POUNCE_DAMAGE 150
+#define LION_TOUCH 0x380066
+#define LION_WALK_TURN (2 * PHD_DEGREE) // = 364
+#define LION_RUN_TURN (5 * PHD_DEGREE) // = 910
+#define LION_ROAR_CHANCE 128
+#define LION_POUNCE_RANGE SQUARE(WALL_L) // = 1048576
+#define LION_DIE_ANIM 7
+
+#define PUMA_DIE_ANIM 4
+
 typedef enum {
     WOLF_EMPTY = 0,
     WOLF_STOP = 1,
@@ -38,7 +49,19 @@ typedef enum {
     WOLF_BITE = 12,
 } WOLF_ANIM;
 
+typedef enum {
+    LION_EMPTY = 0,
+    LION_STOP = 1,
+    LION_WALK = 2,
+    LION_RUN = 3,
+    LION_ATTACK1 = 4,
+    LION_DEATH = 5,
+    LION_WARNING = 6,
+    LION_ATTACK2 = 7,
+} LION_ANIM;
+
 static BITE_INFO WolfJawBite = { 0, -14, 174, 6 };
+static BITE_INFO LionBite = { -2, -10, 132, 21 };
 
 void InitialiseWolf(int16_t item_num)
 {
@@ -202,8 +225,120 @@ void WolfControl(int16_t item_num)
     CreatureAnimation(item_num, angle, tilt);
 }
 
+void LionControl(int16_t item_num)
+{
+    ITEM_INFO* item = &Items[item_num];
+
+    if (item->status == IS_INVISIBLE) {
+        if (!EnableBaddieAI(item_num, 0)) {
+            return;
+        }
+        item->status = IS_ACTIVE;
+    }
+
+    CREATURE_INFO* lion = item->data;
+    int16_t head = 0;
+    int16_t angle = 0;
+    int16_t tilt = 0;
+
+    if (item->hit_points <= 0) {
+        if (item->current_anim_state != LION_DEATH) {
+            item->current_anim_state = LION_DEATH;
+            if (item->object_number == O_PUMA) {
+                item->anim_number = Objects[O_PUMA].anim_index + PUMA_DIE_ANIM
+                    + (int16_t)(GetRandomControl() / 0x4000);
+            } else if (item->object_number == O_LION) {
+                item->anim_number = Objects[O_LION].anim_index + LION_DIE_ANIM
+                    + (int16_t)(GetRandomControl() / 0x4000);
+            } else {
+                item->anim_number = Objects[O_LIONESS].anim_index
+                    + LION_DIE_ANIM + (int16_t)(GetRandomControl() / 0x4000);
+            }
+            item->frame_number = Anims[item->anim_number].frame_base;
+        }
+    } else {
+        AI_INFO info;
+        CreatureAIInfo(item, &info);
+
+        if (info.ahead) {
+            head = info.angle;
+        }
+
+        CreatureMood(item, &info, 1);
+
+        angle = CreatureTurn(item, lion->maximum_turn);
+
+        switch (item->current_anim_state) {
+        case LION_STOP:
+            if (item->required_anim_state) {
+                item->goal_anim_state = item->required_anim_state;
+            } else if (lion->mood == MOOD_BORED) {
+                item->goal_anim_state = LION_WALK;
+            } else if (info.ahead && (item->touch_bits & LION_TOUCH)) {
+                item->goal_anim_state = LION_ATTACK2;
+            } else if (info.ahead && info.distance < LION_POUNCE_RANGE) {
+                item->goal_anim_state = LION_ATTACK1;
+            } else {
+                item->goal_anim_state = LION_RUN;
+            }
+            break;
+
+        case LION_WALK:
+            lion->maximum_turn = LION_WALK_TURN;
+            if (lion->mood != MOOD_BORED) {
+                item->goal_anim_state = LION_STOP;
+            } else if (GetRandomControl() < LION_ROAR_CHANCE) {
+                item->required_anim_state = LION_WARNING;
+                item->goal_anim_state = LION_STOP;
+            }
+            break;
+
+        case LION_RUN:
+            lion->maximum_turn = LION_RUN_TURN;
+            tilt = angle;
+            if (lion->mood == MOOD_BORED) {
+                item->goal_anim_state = LION_STOP;
+            } else if (info.ahead && info.distance < LION_POUNCE_RANGE) {
+                item->goal_anim_state = LION_STOP;
+            } else if ((item->touch_bits & LION_TOUCH) && info.ahead) {
+                item->goal_anim_state = LION_STOP;
+            } else if (
+                lion->mood != MOOD_ESCAPE
+                && GetRandomControl() < LION_ROAR_CHANCE) {
+                item->required_anim_state = LION_WARNING;
+                item->goal_anim_state = LION_STOP;
+            }
+            break;
+
+        case LION_ATTACK1:
+            if (item->required_anim_state == LION_EMPTY
+                && (item->touch_bits & LION_TOUCH)) {
+                LaraItem->hit_points -= LION_POUNCE_DAMAGE;
+                LaraItem->hit_status = 1;
+                item->required_anim_state = LION_STOP;
+            }
+            break;
+
+        case LION_ATTACK2:
+            if (item->required_anim_state == LION_EMPTY
+                && (item->touch_bits & LION_TOUCH)) {
+                CreatureEffect(item, &LionBite, DoBloodSplat);
+                LaraItem->hit_points -= LION_BITE_DAMAGE;
+                LaraItem->hit_status = 1;
+                item->required_anim_state = LION_STOP;
+            }
+            break;
+        }
+    }
+
+    CreatureTilt(item, tilt);
+    CreatureHead(item, head);
+    CreatureAnimation(item_num, angle, tilt);
+}
+
 void T1MInjectGameWolf()
 {
     INJECT(0x0043DF20, InitialiseWolf);
     INJECT(0x0043DF50, WolfControl);
+    INJECT(0x0043E390, LionControl);
 }
--- src/game/wolf.h
+++ src/game/wolf.h
@@ -3,12 +3,9 @@
 
 #include <stdint.h>
 
-// clang-format off
-#define LionControl             ((void          (*)(int16_t item_num))0x0043E390)
-// clang-format on
-
 void InitialiseWolf(int16_t item_num);
 void WolfControl(int16_t item_num);
+void LionControl(int16_t item_num);
 
 void T1MInjectGameWolf();
 
