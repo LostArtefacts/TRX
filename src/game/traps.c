07d9438e3e30688de2ae28436b8b63971af8680a
rr- <rr-@sakuya.pl>
2021-03-07 17:45:01 +0100

    port SpikeCollision

--- src/game/traps.c
+++ src/game/traps.c
@@ -11,6 +11,7 @@
 #include "util.h"
 
 #define ROLLINGBALL_DAMAGE_AIR 100
+#define SPIKE_DAMAGE 15
 
 void InitialiseRollingBall(int16_t item_num)
 {
@@ -162,6 +163,49 @@ void RollingBallCollision(
     }
 }
 
+void SpikeCollision(int16_t item_num, ITEM_INFO* lara_item, COLL_INFO* coll)
+{
+    ITEM_INFO* item = &Items[item_num];
+    if (lara_item->hit_points < 0) {
+        return;
+    }
+
+    if (!TestBoundsCollide(item, lara_item, coll->radius)) {
+        return;
+    }
+    if (!TestCollision(item, lara_item)) {
+        return;
+    }
+
+    int32_t num = GetRandomControl() / 24576;
+    if (lara_item->gravity_status) {
+        if (lara_item->fall_speed > 0) {
+            lara_item->hit_points = -1;
+            num = 20;
+        }
+    } else if (lara_item->speed < 30) {
+        return;
+    }
+
+    lara_item->hit_points -= SPIKE_DAMAGE;
+    while (num > 0) {
+        int32_t x = lara_item->pos.x + (GetRandomControl() - 0x4000) / 256;
+        int32_t z = lara_item->pos.z + (GetRandomControl() - 0x4000) / 256;
+        int32_t y = lara_item->pos.y - GetRandomControl() / 64;
+        DoBloodSplat(x, y, z, 20, GetRandomControl(), item->room_number);
+        num--;
+    }
+
+    if (lara_item->hit_points <= 0) {
+        lara_item->current_anim_state = AS_DEATH;
+        lara_item->goal_anim_state = AS_DEATH;
+        lara_item->anim_number = AA_SPIKE_DEATH;
+        lara_item->frame_number = AF_SPIKE_DEATH;
+        lara_item->pos.y = item->pos.y;
+        lara_item->gravity_status = 0;
+    }
+}
+
 void FlameControl(int16_t fx_num)
 {
     FX_INFO* fx = &Effects[fx_num];
@@ -280,6 +324,7 @@ void T1MInjectGameTraps()
     INJECT(0x0043A010, InitialiseRollingBall);
     INJECT(0x0043A050, RollingBallControl);
     INJECT(0x0043A2B0, RollingBallCollision);
+    INJECT(0x0043A520, SpikeCollision);
     INJECT(0x0043B2A0, FlameControl);
     INJECT(0x0043B430, LavaBurn);
 }
