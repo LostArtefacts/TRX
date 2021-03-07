678842182e9f6e6e8ee69184bad994632c4a8a50
rr- <rr-@sakuya.pl>
2021-03-07 20:39:44 +0100

    port GetJointAbsPosition

--- src/game/sphere.c
+++ src/game/sphere.c
@@ -130,8 +130,62 @@ int32_t GetSpheres(ITEM_INFO* item, SPHERE* ptr, int32_t world_space)
     return object->nmeshes;
 }
 
+void GetJointAbsPosition(ITEM_INFO* item, PHD_VECTOR* vec, int32_t joint)
+{
+    OBJECT_INFO* object = &Objects[item->object_number];
+
+    phd_PushUnitMatrix();
+    PhdMatrixPtr->_03 = 0;
+    PhdMatrixPtr->_13 = 0;
+    PhdMatrixPtr->_23 = 0;
+
+    phd_RotYXZ(item->pos.y_rot, item->pos.x_rot, item->pos.z_rot);
+
+    int16_t* frame = GetBestFrame(item);
+    phd_TranslateRel(
+        frame[FRAME_POS_X], frame[FRAME_POS_Y], frame[FRAME_POS_Z]);
+
+    int32_t* packed_rotation = (int32_t*)(frame + FRAME_ROT);
+    phd_RotYXZpack(*packed_rotation++);
+
+    int32_t* bone = &AnimBones[object->bone_index];
+
+    int16_t* extra_rotation = (int16_t*)item->data;
+    for (int i = 0; i < joint; i++) {
+        int32_t bone_extra_flags = bone[0];
+        if (bone_extra_flags & BEB_POP) {
+            phd_PopMatrix();
+        }
+        if (bone_extra_flags & BEB_PUSH) {
+            phd_PushMatrix();
+        }
+
+        phd_TranslateRel(bone[1], bone[2], bone[3]);
+        phd_RotYXZpack(*packed_rotation++);
+
+        if (bone_extra_flags & BEB_ROT_Y) {
+            phd_RotY(*extra_rotation++);
+        }
+        if (bone_extra_flags & BEB_ROT_X) {
+            phd_RotX(*extra_rotation++);
+        }
+        if (bone_extra_flags & BEB_ROT_Z) {
+            phd_RotZ(*extra_rotation++);
+        }
+
+        bone += 4;
+    }
+
+    phd_TranslateRel(vec->x, vec->y, vec->z);
+    vec->x = (PhdMatrixPtr->_03 >> W2V_SHIFT) + item->pos.x;
+    vec->y = (PhdMatrixPtr->_13 >> W2V_SHIFT) + item->pos.y;
+    vec->z = (PhdMatrixPtr->_23 >> W2V_SHIFT) + item->pos.z;
+    phd_PopMatrix();
+}
+
 void T1MInjectGameSphere()
 {
     INJECT(0x00439130, TestCollision);
     INJECT(0x00439260, GetSpheres);
+    INJECT(0x00439550, GetJointAbsPosition);
 }
--- src/game/sphere.h
+++ src/game/sphere.h
@@ -5,12 +5,12 @@
 #include <stdint.h>
 
 // clang-format off
-#define GetJointAbsPosition ((void          (*)(ITEM_INFO* item, PHD_VECTOR* vec, int32_t joint))0x00439550)
 #define BaddieBiteEffect    ((void          (*)(ITEM_INFO *item, BITE_INFO *bite))0x004396F0)
 // clang-format on
 
 int32_t TestCollision(ITEM_INFO* item, ITEM_INFO* lara_item);
 int32_t GetSpheres(ITEM_INFO* item, SPHERE* slist, int32_t world_space);
+void GetJointAbsPosition(ITEM_INFO* item, PHD_VECTOR* vec, int32_t joint);
 
 void T1MInjectGameSphere();
 
