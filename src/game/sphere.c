199e42a2f766da19a5f8afda4169754cc681d655
rr- <rr-@sakuya.pl>
2021-03-07 20:30:57 +0100

    port GetSpheres

--- src/game/sphere.c
+++ src/game/sphere.c
@@ -1,4 +1,7 @@
+#include "3dsystem/3d_gen.h"
+#include "game/draw.h"
 #include "game/sphere.h"
+#include "game/vars.h"
 #include "util.h"
 
 int32_t TestCollision(ITEM_INFO* item, ITEM_INFO* lara_item)
@@ -37,7 +40,98 @@ int32_t TestCollision(ITEM_INFO* item, ITEM_INFO* lara_item)
     return flags;
 }
 
+int32_t GetSpheres(ITEM_INFO* item, SPHERE* ptr, int32_t world_space)
+{
+    static int16_t null_rotation[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    if (!item) {
+        return 0;
+    }
+
+    int32_t x;
+    int32_t y;
+    int32_t z;
+    if (world_space) {
+        x = item->pos.x;
+        y = item->pos.y;
+        z = item->pos.z;
+        phd_PushUnitMatrix();
+        PhdMatrixPtr->_03 = 0;
+        PhdMatrixPtr->_13 = 0;
+        PhdMatrixPtr->_23 = 0;
+    } else {
+        x = 0;
+        y = 0;
+        z = 0;
+        phd_PushMatrix();
+        phd_TranslateAbs(item->pos.x, item->pos.y, item->pos.z);
+    }
+
+    phd_RotYXZ(item->pos.y_rot, item->pos.x_rot, item->pos.z_rot);
+
+    int16_t* frame = GetBestFrame(item);
+    phd_TranslateRel(
+        frame[FRAME_POS_X], frame[FRAME_POS_Y], frame[FRAME_POS_Z]);
+
+    int32_t* packed_rotation = (int32_t*)(frame + FRAME_ROT);
+    phd_RotYXZpack(*packed_rotation++);
+
+    OBJECT_INFO* object = &Objects[item->object_number];
+    int16_t** meshpp = &Meshes[object->mesh_index];
+    int32_t* bone = &AnimBones[object->bone_index];
+
+    int16_t* objptr = *meshpp++;
+    phd_PushMatrix();
+    phd_TranslateRel(objptr[0], objptr[1], objptr[2]);
+    ptr->x = x + (PhdMatrixPtr->_03 >> W2V_SHIFT);
+    ptr->y = y + (PhdMatrixPtr->_13 >> W2V_SHIFT);
+    ptr->z = z + (PhdMatrixPtr->_23 >> W2V_SHIFT);
+    ptr->r = objptr[3];
+    ptr++;
+    phd_PopMatrix();
+
+    int16_t* extra_rotation = item->data ? item->data : &null_rotation;
+    for (int i = 1; i < object->nmeshes; i++) {
+        int32_t bone_extra_flags = bone[0];
+        if (bone_extra_flags & BEB_POP) {
+            phd_PopMatrix();
+        }
+        if (bone_extra_flags & BEB_PUSH) {
+            phd_PushMatrix();
+        }
+
+        phd_TranslateRel(bone[1], bone[2], bone[3]);
+        phd_RotYXZpack(*packed_rotation++);
+
+        if (bone_extra_flags & BEB_ROT_Y) {
+            phd_RotY(*extra_rotation++);
+        }
+        if (bone_extra_flags & BEB_ROT_X) {
+            phd_RotX(*extra_rotation++);
+        }
+        if (bone_extra_flags & BEB_ROT_Z) {
+            phd_RotZ(*extra_rotation++);
+        }
+
+        objptr = *meshpp++;
+        phd_PushMatrix();
+        phd_TranslateRel(objptr[0], objptr[1], objptr[2]);
+        ptr->x = x + (PhdMatrixPtr->_03 >> W2V_SHIFT);
+        ptr->y = y + (PhdMatrixPtr->_13 >> W2V_SHIFT);
+        ptr->z = z + (PhdMatrixPtr->_23 >> W2V_SHIFT);
+        ptr->r = objptr[3];
+        phd_PopMatrix();
+
+        ptr++;
+        bone += 4;
+    }
+
+    phd_PopMatrix();
+    return object->nmeshes;
+}
+
 void T1MInjectGameSphere()
 {
     INJECT(0x00439130, TestCollision);
+    INJECT(0x00439260, GetSpheres);
 }
