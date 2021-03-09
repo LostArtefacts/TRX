fbda2741179c017b3e514ed9850a6a3421182d31
rr- <rr-@sakuya.pl>
2021-03-09 23:30:30 +0100

    port CreateStartInfo

--- src/game/savegame.c
+++ src/game/savegame.c
@@ -1,3 +1,4 @@
+#include "game/inv.h"
 #include "game/savegame.h"
 #include "game/vars.h"
 #include "util.h"
@@ -75,8 +76,60 @@ void ModifyStartInfo(int32_t level_num)
     }
 }
 
+void CreateStartInfo(int level_num)
+{
+    START_INFO *start = &SaveGame[0].start[level_num];
+
+    start->available = 1;
+    start->costume = 0;
+
+    start->pistol_ammo = 1000;
+    if (Inv_RequestItem(O_GUN_ITEM)) {
+        start->got_pistols = 1;
+    } else {
+        start->got_pistols = 0;
+    }
+
+    if (Inv_RequestItem(O_MAGNUM_ITEM)) {
+        start->magnum_ammo = Lara.magnums.ammo;
+        start->got_magnums = 1;
+    } else {
+        start->magnum_ammo = Inv_RequestItem(O_MAG_AMMO_ITEM) * MAGNUM_AMMO_QTY;
+        start->got_magnums = 0;
+    }
+
+    if (Inv_RequestItem(O_UZI_ITEM)) {
+        start->uzi_ammo = Lara.uzis.ammo;
+        start->got_uzis = 1;
+    } else {
+        start->uzi_ammo = Inv_RequestItem(O_UZI_AMMO_ITEM) * UZI_AMMO_QTY;
+        start->got_uzis = 0;
+    }
+
+    if (Inv_RequestItem(O_SHOTGUN_ITEM)) {
+        start->shotgun_ammo = Lara.shotgun.ammo;
+        start->got_shotgun = 1;
+    } else {
+        start->shotgun_ammo =
+            Inv_RequestItem(O_SG_AMMO_ITEM) * SHOTGUN_AMMO_QTY;
+        start->got_shotgun = 0;
+    }
+
+    start->num_medis = Inv_RequestItem(O_MEDI_ITEM);
+    start->num_big_medis = Inv_RequestItem(O_BIGMEDI_ITEM);
+    start->num_scions = Inv_RequestItem(O_SCION_ITEM);
+
+    start->gun_type = Lara.gun_type;
+    if (Lara.gun_status == LGS_READY) {
+        start->gun_status = LGS_READY;
+    } else {
+        start->gun_status = LGS_ARMLESS;
+    }
+}
+
 void T1MInjectGameSaveGame()
 {
     INJECT(0x004344D0, InitialiseStartInfo);
     INJECT(0x00434520, ModifyStartInfo);
+    INJECT(0x004345E0, CreateStartInfo)
 }
--- src/game/savegame.h
+++ src/game/savegame.h
@@ -4,13 +4,13 @@
 #include <stdint.h>
 
 // clang-format off
-#define CreateStartInfo         ((void          (*)(int32_t level_num))0x004345E0)
 #define CreateSaveGameInfo      ((void          (*)())0x00434720)
 #define ExtractSaveGameInfo     ((void          (*)())0x00434F90)
 // clang-format on
 
 void InitialiseStartInfo();
 void ModifyStartInfo(int32_t level_num);
+void CreateStartInfo(int level_num);
 
 void T1MInjectGameSaveGame();
 
