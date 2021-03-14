fd2c7af7c9bce7227a2c3bf6d4413ada4d0d9ad6
rr- <rr-@sakuya.pl>
2021-03-14 16:20:31 +0100

    port S_CDPlay

--- src/specific/sndpc.c
+++ src/specific/sndpc.c
@@ -1,5 +1,6 @@
 #include "specific/sndpc.h"
 
+#include "game/sound.h"
 #include "game/vars.h"
 
 #include "util.h"
@@ -14,8 +15,44 @@ void S_CDVolume(int16_t volume)
     auxSetVolume(AuxDeviceID, volume_aux);
 }
 
+void S_CDPlay(int16_t track)
+{
+    if (!OptionMusicVolume) {
+        return;
+    }
+
+    if (track == 13) {
+        SoundEffect(SFX_SECRET, NULL, SPM_ALWAYS);
+    } else if (track <= 2 || track >= 22) {
+        if (CDTrack > 0) {
+            if (CDTrack >= 26 && CDTrack <= 56) {
+                StopSoundEffect(SFX_GYM_HINT_01 + CDTrack - 26, NULL);
+            } else {
+                CDStop();
+            }
+            CDTrack = 0;
+        }
+        if (track >= 26 && track <= 56) {
+            SoundEffect(SFX_GYM_HINT_01 + track - 26, NULL, SPM_ALWAYS);
+            CDTrack = track;
+        } else if (track == 2) {
+            CDPlay(2);
+            CDTrack = 2;
+        } else if (track >= 22 && track <= 25) {
+            CDPlay(track - 15);
+            CDTrack = track;
+        } else {
+            if (track > 56) {
+                CDPlay(track - 54);
+            }
+            CDTrack = track;
+        }
+    }
+}
+
 void T1MInjectSpecificSndPC()
 {
+    INJECT(0x00438D40, S_CDPlay);
     // NOTE: this is a nullsub in OG and is called in many different places
     // for many different purposes so it's not injected.
     // INJECT(0x00437F30, S_CDVolume);
--- src/specific/sndpc.h
+++ src/specific/sndpc.h
@@ -4,14 +4,16 @@
 #include <stdint.h>
 
 // clang-format off
+#define CDStop                  ((void          (*)())0x00437F80)
+#define CDPlay                  ((void          (*)(int16_t track))0x00437FB0)
 #define S_CDLoop                ((void          (*)())0x004380B0)
 #define S_CDStop                ((void          (*)())0x00438E40)
-#define S_CDPlay                ((void          (*)(int16_t track))0x00438D40)
 #define S_SoundStopAllSamples   ((void          (*)())0x00438CC0)
 #define S_StartSyncedAudio      ((void          (*)(int32_t num))0x00439030)
 #define SoundInit               ((int32_t       (*)())0x00437E00)
 // clang-format on
 
+void S_CDPlay(int16_t track);
 void S_CDVolume(int16_t volume);
 
 void T1MInjectSpecificSndPC();
