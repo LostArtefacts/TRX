LostArtefacts/TRX

commit 1a93e85ab43941fd6b525457f9aafb32676a73b0

fix gcc warnings on Linux

src/specific/ati.c
@@ -207,66 +207,66 @@ C3D_EC ATI3DCIF_NullSub()
     return C3D_EC_GENFAIL;
 }
 
-C3D_EC __stdcall ATI3DCIF_GetInfo(PC3D_3DCIFINFO info)
+C3D_EC ATI3DCIF_GetInfo(PC3D_3DCIFINFO info)
 {
     return (*ATI3DCIF_GetInfo_lib)(info);
 }
 
-C3D_EC __stdcall ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap)
+C3D_EC ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap)
 {
     return (*ATI3DCIF_TextureReg_lib)(ptmapToReg, phtmap);
 }
 
-C3D_EC __stdcall ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg)
+C3D_EC ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg)
 {
     return (*ATI3DCIF_TextureUnreg_lib)(htxToUnreg);
 }
 
-C3D_EC __stdcall ATI3DCIF_TexturePaletteCreate(
+C3D_EC ATI3DCIF_TexturePaletteCreate(
     C3D_ECI_TMAP_TYPE epalette, void *pPalette, C3D_PHTXPAL phtpalCreated)
 {
     return (*ATI3DCIF_TexturePaletteCreate_lib)(
         epalette, pPalette, phtpalCreated);
 }
 
-C3D_EC __stdcall ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy)
+C3D_EC ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy)
 {
     return (*ATI3DCIF_TexturePaletteDestroy_lib)(htxpalToDestroy);
 }
 
-C3D_HRC __stdcall ATI3DCIF_ContextCreate()
+C3D_HRC ATI3DCIF_ContextCreate()
 {
     return (*ATI3DCIF_ContextCreate_lib)();
 }
 
-C3D_EC __stdcall ATI3DCIF_ContextDestroy(C3D_HRC hRC)
+C3D_EC ATI3DCIF_ContextDestroy(C3D_HRC hRC)
 {
     return (*ATI3DCIF_ContextDestroy_lib)(hRC);
 }
 
-C3D_EC __stdcall ATI3DCIF_ContextSetState(
+C3D_EC ATI3DCIF_ContextSetState(
     C3D_HRC hRC, C3D_ERSID eRStateID, C3D_PRSDATA pRStateData)
 {
     return (*ATI3DCIF_ContextSetState_lib)(hRC, eRStateID, pRStateData);
 }
 
-C3D_EC __stdcall ATI3DCIF_RenderBegin(C3D_HRC hRC)
+C3D_EC ATI3DCIF_RenderBegin(C3D_HRC hRC)
 {
     return (*ATI3DCIF_RenderBegin_lib)(hRC);
 }
 
-C3D_EC __stdcall ATI3DCIF_RenderEnd()
+C3D_EC ATI3DCIF_RenderEnd()
 {
     return (*ATI3DCIF_RenderEnd_lib)();
 }
 
-C3D_EC __stdcall ATI3DCIF_RenderPrimStrip(
+C3D_EC ATI3DCIF_RenderPrimStrip(
     C3D_VSTRIP vStrip, C3D_UINT32 u32NumVert)
 {
     return (*ATI3DCIF_RenderPrimStrip_lib)(vStrip, u32NumVert);
 }
 
-C3D_EC __stdcall ATI3DCIF_RenderPrimList(C3D_VLIST vList, C3D_UINT32 u32NumVert)
+C3D_EC ATI3DCIF_RenderPrimList(C3D_VLIST vList, C3D_UINT32 u32NumVert)
 {
     return (*ATI3DCIF_RenderPrimList_lib)(vList, u32NumVert);
 }

src/specific/ati.h
@@ -446,21 +446,21 @@ C3D_EC InitATI3DCIF();
 C3D_EC ShutdownATI3DCIF();
 
 C3D_EC ATI3DCIF_NullSub();
-C3D_EC __stdcall ATI3DCIF_GetInfo(C3D_3DCIFINFO *info);
-C3D_EC __stdcall ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap);
-C3D_EC __stdcall ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg);
-C3D_EC __stdcall ATI3DCIF_TexturePaletteCreate(
+C3D_EC ATI3DCIF_GetInfo(C3D_3DCIFINFO *info);
+C3D_EC ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap);
+C3D_EC ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg);
+C3D_EC ATI3DCIF_TexturePaletteCreate(
     C3D_ECI_TMAP_TYPE epalette, void *pPalette, C3D_PHTXPAL phtpalCreated);
-C3D_EC __stdcall ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy);
-C3D_HRC __stdcall ATI3DCIF_ContextCreate();
-C3D_EC __stdcall ATI3DCIF_ContextDestroy(C3D_HRC hRC);
-C3D_EC __stdcall ATI3DCIF_ContextSetState(
+C3D_EC ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy);
+C3D_HRC ATI3DCIF_ContextCreate();
+C3D_EC ATI3DCIF_ContextDestroy(C3D_HRC hRC);
+C3D_EC ATI3DCIF_ContextSetState(
     C3D_HRC hRC, C3D_ERSID eRStateID, C3D_PRSDATA pRStateData);
-C3D_EC __stdcall ATI3DCIF_RenderBegin(C3D_HRC hRC);
-C3D_EC __stdcall ATI3DCIF_RenderEnd();
-C3D_EC __stdcall ATI3DCIF_RenderPrimStrip(
+C3D_EC ATI3DCIF_RenderBegin(C3D_HRC hRC);
+C3D_EC ATI3DCIF_RenderEnd();
+C3D_EC ATI3DCIF_RenderPrimStrip(
     C3D_VSTRIP vStrip, C3D_UINT32 u32NumVert);
-C3D_EC __stdcall ATI3DCIF_RenderPrimList(
+C3D_EC ATI3DCIF_RenderPrimList(
     C3D_VLIST vList, C3D_UINT32 u32NumVert);
 
 void T1MInjectSpecificATI();

test/test.h
@@ -12,9 +12,9 @@ extern size_t fails;
 #define TEST_RESULTS()                                                         \
     do {                                                                       \
         if (fails == 0) {                                                      \
-            printf("ALL TESTS PASSED (%d/%d)\n", tests, tests);                \
+            printf("ALL TESTS PASSED (%lu/%lu)\n", tests, tests);                \
         } else {                                                               \
-            printf("SOME TESTS FAILED (%d/%d)\n", tests - fails, tests);       \
+            printf("SOME TESTS FAILED (%lu/%lu)\n", tests - fails, tests);       \
         }                                                                      \
     } while (0)
 
@@ -33,12 +33,13 @@ extern size_t fails;
         if (!(equality)) {                                                     \
             fails++;                                                           \
             printf(                                                            \
-                "%s:%d (" format " != " format ")\n", __FILE__, __LINE__, (a), \
+                "%s:%u (" format " != " format ")\n", __FILE__, __LINE__, (a), \
                 (b));                                                          \
         }                                                                      \
     } while (0)
 
 #define ASSERT_INT_EQUAL(a, b) ASSERT_EQUAL_BASE((a) == (b), a, b, "%d")
+#define ASSERT_ULONG_EQUAL(a, b) ASSERT_EQUAL_BASE((a) == (b), a, b, "%lu")
 #define ASSERT_STR_EQUAL(a, b) ASSERT_EQUAL_BASE(!strcmp(a, b), a, b, "%s")
 
 #endif

src/filesystem.h
@@ -1,7 +1,7 @@
 #ifndef T1M_FILESYSTEM_H
 #define T1M_FILESYSTEM_H
 
-#include <stdint.h>
+#include <stddef.h>
 
 typedef enum {
     FILE_SEEK_SET,

src/game/ai/bear.c
@@ -94,7 +94,7 @@ void BearControl(int16_t item_num)
             bear->flags = 1;
         }
 
-        switch ((signed __int16)item->current_anim_state) {
+        switch ((int16_t)item->current_anim_state) {
         case BEAR_STOP:
             if (dead_enemy) {
                 if (info.bite && info.distance < BEAR_EAT_RANGE) {

src/specific/file.h
@@ -1,6 +1,7 @@
 #ifndef T1M_SPECIFIC_FILE_H
 #define T1M_SPECIFIC_FILE_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 int32_t LoadLevel(const char *filename, int32_t level_num);

test/test_gameflow.c
@@ -24,7 +24,7 @@ static char *read_file_text(const char *path)
 static void
 assert_json_keys_equal(struct json_object_s *obj1, struct json_object_s *obj2)
 {
-    ASSERT_INT_EQUAL(obj1->length, obj2->length);
+    ASSERT_ULONG_EQUAL(obj1->length, obj2->length);
     struct json_object_element_s *obj1_elem = obj1->start;
     struct json_object_element_s *obj2_elem = obj2->start;
     while (obj1_elem) {

commit 7d8e15277497a9ebb2f630ad1c88eaa704b73ca4

fix crash related to ATI3DCIF calls
This fixes the regression from 1a93e85ab43941fd6b525457f9aafb32676a73b0.

src/specific/ati.c
@@ -207,66 +207,66 @@ C3D_EC ATI3DCIF_NullSub()
     return C3D_EC_GENFAIL;
 }
 
-C3D_EC ATI3DCIF_GetInfo(PC3D_3DCIFINFO info)
+C3D_EC __stdcall ATI3DCIF_GetInfo(PC3D_3DCIFINFO info)
 {
     return (*ATI3DCIF_GetInfo_lib)(info);
 }
 
-C3D_EC ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap)
+C3D_EC __stdcall ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap)
 {
     return (*ATI3DCIF_TextureReg_lib)(ptmapToReg, phtmap);
 }
 
-C3D_EC ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg)
+C3D_EC __stdcall ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg)
 {
     return (*ATI3DCIF_TextureUnreg_lib)(htxToUnreg);
 }
 
-C3D_EC ATI3DCIF_TexturePaletteCreate(
+C3D_EC __stdcall ATI3DCIF_TexturePaletteCreate(
     C3D_ECI_TMAP_TYPE epalette, void *pPalette, C3D_PHTXPAL phtpalCreated)
 {
     return (*ATI3DCIF_TexturePaletteCreate_lib)(
         epalette, pPalette, phtpalCreated);
 }
 
-C3D_EC ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy)
+C3D_EC __stdcall ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy)
 {
     return (*ATI3DCIF_TexturePaletteDestroy_lib)(htxpalToDestroy);
 }
 
-C3D_HRC ATI3DCIF_ContextCreate()
+C3D_HRC __stdcall ATI3DCIF_ContextCreate()
 {
     return (*ATI3DCIF_ContextCreate_lib)();
 }
 
-C3D_EC ATI3DCIF_ContextDestroy(C3D_HRC hRC)
+C3D_EC __stdcall ATI3DCIF_ContextDestroy(C3D_HRC hRC)
 {
     return (*ATI3DCIF_ContextDestroy_lib)(hRC);
 }
 
-C3D_EC ATI3DCIF_ContextSetState(
+C3D_EC __stdcall ATI3DCIF_ContextSetState(
     C3D_HRC hRC, C3D_ERSID eRStateID, C3D_PRSDATA pRStateData)
 {
     return (*ATI3DCIF_ContextSetState_lib)(hRC, eRStateID, pRStateData);
 }
 
-C3D_EC ATI3DCIF_RenderBegin(C3D_HRC hRC)
+C3D_EC __stdcall ATI3DCIF_RenderBegin(C3D_HRC hRC)
 {
     return (*ATI3DCIF_RenderBegin_lib)(hRC);
 }
 
-C3D_EC ATI3DCIF_RenderEnd()
+C3D_EC __stdcall ATI3DCIF_RenderEnd()
 {
     return (*ATI3DCIF_RenderEnd_lib)();
 }
 
-C3D_EC ATI3DCIF_RenderPrimStrip(
+C3D_EC __stdcall ATI3DCIF_RenderPrimStrip(
     C3D_VSTRIP vStrip, C3D_UINT32 u32NumVert)
 {
     return (*ATI3DCIF_RenderPrimStrip_lib)(vStrip, u32NumVert);
 }
 
-C3D_EC ATI3DCIF_RenderPrimList(C3D_VLIST vList, C3D_UINT32 u32NumVert)
+C3D_EC __stdcall ATI3DCIF_RenderPrimList(C3D_VLIST vList, C3D_UINT32 u32NumVert)
 {
     return (*ATI3DCIF_RenderPrimList_lib)(vList, u32NumVert);
 }

src/specific/ati.h
@@ -446,21 +446,21 @@ C3D_EC InitATI3DCIF();
 C3D_EC ShutdownATI3DCIF();
 
 C3D_EC ATI3DCIF_NullSub();
-C3D_EC ATI3DCIF_GetInfo(C3D_3DCIFINFO *info);
-C3D_EC ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap);
-C3D_EC ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg);
-C3D_EC ATI3DCIF_TexturePaletteCreate(
+C3D_EC __stdcall ATI3DCIF_GetInfo(C3D_3DCIFINFO *info);
+C3D_EC __stdcall ATI3DCIF_TextureReg(C3D_PTMAP ptmapToReg, C3D_PHTX phtmap);
+C3D_EC __stdcall ATI3DCIF_TextureUnreg(C3D_HTX htxToUnreg);
+C3D_EC __stdcall ATI3DCIF_TexturePaletteCreate(
     C3D_ECI_TMAP_TYPE epalette, void *pPalette, C3D_PHTXPAL phtpalCreated);
-C3D_EC ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy);
-C3D_HRC ATI3DCIF_ContextCreate();
-C3D_EC ATI3DCIF_ContextDestroy(C3D_HRC hRC);
-C3D_EC ATI3DCIF_ContextSetState(
+C3D_EC __stdcall ATI3DCIF_TexturePaletteDestroy(C3D_HTXPAL htxpalToDestroy);
+C3D_HRC __stdcall ATI3DCIF_ContextCreate();
+C3D_EC __stdcall ATI3DCIF_ContextDestroy(C3D_HRC hRC);
+C3D_EC __stdcall ATI3DCIF_ContextSetState(
     C3D_HRC hRC, C3D_ERSID eRStateID, C3D_PRSDATA pRStateData);
-C3D_EC ATI3DCIF_RenderBegin(C3D_HRC hRC);
-C3D_EC ATI3DCIF_RenderEnd();
-C3D_EC ATI3DCIF_RenderPrimStrip(
+C3D_EC __stdcall ATI3DCIF_RenderBegin(C3D_HRC hRC);
+C3D_EC __stdcall ATI3DCIF_RenderEnd();
+C3D_EC __stdcall ATI3DCIF_RenderPrimStrip(
     C3D_VSTRIP vStrip, C3D_UINT32 u32NumVert);
-C3D_EC ATI3DCIF_RenderPrimList(
+C3D_EC __stdcall ATI3DCIF_RenderPrimList(
     C3D_VLIST vList, C3D_UINT32 u32NumVert);
 
 void T1MInjectSpecificATI();

test/test.h
@@ -12,9 +12,9 @@ extern size_t fails;
 #define TEST_RESULTS()                                                         \
     do {                                                                       \
         if (fails == 0) {                                                      \
-            printf("ALL TESTS PASSED (%lu/%lu)\n", tests, tests);                \
+            printf("ALL TESTS PASSED (%lu/%lu)\n", tests, tests);              \
         } else {                                                               \
-            printf("SOME TESTS FAILED (%lu/%lu)\n", tests - fails, tests);       \
+            printf("SOME TESTS FAILED (%lu/%lu)\n", tests - fails, tests);     \
         }                                                                      \
     } while (0)
 