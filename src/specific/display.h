84294b867dc4e925b71952d5c776fd58b0702ef8
rr- <dash@wind.garden>
2021-10-15 21:37:25 +0200

    port S_CopyBufferToScreen

--- src/specific/display.h
+++ src/specific/display.h
@@ -3,16 +3,13 @@
 
 #include <stdint.h>
 
-// clang-format off
-#define S_CopyBufferToScreen    ((void          (*)())0x00416A60)
-// clang-format on
-
 void SetupScreenSize();
 void TempVideoAdjust(int32_t hi_res, double sizer);
 void TempVideoRemove();
 void S_NoFade();
 void S_FadeInInventory(int32_t fade);
 void S_FadeOutInventory(int32_t fade);
+void S_CopyBufferToScreen();
 
 void T1MInjectSpecificDisplay();
 
