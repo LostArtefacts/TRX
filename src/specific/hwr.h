a1a1922d170539c445bf38cf4ae98d8ed25870ea
rr- <dash@wind.garden>
2021-10-23 21:01:38 +0200

    port HWR_DrawFlatTriangle

--- src/specific/hwr.h
+++ src/specific/hwr.h
@@ -17,7 +17,6 @@
 #define HWR_DrawSprite              ((void      (*)(int32_t x1, int32_t x2, int32_t y1, int32_t y2, int32_t z, int16_t sprnum, int16_t shade))0x0040C425)
 #define HWR_InitPolyList            ((void      (*)())0x0040D0F7)
 #define HWR_OutputPolyList          ((void      (*)())0x0040D2E0)
-#define HWR_DrawFlatTriangle        ((void (*)(PHD_VBUF *vn1, PHD_VBUF *vn2, PHD_VBUF *vn3, int32_t color))0x00409C0F)
 #define HWR_DrawTexturedTriangle    ((void (*)(PHD_VBUF *vn1, PHD_VBUF *vn2, PHD_VBUF *vn3, int16_t tpage, uint16_t *u1, uint16_t *u2, uint16_t *u3))0x0040B510)
 #define HWR_DrawTexturedQuad        ((void (*)(PHD_VBUF *vn1, PHD_VBUF *vn2, PHD_VBUF *vn3, PHD_VBUF *vn4, uint16_t tpage, uint16_t *u1, uint16_t *u2, uint16_t *u3, uint16_t *u4))0x0040BBE2)
 // clang-format on
@@ -62,6 +61,9 @@ void HWR_PrepareFMV();
 void HWR_FMVDone();
 void HWR_FMVInit();
 void HWR_SetupRenderContextAndRender();
+
+void HWR_DrawFlatTriangle(
+    PHD_VBUF *vn1, PHD_VBUF *vn2, PHD_VBUF *vn3, int32_t color);
 
 const int16_t *HWR_InsertObjectG3(const int16_t *obj_ptr, int32_t number);
 const int16_t *HWR_InsertObjectG4(const int16_t *obj_ptr, int32_t number);
