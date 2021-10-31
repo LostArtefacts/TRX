35cd0eaf43b1a352c03aaafc23b866da5c78928c
rr- <dash@wind.garden>
2021-10-31 20:57:42 +0100

    vars: pull InvItemMagnumAmmo

--- src/game/inv.h
+++ src/game/inv.h
@@ -41,6 +41,7 @@ extern INVENTORY_ITEM InvItemUzi;
 extern INVENTORY_ITEM InvItemGrenade;
 extern INVENTORY_ITEM InvItemPistolAmmo;
 extern INVENTORY_ITEM InvItemShotgunAmmo;
+extern INVENTORY_ITEM InvItemMagnumAmmo;
 
 extern TEXTSTRING *InvItemText[2];
 extern TEXTSTRING *InvRingText;
