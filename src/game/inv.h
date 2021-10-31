2757b704d1025c7d29ff673e67fc0cae2f43dd40
rr- <dash@wind.garden>
2021-10-31 20:47:22 +0100

    vars: pull InvItemKey3

--- src/game/inv.h
+++ src/game/inv.h
@@ -32,6 +32,7 @@ extern INVENTORY_ITEM InvItemPuzzle3;
 extern INVENTORY_ITEM InvItemPuzzle4;
 extern INVENTORY_ITEM InvItemKey1;
 extern INVENTORY_ITEM InvItemKey2;
+extern INVENTORY_ITEM InvItemKey3;
 
 extern TEXTSTRING *InvItemText[2];
 extern TEXTSTRING *InvRingText;
