010f784a774ee9967fc257a721bdf1525e675814
rr- <dash@wind.garden>
2021-11-05 22:51:01 +0100

    s/music: rename globals

--- src/specific/music.c
+++ src/specific/music.c
@@ -3,9 +3,9 @@
 #include "global/vars_platform.h"
 #include "log.h"
 
-static uint32_t MCIDeviceID = 0;
-static uint32_t AuxDeviceID = 0;
-static int32_t MusicNumTracks = 0;
+static uint32_t S_Music_MCIDeviceID = 0;
+static uint32_t S_Music_AuxDeviceID = 0;
+static int32_t S_Music_NumTracks = 0;
 
 bool S_Music_Init()
 {
@@ -22,22 +22,23 @@ bool S_Music_Init()
         LOG_ERROR("cannot initailize music device: %x", result);
         return false;
     }
-    MCIDeviceID = open_parms.wDeviceID;
+    S_Music_MCIDeviceID = open_parms.wDeviceID;
 
-    AuxDeviceID = 0;
+    S_Music_AuxDeviceID = 0;
     for (int i = 0; i < auxGetNumDevs(); i++) {
         AUXCAPSA caps;
         auxGetDevCapsA((UINT_PTR)i, &caps, sizeof(AUXCAPSA));
         if (caps.wTechnology == AUXCAPS_CDAUDIO) {
-            AuxDeviceID = i;
+            S_Music_AuxDeviceID = i;
         }
     }
 
     MCI_STATUS_PARMS status_parms;
     status_parms.dwItem = MCI_STATUS_NUMBER_OF_TRACKS;
     mciSendCommandA(
-        MCIDeviceID, MCI_STATUS, MCI_STATUS_ITEM, (DWORD_PTR)&status_parms);
-    MusicNumTracks = status_parms.dwReturn;
+        S_Music_MCIDeviceID, MCI_STATUS, MCI_STATUS_ITEM,
+        (DWORD_PTR)&status_parms);
+    S_Music_NumTracks = status_parms.dwReturn;
     return true;
 }
 
@@ -45,7 +46,7 @@ void S_Music_AdjustVolume(int16_t volume)
 {
     int32_t volume_aux = volume * 0xFFFF / 0xFF;
     volume_aux |= volume_aux << 16;
-    auxSetVolume(AuxDeviceID, volume_aux);
+    auxSetVolume(S_Music_AuxDeviceID, volume_aux);
 }
 
 void S_Music_Pause()
@@ -54,7 +55,7 @@ void S_Music_Pause()
     MCI_GENERIC_PARMS pause_parms;
 
     result = mciSendCommandA(
-        MCIDeviceID, MCI_PAUSE, MCI_WAIT, (DWORD_PTR)&pause_parms);
+        S_Music_MCIDeviceID, MCI_PAUSE, MCI_WAIT, (DWORD_PTR)&pause_parms);
     if (result) {
         LOG_ERROR("Error while calling mciSendCommandA: 0x%lx", result);
     }
@@ -66,7 +67,7 @@ void S_Music_Unpause()
     MCI_GENERIC_PARMS pause_parms;
 
     result = mciSendCommandA(
-        MCIDeviceID, MCI_RESUME, MCI_WAIT, (DWORD_PTR)&pause_parms);
+        S_Music_MCIDeviceID, MCI_RESUME, MCI_WAIT, (DWORD_PTR)&pause_parms);
     if (result) {
         LOG_ERROR("Error while calling mciSendCommandA: 0x%lx", result);
     }
@@ -77,7 +78,8 @@ bool S_Music_Play(int16_t track)
     MCI_SET_PARMS set_parms;
     set_parms.dwTimeFormat = MCI_FORMAT_TMSF;
     if (mciSendCommandA(
-            MCIDeviceID, MCI_SET, MCI_SET_TIME_FORMAT, (DWORD_PTR)&set_parms)) {
+            S_Music_MCIDeviceID, MCI_SET, MCI_SET_TIME_FORMAT,
+            (DWORD_PTR)&set_parms)) {
         return false;
     }
 
@@ -86,13 +88,13 @@ bool S_Music_Play(int16_t track)
     open_parms.dwCallback = (DWORD_PTR)TombHWND;
 
     DWORD_PTR dwFlags = MCI_NOTIFY | MCI_FROM;
-    if (track != MusicNumTracks) {
+    if (track != S_Music_NumTracks) {
         open_parms.dwTo = track + 1;
         dwFlags |= MCI_TO;
     }
 
     if (mciSendCommandA(
-            MCIDeviceID, MCI_PLAY, dwFlags, (DWORD_PTR)&open_parms)) {
+            S_Music_MCIDeviceID, MCI_PLAY, dwFlags, (DWORD_PTR)&open_parms)) {
         return false;
     }
 
@@ -103,5 +105,5 @@ bool S_Music_Stop()
 {
     MCI_GENERIC_PARMS gen_parms;
     return !mciSendCommandA(
-        MCIDeviceID, MCI_STOP, MCI_WAIT, (DWORD_PTR)&gen_parms);
+        S_Music_MCIDeviceID, MCI_STOP, MCI_WAIT, (DWORD_PTR)&gen_parms);
 }
