685a48e1b30d4400d96d96c968a74f3f01e944b4
rr- <dash@wind.garden>
2021-11-20 22:16:07 +0100

    s/fmv: treat winplay errors as non-fatal

--- src/specific/s_fmv.h
+++ src/specific/s_fmv.h
@@ -1,9 +1,10 @@
 #ifndef T1M_SPECIFIC_S_FMV_H
 #define T1M_SPECIFIC_S_FMV_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
-void S_FMV_Init();
+bool S_FMV_Init();
 void S_FMV_Play(const char *file_path);
 
 #endif
