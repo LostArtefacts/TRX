2a9daf9a8af372a6530721e24779a9c28537e484
rr- <dash@wind.garden>
2021-11-20 22:40:57 +0100

    s/picture: load images with libavcodec

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,6 +9,19 @@ if [ ! -f /app/build/build.ninja ]; then
     fi
 fi
 
+cp /usr/lib/gcc/i686-w64-mingw32/9.3-posix/libgomp-1.dll /app/build/
+cp /usr/lib/gcc/i686-w64-mingw32/9.3-posix/libstdc++-6.dll /app/build/
+cp /usr/lib/gcc/i686-w64-mingw32/9.3-posix/libgcc_s_sjlj-1.dll /app/build/
+cp /usr/i686-w64-mingw32/lib/libwinpthread-1.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/swscale-6.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/swresample-4.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/avutil-57.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/avfilter-8.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/avcodec-59.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/avdevice-59.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/zlib1.dll /app/build/
+cp /usr/i686-w64-mingw32/bin/avformat-59.dll /app/build/
+
 cd /app/build; meson compile
 
 if [ "$TARGET" = release ]; then
