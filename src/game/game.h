189c81df1ecc687698ff2c14fd4476d6a06939b7
rr- <dash@wind.garden>
2021-12-16 17:07:42 +0100

    game: inline GetSavedGamesList

--- src/game/game.h
+++ src/game/game.h
@@ -7,7 +7,6 @@
 int32_t StartGame(int32_t level_num, GAMEFLOW_LEVEL_TYPE level_type);
 int32_t StopGame();
 int32_t GameLoop(int32_t demo_mode);
-void GetSavedGamesList(REQUEST_INFO *req);
 int32_t LevelCompleteSequence(int32_t level_num);
 void LevelStats(int32_t level_num);
 int32_t S_LoadGame(SAVEGAME_INFO *save, int32_t slot);
