aed6b6d26b76d9bf263bcd5240881644c7d5410d
walkawayy <81546780+walkawayy@users.noreply.github.com>
2022-01-08 23:05:19 -0500

    input: allow holding down forward/back to move through saves
    Resolves #171.

--- src/game/input.c
+++ src/game/input.c
@@ -1,6 +1,7 @@
 #include "game/input.h"
 
 #include "specific/s_input.h"
+#include "log.h"
 
 INPUT_STATE g_Input = { 0 };
 INPUT_STATE g_InputDB = { 0 };
@@ -12,6 +13,25 @@ INPUT_STATE Input_GetDebounced(INPUT_STATE input)
 {
     INPUT_STATE result;
     result.any = input.any & ~g_OldInputDB.any;
+
+    // Allow holding down key to move faster
+    static int32_t holdBack = -12;
+    static int32_t holdForward = -12;
+
+    if (input.forward || !input.back) {
+        holdBack = -12;
+    } else if (input.back && ++holdBack >= 3) {
+        result.back = 1;
+        holdBack = 0;
+    }
+
+    if (!input.forward || input.back) {
+        holdForward = -12;
+    } else if (input.forward && ++holdForward >= 3) {
+        result.forward = 1;
+        holdForward = 0;
+    }
+
     g_OldInputDB = input;
     return result;
 }
