1289ec630b3701fa67a567f8e392dc270d41e6fe
walkawayy <81546780+walkawayy@users.noreply.github.com>
2022-01-11 12:08:27 -0500

    input: use constants for input debounce

--- src/game/input.c
+++ src/game/input.c
@@ -2,11 +2,14 @@
 
 #include "specific/s_input.h"
 
+#define DELAY_FRAMES 12
+#define HOLD_FRAMES 3
+
 INPUT_STATE g_Input = { 0 };
 INPUT_STATE g_InputDB = { 0 };
 INPUT_STATE g_OldInputDB = { 0 };
-static int32_t m_HoldBack = -12;
-static int32_t m_HoldForward = -12;
+static int32_t m_HoldBack = 0;
+static int32_t m_HoldForward = 0;
 
 static INPUT_STATE Input_GetDebounced(INPUT_STATE input);
 
@@ -17,17 +20,17 @@ INPUT_STATE Input_GetDebounced(INPUT_STATE input)
 
     // Allow holding down key to move faster
     if (input.forward || !input.back) {
-        m_HoldBack = -12;
-    } else if (input.back && ++m_HoldBack >= 3) {
-        result.back = 1;
         m_HoldBack = 0;
+    } else if (input.back && ++m_HoldBack >= DELAY_FRAMES + HOLD_FRAMES) {
+        result.back = 1;
+        m_HoldBack = DELAY_FRAMES;
     }
 
     if (!input.forward || input.back) {
-        m_HoldForward = -12;
-    } else if (input.forward && ++m_HoldForward >= 3) {
-        result.forward = 1;
         m_HoldForward = 0;
+    } else if (input.forward && ++m_HoldForward >= DELAY_FRAMES + HOLD_FRAMES) {
+        result.forward = 1;
+        m_HoldForward = DELAY_FRAMES;
     }
 
     g_OldInputDB = input;
