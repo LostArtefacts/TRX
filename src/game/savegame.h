85e46bd97bd98fdd79588eeb07253ecebf68a89a
rr- <dash@wind.garden>
2022-01-13 02:19:06 +0100

    savegame: make functions private

--- src/game/savegame.h
+++ src/game/savegame.h
@@ -11,17 +11,6 @@ void CreateStartInfo(int level_num);
 void CreateSaveGameInfo();
 void ExtractSaveGameInfo();
 
-void ResetSG();
-void SkipSG(int size);
-void ReadSG(void *pointer, int size);
-void ReadSGARM(LARA_ARM *arm);
-void ReadSGLara(LARA_INFO *lara);
-void ReadSGLOT(LOT_INFO *lot);
-void WriteSG(void *pointer, int size);
-void WriteSGARM(LARA_ARM *arm);
-void WriteSGLara(LARA_INFO *lara);
-void WriteSGLOT(LOT_INFO *lot);
-
 bool SaveGame_LoadFromFile(SAVEGAME_INFO *save, int32_t slot);
 bool SaveGame_SaveToFile(SAVEGAME_INFO *save, int32_t slot);
 void SaveGame_ScanSavedGames();
