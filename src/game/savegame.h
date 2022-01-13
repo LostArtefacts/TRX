1942b5a4a529c88228e733485e73a2c2fc14f07f
rr- <dash@wind.garden>
2022-01-13 02:34:28 +0100

    savegame: improve function signatures

--- src/game/savegame.h
+++ src/game/savegame.h
@@ -8,8 +8,8 @@ void InitialiseStartInfo();
 void ModifyStartInfo(int32_t level_num);
 void CreateStartInfo(int level_num);
 
-void CreateSaveGameInfo();
-void ExtractSaveGameInfo();
+void SaveGame_SaveToSave(SAVEGAME_INFO *save);
+void SaveGame_LoadFromSave(SAVEGAME_INFO *save);
 
 bool SaveGame_LoadFromFile(SAVEGAME_INFO *save, int32_t slot);
 bool SaveGame_SaveToFile(SAVEGAME_INFO *save, int32_t slot);
