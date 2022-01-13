2276a6939ca4c4873f9d3666cd56378f00a02ef1
rr- <dash@wind.garden>
2022-01-14 00:59:31 +0100

    savegame: add documentation

--- src/game/savegame.c
+++ src/game/savegame.c
@@ -23,6 +23,14 @@
 #include <stdio.h>
 #include <stddef.h>
 
+// Loading a saved game is divided into two phases. First, the game reads the
+// savegame file contents to look for the level number. The rest of the save
+// data is stored in a special buffer in the g_GameInfo. Then the engine
+// continues to execute the normal game flow and loads the specified level.
+// Second phase occurs after everything finishes loading, e.g. items,
+// creatures, triggers etc., and is what actually sets Lara's health, creatures
+// status, triggers, inventory etc.
+
 #define SAVE_CREATURE (1 << 7)
 
 typedef struct SAVEGAME_ITEM_STATS {
