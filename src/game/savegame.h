e68cbf7d5bc97c8fdea9b7ba661b886e15091e5b
rr- <dash@wind.garden>
2022-01-14 01:46:07 +0100

    savegame: improve function names

--- src/game/savegame.h
+++ src/game/savegame.h
@@ -8,9 +8,8 @@ void InitialiseStartInfo();
 void ModifyStartInfo(int32_t level_num);
 void CreateStartInfo(int level_num);
 
-void SaveGame_SaveToSave(GAME_INFO *save);
-void SaveGame_LoadFromSave(GAME_INFO *save);
+bool SaveGame_LoadSaveBufferFromFile(GAME_INFO *save, int32_t slot);
+void SaveGame_ApplySaveBuffer(GAME_INFO *save);
 
-bool SaveGame_LoadFromFile(GAME_INFO *save, int32_t slot);
 bool SaveGame_SaveToFile(GAME_INFO *save, int32_t slot);
 void SaveGame_ScanSavedGames();
