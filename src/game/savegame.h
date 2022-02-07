844ed82fd086e4bcac85468abd743538dc7705d8
rr- <dash@wind.garden>
2022-02-07 19:21:09 +0100

    misc: move documentation to headers

--- src/game/savegame.h
+++ src/game/savegame.h
@@ -4,6 +4,14 @@
 
 #include <stdint.h>
 
+// Loading a saved game is divided into two phases. First, the game reads the
+// savegame file contents to look for the level number. The rest of the save
+// data is stored in a special buffer in the g_GameInfo. Then the engine
+// continues to execute the normal game flow and loads the specified level.
+// Second phase occurs after everything finishes loading, e.g. items,
+// creatures, triggers etc., and is what actually sets Lara's health, creatures
+// status, triggers, inventory etc.
+
 void InitialiseStartInfo();
 void ModifyStartInfo(int32_t level_num);
 void CreateStartInfo(int level_num);
