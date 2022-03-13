LostArtefacts/TRX

commit f14ec7e036e80ddc83d9cd3c9124dbbf6ce51bf9

text: add left/right arrow support and new text functions (#456)

src/game/text.c
@@ -13,6 +13,7 @@
 #define TEXT_BOX_OFFSET 2
 #define TEXT_MAX_STRING_SIZE 100
 #define TEXT_MAX_STRINGS 64
+#define RIGHT_ARROW_SYM 109
 
 static int16_t m_TextstringCount = 0;
 static TEXTSTRING m_TextstringTable[TEXT_MAX_STRINGS] = { 0 };
@@ -38,19 +39,19 @@ static int8_t m_TextSpacing[110] = {
 };
 
 static int8_t m_TextASCIIMap[95] = {
-    0 /* */,   64 /*!*/,  66 /*"*/,  78 /*#*/, 77 /*$*/, 74 /*%*/, 78 /*&*/,
-    79 /*'*/,  69 /*(*/,  70 /*)*/,  92 /***/, 72 /*+*/, 63 /*,*/, 71 /*-*/,
-    62 /*.*/,  68 /**/,   52 /*0*/,  53 /*1*/, 54 /*2*/, 55 /*3*/, 56 /*4*/,
-    57 /*5*/,  58 /*6*/,  59 /*7*/,  60 /*8*/, 61 /*9*/, 73 /*:*/, 73 /*;*/,
-    66 /*<*/,  74 /*=*/,  75 /*>*/,  65 /*?*/, 0 /**/,   0 /*A*/,  1 /*B*/,
-    2 /*C*/,   3 /*D*/,   4 /*E*/,   5 /*F*/,  6 /*G*/,  7 /*H*/,  8 /*I*/,
-    9 /*J*/,   10 /*K*/,  11 /*L*/,  12 /*M*/, 13 /*N*/, 14 /*O*/, 15 /*P*/,
-    16 /*Q*/,  17 /*R*/,  18 /*S*/,  19 /*T*/, 20 /*U*/, 21 /*V*/, 22 /*W*/,
-    23 /*X*/,  24 /*Y*/,  25 /*Z*/,  80 /*[*/, 76 /*\*/, 81 /*]*/, 97 /*^*/,
-    98 /*_*/,  77 /*`*/,  26 /*a*/,  27 /*b*/, 28 /*c*/, 29 /*d*/, 30 /*e*/,
-    31 /*f*/,  32 /*g*/,  33 /*h*/,  34 /*i*/, 35 /*j*/, 36 /*k*/, 37 /*l*/,
-    38 /*m*/,  39 /*n*/,  40 /*o*/,  41 /*p*/, 42 /*q*/, 43 /*r*/, 44 /*s*/,
-    45 /*t*/,  46 /*u*/,  47 /*v*/,  48 /*w*/, 49 /*x*/, 50 /*y*/, 51 /*z*/,
+    0 /* */,   64 /*!*/,  66 /*"*/,  78 /*#*/, 77 /*$*/,  74 /*%*/, 78 /*&*/,
+    79 /*'*/,  69 /*(*/,  70 /*)*/,  92 /***/, 72 /*+*/,  63 /*,*/, 71 /*-*/,
+    62 /*.*/,  68 /**/,   52 /*0*/,  53 /*1*/, 54 /*2*/,  55 /*3*/, 56 /*4*/,
+    57 /*5*/,  58 /*6*/,  59 /*7*/,  60 /*8*/, 61 /*9*/,  73 /*:*/, 73 /*;*/,
+    66 /*<*/,  74 /*=*/,  75 /*>*/,  65 /*?*/, 108 /*@*/, 0 /*A*/,  1 /*B*/,
+    2 /*C*/,   3 /*D*/,   4 /*E*/,   5 /*F*/,  6 /*G*/,   7 /*H*/,  8 /*I*/,
+    9 /*J*/,   10 /*K*/,  11 /*L*/,  12 /*M*/, 13 /*N*/,  14 /*O*/, 15 /*P*/,
+    16 /*Q*/,  17 /*R*/,  18 /*S*/,  19 /*T*/, 20 /*U*/,  21 /*V*/, 22 /*W*/,
+    23 /*X*/,  24 /*Y*/,  25 /*Z*/,  80 /*[*/, 76 /*\*/,  81 /*]*/, 97 /*^*/,
+    98 /*_*/,  77 /*`*/,  26 /*a*/,  27 /*b*/, 28 /*c*/,  29 /*d*/, 30 /*e*/,
+    31 /*f*/,  32 /*g*/,  33 /*h*/,  34 /*i*/, 35 /*j*/,  36 /*k*/, 37 /*l*/,
+    38 /*m*/,  39 /*n*/,  40 /*o*/,  41 /*p*/, 42 /*q*/,  43 /*r*/, 44 /*s*/,
+    45 /*t*/,  46 /*u*/,  47 /*v*/,  48 /*w*/, 49 /*x*/,  50 /*y*/, 51 /*z*/,
     100 /*{*/, 101 /*|*/, 102 /*}*/, 67 /*~*/
 };
 
@@ -127,6 +128,15 @@ void Text_ChangeText(TEXTSTRING *textstring, const char *string)
     }
 }
 
+void Text_SetPos(TEXTSTRING *textstring, int16_t x, int16_t y)
+{
+    if (!textstring) {
+        return;
+    }
+    textstring->pos.x = x;
+    textstring->pos.y = y;
+}
+
 void Text_SetScale(TEXTSTRING *textstring, int32_t scale_h, int32_t scale_v)
 {
     if (!textstring) {
@@ -148,6 +158,14 @@ void Text_Flash(TEXTSTRING *textstring, bool enable, int16_t rate)
     } else {
         textstring->flags.flash = 0;
     }
+}
+
+void Text_Hide(TEXTSTRING *textstring, bool enable)
+{
+    if (!textstring) {
+        return;
+    }
+    textstring->flags.hide = enable;
 }
 
 void Text_AddBackground(
@@ -291,6 +309,11 @@ void Text_Draw()
 static void Text_DrawText(TEXTSTRING *textstring)
 {
     int sx, sy, sh, sv;
+
+    if (textstring->flags.hide) {
+        return;
+    }
+
     if (textstring->flags.flash) {
         textstring->flash.count -= (int16_t)g_Camera.number_frames;
         if (textstring->flash.count <= -textstring->flash.rate) {
@@ -336,6 +359,8 @@ static void Text_DrawText(TEXTSTRING *textstring)
         int32_t sprite_num = letter;
         if (letter >= 16) {
             sprite_num = m_TextASCIIMap[letter - 32];
+        } else if (letter == '\t') {
+            sprite_num = RIGHT_ARROW_SYM;
         } else if (letter >= 11) {
             sprite_num = letter + 91;
         } else {

src/game/text.h
@@ -10,8 +10,10 @@
 void Text_Init();
 TEXTSTRING *Text_Create(int16_t x, int16_t y, const char *string);
 void Text_ChangeText(TEXTSTRING *textstring, const char *string);
+void Text_SetPos(TEXTSTRING *textstring, int16_t x, int16_t y);
 void Text_SetScale(TEXTSTRING *textstring, int32_t scale_h, int32_t scale_v);
 void Text_Flash(TEXTSTRING *textstring, bool enable, int16_t rate);
+void Text_Hide(TEXTSTRING *textstring, bool enable);
 void Text_AddBackground(
     TEXTSTRING *textstring, int16_t w, int16_t h, int16_t x, int16_t y);
 void Text_RemoveBackground(TEXTSTRING *textstring);

commit 5ea9ea4581c95d43b0291ef6235b9acca555a5c5

text: fix broken 8 glyph
Resolves #480. Regression from f14ec7e0.

src/game/option_control.c
@@ -105,11 +105,12 @@ static void Option_ControlInitText()
     int16_t max_y = 0;
 
     m_TextArrowLeft = Text_Create(
-        -75, TOP_Y - BORDER + (HEADER_HEIGHT + BORDER - ROW_HEIGHT) / 2, "@");
+        -75, TOP_Y - BORDER + (HEADER_HEIGHT + BORDER - ROW_HEIGHT) / 2,
+        "\200");
     Text_CentreH(m_TextArrowLeft, 1);
     Text_CentreV(m_TextArrowLeft, 1);
     m_TextArrowRight = Text_Create(
-        70, TOP_Y - BORDER + (HEADER_HEIGHT + BORDER - ROW_HEIGHT) / 2, "\t");
+        70, TOP_Y - BORDER + (HEADER_HEIGHT + BORDER - ROW_HEIGHT) / 2, "\201");
     Text_CentreH(m_TextArrowRight, 1);
     Text_CentreV(m_TextArrowRight, 1);
 

src/game/option_graphics.c
@@ -128,7 +128,7 @@ static void Option_GraphicsInitText()
     Text_AddBackground(m_Text[TEXT_TITLE_BORDER], width, height, 0, 0);
     Text_AddOutline(m_Text[TEXT_TITLE_BORDER], 1);
 
-    m_Text[TEXT_LEFT_ARROW] = Text_Create(0, 0, "@");
+    m_Text[TEXT_LEFT_ARROW] = Text_Create(0, 0, "\200");
     Text_CentreV(m_Text[TEXT_LEFT_ARROW], 1);
     Text_SetPos(
         m_Text[TEXT_LEFT_ARROW], m_Text[TEXT_PERSPECTIVE_TOGGLE]->pos.x - 20,
@@ -136,7 +136,7 @@ static void Option_GraphicsInitText()
     m_HideArrowLeft =
         g_Config.rendering.enable_perspective_filter ? false : true;
 
-    m_Text[TEXT_RIGHT_ARROW] = Text_Create(0, 0, "\t");
+    m_Text[TEXT_RIGHT_ARROW] = Text_Create(0, 0, "\201");
     Text_CentreV(m_Text[TEXT_RIGHT_ARROW], 1);
     Text_SetPos(
         m_Text[TEXT_RIGHT_ARROW], m_Text[TEXT_PERSPECTIVE_TOGGLE]->pos.x + 40,

src/game/option_passport.c
@@ -88,12 +88,12 @@ static void Option_PassportInitSaveRequester();
 
 static void Option_PassportInitText()
 {
-    m_Text[TEXT_LEFT_ARROW] = Text_Create(-85, -15, "@");
+    m_Text[TEXT_LEFT_ARROW] = Text_Create(-85, -15, "\200");
     Text_AlignBottom(m_Text[TEXT_LEFT_ARROW], 1);
     Text_CentreH(m_Text[TEXT_LEFT_ARROW], 1);
     Text_Hide(m_Text[TEXT_LEFT_ARROW], true);
 
-    m_Text[TEXT_RIGHT_ARROW] = Text_Create(70, -15, "\t");
+    m_Text[TEXT_RIGHT_ARROW] = Text_Create(70, -15, "\201");
     Text_AlignBottom(m_Text[TEXT_RIGHT_ARROW], 1);
     Text_CentreH(m_Text[TEXT_RIGHT_ARROW], 1);
     Text_Hide(m_Text[TEXT_RIGHT_ARROW], true);

src/game/option_sound.c
@@ -34,8 +34,8 @@ static void Option_SoundInitText()
 {
     char buf[20];
 
-    m_Text[TEXT_LEFT_ARROW] = Text_Create(-45, 0, "@");
-    m_Text[TEXT_RIGHT_ARROW] = Text_Create(40, 0, "\t");
+    m_Text[TEXT_LEFT_ARROW] = Text_Create(-45, 0, "\200");
+    m_Text[TEXT_RIGHT_ARROW] = Text_Create(40, 0, "\201");
 
     if (g_Config.music_volume > 10) {
         g_Config.music_volume = 10;

src/game/output.c
@@ -849,18 +849,16 @@ void Output_AnimateTextures(int32_t ticks)
 
 static void Output_DrawBlackScreen(uint8_t alpha)
 {
-    if (alpha) {
-        int32_t sx = 0;
-        int32_t sy = 0;
-        int32_t sw = ViewPort_GetMaxX();
-        int32_t sh = ViewPort_GetMaxY();
+    int32_t sx = 0;
+    int32_t sy = 0;
+    int32_t sw = ViewPort_GetMaxX();
+    int32_t sh = ViewPort_GetMaxY();
 
-        RGBA8888 background = { 0, 0, 0, alpha };
-        S_Output_DisableDepthTest();
-        S_Output_ClearDepthBuffer();
-        Output_DrawScreenFlatQuad(sx, sy, sw, sh, background);
-        S_Output_EnableDepthTest();
-    }
+    RGBA8888 background = { 0, 0, 0, alpha };
+    S_Output_DisableDepthTest();
+    S_Output_ClearDepthBuffer();
+    Output_DrawScreenFlatQuad(sx, sy, sw, sh, background);
+    S_Output_EnableDepthTest();
 }
 
 static void Output_FadeAnimate(int ticks)

src/game/text.c
@@ -13,6 +13,7 @@
 #define TEXT_BOX_OFFSET 2
 #define TEXT_MAX_STRING_SIZE 100
 #define TEXT_MAX_STRINGS 64
+#define LEFT_ARROW_SYM 108
 #define RIGHT_ARROW_SYM 109
 
 static int16_t m_TextstringCount = 0;
@@ -39,23 +40,39 @@ static int8_t m_TextSpacing[110] = {
 };
 
 static int8_t m_TextASCIIMap[95] = {
-    0 /* */,   64 /*!*/,  66 /*"*/,  78 /*#*/, 77 /*$*/,  74 /*%*/, 78 /*&*/,
-    79 /*'*/,  69 /*(*/,  70 /*)*/,  92 /***/, 72 /*+*/,  63 /*,*/, 71 /*-*/,
-    62 /*.*/,  68 /**/,   52 /*0*/,  53 /*1*/, 54 /*2*/,  55 /*3*/, 56 /*4*/,
-    57 /*5*/,  58 /*6*/,  59 /*7*/,  60 /*8*/, 61 /*9*/,  73 /*:*/, 73 /*;*/,
-    66 /*<*/,  74 /*=*/,  75 /*>*/,  65 /*?*/, 108 /*@*/, 0 /*A*/,  1 /*B*/,
-    2 /*C*/,   3 /*D*/,   4 /*E*/,   5 /*F*/,  6 /*G*/,   7 /*H*/,  8 /*I*/,
-    9 /*J*/,   10 /*K*/,  11 /*L*/,  12 /*M*/, 13 /*N*/,  14 /*O*/, 15 /*P*/,
-    16 /*Q*/,  17 /*R*/,  18 /*S*/,  19 /*T*/, 20 /*U*/,  21 /*V*/, 22 /*W*/,
-    23 /*X*/,  24 /*Y*/,  25 /*Z*/,  80 /*[*/, 76 /*\*/,  81 /*]*/, 97 /*^*/,
-    98 /*_*/,  77 /*`*/,  26 /*a*/,  27 /*b*/, 28 /*c*/,  29 /*d*/, 30 /*e*/,
-    31 /*f*/,  32 /*g*/,  33 /*h*/,  34 /*i*/, 35 /*j*/,  36 /*k*/, 37 /*l*/,
-    38 /*m*/,  39 /*n*/,  40 /*o*/,  41 /*p*/, 42 /*q*/,  43 /*r*/, 44 /*s*/,
-    45 /*t*/,  46 /*u*/,  47 /*v*/,  48 /*w*/, 49 /*x*/,  50 /*y*/, 51 /*z*/,
+    0 /* */,   64 /*!*/,  66 /*"*/,  78 /*#*/, 77 /*$*/, 74 /*%*/, 78 /*&*/,
+    79 /*'*/,  69 /*(*/,  70 /*)*/,  92 /***/, 72 /*+*/, 63 /*,*/, 71 /*-*/,
+    62 /*.*/,  68 /**/,   52 /*0*/,  53 /*1*/, 54 /*2*/, 55 /*3*/, 56 /*4*/,
+    57 /*5*/,  58 /*6*/,  59 /*7*/,  60 /*8*/, 61 /*9*/, 73 /*:*/, 73 /*;*/,
+    66 /*<*/,  74 /*=*/,  75 /*>*/,  65 /*?*/, 0 /**/,   0 /*A*/,  1 /*B*/,
+    2 /*C*/,   3 /*D*/,   4 /*E*/,   5 /*F*/,  6 /*G*/,  7 /*H*/,  8 /*I*/,
+    9 /*J*/,   10 /*K*/,  11 /*L*/,  12 /*M*/, 13 /*N*/, 14 /*O*/, 15 /*P*/,
+    16 /*Q*/,  17 /*R*/,  18 /*S*/,  19 /*T*/, 20 /*U*/, 21 /*V*/, 22 /*W*/,
+    23 /*X*/,  24 /*Y*/,  25 /*Z*/,  80 /*[*/, 76 /*\*/, 81 /*]*/, 97 /*^*/,
+    98 /*_*/,  77 /*`*/,  26 /*a*/,  27 /*b*/, 28 /*c*/, 29 /*d*/, 30 /*e*/,
+    31 /*f*/,  32 /*g*/,  33 /*h*/,  34 /*i*/, 35 /*j*/, 36 /*k*/, 37 /*l*/,
+    38 /*m*/,  39 /*n*/,  40 /*o*/,  41 /*p*/, 42 /*q*/, 43 /*r*/, 44 /*s*/,
+    45 /*t*/,  46 /*u*/,  47 /*v*/,  48 /*w*/, 49 /*x*/, 50 /*y*/, 51 /*z*/,
     100 /*{*/, 101 /*|*/, 102 /*}*/, 67 /*~*/
 };
 
 static void Text_DrawText(TEXTSTRING *textstring);
+static uint8_t Text_MapLetterToSpriteNum(char letter);
+
+static uint8_t Text_MapLetterToSpriteNum(char letter)
+{
+    if (letter >= 16) {
+        return m_TextASCIIMap[letter - 32];
+    } else if (letter == '\200') {
+        return LEFT_ARROW_SYM;
+    } else if (letter == '\201') {
+        return RIGHT_ARROW_SYM;
+    } else if (letter >= 11) {
+        return letter + 91;
+    } else {
+        return letter + 81;
+    }
+}
 
 void Text_Init()
 {
@@ -253,15 +270,8 @@ int32_t Text_GetWidth(TEXTSTRING *textstring)
             continue;
         }
 
-        if (letter >= 16) {
-            letter = m_TextASCIIMap[letter - 32];
-        } else if (letter >= 11) {
-            letter = letter + 91;
-        } else {
-            letter = letter + 81;
-        }
-
-        width += ((m_TextSpacing[(uint8_t)letter] + textstring->letter_spacing)
+        uint8_t sprite_num = Text_MapLetterToSpriteNum(letter);
+        width += ((m_TextSpacing[sprite_num] + textstring->letter_spacing)
                   * textstring->scale.h)
             / PHD_ONE;
     }
@@ -355,17 +365,7 @@ static void Text_DrawText(TEXTSTRING *textstring)
             continue;
         }
 
-        int32_t sprite_num = letter;
-        if (letter >= 16) {
-            sprite_num = m_TextASCIIMap[letter - 32];
-        } else if (letter == '\t') {
-            sprite_num = RIGHT_ARROW_SYM;
-        } else if (letter >= 11) {
-            sprite_num = letter + 91;
-        } else {
-            sprite_num = letter + 81;
-        }
-
+        uint8_t sprite_num = Text_MapLetterToSpriteNum(letter);
         sx = Screen_GetRenderScale(x);
         sy = Screen_GetRenderScale(y);
         sh = Screen_GetRenderScale(textstring->scale.h);