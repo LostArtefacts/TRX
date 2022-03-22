c8e8e2a872011697ed3f140e016bd1856956757a
rr- <dash@wind.garden>
2022-03-22 23:35:36 +0100

    traps/dart: merge DartEmitter to dart.c

--- src/game/traps/dart.h
+++ src/game/traps/dart.h
@@ -7,3 +7,6 @@ void Dart_Control(int16_t item_num);
 
 void DartEffect_Setup(OBJECT_INFO *obj);
 void DartEffect_Control(int16_t fx_num);
+
+void DartEmitter_Setup(OBJECT_INFO *obj);
+void DartEmitter_Control(int16_t item_num);
