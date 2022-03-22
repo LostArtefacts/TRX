1ee67218d366d884572e01e818b3d24a3089e0d6
rr- <dash@wind.garden>
2022-03-22 17:05:59 +0100

    traps: rename Flame

--- src/game/traps/flame.h
+++ src/game/traps/flame.h
@@ -7,7 +7,8 @@
 #define FLAME_ONFIRE_DAMAGE 5
 #define FLAME_TOONEAR_DAMAGE 3
 
-void SetupFlameEmitter(OBJECT_INFO *obj);
-void SetupFlame(OBJECT_INFO *obj);
-void FlameEmitterControl(int16_t item_num);
-void FlameControl(int16_t fx_num);
+void Flame_Setup(OBJECT_INFO *obj);
+void Flame_Control(int16_t fx_num);
+
+void FlameEmitter_Setup(OBJECT_INFO *obj);
+void FlameEmitter_Control(int16_t item_num);
