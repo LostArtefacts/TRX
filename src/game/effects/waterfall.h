2d4f4db117dfd9511552cd49d9f54e06911c9cda
rr- <dash@wind.garden>
2022-03-24 14:27:16 +0100

    effects: rename Waterfall

--- src/game/effects/waterfall.h
+++ src/game/effects/waterfall.h
@@ -6,5 +6,5 @@
 
 #define WATERFALL_RANGE (WALL_L * 10) // = 10240
 
-void SetupWaterfall(OBJECT_INFO *obj);
-void ControlWaterFall(int16_t item_num);
+void Waterfall_Setup(OBJECT_INFO *obj);
+void Waterfall_Control(int16_t item_num);
