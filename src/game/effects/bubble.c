d5ed86b9bcfb2cc6303fe17a9ea57c7dccca520e
rr- <dash@wind.garden>
2022-03-24 14:20:23 +0100

    effects: rename Bubble

--- src/game/effects/bubble.c
+++ src/game/effects/bubble.c
@@ -8,9 +8,40 @@
 #include "game/sphere.h"
 #include "global/vars.h"
 
-void SetupBubble(OBJECT_INFO *obj)
+void Bubble_Setup(OBJECT_INFO *obj)
 {
-    obj->control = ControlBubble1;
+    obj->control = Bubble_Control;
+}
+
+void Bubble_Control(int16_t fx_num)
+{
+    FX_INFO *fx = &g_Effects[fx_num];
+    fx->pos.y_rot += 9 * PHD_DEGREE;
+    fx->pos.x_rot += 13 * PHD_DEGREE;
+
+    int32_t x = fx->pos.x + ((phd_sin(fx->pos.y_rot) * 11) >> W2V_SHIFT);
+    int32_t y = fx->pos.y - fx->speed;
+    int32_t z = fx->pos.z + ((phd_cos(fx->pos.x_rot) * 8) >> W2V_SHIFT);
+
+    int16_t room_num = fx->room_number;
+    FLOOR_INFO *floor = GetFloor(x, y, z, &room_num);
+    if (!floor || !(g_RoomInfo[room_num].flags & RF_UNDERWATER)) {
+        KillEffect(fx_num);
+        return;
+    }
+
+    int32_t height = GetCeiling(floor, x, y, z);
+    if (height == NO_HEIGHT || y <= height) {
+        KillEffect(fx_num);
+        return;
+    }
+
+    if (fx->room_number != room_num) {
+        EffectNewRoom(fx_num, room_num);
+    }
+    fx->pos.x = x;
+    fx->pos.y = y;
+    fx->pos.z = z;
 }
 
 void LaraBubbles(ITEM_INFO *item)
@@ -46,35 +77,4 @@ void LaraBubbles(ITEM_INFO *item)
             fx->speed = 10 + ((Random_GetDraw() * 6) / 0x8000);
         }
     }
-}
-
-void ControlBubble1(int16_t fx_num)
-{
-    FX_INFO *fx = &g_Effects[fx_num];
-    fx->pos.y_rot += 9 * PHD_DEGREE;
-    fx->pos.x_rot += 13 * PHD_DEGREE;
-
-    int32_t x = fx->pos.x + ((phd_sin(fx->pos.y_rot) * 11) >> W2V_SHIFT);
-    int32_t y = fx->pos.y - fx->speed;
-    int32_t z = fx->pos.z + ((phd_cos(fx->pos.x_rot) * 8) >> W2V_SHIFT);
-
-    int16_t room_num = fx->room_number;
-    FLOOR_INFO *floor = GetFloor(x, y, z, &room_num);
-    if (!floor || !(g_RoomInfo[room_num].flags & RF_UNDERWATER)) {
-        KillEffect(fx_num);
-        return;
-    }
-
-    int32_t height = GetCeiling(floor, x, y, z);
-    if (height == NO_HEIGHT || y <= height) {
-        KillEffect(fx_num);
-        return;
-    }
-
-    if (fx->room_number != room_num) {
-        EffectNewRoom(fx_num, room_num);
-    }
-    fx->pos.x = x;
-    fx->pos.y = y;
-    fx->pos.z = z;
 }
