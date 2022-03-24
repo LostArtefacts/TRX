6cbf5a787ad98ae74e1a8fdf83d238d9060bfb5d
rr- <dash@wind.garden>
2022-03-24 14:19:52 +0100

    effects: rename BodyPart

--- src/game/effects/body_part.h
+++ src/game/effects/body_part.h
@@ -4,6 +4,7 @@
 
 #include <stdint.h>
 
-void SetupBodyPart(OBJECT_INFO *obj);
+void BodyPart_Setup(OBJECT_INFO *obj);
+void BodyPart_Control(int16_t fx_num);
+
 int32_t ExplodingDeath(int16_t item_num, int32_t mesh_bits, int16_t damage);
-void ControlBodyPart(int16_t fx_num);
