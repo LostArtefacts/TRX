0bbd3b1bb4f68b27e7998afb3fd743f0d18ba95e
rr- <dash@wind.garden>
2022-03-24 15:13:47 +0100

    fx: move Earthquake

---
+++ src/game/effect_routines/earthquake.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "global/types.h"
+
+void FX_Earthquake(ITEM_INFO *item);
--- src/game/objects/effects/earthquake.c
+++ src/game/objects/effects/earthquake.c
@@ -26,22 +26,3 @@ void Earthquake_Control(int16_t item_num)
         }
     }
 }
-
-void EarthQuake(ITEM_INFO *item)
-{
-    if (g_FlipTimer == 0) {
-        Sound_Effect(SFX_EXPLOSION, NULL, SPM_NORMAL);
-        g_Camera.bounce = -250;
-    } else if (g_FlipTimer == 3) {
-        Sound_Effect(SFX_ROLLING_BALL, NULL, SPM_NORMAL);
-    } else if (g_FlipTimer == 35) {
-        Sound_Effect(SFX_EXPLOSION, NULL, SPM_NORMAL);
-    } else if (g_FlipTimer == 20 || g_FlipTimer == 50 || g_FlipTimer == 70) {
-        Sound_Effect(SFX_T_REX_FOOTSTOMP, NULL, SPM_NORMAL);
-    }
-
-    g_FlipTimer++;
-    if (g_FlipTimer == 105) {
-        g_FlipEffect = -1;
-    }
-}
