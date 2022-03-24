b2a29ca8adbfe222aa64b1296610fe0c44839f71
rr- <dash@wind.garden>
2022-03-24 14:25:49 +0100

    effects: rename Ricochet

--- src/game/effects/ricochet.c
+++ src/game/effects/ricochet.c
@@ -5,9 +5,18 @@
 #include "game/sound.h"
 #include "global/vars.h"
 
-void SetupRicochet(OBJECT_INFO *obj)
+void Ricochet_Setup(OBJECT_INFO *obj)
 {
-    obj->control = ControlRicochet1;
+    obj->control = Ricochet_Control;
+}
+
+void Ricochet_Control(int16_t fx_num)
+{
+    FX_INFO *fx = &g_Effects[fx_num];
+    fx->counter--;
+    if (!fx->counter) {
+        KillEffect(fx_num);
+    }
 }
 
 void Ricochet(GAME_VECTOR *pos)
@@ -22,14 +31,5 @@ void Ricochet(GAME_VECTOR *pos)
         fx->object_number = O_RICOCHET1;
         fx->frame_number = -3 * Random_GetDraw() / 0x8000;
         Sound_Effect(SFX_LARA_RICOCHET, &fx->pos, SPM_NORMAL);
-    }
-}
-
-void ControlRicochet1(int16_t fx_num)
-{
-    FX_INFO *fx = &g_Effects[fx_num];
-    fx->counter--;
-    if (!fx->counter) {
-        KillEffect(fx_num);
     }
 }
