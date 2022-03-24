8f23c35ba115ef04f93fe75a50652e07dca34065
rr- <dash@wind.garden>
2022-03-24 15:40:16 +0100

    effects: rename Splash_Spawn

--- src/game/objects/effects/splash.h
+++ src/game/objects/effects/splash.h
@@ -2,9 +2,6 @@
 
 #include "global/types.h"
 
-#include <stdint.h>
-
 void Splash_Setup(OBJECT_INFO *obj);
 void Splash_Control(int16_t fx_num);
-
-void Splash(ITEM_INFO *item);
+void Splash_Spawn(ITEM_INFO *item);
