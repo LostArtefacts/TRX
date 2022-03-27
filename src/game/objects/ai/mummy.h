8b2d7e3c5685a7348396fe58f2a31c73d8280d9b
rr- <dash@wind.garden>
2022-03-27 22:10:45 +0200

    ai/mummy: update naming

--- src/game/objects/ai/mummy.h
+++ src/game/objects/ai/mummy.h
@@ -12,6 +12,6 @@ typedef enum {
     MUMMY_DEATH = 2,
 } MUMMY_ANIM;
 
-void SetupMummy(OBJECT_INFO *obj);
-void InitialiseMummy(int16_t item_num);
-void MummyControl(int16_t item_num);
+void Mummy_Setup(OBJECT_INFO *obj);
+void Mummy_Initialise(int16_t item_num);
+void Mummy_Control(int16_t item_num);
