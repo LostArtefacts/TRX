b576d34706fd144c5f1f8de2307eb16061598796
walkawayy <81546780+walkawayy@users.noreply.github.com>
2022-04-21 18:57:24 -0400

    fix legacy saves' stats

--- src/game/savegame_legacy.c
+++ src/game/savegame_legacy.c
@@ -455,6 +455,9 @@ bool Savegame_Legacy_LoadFromFile(MYFILE *fp, GAME_INFO *game_info)
         }
     }
 
+    Savegame_Legacy_SetCurrentPosition(g_CurrentLevel);
+    game_info->death_counter_supported = false;
+
     uint32_t temp_timer = 0;
     uint32_t temp_kill_count = 0;
     uint16_t temp_secret_flags = 0;
@@ -469,9 +472,6 @@ bool Savegame_Legacy_LoadFromFile(MYFILE *fp, GAME_INFO *game_info)
         &game_info->current[g_CurrentLevel].stats.pickup_count,
         sizeof(uint8_t));
     Savegame_Legacy_Read(&game_info->bonus_flag, sizeof(uint8_t));
-
-    Savegame_Legacy_SetCurrentPosition(g_CurrentLevel);
-    game_info->death_counter_supported = false;
 
     Lara_InitialiseInventory(g_CurrentLevel);
     SAVEGAME_LEGACY_ITEM_STATS item_stats = { 0 };
