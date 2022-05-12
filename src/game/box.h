ed975a6a5b014cc2e70af508b14999ccba02b621
rr- <dash@wind.garden>
2022-05-12 22:40:51 +0200

    creature: rename CreatureCreature

--- src/game/box.h
+++ src/game/box.h
@@ -9,7 +9,6 @@ int32_t StalkBox(ITEM_INFO *item, int16_t box_number);
 int32_t EscapeBox(ITEM_INFO *item, int16_t box_number);
 int32_t ValidBox(ITEM_INFO *item, int16_t zone_number, int16_t box_number);
 int32_t CalculateTarget(PHD_VECTOR *target, ITEM_INFO *item, LOT_INFO *LOT);
-int32_t CreatureCreature(int16_t item_num);
 int32_t BadFloor(
     int32_t x, int32_t y, int32_t z, int16_t box_height, int16_t next_height,
     int16_t room_number, LOT_INFO *LOT);
