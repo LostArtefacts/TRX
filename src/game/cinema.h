ea23681999bd207cbb7b677b1720fb0c10204131
rr- <dash@wind.garden>
2022-05-13 23:33:52 +0200

    cinema: rename DoCinematic

--- src/game/cinema.h
+++ src/game/cinema.h
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-bool DoCinematic(int32_t nframes);
 void ControlCinematicPlayer(int16_t item_num);
 void ControlCinematicPlayer4(int16_t item_num);
 void InitialisePlayer1(int16_t item_num);
