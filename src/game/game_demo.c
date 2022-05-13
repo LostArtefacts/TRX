dec484a4396c35beed930bfff217a1f9dce6d0b6
rr- <dash@wind.garden>
2022-05-13 23:02:41 +0200

    demo: rename LoadLaraDemoPos and ProcessDemoInput

--- src/game/game_demo.c
+++ src/game/game_demo.c
@@ -1,10 +1,12 @@
-#include "game/demo.h"
+#include "game/game.h"
 
 #include "config.h"
-#include "game/game.h"
 #include "game/gameflow.h"
+#include "game/input.h"
+#include "game/items.h"
 #include "game/level.h"
 #include "game/random.h"
+#include "game/room.h"
 #include "game/text.h"
 #include "global/const.h"
 #include "global/types.h"
@@ -12,8 +14,65 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 static int32_t m_DemoLevel = -1;
+static uint32_t *m_DemoPtr = NULL;
+
+static void Game_Demo_LoadLaraPos(void);
+
+static void Game_Demo_LoadLaraPos(void)
+{
+    m_DemoPtr = g_DemoData;
+    ITEM_INFO *item = g_LaraItem;
+    item->pos.x = *m_DemoPtr++;
+    item->pos.y = *m_DemoPtr++;
+    item->pos.z = *m_DemoPtr++;
+    item->pos.x_rot = *m_DemoPtr++;
+    item->pos.y_rot = *m_DemoPtr++;
+    item->pos.z_rot = *m_DemoPtr++;
+    int16_t room_num = *m_DemoPtr++;
+
+    if (item->room_number != room_num) {
+        Item_NewRoom(g_Lara.item_number, room_num);
+    }
+
+    FLOOR_INFO *floor =
+        Room_GetFloor(item->pos.x, item->pos.y, item->pos.z, &room_num);
+    item->floor = Room_GetHeight(floor, item->pos.x, item->pos.y, item->pos.z);
+}
+
+bool Game_Demo_ProcessInput(void)
+{
+    if (m_DemoPtr >= &g_DemoData[DEMO_COUNT_MAX] || (int)*m_DemoPtr == -1) {
+        return false;
+    }
+
+    // Translate demo inputs (that use TombATI key values) to T1M inputs.
+    g_Input = (INPUT_STATE) {
+        0,
+        .forward = (bool)(*m_DemoPtr & (1 << 0)),
+        .back = (bool)(*m_DemoPtr & (1 << 1)),
+        .left = (bool)(*m_DemoPtr & (1 << 2)),
+        .right = (bool)(*m_DemoPtr & (1 << 3)),
+        .jump = (bool)(*m_DemoPtr & (1 << 4)),
+        .draw = (bool)(*m_DemoPtr & (1 << 5)),
+        .action = (bool)(*m_DemoPtr & (1 << 6)),
+        .slow = (bool)(*m_DemoPtr & (1 << 7)),
+        .option = (bool)(*m_DemoPtr & (1 << 8)),
+        .look = (bool)(*m_DemoPtr & (1 << 9)),
+        .step_left = (bool)(*m_DemoPtr & (1 << 10)),
+        .step_right = (bool)(*m_DemoPtr & (1 << 11)),
+        .roll = (bool)(*m_DemoPtr & (1 << 12)),
+        .select = (bool)(*m_DemoPtr & (1 << 20)),
+        .deselect = (bool)(*m_DemoPtr & (1 << 21)),
+        .save = (bool)(*m_DemoPtr & (1 << 22)),
+        .load = (bool)(*m_DemoPtr & (1 << 23)),
+    };
+
+    m_DemoPtr++;
+    return true;
+}
 
 int32_t Game_Demo(void)
 {
@@ -57,7 +116,7 @@ int32_t Game_Demo(void)
     g_Config.enable_enhanced_look = 0;
 
     if (Level_Initialise(m_DemoLevel)) {
-        LoadLaraDemoPos();
+        Game_Demo_LoadLaraPos();
 
         Random_SeedDraw(0xD371F947);
         Random_SeedControl(0xD371F947);
