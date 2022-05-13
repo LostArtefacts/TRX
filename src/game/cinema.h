4882d00d9f4ab4415a687b5a16b90e1ed24be80d
rr- <dash@wind.garden>
2022-05-13 23:09:20 +0200

    cinema: rename InGameCinematicCamera

--- src/game/cinema.h
+++ src/game/cinema.h
@@ -13,4 +13,3 @@ void ControlCinematicPlayer(int16_t item_num);
 void ControlCinematicPlayer4(int16_t item_num);
 void InitialisePlayer1(int16_t item_num);
 void InitialiseGenPlayer(int16_t item_num);
-void InGameCinematicCamera(void);
