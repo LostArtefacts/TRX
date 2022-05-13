03ae5fe196f578309723f0a0d053ff6733b9eed5
rr- <dash@wind.garden>
2022-05-13 23:45:43 +0200

    setup: abstract cutscene player setup away

--- src/game/objects/creatures/cutscene_player.h
+++ src/game/objects/creatures/cutscene_player.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include "global/types.h"
+
 #include <stdint.h>
+
+void CutscenePlayer1_Setup(OBJECT_INFO *obj);
+void CutscenePlayer2_Setup(OBJECT_INFO *obj);
+void CutscenePlayer3_Setup(OBJECT_INFO *obj);
+void CutscenePlayer4_Setup(OBJECT_INFO *obj);
 
 void CutscenePlayer_Initialise(int16_t item_num);
 void CutscenePlayer_Control(int16_t item_num);
