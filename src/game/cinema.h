e1ebe24eca23fe6e0481c2db9687e0d662dfbbc5
rr- <dash@wind.garden>
2022-05-13 23:21:41 +0200

    cinema: rename CalculateCinematicCamera

--- src/game/cinema.h
+++ src/game/cinema.h
@@ -8,7 +8,6 @@ int32_t StartCinematic(int32_t level_num);
 int32_t StopCinematic(int32_t level_num);
 int32_t CinematicLoop(void);
 bool DoCinematic(int32_t nframes);
-void CalculateCinematicCamera(void);
 void ControlCinematicPlayer(int16_t item_num);
 void ControlCinematicPlayer4(int16_t item_num);
 void InitialisePlayer1(int16_t item_num);
