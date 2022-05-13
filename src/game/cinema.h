4bcf9e69f75dd788a4892ce8251a8f7480c48adf
rr- <dash@wind.garden>
2022-05-13 23:41:16 +0200

    cinema: rename ControlCinematicPlayer4

--- src/game/cinema.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-void ControlCinematicPlayer4(int16_t item_num);
--- src/game/objects/creatures/cutscene_player.h
+++ src/game/objects/creatures/cutscene_player.h
@@ -6,3 +6,4 @@ void CutscenePlayer_Initialise(int16_t item_num);
 void CutscenePlayer_Control(int16_t item_num);
 
 void CutscenePlayer1_Initialise(int16_t item_num);
+void CutscenePlayer4_Control(int16_t item_num);
