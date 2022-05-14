ed5b5c1c7d706c89fe60e84bfb959aac02f0e2e2
rr- <dash@wind.garden>
2022-05-14 10:52:39 +0200

    box: rename ValidBox

--- src/game/box.h
+++ src/game/box.h
@@ -10,7 +10,7 @@ bool Box_UpdateLOT(LOT_INFO *LOT, int32_t expansion);
 void Box_TargetBox(LOT_INFO *LOT, int16_t box_number);
 bool Box_StalkBox(ITEM_INFO *item, int16_t box_number);
 bool Box_EscapeBox(ITEM_INFO *item, int16_t box_number);
-int32_t ValidBox(ITEM_INFO *item, int16_t zone_number, int16_t box_number);
+bool Box_ValidBox(ITEM_INFO *item, int16_t zone_number, int16_t box_number);
 int32_t CalculateTarget(PHD_VECTOR *target, ITEM_INFO *item, LOT_INFO *LOT);
 int32_t BadFloor(
     int32_t x, int32_t y, int32_t z, int16_t box_height, int16_t next_height,
