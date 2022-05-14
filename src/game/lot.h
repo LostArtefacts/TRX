354293a1b717638be66bcdd0870a4f6dbe09609e
rr- <dash@wind.garden>
2022-05-14 10:57:15 +0200

    lot: rename InitialiseLOT

--- src/game/lot.h
+++ src/game/lot.h
@@ -10,5 +10,5 @@ void LOT_DisableBaddieAI(int16_t item_num);
 bool LOT_EnableBaddieAI(int16_t item_num, int32_t always);
 void LOT_InitialiseSlot(int16_t item_num, int32_t slot);
 void LOT_CreateZone(ITEM_INFO *item);
-int32_t InitialiseLOT(LOT_INFO *LOT);
+void LOT_InitialiseLOT(LOT_INFO *LOT);
 void ClearLOT(LOT_INFO *LOT);
