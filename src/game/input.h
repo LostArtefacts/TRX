aa18ef84f960d6ec4f97643c3deb7f6f1033c3e9
rr- <dash@wind.garden>
2022-05-15 23:38:11 +0200

    input: rename INPUT_KEY to INPUT_ROLE
    It's essential to distinguish between platform scancodes and key roles.
    The naming used so far was less than perfect.

--- src/game/input.h
+++ src/game/input.h
@@ -7,7 +7,7 @@
 extern INPUT_STATE g_Input;
 extern INPUT_STATE g_InputDB;
 extern INPUT_STATE g_OldInputDB;
-extern bool g_ConflictLayout[INPUT_KEY_NUMBER_OF];
+extern bool g_ConflictLayout[INPUT_ROLE_NUMBER_OF];
 
 void Input_Init(void);
 void Input_Shutdown(void);
