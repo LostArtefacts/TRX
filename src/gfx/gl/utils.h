74175028c666a84ab63c5f5a23b2b9f429f0f70a
Carlo Bramini <30959007+carlo-bramini@users.noreply.github.com>
2022-12-15 17:46:00 +0100

    GFX_GL: fix a copy-paste error.
    MSVC complains the use of macro GFX_GL_CheckError(), saying that it needs a parameter. By looking its declaration into src\gfx\gl\utils.h, I have seen that it is declared with a "void" inside. Probably, in the past GFX_GL_CheckError() was a function and the "void" was forgotten when it had been converted into a macro. GCC doesn't suffer much, but CL thinks instead that "void" is a required parameter. Hopefully, removing that thing made all compilers happy.

--- src/gfx/gl/utils.h
+++ src/gfx/gl/utils.h
@@ -3,7 +3,7 @@
 #include "gfx/gl/gl_core_3_3.h"
 #include "log.h"
 
-#define GFX_GL_CheckError(void)                                                \
+#define GFX_GL_CheckError()                                                    \
     {                                                                          \
         for (GLenum err; (err = glGetError()) != GL_NO_ERROR;) {               \
             LOG_ERROR("glGetError: (%s)", GFX_GL_GetErrorString(err));         \
