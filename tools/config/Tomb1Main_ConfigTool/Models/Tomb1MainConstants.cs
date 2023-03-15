e7a6bb7850734ebb15fc5150a8ff72b0a4a37617
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-03-15 20:25:39 +0000

    tools: launch UB from the config tool
    Resolves #739.

--- tools/config/Tomb1Main_ConfigTool/Models/Tomb1MainConstants.cs
+++ tools/config/Tomb1Main_ConfigTool/Models/Tomb1MainConstants.cs
@@ -5,5 +5,6 @@ public static class Tomb1MainConstants
     public const string ConfigFilterExtension = "|*.json5";
     public const string ConfigPath = @"cfg\Tomb1Main.json5";
     public const string ExecutableName = "Tomb1Main.exe";
+    public const string GoldArgs = "-gold";
     public const string GitHubURL = "https://github.com/rr-/Tomb1Main";
 }
--- tools/config/Tomb1Main_ConfigTool/Utils/ProcessUtils.cs
+++ tools/config/Tomb1Main_ConfigTool/Utils/ProcessUtils.cs
@@ -1,15 +1,21 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Tomb1Main_ConfigTool.Utils;
 
 public static class ProcessUtils
 {
-    public static void Start(string fileName)
+    public static void Start(string fileName, string arguments = null)
     {
         Process.Start(new ProcessStartInfo
         {
             FileName = fileName,
-            UseShellExecute = true
+            Arguments = arguments,
+            UseShellExecute = true,
+            WorkingDirectory = new Uri(fileName).IsFile
+                ? Path.GetDirectoryName(fileName)
+                : null
         });
     }
 }
