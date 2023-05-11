24a260822adf2b8018dac6dc31d6d7953944b0a2
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-05-11 18:14:11 +0100

    room: fix ceiling height miscalculation
    Resolves #323.

--- src/game/room.c
+++ src/game/room.c
@@ -320,9 +320,9 @@ int16_t Room_GetCeiling(FLOOR_INFO *floor, int32_t x, int32_t y, int32_t z)
 
     if (f->index) {
         data = &g_FloorData[f->index];
-        type = *data++ & DATA_TYPE;
+        type = *data & DATA_TYPE;
 
-        if (type == FT_TILT) {
+        if (!(*data++ & END_BIT) && type == FT_TILT) {
             data++;
             type = *data++ & DATA_TYPE;
         }
