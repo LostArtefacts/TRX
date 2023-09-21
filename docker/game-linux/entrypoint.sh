762dd09ed92fac80c912dab2d5d312d0238c9171
Marcin Kurczewski <dash@wind.garden>
2023-09-21 19:32:05 +0200

    build: fix Linux binaries not being executable

--- docker/game-linux/entrypoint.sh
+++ docker/game-linux/entrypoint.sh
@@ -3,16 +3,18 @@ set -x
 set -e
 
 export CFLAGS=-DDOCKER_BUILD
+EXE_FILE=Tomb1Main
 
 if [ ! -f /app/build/linux/build.ninja ]; then
     meson --buildtype "$TARGET" /app/build/linux/ --pkg-config-path=$PKG_CONFIG_PATH
 fi
 
-cd /app/build/linux; meson compile
+cd /app/build/linux
+meson compile
 
 if [ "$TARGET" = release ]; then
-    exe_file=Tomb1Main
-    if ! upx -t "$exe_file"; then
-        strip "$exe_file" && upx "$exe_file"
+    if ! upx -t "$EXE_FILE"; then
+        strip "$EXE_FILE"
+        upx "$EXE_FILE"
     fi
 fi
