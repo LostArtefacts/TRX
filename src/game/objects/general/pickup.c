df8ecbabfe18dac73e3ed6eeaff1c5b6ddea60cd
walkawayy <81546780+walkawayy@users.noreply.github.com>
2023-09-25 08:53:40 -0400

    fix non controlled Pickup_Collision

--- src/game/objects/general/pickup.c
+++ src/game/objects/general/pickup.c
@@ -102,7 +102,7 @@ void Pickup_Collision(int16_t item_num, ITEM_INFO *lara_item, COLL_INFO *coll)
         }
 
         if (lara_item->current_anim_state == LS_PICKUP) {
-            if (Item_TestFrame(lara_item, LF_PICKUP_ERASE)) {
+            if (!Item_TestFrame(lara_item, LF_PICKUP_ERASE)) {
                 goto cleanup;
             }
             PickUp_GetAllAtLaraPos(item, lara_item);
