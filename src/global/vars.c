59fe81522ff3ae11a972ae019abd7ad0cb31225d
Alan Swanson <reiver@improbability.net>
2023-10-08 20:15:33 +0100

    build: fix failure with non-docker meson build (#1036)
    Parameters g_TR1XVersion and g_GameFlowDefaultStrings are always created
    in init.c when using meson build which runs tools/generate_init. This
    caused multiple definition errors against src/global/vars.c when DOCKER_BUILD
    was not set.

--- src/global/vars.c
+++ src/global/vars.c
@@ -101,7 +101,7 @@ int32_t g_LsAdder = 0;
 int32_t g_LsDivider = 0;
 SHADOW_INFO g_ShadowInfo = { 0 };
 
-#ifndef DOCKER_BUILD
+#ifndef MESON_BUILD
 const char *g_TR1XVersion = "TR1X (non-Docker build)";
 GAMEFLOW_DEFAULT_STRING g_GameFlowDefaultStrings[] = {
     { 0, NULL },
