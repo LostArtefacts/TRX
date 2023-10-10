888a83fd206a2e3ddb70ffa4898cfc71d46b963a
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-10-10 13:45:27 +0100

    installer: replace obsolete WebClient
    Replaced using the obsolete WebClient with a custom HttpClient that
    supports progress tracking.

---
+++ tools/installer/Installer/Utils/HttpProgressClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Installer.Utils;
+
+public class HttpProgressClient
+{
+    public delegate void ProgressChangedHandler(long totalBytesToReceive, long bytesReceived);
+    public event ProgressChangedHandler? DownloadProgressChanged;
+
+    public async Task<byte[]> DownloadDataTaskAsync(Uri uri)
+    {
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.CacheControl = new()
+        {
+            NoCache = true
+        };
+
+        HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        long totalBytes = response.Content.Headers.ContentLength ?? 0;
+
+        using Stream contentStream = await response.Content.ReadAsStreamAsync();
+        return await ProcessContentStream(totalBytes, contentStream);
+    }
+
+    private async Task<byte[]> ProcessContentStream(long totalBytes, Stream contentStream)
+    {
+        long totalBytesRead = 0;
+        byte[] buffer = new byte[8192];
+
+        using MemoryStream outputStream = new();
+        while (true)
+        {
+            int bytesRead = await contentStream.ReadAsync(buffer);
+            if (bytesRead == 0)
+            {
+                break;
+            }
+
+            await outputStream.WriteAsync(buffer.AsMemory(0, bytesRead));
+            totalBytesRead += bytesRead;
+
+            DownloadProgressChanged?.Invoke(totalBytes, totalBytesRead);
+        }
+
+        return outputStream.ToArray();
+    }
+}
