dd3706277a34ac9d8cb768a0f719277d1bd3c1f7
Marcin Kurczewski <dash@wind.garden>
2024-02-15 10:05:42 +0100

    clock: hide unused apis

--- src/game/clock.h
+++ src/game/clock.h
@@ -6,6 +6,5 @@
 void Clock_CycleTurboSpeed(void);
 bool Clock_Init(void);
 int32_t Clock_GetMS(void);
-int32_t Clock_Sync(void);
 int32_t Clock_SyncTicks(int32_t target);
 void Clock_GetDateTime(char *date_time);
