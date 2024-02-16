LostArtefacts/TRX

commit fc090401d0edf6e3191254f35efcedcc1e14f6e9

phase: introduce

src/game/phase/phase_game.c
@@ -1,0 +1,140 @@
+#include "game/phase/phase_game.h"
+
+#include "game/camera.h"
+#include "game/effects.h"
+#include "game/game.h"
+#include "game/input.h"
+#include "game/inventory.h"
+#include "game/items.h"
+#include "game/lara.h"
+#include "game/lara/lara_cheat.h"
+#include "game/lara/lara_hair.h"
+#include "game/output.h"
+#include "game/overlay.h"
+#include "game/shell.h"
+#include "game/sound.h"
+#include "global/const.h"
+#include "global/types.h"
+#include "global/vars.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static const int32_t m_AnimationRate = 0x8000;
+static int32_t m_FrameCount = 0;
+
+static int32_t Phase_Game_Control(int32_t nframes);
+static int32_t Phase_Game_Draw(void);
+
+static int32_t Phase_Game_Control(int32_t nframes)
+{
+    int32_t return_val = 0;
+    if (nframes > MAX_FRAMES) {
+        nframes = MAX_FRAMES;
+    }
+
+    m_FrameCount += m_AnimationRate * nframes;
+    while (m_FrameCount >= 0) {
+        Lara_CheckCheatMode();
+        if (g_LevelComplete) {
+            return GF_NOP_BREAK;
+        }
+
+        Input_Update();
+        Shell_ProcessInput();
+        Game_ProcessInput();
+
+        if (g_GameInfo.current_level_type == GFL_DEMO) {
+            if (g_Input.any) {
+                return GF_EXIT_TO_TITLE;
+            }
+            if (!Game_Demo_ProcessInput()) {
+                return GF_EXIT_TO_TITLE;
+            }
+        }
+
+        if (g_Lara.death_timer > DEATH_WAIT
+            || (g_Lara.death_timer > DEATH_WAIT_MIN && g_Input.any
+                && !g_Input.fly_cheat)
+            || g_OverlayFlag == 2) {
+            if (g_GameInfo.current_level_type == GFL_DEMO) {
+                return GF_EXIT_TO_TITLE;
+            }
+            if (g_OverlayFlag == 2) {
+                g_OverlayFlag = 1;
+                return_val = Inv_Display(INV_DEATH_MODE);
+                if (return_val != GF_NOP) {
+                    return return_val;
+                }
+            } else {
+                g_OverlayFlag = 2;
+            }
+        }
+
+        if ((g_InputDB.option || g_Input.save || g_Input.load
+             || g_OverlayFlag <= 0)
+            && !g_Lara.death_timer) {
+            if (g_Camera.type == CAM_CINEMATIC) {
+                g_OverlayFlag = 0;
+            } else if (g_OverlayFlag > 0) {
+                if (g_Input.load) {
+                    g_OverlayFlag = -1;
+                } else if (g_Input.save) {
+                    g_OverlayFlag = -2;
+                } else {
+                    g_OverlayFlag = 0;
+                }
+            } else {
+                if (g_OverlayFlag == -1) {
+                    return_val = Inv_Display(INV_LOAD_MODE);
+                } else if (g_OverlayFlag == -2) {
+                    return_val = Inv_Display(INV_SAVE_MODE);
+                } else {
+                    return_val = Inv_Display(INV_GAME_MODE);
+                }
+
+                g_OverlayFlag = 1;
+                if (return_val != GF_NOP) {
+                    return return_val;
+                }
+            }
+        }
+
+        if (!g_Lara.death_timer && g_InputDB.pause) {
+            Game_SetStatus(GS_IN_PAUSE);
+        } else {
+            Item_Control();
+            Effect_Control();
+
+            Lara_Control();
+            Lara_Hair_Control();
+
+            Camera_Update();
+            Sound_ResetAmbient();
+            Effect_RunActiveFlipEffect();
+            Sound_UpdateEffects();
+            g_GameInfo.current[g_CurrentLevel].stats.timer++;
+            Overlay_BarHealthTimerTick();
+        }
+
+        m_FrameCount -= 0x10000;
+    }
+
+    return GF_NOP;
+}
+
+static int32_t Phase_Game_Draw(void)
+{
+    Game_DrawScene(true);
+    g_Camera.number_frames = Output_DumpScreen();
+    Output_AnimateTextures(g_Camera.number_frames);
+    return g_Camera.number_frames;
+}
+
+PHASER g_GamePhaser = {
+    .start = NULL,
+    .end = NULL,
+    .control = Phase_Game_Control,
+    .draw = Phase_Game_Draw,
+};

meson.build
@@ -231,6 +231,8 @@ sources = [
   'src/game/output.c',
   'src/game/overlay.c',
   'src/game/packer.c',
+  'src/game/phase/phase.c',
+  'src/game/phase/phase_game.c',
   'src/game/picture.c',
   'src/game/random.c',
   'src/game/requester.c',

src/game/game.h
@@ -23,5 +23,4 @@ void Game_Demo(void);
 bool Game_Demo_ProcessInput(void);
 bool Game_Pause(void);
 
-int32_t Game_ProcessFrame(void);
 void Game_DrawScene(bool draw_overlay);

src/game/game/game.c
@@ -2,18 +2,14 @@
 
 #include "config.h"
 #include "game/camera.h"
-#include "game/effects.h"
 #include "game/gameflow.h"
 #include "game/input.h"
 #include "game/inventory.h"
-#include "game/items.h"
 #include "game/lara.h"
-#include "game/lara/lara_cheat.h"
-#include "game/lara/lara_hair.h"
 #include "game/level.h"
 #include "game/music.h"
 #include "game/output.h"
-#include "game/overlay.h"
+#include "game/phase/phase.h"
 #include "game/savegame.h"
 #include "game/shell.h"
 #include "game/sound.h"
@@ -29,110 +25,7 @@
         Output_DumpScreen();                                                   \
     } while (g_Input.key);
 
-static const int32_t m_AnimationRate = 0x8000;
-static int32_t m_FrameCount = 0;
 static GAME_STATUS m_CurrentStatus = GS_INITIAL;
-
-static int32_t Game_Control(int32_t nframes, GAMEFLOW_LEVEL_TYPE level_type);
-
-static int32_t Game_Control(int32_t nframes, GAMEFLOW_LEVEL_TYPE level_type)
-{
-    int32_t return_val = 0;
-    if (nframes > MAX_FRAMES) {
-        nframes = MAX_FRAMES;
-    }
-
-    m_FrameCount += m_AnimationRate * nframes;
-    while (m_FrameCount >= 0) {
-        Lara_CheckCheatMode();
-        if (g_LevelComplete) {
-            return GF_NOP_BREAK;
-        }
-
-        Input_Update();
-        Shell_ProcessInput();
-        Game_ProcessInput();
-
-        if (level_type == GFL_DEMO) {
-            if (g_Input.any) {
-                return GF_EXIT_TO_TITLE;
-            }
-            if (!Game_Demo_ProcessInput()) {
-                return GF_EXIT_TO_TITLE;
-            }
-        }
-
-        if (g_Lara.death_timer > DEATH_WAIT
-            || (g_Lara.death_timer > DEATH_WAIT_MIN && g_Input.any
-                && !g_Input.fly_cheat)
-            || g_OverlayFlag == 2) {
-            if (level_type == GFL_DEMO) {
-                return GF_EXIT_TO_TITLE;
-            }
-            if (g_OverlayFlag == 2) {
-                g_OverlayFlag = 1;
-                return_val = Inv_Display(INV_DEATH_MODE);
-                if (return_val != GF_NOP) {
-                    return return_val;
-                }
-            } else {
-                g_OverlayFlag = 2;
-            }
-        }
-
-        if ((g_InputDB.option || g_Input.save || g_Input.load
-             || g_OverlayFlag <= 0)
-            && !g_Lara.death_timer) {
-            if (g_Camera.type == CAM_CINEMATIC) {
-                g_OverlayFlag = 0;
-            } else if (g_OverlayFlag > 0) {
-                if (g_Input.load) {
-                    g_OverlayFlag = -1;
-                } else if (g_Input.save) {
-                    g_OverlayFlag = -2;
-                } else {
-                    g_OverlayFlag = 0;
-                }
-            } else {
-                if (g_OverlayFlag == -1) {
-                    return_val = Inv_Display(INV_LOAD_MODE);
-                } else if (g_OverlayFlag == -2) {
-                    return_val = Inv_Display(INV_SAVE_MODE);
-                } else {
-                    return_val = Inv_Display(INV_GAME_MODE);
-                }
-
-                g_OverlayFlag = 1;
-                if (return_val != GF_NOP) {
-                    return return_val;
-                }
-            }
-        }
-
-        if (!g_Lara.death_timer && g_InputDB.pause) {
-            if (Game_Pause()) {
-                return GF_EXIT_TO_TITLE;
-            }
-        }
-
-        Item_Control();
-        Effect_Control();
-
-        Lara_Control();
-        Lara_Hair_Control();
-
-        Camera_Update();
-        Sound_ResetAmbient();
-        Effect_RunActiveFlipEffect();
-        Sound_UpdateEffects();
-        g_GameInfo.current[g_CurrentLevel].stats.timer++;
-        Overlay_BarHealthTimerTick();
-
-        m_FrameCount -= 0x10000;
-    }
-
-    return GF_NOP;
-}
 
 void Game_ProcessInput(void)
 {
@@ -172,6 +65,7 @@ GAME_STATUS Game_GetStatus(void)
 
 void Game_SetStatus(GAME_STATUS status)
 {
+    Phase_Set(PHASE_GAME);
     m_CurrentStatus = status;
 }
 
@@ -376,6 +270,9 @@ void Game_DisplayPicture(const char *path, double display_time)
 
 int32_t Game_Loop(GAMEFLOW_LEVEL_TYPE level_type)
 {
+    g_GameInfo.current_level_type = level_type;
+    Game_SetStatus(GS_IN_GAME);
+
     g_OverlayFlag = 1;
     Camera_Initialise();
 
@@ -395,11 +292,11 @@ int32_t Game_Loop(GAMEFLOW_LEVEL_TYPE level_type)
     int32_t nframes = 1;
     int32_t ret;
     while (1) {
-        ret = Game_Control(nframes, level_type);
+        ret = Phase_Control(nframes);
         if (ret != GF_NOP) {
             break;
         }
-        nframes = Game_ProcessFrame();
+        nframes = Phase_Draw();
 
         if (ask_for_save) {
             int32_t return_val = Inv_Display(INV_SAVE_CRYSTAL_MODE);

src/game/game/game_cutscene.c
@@ -10,6 +10,7 @@
 #include "game/lara/lara_hair.h"
 #include "game/level.h"
 #include "game/music.h"
+#include "game/output.h"
 #include "game/shell.h"
 #include "game/sound.h"
 #include "global/const.h"
@@ -127,11 +128,20 @@ int32_t Game_Cutscene_Stop(int32_t level_num)
 
 int32_t Game_Cutscene_Loop(void)
 {
+    Game_SetStatus(GS_IN_GAME);
+
     Game_Cutscene_Control(2);
-    Game_ProcessFrame();
+
+    Game_DrawScene(true);
+    g_Camera.number_frames = Output_DumpScreen();
+    Output_AnimateTextures(g_Camera.number_frames);
+
     int32_t nframes;
     do {
-        nframes = Game_ProcessFrame();
+        Game_DrawScene(true);
+        g_Camera.number_frames = Output_DumpScreen();
+        Output_AnimateTextures(g_Camera.number_frames);
+        nframes = g_Camera.number_frames;
     } while (!Game_Cutscene_Control(nframes));
     return GF_NOP;
 }

src/game/game/game_draw.c
@@ -11,14 +11,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-int32_t Game_ProcessFrame(void)
-{
-    Game_DrawScene(true);
-    g_Camera.number_frames = Output_DumpScreen();
-    Output_AnimateTextures(g_Camera.number_frames);
-    return g_Camera.number_frames;
-}
-
 void Game_DrawScene(bool draw_overlay)
 {
     if (g_Objects[O_LARA].loaded) {

src/game/phase/phase.c
@@ -1,0 +1,53 @@
+#include "game/phase/phase.h"
+
+#include "game/phase/phase_game.h"
+#include "global/types.h"
+#include "log.h"
+
+#include <stddef.h>
+
+static PHASE m_Phase = PHASE_UNKNOWN;
+static PHASER *m_Phaser = NULL;
+
+void Phase_Set(const PHASE phase)
+{
+    if (phase == m_Phase) {
+        return;
+    }
+
+    m_Phase = phase;
+    if (m_Phaser && m_Phaser->end) {
+        m_Phaser->end();
+    }
+
+    LOG_DEBUG("%d", phase);
+    switch (phase) {
+    case PHASE_GAME:
+        m_Phaser = &g_GamePhaser;
+        break;
+
+    case PHASE_UNKNOWN:
+        m_Phaser = NULL;
+        break;
+    }
+
+    if (m_Phaser->start) {
+        m_Phaser->start();
+    }
+}
+
+int32_t Phase_Control(int32_t nframes)
+{
+    if (m_Phaser && m_Phaser->control) {
+        return m_Phaser->control(nframes);
+    }
+    return GF_NOP;
+}
+
+int32_t Phase_Draw(void)
+{
+    if (m_Phaser && m_Phaser->draw) {
+        return m_Phaser->draw();
+    }
+    return 1;
+}

src/game/phase/phase.h
@@ -1,0 +1,19 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum PHASE {
+    PHASE_UNKNOWN,
+    PHASE_GAME,
+} PHASE;
+
+typedef struct PHASER {
+    void (*start)();
+    void (*end)();
+    int32_t (*control)(int32_t nframes);
+    int32_t (*draw)(void);
+} PHASER;
+
+void Phase_Set(PHASE phase);
+int32_t Phase_Control(int32_t nframes);
+int32_t Phase_Draw(void);

commit 249784d337aeb222bffd5c4fb6c6b8a72c692831

demo: fix not displaying text
A regression made in fc090401.

src/game/phase/phase_game.c
@@ -13,6 +13,7 @@
 #include "game/overlay.h"
 #include "game/shell.h"
 #include "game/sound.h"
+#include "game/text.h"
 #include "global/const.h"
 #include "global/types.h"
 #include "global/vars.h"
@@ -127,6 +128,7 @@ static int32_t Phase_Game_Control(int32_t nframes)
 static int32_t Phase_Game_Draw(void)
 {
     Game_DrawScene(true);
+    Text_Draw();
     g_Camera.number_frames = Output_DumpScreen();
     Output_AnimateTextures(g_Camera.number_frames);
     return g_Camera.number_frames;