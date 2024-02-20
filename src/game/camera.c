LostArtefacts/TRX

commit 7d7e619f527fe04ff99220f6d441913594a13dbc

camera: fix camera being used as a temp container
While this has no effect on the current code (at least in theory),
it enables some of the refactors that I have planned down the line.

src/game/camera.c
@@ -22,6 +22,7 @@ static double m_ManualCameraMultiplier[11] = {
     1.0, .5, .625, .75, .875, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0,
 };
 
+static void Camera_Apply(void);
 static bool Camera_BadPosition(
     int32_t x, int32_t y, int32_t z, int16_t room_num);
 static int32_t Camera_ShiftClamp(GAME_VECTOR *pos, int32_t clamp);
@@ -40,6 +41,13 @@ static void Camera_Move(GAME_VECTOR *ideal, int32_t speed);
 static void Camera_LoadCutsceneFrame(void);
 static void Camera_OffsetAdditionalAngle(int16_t delta);
 static void Camera_OffsetAdditionalElevation(int16_t delta);
+
+static void Camera_Apply(void)
+{
+    Matrix_LookAt(
+        g_Camera.pos.x, g_Camera.pos.y, g_Camera.pos.z, g_Camera.target.x,
+        g_Camera.target.y, g_Camera.target.z, g_Camera.roll);
+}
 
 static bool Camera_BadPosition(
     int32_t x, int32_t y, int32_t z, int16_t room_num)
@@ -352,16 +360,7 @@ static void Camera_Move(GAME_VECTOR *ideal, int32_t speed)
         g_Camera.shift = 0;
     }
 
-    Room_GetFloor(
-        g_Camera.pos.x, g_Camera.pos.y + g_Camera.shift, g_Camera.pos.z,
-        &g_Camera.pos.room_number);
-
-    Matrix_LookAt(
-        g_Camera.pos.x, g_Camera.pos.y + g_Camera.shift, g_Camera.pos.z,
-        g_Camera.target.x, g_Camera.target.y, g_Camera.target.z, 0);
-
-    g_Camera.actual_angle = Math_Atan(
-        g_Camera.target.z - g_Camera.pos.z, g_Camera.target.x - g_Camera.pos.x);
+    Camera_Apply();
 }
 
 static void Camera_LoadCutsceneFrame(void)
@@ -390,13 +389,7 @@ static void Camera_LoadCutsceneFrame(void)
 
     Viewport_SetFOV(ref->fov);
 
-    Matrix_LookAt(
-        g_Camera.pos.x, g_Camera.pos.y, g_Camera.pos.z, g_Camera.target.x,
-        g_Camera.target.y, g_Camera.target.z, g_Camera.roll);
-
-    Room_GetFloor(
-        g_Camera.pos.x, g_Camera.pos.y, g_Camera.pos.z,
-        &g_Camera.pos.room_number);
+    Camera_Apply();
 }
 
 static void Camera_OffsetAdditionalAngle(int16_t delta)
@@ -775,26 +768,25 @@ void Camera_OffsetReset(void)
 
 void Camera_UpdateCutscene(void)
 {
-    XYZ_32 cam_pos;
-    XYZ_32 cam_tar;
-
     CINE_CAMERA *ref = &g_CineCamera[g_CineFrame];
 
     int32_t c = Math_Cos(g_Camera.target_angle);
     int32_t s = Math_Sin(g_Camera.target_angle);
 
-    cam_tar.x = g_Camera.pos.x + ((ref->tx * c + ref->tz * s) >> W2V_SHIFT);
-    cam_tar.y = g_Camera.pos.y + ref->ty;
-    cam_tar.z = g_Camera.pos.z + ((ref->tz * c - ref->tx * s) >> W2V_SHIFT);
-    cam_pos.x = g_Camera.pos.x + ((ref->cz * s + ref->cx * c) >> W2V_SHIFT);
-    cam_pos.y = g_Camera.pos.y + ref->cy;
-    cam_pos.z = g_Camera.pos.z + ((ref->cz * c - ref->cx * s) >> W2V_SHIFT);
+    g_Camera.target.x =
+        g_CinePosition.pos.x + ((ref->tx * c + ref->tz * s) >> W2V_SHIFT);
+    g_Camera.target.y = g_CinePosition.pos.y + ref->ty;
+    g_Camera.target.z =
+        g_CinePosition.pos.z + ((ref->tz * c - ref->tx * s) >> W2V_SHIFT);
+    g_Camera.pos.x =
+        g_CinePosition.pos.x + ((ref->cz * s + ref->cx * c) >> W2V_SHIFT);
+    g_Camera.pos.y = g_CinePosition.pos.y + ref->cy;
+    g_Camera.pos.z =
+        g_CinePosition.pos.z + ((ref->cz * c - ref->cx * s) >> W2V_SHIFT);
+    g_Camera.roll = ref->roll;
 
     Viewport_SetFOV(ref->fov);
-
-    Matrix_LookAt(
-        cam_pos.x, cam_pos.y, cam_pos.z, cam_tar.x, cam_tar.y, cam_tar.z,
-        ref->roll);
+    Camera_Apply();
 }
 
 void Camera_RefreshFromTrigger(int16_t type, int16_t *data)

src/game/game/game_cutscene.c
@@ -25,7 +25,7 @@ static void Game_Cutscene_InitialiseHair(int32_t level_num)
         return;
     }
 
-    int16_t lara_item_num = -1;
+    int16_t lara_item_num = NO_ITEM;
     for (int i = 0; i < g_LevelItemCount; i++) {
         if (g_Items[i].object_number == lara_type) {
             lara_item_num = i;
@@ -33,7 +33,7 @@ static void Game_Cutscene_InitialiseHair(int32_t level_num)
         }
     }
 
-    if (lara_item_num == -1) {
+    if (lara_item_num == NO_ITEM) {
         return;
     }
 

src/game/gameflow.c
@@ -1363,13 +1363,13 @@ GameFlow_InterpretSequence(int32_t level_num, GAMEFLOW_LEVEL_TYPE level_type)
                 | ((int32_t)(intptr_t)seq->data & ((1 << 6) - 1));
 
         case GFS_SET_CAM_X:
-            g_Camera.pos.x = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.x = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_Y:
-            g_Camera.pos.y = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.y = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_Z:
-            g_Camera.pos.z = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.z = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_ANGLE:
             g_Camera.target_angle = (int32_t)(intptr_t)seq->data;
@@ -1531,13 +1531,13 @@ GameFlow_StorySoFar(int32_t level_num, int32_t savegame_level)
                 | ((int32_t)(intptr_t)seq->data & ((1 << 6) - 1));
 
         case GFS_SET_CAM_X:
-            g_Camera.pos.x = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.x = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_Y:
-            g_Camera.pos.y = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.y = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_Z:
-            g_Camera.pos.z = (int32_t)(intptr_t)seq->data;
+            g_CinePosition.pos.z = (int32_t)(intptr_t)seq->data;
             break;
         case GFS_SET_CAM_ANGLE:
             g_Camera.target_angle = (int32_t)(intptr_t)seq->data;

src/game/objects/creatures/cutscene_player.c
@@ -41,9 +41,9 @@ void CutscenePlayer_Control(int16_t item_num)
 {
     ITEM_INFO *item = &g_Items[item_num];
     item->rot.y = g_Camera.target_angle;
-    item->pos.x = g_Camera.pos.x;
-    item->pos.y = g_Camera.pos.y;
-    item->pos.z = g_Camera.pos.z;
+    item->pos.x = g_CinePosition.pos.x;
+    item->pos.y = g_CinePosition.pos.y;
+    item->pos.z = g_CinePosition.pos.z;
     Item_Animate(item);
 }
 
@@ -53,9 +53,9 @@ void CutscenePlayer1_Initialise(int16_t item_num)
 
     ITEM_INFO *item = &g_Items[item_num];
     g_Camera.pos.room_number = item->room_number;
-    g_Camera.pos.x = item->pos.x;
-    g_Camera.pos.y = item->pos.y;
-    g_Camera.pos.z = item->pos.z;
+    g_CinePosition.pos.x = item->pos.x;
+    g_CinePosition.pos.y = item->pos.y;
+    g_CinePosition.pos.z = item->pos.z;
     g_Camera.target_angle = 0;
     item->rot.y = 0;
 }

src/global/types.h
@@ -1834,7 +1834,6 @@ typedef struct CAMERA_INFO {
     int32_t target_distance;
     int32_t target_square;
     int16_t target_angle;
-    int16_t actual_angle;
     int16_t target_elevation;
     int16_t box;
     int16_t number;

commit 120b9cac3ed36105f1c803ae711078813523e987

camera: fix clipping issues
Fixes regression introduced in 7d7e619f527fe04ff99220f6d441913594a13dbc

src/game/camera.c
@@ -45,8 +45,8 @@ static void Camera_OffsetAdditionalElevation(int16_t delta);
 static void Camera_Apply(void)
 {
     Matrix_LookAt(
-        g_Camera.pos.x, g_Camera.pos.y, g_Camera.pos.z, g_Camera.target.x,
-        g_Camera.target.y, g_Camera.target.z, g_Camera.roll);
+        g_Camera.pos.x, g_Camera.pos.y + g_Camera.shift, g_Camera.pos.z,
+        g_Camera.target.x, g_Camera.target.y, g_Camera.target.z, g_Camera.roll);
 }
 
 static bool Camera_BadPosition(
@@ -386,9 +386,9 @@ static void Camera_LoadCutsceneFrame(void)
     g_Camera.pos.z =
         g_CinePosition.pos.z + ((c * ref->cz - s * ref->cx) >> W2V_SHIFT);
     g_Camera.roll = ref->roll;
+    g_Camera.shift = 0;
 
     Viewport_SetFOV(ref->fov);
-
     Camera_Apply();
 }
 
@@ -784,6 +784,7 @@ void Camera_UpdateCutscene(void)
     g_Camera.pos.z =
         g_CinePosition.pos.z + ((ref->cz * c - ref->cx * s) >> W2V_SHIFT);
     g_Camera.roll = ref->roll;
+    g_Camera.shift = 0;
 
     Viewport_SetFOV(ref->fov);
     Camera_Apply();