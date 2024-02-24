d6bc7a8c125b10498615f382b2ffdea8428d350e
Eike Hein <sho@eikehein.com>
2024-02-24 03:44:00 +0100

    specific: Fix frame timing on macOS
    - `SDL_GetPerformanceFrequency` returns a very high value on Apple
      Silicon and requires an appropriate data type to store.
    - The calculation of `delay_sec` could underflow if we're running
      behind and `target_counter - m_Counter` is negative, leading to
      a very high positive value and essentially freezing the
      rendering.

--- src/specific/s_clock.c
+++ src/specific/s_clock.c
@@ -6,11 +6,11 @@
 #include <SDL2/SDL_timer.h>
 
 static Uint64 m_Counter = 0;
-static double m_Frequency = 0.0;
+static Uint64 m_Frequency = 0;
 
 bool S_Clock_Init(void)
 {
-    m_Frequency = (double)SDL_GetPerformanceFrequency();
+    m_Frequency = SDL_GetPerformanceFrequency();
     m_Counter = SDL_GetPerformanceCounter();
     return true;
 }
@@ -32,8 +32,9 @@ int32_t S_Clock_SyncTicks(void)
             last_counter + TICKS_PER_FRAME * (m_Frequency / fps);
         const double elapsed_sec =
             (double)(m_Counter - last_counter) / m_Frequency;
-        const double delay_sec =
-            (double)(target_counter - m_Counter) / m_Frequency;
+        const double delay_sec = m_Counter <= target_counter
+            ? (double)(target_counter - m_Counter) / m_Frequency
+            : 0.0;
         int32_t delay_ms = delay_sec * 1000;
         if (delay_ms > 0) {
             SDL_Delay(delay_ms);
