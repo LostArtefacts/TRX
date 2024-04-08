ebe4e15c2004607d49c1474dacd55f094ae6ff32
Marcin Kurczewski <dash@wind.garden>
2024-04-09 01:03:44 +0200

    build: fix DATA_DIR location

--- tools/shared/common.py
+++ tools/shared/common.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 TOOLS_DIR = Path(__file__).parent.parent
 REPO_DIR = TOOLS_DIR.parent
-DATA_DIR = REPO_DIR / "src"
+DATA_DIR = REPO_DIR / "data"
 SRC_DIR = REPO_DIR / "src"
