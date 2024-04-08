c4e3d05bc319d982b8904dabd64580dde9c3d84d
Marcin Kurczewski <dash@wind.garden>
2024-04-09 01:34:10 +0200

    console: add OSD messages for texture filters etc

--- src/game/shell.c
+++ src/game/shell.c
@@ -300,16 +300,40 @@ void Shell_ProcessInput(void)
     if (g_InputDB.toggle_bilinear_filter) {
         g_Config.rendering.texture_filter =
             (g_Config.rendering.texture_filter + 1) % GFX_TF_NUMBER_OF;
+
+        switch (g_Config.rendering.texture_filter) {
+        case GFX_TF_NN:
+            Console_Log(
+                g_GameFlow.strings[GS_OSD_TEXTURE_FILTER_SET],
+                g_GameFlow.strings[GS_OSD_TEXTURE_FILTER_NN]);
+            break;
+        case GFX_TF_BILINEAR:
+            Console_Log(
+                g_GameFlow.strings[GS_OSD_TEXTURE_FILTER_SET],
+                g_GameFlow.strings[GS_OSD_TEXTURE_FILTER_BILINEAR]);
+            break;
+        case GFX_TF_NUMBER_OF:
+            break;
+        }
+
         Config_Write();
     }
 
     if (g_InputDB.toggle_perspective_filter) {
         g_Config.rendering.enable_perspective_filter ^= true;
+        Console_Log(g_GameFlow.strings
+                        [g_Config.rendering.enable_perspective_filter
+                             ? GS_OSD_PERSPECTIVE_FILTER_ON
+                             : GS_OSD_PERSPECTIVE_FILTER_OFF]);
         Config_Write();
     }
 
     if (g_InputDB.toggle_fps_counter) {
         g_Config.rendering.enable_fps_counter ^= true;
+        Console_Log(g_GameFlow.strings
+                        [g_Config.rendering.enable_fps_counter
+                             ? GS_OSD_FPS_COUNTER_ON
+                             : GS_OSD_FPS_COUNTER_OFF]);
         Config_Write();
     }
 
