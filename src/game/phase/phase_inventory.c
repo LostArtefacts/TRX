LostArtefacts/TRX

commit 625b7ac6c8fd2196ba208ee90e0877b9b146d666

inventory: animate passport at 60 fps

src/game/phase/phase_inventory.c
@@ -11,7 +11,9 @@
 #include "game/inventory/inventory_vars.h"
 #include "game/lara.h"
 #include "game/music.h"
+#include "game/objects/common.h"
 #include "game/option.h"
+#include "game/option/option_compass.h"
 #include "game/output.h"
 #include "game/overlay.h"
 #include "game/savegame.h"
@@ -57,6 +59,9 @@ static void Inv_Construct(void);
 static GAMEFLOW_OPTION Inv_Close(GAME_OBJECT_ID inv_chosen);
 static void Inv_SelectMeshes(INVENTORY_ITEM *inv_item);
 static bool Inv_AnimateItem(INVENTORY_ITEM *inv_item);
+static int32_t InvItem_GetFrames(
+    const INVENTORY_ITEM *inv_item, FRAME_INFO **out_frame1,
+    FRAME_INFO **out_frame2, int32_t *out_rate);
 static void Inv_DrawItem(INVENTORY_ITEM *inv_item);
 
 static void Phase_Inventory_Start(void *arg);
@@ -76,7 +81,9 @@ static void Inv_Draw(RING_INFO *ring, IMOTION_INFO *motion)
             m_OldCamera.pos.x, m_OldCamera.pos.y + m_OldCamera.shift,
             m_OldCamera.pos.z, m_OldCamera.target.x, m_OldCamera.target.y,
             m_OldCamera.target.z, 0);
+        Interpolation_Disable();
         Game_DrawScene(false);
+        Interpolation_Enable();
 
         int32_t width = Screen_GetResWidth();
         int32_t height = Screen_GetResHeight();
@@ -128,6 +135,14 @@ static void Inv_Draw(RING_INFO *ring, IMOTION_INFO *motion)
 
     Matrix_Pop();
     Viewport_SetFOV(old_fov);
+
+    if ((motion->status != RNG_OPENING
+         || (g_InvMode != INV_TITLE_MODE || !Output_FadeIsAnimating()))
+        && motion->status != RNG_DONE) {
+        for (int i = 0; i < Clock_GetFrameAdvance(); i++) {
+            Inv_Ring_DoMotions(ring);
+        }
+    }
 }
 
 static void Inv_Construct(void)
@@ -155,7 +170,6 @@ static void Inv_Construct(void)
         inv_item->drawn_meshes = inv_item->which_meshes;
         inv_item->current_frame = 0;
         inv_item->goal_frame = inv_item->current_frame;
-        inv_item->anim_count = 0;
         inv_item->y_rot = 0;
     }
 
@@ -163,7 +177,6 @@ static void Inv_Construct(void)
         INVENTORY_ITEM *inv_item = g_InvOptionList[i];
         inv_item->current_frame = 0;
         inv_item->goal_frame = 0;
-        inv_item->anim_count = 0;
         inv_item->y_rot = 0;
     }
 
@@ -187,7 +200,7 @@ static GAMEFLOW_OPTION Inv_Close(GAME_OBJECT_ID inv_chosen)
         return GF_PHASE_CONTINUE;
     }
 
-    Inv_Ring_RemoveAlText();
+    Inv_Ring_RemoveAllText();
     m_InvChosen = NO_OBJECT;
 
     if (m_VersionText) {
@@ -304,19 +317,53 @@ static bool Inv_AnimateItem(INVENTORY_ITEM *inv_item)
         Inv_SelectMeshes(inv_item);
         return false;
     }
-    if (inv_item->anim_count) {
-        inv_item->anim_count--;
-    } else {
-        inv_item->anim_count = inv_item->anim_speed;
-        inv_item->current_frame += inv_item->anim_direction;
-        if (inv_item->current_frame >= inv_item->frames_total) {
-            inv_item->current_frame = 0;
-        } else if (inv_item->current_frame < 0) {
-            inv_item->current_frame = inv_item->frames_total - 1;
-        }
+    inv_item->current_frame += inv_item->anim_direction;
+    if (inv_item->current_frame >= inv_item->frames_total) {
+        inv_item->current_frame = 0;
+    } else if (inv_item->current_frame < 0) {
+        inv_item->current_frame = inv_item->frames_total - 1;
     }
     Inv_SelectMeshes(inv_item);
     return true;
+}
+
+static int32_t InvItem_GetFrames(
+    const INVENTORY_ITEM *inv_item, FRAME_INFO **out_frame1,
+    FRAME_INFO **out_frame2, int32_t *out_rate)
+{
+    const IMOTION_INFO *motion = &m_Motion;
+    const OBJECT_INFO *obj = &g_Objects[inv_item->object_number];
+    if (motion->status != RNG_SELECTED && motion->status != RNG_CLOSING_ITEM) {
+        // only apply to animations, eg. the states where Inv_AnimateItem is
+        // being actively called
+        goto fallback;
+    }
+
+    if (inv_item->current_frame == inv_item->goal_frame
+        || inv_item->frames_total == 1 || g_Config.rendering.fps == 30) {
+        goto fallback;
+    }
+
+    const int32_t cur_frame_num = inv_item->current_frame;
+    int32_t next_frame_num = inv_item->current_frame + inv_item->anim_direction;
+    if (next_frame_num < 0) {
+        next_frame_num = 0;
+    }
+    if (next_frame_num >= inv_item->frames_total) {
+        next_frame_num = 0;
+    }
+
+    *out_frame1 = &obj->frame_base[cur_frame_num];
+    *out_frame2 = &obj->frame_base[next_frame_num];
+    *out_rate = 10;
+    return (Clock_GetTickProgress() - 0.5) * 10.0;
+
+    // OG
+fallback:
+    *out_frame1 = &obj->frame_base[inv_item->current_frame];
+    *out_frame2 = *out_frame1;
+    *out_rate = 1;
+    return 0;
 }
 
 static void Inv_DrawItem(INVENTORY_ITEM *inv_item)
@@ -324,24 +371,25 @@ static void Inv_DrawItem(INVENTORY_ITEM *inv_item)
     const RING_INFO *ring = &m_Ring;
     const IMOTION_INFO *motion = &m_Motion;
 
+    const int32_t advance = Clock_GetFrameAdvance();
     if (motion->status == RNG_DONE) {
         g_LsAdder = LOW_LIGHT;
     } else if (inv_item == ring->list[ring->current_object]) {
-        for (int j = 0; j < Clock_GetFrameAdvance(); j++) {
-            if (ring->rotating) {
-                g_LsAdder = LOW_LIGHT;
+        if (ring->rotating) {
+            g_LsAdder = LOW_LIGHT;
+            for (int j = 0; j < advance; j++) {
                 if (inv_item->y_rot < 0) {
                     inv_item->y_rot += 512;
                 } else if (inv_item->y_rot > 0) {
                     inv_item->y_rot -= 512;
                 }
-            } else if (
-                motion->status == RNG_SELECTED
-                || motion->status == RNG_DESELECTING
-                || motion->status == RNG_SELECTING
-                || motion->status == RNG_DESELECT
-                || motion->status == RNG_CLOSING_ITEM) {
-                g_LsAdder = HIGH_LIGHT;
+            }
+        } else if (
+            motion->status == RNG_SELECTED || motion->status == RNG_DESELECTING
+            || motion->status == RNG_SELECTING || motion->status == RNG_DESELECT
+            || motion->status == RNG_CLOSING_ITEM) {
+            g_LsAdder = HIGH_LIGHT;
+            for (int j = 0; j < advance; j++) {
                 if (inv_item->y_rot != inv_item->y_rot_sel) {
                     if (inv_item->y_rot_sel - inv_item->y_rot > 0
                         && inv_item->y_rot_sel - inv_item->y_rot < 0x8000) {
@@ -351,17 +399,19 @@ static void Inv_DrawItem(INVENTORY_ITEM *inv_item)
                     }
                     inv_item->y_rot &= 0xFC00u;
                 }
-            } else if (
-                ring->number_of_objects == 1
-                || (!g_Input.menu_left && !g_Input.menu_right)
-                || !g_Input.menu_left) {
-                g_LsAdder = HIGH_LIGHT;
+            }
+        } else if (
+            ring->number_of_objects == 1
+            || (!g_Input.menu_left && !g_Input.menu_right)
+            || !g_Input.menu_left) {
+            g_LsAdder = HIGH_LIGHT;
+            for (int j = 0; j < advance; j++) {
                 inv_item->y_rot += 256;
             }
         }
     } else {
         g_LsAdder = LOW_LIGHT;
-        for (int j = 0; j < Clock_GetFrameAdvance(); j++) {
+        for (int j = 0; j < advance; j++) {
             if (inv_item->y_rot < 0) {
                 inv_item->y_rot += 256;
             } else if (inv_item->y_rot > 0) {
@@ -425,58 +475,28 @@ static void Inv_DrawItem(INVENTORY_ITEM *inv_item)
         }
     }
 
-    const FRAME_INFO *const frame = &obj->frame_base[inv_item->current_frame];
-
-    Matrix_Push();
-
-    int32_t clip = Output_GetObjectBounds(&frame->bounds);
-    if (clip) {
-        Matrix_TranslateRel(frame->offset.x, frame->offset.y, frame->offset.z);
-        int32_t *packed_rotation = frame->mesh_rots;
-        Matrix_RotYXZpack(*packed_rotation++);
-
-        int32_t mesh_num = 1;
-
-        int32_t *bone = &g_AnimBones[obj->bone_index];
-        if (inv_item->drawn_meshes & mesh_num) {
-            Output_DrawPolygons(g_Meshes[obj->mesh_index], clip);
-        }
-
-        for (int i = 1; i < obj->nmeshes; i++) {
-            mesh_num *= 2;
-
-            int32_t bone_extra_flags = bone[0];
-            if (bone_extra_flags & BEB_POP) {
-                Matrix_Pop();
-            }
-            if (bone_extra_flags & BEB_PUSH) {
-                Matrix_Push();
-            }
-
-            Matrix_TranslateRel(bone[1], bone[2], bone[3]);
-            Matrix_RotYXZpack(*packed_rotation++);
-
-            if (inv_item->object_number == O_MAP_OPTION && i == 1) {
-                int16_t delta =
-                    -inv_item->y_rot - g_LaraItem->rot.y - m_CompassNeedle;
-                m_CompassSpeed = m_CompassSpeed * 19 / 20 + delta / 50;
-                m_CompassNeedle += m_CompassSpeed;
-                Matrix_RotY(m_CompassNeedle);
-            }
-
-            if (inv_item->drawn_meshes & mesh_num) {
-                Output_DrawPolygons(g_Meshes[obj->mesh_index + i], clip);
-            }
-
-            bone += 4;
-        }
+    int32_t rate;
+    FRAME_INFO *frame1;
+    FRAME_INFO *frame2;
+    const int32_t frac = InvItem_GetFrames(inv_item, &frame1, &frame2, &rate);
+    if (inv_item->object_number == O_MAP_OPTION) {
+        int16_t delta = -inv_item->y_rot - g_LaraItem->rot.y - m_CompassNeedle;
+        m_CompassSpeed = m_CompassSpeed * 19 / 20 + delta / 50;
+        m_CompassNeedle += m_CompassSpeed;
+        const int16_t extra_rotation[1] = { m_CompassNeedle };
+        int32_t *const bone = &g_AnimBones[obj->bone_index];
+        bone[0] |= BEB_ROT_Y;
+        Object_DrawInterpolatedObject(
+            obj, inv_item->drawn_meshes, extra_rotation, frame1, frame2, frac,
+            rate);
+    } else {
+        Object_DrawInterpolatedObject(
+            obj, inv_item->drawn_meshes, NULL, frame1, frame2, frac, rate);
     }
-    Matrix_Pop();
 }
 
 static void Phase_Inventory_Start(void *arg)
 {
-    Interpolation_Enable();
     Interpolation_Remember();
 
     INV_MODE inv_mode = (INV_MODE)arg;
@@ -589,7 +609,6 @@ static GAMEFLOW_OPTION Phase_Inventory_ControlFrame(void)
                 g_IDelay = false;
             }
         }
-        Inv_Ring_DoMotions(ring);
     }
 
     ring->camera.pos.z = ring->radius + CAMERA_2_RING;
@@ -860,11 +879,8 @@ static GAMEFLOW_OPTION Phase_Inventory_ControlFrame(void)
         }
 
         bool busy = false;
-        for (int j = 0; j < Clock_GetFrameAdvance(); j++) {
-            busy = false;
-            if (inv_item->y_rot == inv_item->y_rot_sel) {
-                busy = Inv_AnimateItem(inv_item);
-            }
+        if (inv_item->y_rot == inv_item->y_rot_sel) {
+            busy = Inv_AnimateItem(inv_item);
         }
 
         if (!busy && !g_IDelay) {
@@ -923,17 +939,15 @@ static GAMEFLOW_OPTION Phase_Inventory_ControlFrame(void)
 
     case RNG_CLOSING_ITEM: {
         INVENTORY_ITEM *inv_item = ring->list[ring->current_object];
-        for (int j = 0; j < Clock_GetFrameAdvance(); j++) {
-            if (!Inv_AnimateItem(inv_item)) {
-                if (inv_item->object_number == O_PASSPORT_OPTION) {
-                    inv_item->object_number = O_PASSPORT_CLOSED;
-                    inv_item->current_frame = 0;
-                }
-                motion->count = SELECTING_FRAMES;
-                motion->status = motion->status_target;
-                Inv_Ring_MotionItemDeselect(ring, inv_item);
-                break;
+        if (!Inv_AnimateItem(inv_item)) {
+            if (inv_item->object_number == O_PASSPORT_OPTION) {
+                inv_item->object_number = O_PASSPORT_CLOSED;
+                inv_item->current_frame = 0;
             }
+            motion->count = SELECTING_FRAMES;
+            motion->status = motion->status_target;
+            Inv_Ring_MotionItemDeselect(ring, inv_item);
+            break;
         }
         break;
     }
@@ -983,7 +997,7 @@ static GAMEFLOW_OPTION Phase_Inventory_ControlFrame(void)
         || motion->status == RNG_OPTION2MAIN
         || motion->status == RNG_EXITING_INVENTORY || motion->status == RNG_DONE
         || ring->rotating) {
-        Inv_Ring_RemoveAlText();
+        Inv_Ring_RemoveAllText();
     }
 
     return GF_PHASE_CONTINUE;
@@ -1003,7 +1017,6 @@ static GAMEFLOW_OPTION Phase_Inventory_Control(int32_t nframes)
 
 static void Phase_Inventory_End(void)
 {
-    Interpolation_Disable();
     if (g_Config.enable_buffering) {
         g_OldInputDB.any = 0;
     }

src/game/interpolation.c
@@ -39,12 +39,11 @@
     (target)->interp.result.member = Math_AngleMean(                           \
         (target)->interp.prev.member, (target)->member, (ratio))
 
-static bool m_IsEnabled = false;
+static bool m_IsEnabled = true;
 
 bool Interpolation_IsEnabled(void)
 {
-    return m_IsEnabled
-        && (Phase_Get() != PHASE_INVENTORY && g_Config.rendering.fps == 60);
+    return m_IsEnabled && g_Config.rendering.fps == 60;
 }
 
 void Interpolation_Disable(void)

src/game/inventory/inventory_ring.c
@@ -1,6 +1,7 @@
 #include "game/inventory/inventory_ring.h"
 
 #include "config.h"
+#include "game/clock.h"
 #include "game/game_string.h"
 #include "game/inventory.h"
 #include "game/inventory/inventory_vars.h"
@@ -150,7 +151,7 @@ void Inv_Ring_RemoveHeader(RING_INFO *ring)
     }
 }
 
-void Inv_Ring_RemoveAlText(void)
+void Inv_Ring_RemoveAllText(void)
 {
     for (int i = 0; i < IT_NUMBER_OF; i++) {
         if (g_InvItemText[i]) {

src/game/inventory/inventory_ring.h
@@ -10,7 +10,7 @@ void Inv_Ring_Init(
 
 void Inv_Ring_InitHeader(RING_INFO *ring);
 void Inv_Ring_RemoveHeader(RING_INFO *ring);
-void Inv_Ring_RemoveAlText(void);
+void Inv_Ring_RemoveAllText(void);
 void Inv_Ring_Active(INVENTORY_ITEM *inv_item);
 
 void Inv_Ring_GetView(RING_INFO *ring, XYZ_32 *view_pos, XYZ_16 *view_rot);

src/game/inventory/inventory_vars.c
@@ -83,8 +83,6 @@ INVENTORY_ITEM g_InvItemCompass = {
     .goal_frame = 0,
     .open_frame = 10,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4352,
     .pt_xrot = 0,
     .x_rot_sel = -8192,
@@ -109,8 +107,6 @@ INVENTORY_ITEM g_InvItemMedi = {
     .goal_frame = 0,
     .open_frame = 25,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4032,
     .pt_xrot = 0,
     .x_rot_sel = -7296,
@@ -135,8 +131,6 @@ INVENTORY_ITEM g_InvItemBigMedi = {
     .goal_frame = 0,
     .open_frame = 19,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3616,
     .pt_xrot = 0,
     .x_rot_sel = -8160,
@@ -161,8 +155,6 @@ INVENTORY_ITEM g_InvItemLeadBar = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3616,
     .pt_xrot = 0,
     .x_rot_sel = -8160,
@@ -187,8 +179,6 @@ INVENTORY_ITEM g_InvItemPickup1 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -213,8 +203,6 @@ INVENTORY_ITEM g_InvItemPickup2 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -239,8 +227,6 @@ INVENTORY_ITEM g_InvItemScion = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -265,8 +251,6 @@ INVENTORY_ITEM g_InvItemPuzzle1 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -291,8 +275,6 @@ INVENTORY_ITEM g_InvItemPuzzle2 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -317,8 +299,6 @@ INVENTORY_ITEM g_InvItemPuzzle3 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -343,8 +323,6 @@ INVENTORY_ITEM g_InvItemPuzzle4 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -369,8 +347,6 @@ INVENTORY_ITEM g_InvItemKey1 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -395,8 +371,6 @@ INVENTORY_ITEM g_InvItemKey2 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -421,8 +395,6 @@ INVENTORY_ITEM g_InvItemKey3 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -447,8 +419,6 @@ INVENTORY_ITEM g_InvItemKey4 = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 7200,
     .pt_xrot = 0,
     .x_rot_sel = -4352,
@@ -473,8 +443,6 @@ INVENTORY_ITEM g_InvItemPistols = {
     .goal_frame = 0,
     .open_frame = 11,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -499,8 +467,6 @@ INVENTORY_ITEM g_InvItemShotgun = {
     .goal_frame = 0,
     .open_frame = 12,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = 0,
@@ -525,8 +491,6 @@ INVENTORY_ITEM g_InvItemMagnum = {
     .goal_frame = 0,
     .open_frame = 11,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -551,8 +515,6 @@ INVENTORY_ITEM g_InvItemUzi = {
     .goal_frame = 0,
     .open_frame = 12,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -577,8 +539,6 @@ INVENTORY_ITEM g_InvItemGrenade = {
     .goal_frame = 0,
     .open_frame = 14,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 5024,
     .pt_xrot = 0,
     .x_rot_sel = 0,
@@ -603,8 +563,6 @@ INVENTORY_ITEM g_InvItemPistolAmmo = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -629,8 +587,6 @@ INVENTORY_ITEM g_InvItemShotgunAmmo = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -655,8 +611,6 @@ INVENTORY_ITEM g_InvItemMagnumAmmo = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -681,8 +635,6 @@ INVENTORY_ITEM g_InvItemUziAmmo = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 3200,
     .pt_xrot = 0,
     .x_rot_sel = -3808,
@@ -707,8 +659,6 @@ INVENTORY_ITEM g_InvItemGame = {
     .goal_frame = 0,
     .open_frame = 14,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4640,
     .pt_xrot = 0,
     .x_rot_sel = -4320,
@@ -733,8 +683,6 @@ INVENTORY_ITEM g_InvItemDetails = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4224,
     .pt_xrot = 0,
     .x_rot_sel = -6720,
@@ -759,8 +707,6 @@ INVENTORY_ITEM g_InvItemSound = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4832,
     .pt_xrot = 0,
     .x_rot_sel = -2336,
@@ -785,8 +731,6 @@ INVENTORY_ITEM g_InvItemControls = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 5504,
     .pt_xrot = 0,
     .x_rot_sel = 1536,
@@ -811,8 +755,6 @@ INVENTORY_ITEM g_InvItemLarasHome = {
     .goal_frame = 0,
     .open_frame = 0,
     .anim_direction = 1,
-    .anim_speed = 1,
-    .anim_count = 0,
     .pt_xrot_sel = 4640,
     .pt_xrot = 0,
     .x_rot_sel = -4320,

src/game/objects/common.c
@@ -13,6 +13,8 @@
 #include "math/matrix.h"
 #include "util.h"
 
+#include <assert.h>
+
 const GAME_OBJECT_ID g_EnemyObjects[] = {
     // clang-format off
     O_WOLF,
@@ -457,10 +459,117 @@ void Object_DrawPickupItem(ITEM_INFO *item)
     Matrix_Pop();
 }
 
+void Object_DrawInterpolatedObject(
+    const OBJECT_INFO *const object, uint32_t meshes,
+    const int16_t *extra_rotation, const FRAME_INFO *const frame1,
+    const FRAME_INFO *const frame2, const int32_t frac, const int32_t rate)
+{
+    assert(frame1);
+    int32_t clip = Output_GetObjectBounds(&frame1->bounds);
+    if (!clip) {
+        return;
+    }
+
+    Matrix_Push();
+    int32_t mesh_num = 1;
+    int16_t **meshpp = &g_Meshes[object->mesh_index];
+    int32_t *bone = &g_AnimBones[object->bone_index];
+
+    assert(rate);
+    if (!frac) {
+        Matrix_TranslateRel(
+            frame1->offset.x, frame1->offset.y, frame1->offset.z);
+
+        int32_t *packed_rotation = frame1->mesh_rots;
+        Matrix_RotYXZpack(*packed_rotation++);
+
+        if (meshes & mesh_num) {
+            Output_DrawPolygons(*meshpp++, clip);
+        }
+
+        for (int i = 1; i < object->nmeshes; i++) {
+            int32_t bone_flags = *bone;
+            if (bone_flags & BEB_POP) {
+                Matrix_Pop();
+            }
+
+            if (bone_flags & BEB_PUSH) {
+                Matrix_Push();
+            }
+
+            Matrix_TranslateRel(bone[1], bone[2], bone[3]);
+            Matrix_RotYXZpack(*packed_rotation++);
+
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_Y) != 0) {
+                Matrix_RotY(*extra_rotation++);
+            }
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_X) != 0) {
+                Matrix_RotX(*extra_rotation++);
+            }
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_Z) != 0) {
+                Matrix_RotZ(*extra_rotation++);
+            }
+
+            mesh_num <<= 1;
+            if (meshes & mesh_num) {
+                Output_DrawPolygons(*meshpp, clip);
+            }
+
+            bone += 4;
+            meshpp++;
+        }
+    } else {
+        assert(frame2);
+        Matrix_InitInterpolate(frac, rate);
+        Matrix_TranslateRel_ID(
+            frame1->offset.x, frame1->offset.y, frame1->offset.z,
+            frame2->offset.x, frame2->offset.y, frame2->offset.z);
+        int32_t *packed_rotation1 = frame1->mesh_rots;
+        int32_t *packed_rotation2 = frame2->mesh_rots;
+        Matrix_RotYXZpack_I(*packed_rotation1++, *packed_rotation2++);
+
+        if (meshes & mesh_num) {
+            Output_DrawPolygons_I(*meshpp++, clip);
+        }
+
+        for (int i = 1; i < object->nmeshes; i++) {
+            int32_t bone_flags = *bone;
+            if (bone_flags & BEB_POP) {
+                Matrix_Pop_I();
+            }
+
+            if (bone_flags & BEB_PUSH) {
+                Matrix_Push_I();
+            }
+
+            Matrix_TranslateRel_I(bone[1], bone[2], bone[3]);
+            Matrix_RotYXZpack_I(*packed_rotation1++, *packed_rotation2++);
+
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_Y) != 0) {
+                Matrix_RotY_I(*extra_rotation++);
+            }
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_X) != 0) {
+                Matrix_RotX_I(*extra_rotation++);
+            }
+            if (extra_rotation != NULL && (bone_flags & BEB_ROT_Z) != 0) {
+                Matrix_RotZ_I(*extra_rotation++);
+            }
+
+            mesh_num <<= 1;
+            if (meshes & mesh_num) {
+                Output_DrawPolygons_I(*meshpp, clip);
+            }
+
+            bone += 4;
+            meshpp++;
+        }
+    }
+
+    Matrix_Pop();
+}
+
 void Object_DrawAnimatingItem(ITEM_INFO *item)
 {
-    static int16_t null_rotation[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
     FRAME_INFO *frmptr[2];
     int32_t rate;
     int32_t frac = Item_GetFrames(item, frmptr, &rate);
@@ -478,107 +587,12 @@ void Object_DrawAnimatingItem(ITEM_INFO *item)
         item->interp.result.rot.y, item->interp.result.rot.x,
         item->interp.result.rot.z);
 
-    int32_t clip = Output_GetObjectBounds(&frmptr[0]->bounds);
-    if (!clip) {
-        Matrix_Pop();
-        return;
-    }
-
     Output_CalculateObjectLighting(item, &frmptr[0]->bounds);
-    int16_t *extra_rotation = item->data ? item->data : &null_rotation;
+    const int16_t *extra_rotation = item->data ? item->data : NULL;
 
-    int32_t bit = 1;
-    int16_t **meshpp = &g_Meshes[object->mesh_index];
-    int32_t *bone = &g_AnimBones[object->bone_index];
-
-    if (!frac) {
-        Matrix_TranslateRel(
-            frmptr[0]->offset.x, frmptr[0]->offset.y, frmptr[0]->offset.z);
-
-        int32_t *packed_rotation = frmptr[0]->mesh_rots;
-        Matrix_RotYXZpack(*packed_rotation++);
-
-        if (item->mesh_bits & bit) {
-            Output_DrawPolygons(*meshpp++, clip);
-        }
-
-        for (int i = 1; i < object->nmeshes; i++) {
-            int32_t bone_extra_flags = *bone;
-            if (bone_extra_flags & BEB_POP) {
-                Matrix_Pop();
-            }
-
-            if (bone_extra_flags & BEB_PUSH) {
-                Matrix_Push();
-            }
-
-            Matrix_TranslateRel(bone[1], bone[2], bone[3]);
-            Matrix_RotYXZpack(*packed_rotation++);
-
-            if (bone_extra_flags & BEB_ROT_Y) {
-                Matrix_RotY(*extra_rotation++);
-            }
-            if (bone_extra_flags & BEB_ROT_X) {
-                Matrix_RotX(*extra_rotation++);
-            }
-            if (bone_extra_flags & BEB_ROT_Z) {
-                Matrix_RotZ(*extra_rotation++);
-            }
-
-            bit <<= 1;
-            if (item->mesh_bits & bit) {
-                Output_DrawPolygons(*meshpp, clip);
-            }
-
-            bone += 4;
-            meshpp++;
-        }
-    } else {
-        Matrix_InitInterpolate(frac, rate);
-        Matrix_TranslateRel_ID(
-            frmptr[0]->offset.x, frmptr[0]->offset.y, frmptr[0]->offset.z,
-            frmptr[1]->offset.x, frmptr[1]->offset.y, frmptr[1]->offset.z);
-        int32_t *packed_rotation1 = frmptr[0]->mesh_rots;
-        int32_t *packed_rotation2 = frmptr[1]->mesh_rots;
-        Matrix_RotYXZpack_I(*packed_rotation1++, *packed_rotation2++);
-
-        if (item->mesh_bits & bit) {
-            Output_DrawPolygons_I(*meshpp++, clip);
-        }
-
-        for (int i = 1; i < object->nmeshes; i++) {
-            int32_t bone_extra_flags = *bone;
-            if (bone_extra_flags & BEB_POP) {
-                Matrix_Pop_I();
-            }
-
-            if (bone_extra_flags & BEB_PUSH) {
-                Matrix_Push_I();
-            }
-
-            Matrix_TranslateRel_I(bone[1], bone[2], bone[3]);
-            Matrix_RotYXZpack_I(*packed_rotation1++, *packed_rotation2++);
-
-            if (bone_extra_flags & BEB_ROT_Y) {
-                Matrix_RotY_I(*extra_rotation++);
-            }
-            if (bone_extra_flags & BEB_ROT_X) {
-                Matrix_RotX_I(*extra_rotation++);
-            }
-            if (bone_extra_flags & BEB_ROT_Z) {
-                Matrix_RotZ_I(*extra_rotation++);
-            }
-
-            bit <<= 1;
-            if (item->mesh_bits & bit) {
-                Output_DrawPolygons_I(*meshpp, clip);
-            }
-
-            bone += 4;
-            meshpp++;
-        }
-    }
-
+    Object_DrawInterpolatedObject(
+        &g_Objects[item->object_number], item->mesh_bits, extra_rotation,
+        frmptr[0], frmptr[1], frac, rate);
     Matrix_Pop();
 }
 

commit e755daf4466c53120f86ae90be9aadb5af75f3b7

passport: fix flicker
Resolves #1324
Regression from 625b7ac6

src/game/phase/phase_inventory.c
@@ -331,9 +331,13 @@ static int32_t InvItem_GetFrames(
     const INVENTORY_ITEM *inv_item, FRAME_INFO **out_frame1,
     FRAME_INFO **out_frame2, int32_t *out_rate)
 {
-    const IMOTION_INFO *motion = &m_Motion;
-    const OBJECT_INFO *obj = &g_Objects[inv_item->object_number];
-    if (motion->status != RNG_SELECTED && motion->status != RNG_CLOSING_ITEM) {
+    const RING_INFO *const ring = &m_Ring;
+    const IMOTION_INFO *const motion = &m_Motion;
+    const OBJECT_INFO *const obj = &g_Objects[inv_item->object_number];
+    const INVENTORY_ITEM *const cur_inv_item = ring->list[ring->current_object];
+    if (inv_item != cur_inv_item
+        || (motion->status != RNG_SELECTED
+            && motion->status != RNG_CLOSING_ITEM)) {
         // only apply to animations, eg. the states where Inv_AnimateItem is
         // being actively called
         goto fallback;