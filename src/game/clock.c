LostArtefacts/TRX

commit d5ef02ba76d5e57443bafa41919d26e1522cb6b8

clock: save turbo speed across game relaunches
Resolves #1320

src/game/clock.c
@@ -8,42 +8,42 @@
 #include "global/vars.h"
 #include "log.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <time.h>
 
-#define TURBO_SPEED_COUNT 5
-#define TURBO_SPEED_OFFSET -2
-#define TURBO_SPEED_DEFAULT_IDX 2
+#define CLOCK_TURBO_SPEED_MIN -2
+#define CLOCK_TURBO_SPEED_MAX 2
 
 static double m_Progress = 0.0;
-static int16_t m_TurboSpeedIdx = TURBO_SPEED_DEFAULT_IDX;
-
-static double m_TurboSpeeds[TURBO_SPEED_COUNT] = {
-    0.25, 0.5, 1.0, 2.0, 3.0,
-};
-
-void Clock_CycleTurboSpeed(bool forward)
-{
-    const int32_t idx = m_TurboSpeedIdx + TURBO_SPEED_OFFSET;
-    Clock_SetTurboSpeed(idx + (forward ? 1 : -1));
-}
-
-void Clock_SetTurboSpeed(const int32_t idx)
-{
-    m_TurboSpeedIdx = idx - TURBO_SPEED_OFFSET;
-    CLAMP(m_TurboSpeedIdx, 0, TURBO_SPEED_COUNT - 1);
-    LOG_INFO("Setting speed to %d (%.2f)", idx, Clock_GetSpeedMultiplier());
-    Console_Log(GS(OSD_SPEED_SET), Clock_GetTurboSpeed());
-}
 
 int32_t Clock_GetTurboSpeed(void)
 {
-    return m_TurboSpeedIdx + TURBO_SPEED_OFFSET;
+    return g_Config.rendering.turbo_speed;
+}
+
+void Clock_CycleTurboSpeed(const bool forward)
+{
+    Clock_SetTurboSpeed(g_Config.rendering.turbo_speed + (forward ? 1 : -1));
+}
+
+void Clock_SetTurboSpeed(int32_t value)
+{
+    CLAMP(value, CLOCK_TURBO_SPEED_MIN, CLOCK_TURBO_SPEED_MAX);
+    g_Config.rendering.turbo_speed = value;
+    Console_Log(GS(OSD_SPEED_SET), value);
+    Config_Write();
 }
 
 double Clock_GetSpeedMultiplier(void)
 {
-    return m_TurboSpeeds[m_TurboSpeedIdx];
+    if (g_Config.rendering.turbo_speed > 0) {
+        return g_Config.rendering.turbo_speed;
+    } else if (g_Config.rendering.turbo_speed < 0) {
+        return pow(2.0, g_Config.rendering.turbo_speed);
+    } else {
+        return 1.0;
+    }
 }
 
 void Clock_SetTickProgress(double progress)

src/config.h
@@ -158,6 +158,7 @@ typedef struct {
         bool enable_vsync;
         bool enable_fps_counter;
         float anisotropy_filter;
+        int32_t turbo_speed;
     } rendering;
 
     struct {

src/config_map.c
@@ -103,6 +103,7 @@ const CONFIG_OPTION g_ConfigOptionMap[] = {
     { .name = "enable_round_shadow",            .type = COT_BOOL,   .target = &g_Config.enable_round_shadow,                 .default_value = &(bool){true},                       0},
     { .name = "enable_3d_pickups",              .type = COT_BOOL,   .target = &g_Config.enable_3d_pickups,                   .default_value = &(bool){true},                       0},
     { .name = "anisotropy_filter",              .type = COT_FLOAT,  .target = &g_Config.rendering.anisotropy_filter,         .default_value = &(float){16.0f},                     0},
+    { .name = "turbo_speed",                    .type = COT_INT32,  .target = &g_Config.rendering.turbo_speed,               .default_value = &(int32_t){0},                       0},
     { .name = "walk_to_items",                  .type = COT_BOOL,   .target = &g_Config.walk_to_items,                       .default_value = &(bool){false},                      0},
     { .name = "disable_trex_collision",         .type = COT_BOOL,   .target = &g_Config.disable_trex_collision,              .default_value = &(bool){false},                      0},
     { .name = "start_lara_hitpoints",           .type = COT_INT32,  .target = &g_Config.start_lara_hitpoints,                .default_value = &(int32_t){LARA_HITPOINTS},          0},

src/game/clock.h
@@ -10,9 +10,9 @@ int32_t Clock_SyncTicks(void);
 void Clock_SetTickProgress(double progress);
 double Clock_GetTickProgress(void);
 
-void Clock_CycleTurboSpeed(bool forward);
-void Clock_SetTurboSpeed(const int32_t idx);
 int32_t Clock_GetTurboSpeed(void);
+void Clock_CycleTurboSpeed(bool forward);
+void Clock_SetTurboSpeed(int32_t value);
 double Clock_GetSpeedMultiplier(void);
 
 int32_t Clock_GetMS(void);

commit dcac730ca40ccf151e6ce7e349662c41cbc1e97b

clock: fix turbo speed offset by 1
Regression from d5ef02ba

src/game/clock.c
@@ -38,7 +38,7 @@ void Clock_SetTurboSpeed(int32_t value)
 double Clock_GetSpeedMultiplier(void)
 {
     if (g_Config.rendering.turbo_speed > 0) {
-        return g_Config.rendering.turbo_speed;
+        return 1.0 + g_Config.rendering.turbo_speed;
     } else if (g_Config.rendering.turbo_speed < 0) {
         return pow(2.0, g_Config.rendering.turbo_speed);
     } else {