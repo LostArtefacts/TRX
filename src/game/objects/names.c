0574ef7ffcc82cb5ad69b49338010fb64153d3e0
Marcin Kurczewski <dash@wind.garden>
2024-04-29 20:47:05 +0200

    strings: move to libtrx

--- src/game/objects/names.c
+++ src/game/objects/names.c
@@ -3,9 +3,9 @@
 #include "game/inventory.h"
 #include "game/inventory/inventory_vars.h"
 #include "game/objects/common.h"
-#include "strings.h"
 
 #include <libtrx/memory.h>
+#include <libtrx/strings.h>
 
 #include <string.h>
 
--- src/strings.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-const char *String_CaseSubstring(const char *subject, const char *pattern);
-int32_t String_Match(const char *subject, const char *pattern);
