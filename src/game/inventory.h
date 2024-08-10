52bcb5bf87a0e1c1ab6f4e0008412a861bd574f6
walkawayy <81546780+walkawayy@users.noreply.github.com>
2024-08-10 12:45:56 -0400

    inventory_func: enforce GAME_OBJECT_ID

--- src/game/inventory.h
+++ src/game/inventory.h
@@ -7,10 +7,10 @@
 
 bool Inv_Display(const INV_MODE inv_mode);
 
-bool Inv_AddItem(int32_t item_num);
-void Inv_AddItemNTimes(int32_t item_num, int32_t qty);
+bool Inv_AddItem(GAME_OBJECT_ID object_id);
+void Inv_AddItemNTimes(GAME_OBJECT_ID object_id, int32_t qty);
 void Inv_InsertItem(INVENTORY_ITEM *inv_item);
-int32_t Inv_RequestItem(int item_num);
+int32_t Inv_RequestItem(GAME_OBJECT_ID object_id);
 void Inv_RemoveAllItems(void);
-bool Inv_RemoveItem(int32_t item_num);
-GAME_OBJECT_ID Inv_GetItemOption(GAME_OBJECT_ID item_num);
+bool Inv_RemoveItem(GAME_OBJECT_ID object_id);
+GAME_OBJECT_ID Inv_GetItemOption(GAME_OBJECT_ID object_id);
