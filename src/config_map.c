504e6d8a3e1b54f24f5691b07c19bcee0ba031be
Marcin Kurczewski <dash@wind.garden>
2024-08-24 23:01:35 +0200

    config: move option macros to libtrx

--- src/config_map.c
+++ src/config_map.c
@@ -1,48 +1,13 @@
 #include "config_map.h"
 
 #include "config.h"
+
+// import order guard
+#include <libtrx/config/config_map.h>
+// import order guard
+
 #include "global/enum_str.h"
-
-#include <libtrx/utils.h>
-
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#define CFG_BOOL(target_, default_value_)                                      \
-    { .name = QUOTE(target_),                                                  \
-      .type = COT_BOOL,                                                        \
-      .target = &g_Config.target_,                                             \
-      .default_value = &(bool) { default_value_ },                             \
-      .param = NULL },
-
-#define CFG_INT32(target_, default_value_)                                     \
-    { .name = QUOTE(target_),                                                  \
-      .type = COT_INT32,                                                       \
-      .target = &g_Config.target_,                                             \
-      .default_value = &(int32_t) { default_value_ },                          \
-      .param = NULL },
-
-#define CFG_FLOAT(target_, default_value_)                                     \
-    { .name = QUOTE(target_),                                                  \
-      .type = COT_FLOAT,                                                       \
-      .target = &g_Config.target_,                                             \
-      .default_value = &(float) { default_value_ },                            \
-      .param = NULL },
-
-#define CFG_DOUBLE(target_, default_value_)                                    \
-    { .name = QUOTE(target_),                                                  \
-      .type = COT_DOUBLE,                                                      \
-      .target = &g_Config.target_,                                             \
-      .default_value = &(double) { default_value_ },                           \
-      .param = NULL },
-
-#define CFG_ENUM(target_, default_value_, enum_map)                            \
-    { .name = QUOTE(target_),                                                  \
-      .type = COT_ENUM,                                                        \
-      .target = &g_Config.target_,                                             \
-      .default_value = &(int32_t) { default_value_ },                          \
-      .param = ENUM_STRING_MAP(enum_map) },
+#include "global/types.h"
 
 const CONFIG_OPTION g_ConfigOptionMap[] = {
 #include "config_map.def"
--- src/global/enum_str.c
+++ src/global/enum_str.c
@@ -1,7 +1,3 @@
+#define ENUM_STR_IMPL
+
 #include "global/enum_str.h"
-
-#undef DECLARE_ENUM_STRING_MAP
-#define DECLARE_ENUM_STRING_MAP(type, ...)                                     \
-    const ENUM_STRING_MAP g_EnumStr_##type[] = { __VA_ARGS__ { NULL, -1 } };
-
-#include "global/enum_str.def"
