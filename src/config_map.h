6c5ba8dc97a755c135473933145cfda81c0e3159
Marcin Kurczewski <dash@wind.garden>
2024-08-24 23:01:35 +0200

    config: move option macros to libtrx

--- src/config_map.h
+++ src/config_map.h
@@ -1,19 +1,5 @@
 #pragma once
 
-typedef enum CONFIG_OPTION_TYPE {
-    COT_BOOL = 0,
-    COT_INT32 = 1,
-    COT_FLOAT = 2,
-    COT_DOUBLE = 3,
-    COT_ENUM = 4,
-} CONFIG_OPTION_TYPE;
-
-typedef struct {
-    const char *name;
-    CONFIG_OPTION_TYPE type;
-    const void *target;
-    const void *default_value;
-    const void *param;
-} CONFIG_OPTION;
+#include <libtrx/config/config_option.h>
 
 extern const CONFIG_OPTION g_ConfigOptionMap[];
--- src/global/enum_str.h
+++ src/global/enum_str.h
@@ -1,14 +1,11 @@
 #pragma once
 
-typedef struct {
-    const char *text;
-    int value;
-} ENUM_STRING_MAP;
+// import order guard
+#include <libtrx/enum_str.h>
+// import order guard
 
-#define ENUM_STRING_MAP(type) g_EnumStr_##type
-
-#define DECLARE_ENUM_STRING_MAP(type, ...)                                     \
-    extern const ENUM_STRING_MAP g_EnumStr_##type[];
+// import order guard
+#include "global/types.h"
+// import order guard
 
 #include "global/enum_str.def"
-#include "global/types.h"
