afaf12a9ae19a42e91d4829dabeaaa9a1066cd52
Marcin Kurczewski <dash@wind.garden>
2024-10-07 21:56:15 +0200

    log: add LOG_VAR macro

--- src/libtrx/include/libtrx/log.h
+++ src/libtrx/include/libtrx/log.h
@@ -5,6 +5,20 @@
 #define LOG_ERROR(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define LOG_DEBUG(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
 
+#define LOG_VAR(var)                                                           \
+    _Generic(                                                                  \
+        (var),                                                                 \
+        int: LOG_DEBUG(#var ": %d", var),                                      \
+        int8_t: LOG_DEBUG(#var ": %d", var),                                   \
+        int16_t: LOG_DEBUG(#var ": %d", var),                                  \
+        uint8_t: LOG_DEBUG(#var ": %d", var),                                  \
+        uint16_t: LOG_DEBUG(#var ": %d", var),                                 \
+        uint32_t: LOG_DEBUG(#var ": %d", var),                                 \
+        float: LOG_DEBUG(#var ": %f", var),                                    \
+        double: LOG_DEBUG(#var ": %f", var),                                   \
+        char *: LOG_DEBUG(#var ": %s", var),                                   \
+        default: LOG_DEBUG(#var ": %p", var))
+
 void Log_Init(const char *path);
 void Log_Shutdown(void);
 void Log_Message(
