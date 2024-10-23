aada9b89acd6f6dc7c6de31169533b4e33ac9a00
Marcin Kurczewski <dash@wind.garden>
2024-10-24 00:50:17 +0200

    tr2: port BridgeTilt2_Ceiling

--- src/tr2/game/objects/general/bridge_tilt_2.h
+++ src/tr2/game/objects/general/bridge_tilt_2.h
@@ -4,3 +4,6 @@
 
 void __cdecl BridgeTilt2_Floor(
     ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
+
+void __cdecl BridgeTilt2_Ceiling(
+    ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
