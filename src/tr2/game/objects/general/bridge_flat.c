c8d62cb896db61e5e03960907699eb9916be5838
Marcin Kurczewski <dash@wind.garden>
2024-10-24 00:53:01 +0200

    tr2/objects: make height functions const-correct

--- src/tr2/game/objects/general/bridge_flat.c
+++ src/tr2/game/objects/general/bridge_flat.c
@@ -1,7 +1,7 @@
 #include "game/objects/general/bridge_flat.h"
 
 void __cdecl BridgeFlat_Floor(
-    ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
+    const ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
     int32_t *const out_height)
 {
     if (y <= item->pos.y) {
@@ -10,7 +10,7 @@ void __cdecl BridgeFlat_Floor(
 }
 
 void __cdecl BridgeFlat_Ceiling(
-    ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
+    const ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
     int32_t *const out_height)
 {
     if (y > item->pos.y) {
--- src/tr2/game/objects/general/bridge_tilt_1.h
+++ src/tr2/game/objects/general/bridge_tilt_1.h
@@ -3,7 +3,7 @@
 #include "global/types.h"
 
 void __cdecl BridgeTilt1_Floor(
-    ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
+    const ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
 
 void __cdecl BridgeTilt1_Ceiling(
-    ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
+    const ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
