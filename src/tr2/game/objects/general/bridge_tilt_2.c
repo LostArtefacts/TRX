9351d90efbf404b2ed16976f7df966bca374f816
Marcin Kurczewski <dash@wind.garden>
2024-10-24 00:48:48 +0200

    tr2: port BridgeTilt2_Floor

---
+++ src/tr2/game/objects/general/bridge_tilt_2.c
@@ -0,0 +1,17 @@
+#include "game/objects/general/bridge_tilt_2.h"
+
+#include "global/funcs.h"
+
+void __cdecl BridgeTilt2_Floor(
+    ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
+    int32_t *const out_height)
+{
+    const int32_t offset_height =
+        item->pos.y + (Bridge_GetOffset(item, x, z) / 2);
+
+    if (y > offset_height) {
+        return;
+    }
+
+    *out_height = offset_height;
+}
