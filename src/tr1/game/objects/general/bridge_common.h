54c5b351fd754c7728725c7eebc820716e7dba5d
Marcin Kurczewski <dash@wind.garden>
2024-10-24 00:41:06 +0200

    tr2: port BridgeFlat_Floor

--- src/tr1/game/objects/general/bridge_common.h
+++ src/tr1/game/objects/general/bridge_common.h
@@ -5,5 +5,3 @@
 bool Bridge_IsSameSector(int32_t x, int32_t z, const ITEM *item);
 int32_t Bridge_GetOffset(const ITEM *item, int32_t x, int32_t y, int32_t z);
 void Bridge_FixEmbeddedPosition(int16_t item_num);
-
-void Bridge_Initialise(int16_t item_num);
---
+++ src/tr2/game/objects/general/bridge_flat.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "global/types.h"
+
+void __cdecl BridgeFlat_Floor(
+    ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
