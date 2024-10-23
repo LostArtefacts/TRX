LostArtefacts/TRX

commit d531d0b11b1cdc47a71327a0766ea4465a0f2414

tr2: port Object_SetupBaddyObjects

src/tr2/game/objects/creatures/eel.c
@@ -1,0 +1,26 @@
+#include "game/objects/creatures/eel.h"
+
+#include "game/creature.h"
+#include "game/objects/common.h"
+#include "global/const.h"
+#include "global/funcs.h"
+#include "global/vars.h"
+
+#define EEL_HITPOINTS 5
+
+void Eel_Setup(void)
+{
+    OBJECT *const obj = &g_Objects[O_EEL];
+    if (!obj->loaded) {
+        return;
+    }
+
+    obj->control = Eel_Control;
+    obj->collision = Creature_Collision;
+
+    obj->hit_points = EEL_HITPOINTS;
+
+    obj->water_creature = 1;
+    obj->save_hitpoints = 1;
+    obj->save_anim = 1;
+}

src/tr2/game/objects/common.c
@@ -10,6 +10,10 @@ OBJECT *Object_GetObject(GAME_OBJECT_ID object_id)
     return &g_Objects[object_id];
 }
 
+void Object_DrawDummyItem(const ITEM *const item)
+{
+}
+
 void __cdecl Object_Collision(
     const int16_t item_num, ITEM *const lara_item, COLL_INFO *const coll)
 {

src/tr2/game/objects/common.h
@@ -4,5 +4,7 @@
 
 #include <libtrx/game/objects/common.h>
 
+void Object_DrawDummyItem(const ITEM *item);
+
 void __cdecl Object_Collision(
     int16_t item_num, ITEM *lara_item, COLL_INFO *coll);

src/tr2/game/objects/creatures/bandit.c
@@ -1,0 +1,92 @@
+#include "game/objects/creatures/bandit.h"
+
+#include "game/creature.h"
+#include "game/objects/common.h"
+#include "global/funcs.h"
+#include "global/vars.h"
+
+#include <assert.h>
+
+#define BANDIT1_HITPOINTS 45
+#define BANDIT2_HITPOINTS 50
+#define BANDIT_RADIUS (WALL_L / 10) // = 102
+
+void Bandit1_Setup(void)
+{
+    OBJECT *const obj = Object_GetObject(O_BANDIT_1);
+    if (!obj->loaded) {
+        return;
+    }
+
+    obj->control = Bandit1_Control;
+    obj->collision = Creature_Collision;
+
+    obj->hit_points = BANDIT1_HITPOINTS;
+    obj->radius = BANDIT_RADIUS;
+    obj->shadow_size = UNIT_SHADOW / 2;
+    obj->pivot_length = 0;
+
+    obj->intelligent = 1;
+    obj->save_anim = 1;
+    obj->save_position = 1;
+    obj->save_hitpoints = 1;
+    obj->save_flags = 1;
+
+    g_AnimBones[obj->bone_idx + 24] |= BF_ROT_Y;
+    g_AnimBones[obj->bone_idx + 32] |= BF_ROT_Y;
+}
+
+void Bandit2_Setup(void)
+{
+    OBJECT *const obj = Object_GetObject(O_BANDIT_2);
+    if (!obj->loaded) {
+        return;
+    }
+
+    obj->control = Bandit2_Control;
+    obj->collision = Creature_Collision;
+
+    obj->hit_points = BANDIT2_HITPOINTS;
+    obj->radius = BANDIT_RADIUS;
+    obj->shadow_size = UNIT_SHADOW / 2;
+    obj->pivot_length = 0;
+
+    obj->intelligent = 1;
+    obj->save_anim = 1;
+    obj->save_position = 1;
+    obj->save_hitpoints = 1;
+    obj->save_flags = 1;
+
+    g_AnimBones[obj->bone_idx + 24] |= BF_ROT_Y;
+    g_AnimBones[obj->bone_idx + 32] |= BF_ROT_Y;
+}
+
+void Bandit2B_Setup(void)
+{
+    OBJECT *const obj = Object_GetObject(O_BANDIT_2B);
+    if (!obj->loaded) {
+        return;
+    }
+
+    const OBJECT *const ref_obj = Object_GetObject(O_BANDIT_2);
+    assert(ref_obj->loaded);
+    obj->anim_idx = ref_obj->anim_idx;
+    obj->frame_base = ref_obj->frame_base;
+
+    obj->control = Bandit2_Control;
+    obj->collision = Creature_Collision;
+
+    obj->hit_points = BANDIT2_HITPOINTS;
+    obj->radius = BANDIT_RADIUS;
+    obj->shadow_size = UNIT_SHADOW / 2;
+    obj->pivot_length = 0;
+
+    obj->intelligent = 1;
+    obj->save_anim = 1;
+    obj->save_position = 1;
+    obj->save_hitpoints = 1;
+    obj->save_flags = 1;
+
+    g_AnimBones[obj->bone_idx + 24] |= BF_ROT_Y;
+    g_AnimBones[obj->bone_idx + 32] |= BF_ROT_Y;
+}

src/tr2/game/objects/creatures/bandit.h
@@ -1,0 +1,5 @@
+#pragma once
+
+void Bandit1_Setup(void);
+void Bandit2_Setup(void);
+void Bandit2B_Setup(void);

src/tr2/game/objects/creatures/barracuda.c
@@ -1,0 +1,34 @@
+#include "game/objects/creatures/barracuda.h"
+
+#include "game/creature.h"
+#include "game/objects/common.h"
+#include "global/funcs.h"
+#include "global/vars.h"
+
+#define BARRACUDA_HITPOINTS 12
+#define BARRACUDA_RADIUS (WALL_L / 5) // = 204
+
+void Barracuda_Setup(void)
+{
+    OBJECT *const obj = Object_GetObject(O_BARRACUDA);
+    if (!obj->loaded) {
+        return;
+    }
+
+    obj->control = Baracudda_Control;
+    obj->collision = Creature_Collision;
+
+    obj->hit_points = BARRACUDA_HITPOINTS;
+    obj->radius = BARRACUDA_RADIUS;
+    obj->shadow_size = UNIT_SHADOW / 2;
+    obj->pivot_length = 200;
+
+    obj->intelligent = 1;
+    obj->water_creature = 1;
+    obj->save_anim = 1;
+    obj->save_position = 1;
+    obj->save_hitpoints = 1;
+    obj->save_flags = 1;
+
+    g_AnimBones[obj->bone_idx + 24] |= BF_ROT_Y;
+}

src/tr2/game/objects/creatures/barracuda.h
@@ -1,0 +1,3 @@
+#pragma once
+
+void Barracuda_Setup(void);

commit 3b4ffb84226a885929712f37370ca713e52d249d

tr2/objects/eel: fix setup
Resolves #1736. Regression from d531d0b1.

src/tr2/game/objects/creatures/eel.c
@@ -23,4 +23,5 @@ void Eel_Setup(void)
     obj->water_creature = 1;
     obj->save_hitpoints = 1;
     obj->save_anim = 1;
+    obj->save_flags = 1;
 }