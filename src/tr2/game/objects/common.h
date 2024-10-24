bca9cbace4ec0e6bc818b531207d36107f56d7cf
Marcin Kurczewski <dash@wind.garden>
2024-10-24 10:52:57 +0200

    tr2: port Door_Shut

--- src/tr2/game/objects/common.h
+++ src/tr2/game/objects/common.h
@@ -8,3 +8,6 @@ void Object_DrawDummyItem(const ITEM *item);
 
 void __cdecl Object_Collision(
     int16_t item_num, ITEM *lara_item, COLL_INFO *coll);
+
+void __cdecl Object_Collision_Trap(
+    int16_t item_num, ITEM *lara_item, COLL_INFO *coll);
