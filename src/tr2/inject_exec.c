41c7942b79ca4b65895123921c98f90d6d7a71bd
Marcin Kurczewski <dash@wind.garden>
2024-10-24 13:33:32 +0200

    tr2: port Trapdoor_Floor

--- src/tr2/inject_exec.c
+++ src/tr2/inject_exec.c
@@ -1061,6 +1061,7 @@ static void M_Objects(const bool enable)
     INJECT(enable, 0x0043F1D0, SkidooArmed_Push);
     INJECT(enable, 0x0043F2F0, SkidooArmed_Collision);
     INJECT(enable, 0x00442270, Trapdoor_Control);
+    INJECT(enable, 0x004422C0, Trapdoor_Floor);
     INJECT(enable, 0x00442340, Trapdoor_IsItemOnTop);
     INJECT(enable, 0x00442B30, FlameEmitter_Control);
     INJECT(enable, 0x00442BC0, Flame_Control);
