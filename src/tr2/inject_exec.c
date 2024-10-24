fa683c81cfad844118419487cfd110b6bf2a0a66
Marcin Kurczewski <dash@wind.garden>
2024-10-24 10:41:02 +0200

    tr2: port Drawbridge_Floor

--- src/tr2/inject_exec.c
+++ src/tr2/inject_exec.c
@@ -1023,6 +1023,7 @@ static void M_Objects(const bool enable)
     INJECT(enable, 0x004348C0, Zipline_Collision);
     INJECT(enable, 0x00434980, Zipline_Control);
     INJECT(enable, 0x00435590, Drawbridge_IsItemOnTop);
+    INJECT(enable, 0x00435650, Drawbridge_Floor);
     INJECT(enable, 0x00435700, Lift_Initialise);
     INJECT(enable, 0x00435740, Lift_Control);
     INJECT(enable, 0x00435820, Lift_FloorCeiling);
