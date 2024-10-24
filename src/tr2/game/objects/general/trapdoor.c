d3e66c2cd3df0152e87db3c98a076b5cee62a1bd
Marcin Kurczewski <dash@wind.garden>
2024-10-24 13:33:32 +0200

    tr2: port Trapdoor_Floor

--- src/tr2/game/objects/general/trapdoor.c
+++ src/tr2/game/objects/general/trapdoor.c
@@ -39,6 +39,20 @@ int32_t __cdecl Trapdoor_IsItemOnTop(
     return false;
 }
 
+void __cdecl Trapdoor_Floor(
+    const ITEM *const item, const int32_t x, const int32_t y, const int32_t z,
+    int32_t *const out_height)
+{
+    if (!Trapdoor_IsItemOnTop(item, x, z)) {
+        return;
+    } else if (item->current_anim_state != TRAPDOOR_STATE_CLOSED) {
+        return;
+    } else if (y > item->pos.y || item->pos.y > *out_height) {
+        return;
+    }
+    *out_height = item->pos.y;
+}
+
 void __cdecl Trapdoor_Control(const int16_t item_num)
 {
     ITEM *const item = Item_Get(item_num);
