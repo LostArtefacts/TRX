c7296dbf9252dfb979a182c766eb0ace0a5aa3c8
Marcin Kurczewski <dash@wind.garden>
2024-10-24 13:35:50 +0200

    tr2: port Trapdoor_Ceiling

--- src/tr2/game/objects/general/trapdoor.h
+++ src/tr2/game/objects/general/trapdoor.h
@@ -6,4 +6,6 @@ int32_t __cdecl Trapdoor_IsItemOnTop(const ITEM *item, int32_t x, int32_t z);
 
 void __cdecl Trapdoor_Floor(
     const ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
+void __cdecl Trapdoor_Ceiling(
+    const ITEM *item, int32_t x, int32_t y, int32_t z, int32_t *out_height);
 void __cdecl Trapdoor_Control(int16_t item_num);
