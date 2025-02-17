c2170fa64f9462e6e6451f73cc8184ba5c4866cb
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-02-17 13:28:12 +0000

    pathing/box: make box array local

--- src/libtrx/game/pathing/box.c
+++ src/libtrx/game/pathing/box.c
@@ -2,12 +2,12 @@
 #include "game/pathing.h"
 
 static int32_t m_BoxCount = 0;
-BOX_INFO *g_Boxes = nullptr;
+static BOX_INFO *m_Boxes = nullptr;
 
 void Box_InitialiseBoxes(const int32_t num_boxes)
 {
     m_BoxCount = num_boxes;
-    g_Boxes = num_boxes == 0
+    m_Boxes = num_boxes == 0
         ? nullptr
         : GameBuf_Alloc(sizeof(BOX_INFO) * num_boxes, GBUF_BOXES);
 }
@@ -19,5 +19,5 @@ int32_t Box_GetCount(void)
 
 BOX_INFO *Box_GetBox(const int32_t box_idx)
 {
-    return g_Boxes == nullptr ? nullptr : &g_Boxes[box_idx];
+    return m_Boxes == nullptr ? nullptr : &m_Boxes[box_idx];
 }
--- src/libtrx/include/libtrx/game/pathing/box.h
+++ src/libtrx/include/libtrx/game/pathing/box.h
@@ -2,8 +2,6 @@
 
 #include "./types.h"
 
-extern BOX_INFO *g_Boxes; // TODO: make local
-
 void Box_InitialiseBoxes(int32_t num_boxes);
 int32_t Box_GetCount(void);
 BOX_INFO *Box_GetBox(int32_t box_idx);
