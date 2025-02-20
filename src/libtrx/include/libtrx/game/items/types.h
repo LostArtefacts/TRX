lahm86/Tomb1Main

commit 9ec44fd85c1a24e57a8bbbbbc2f1012cf4794668

tr2/objects: use new constructor-based setup

src/tr2/game/objects/general/pickup.c
@@ -63,9 +63,10 @@ static int16_t m_PickupBoundsUW[12] = {
 
 static void M_DoPickup(int16_t item_num);
 static void M_DoFlarePickup(int16_t item_num);
-
 static void M_DoAboveWater(int16_t item, ITEM *lara_item);
 static void M_DoUnderwater(int16_t item, ITEM *lara_item);
+static void M_Setup(OBJECT *obj);
+static void M_Draw(const ITEM *item);
 
 static void M_DoPickup(const int16_t item_num)
 {
@@ -216,15 +217,15 @@ cleanup:
     item->rot = old_rot;
 }
 
-void Pickup_Setup(OBJECT *const obj)
+static void M_Setup(OBJECT *const obj)
 {
     obj->collision_func = Pickup_Collision;
-    obj->draw_func = Pickup_Draw;
+    obj->draw_func = M_Draw;
     obj->save_position = 1;
     obj->save_flags = 1;
 }
 
-void Pickup_Draw(const ITEM *const item)
+static void M_Draw(const ITEM *const item)
 {
     if (!g_Config.visuals.enable_3d_pickups) {
         Object_DrawSpriteItem(item);
@@ -322,7 +323,7 @@ void Pickup_Collision(
     }
 }
 
-int32_t Pickup_Trigger(int16_t item_num)
+bool Pickup_Trigger(const int16_t item_num)
 {
     ITEM *const item = Item_Get(item_num);
     if (item->status != IS_INVISIBLE) {
@@ -332,3 +333,34 @@ int32_t Pickup_Trigger(int16_t item_num)
     item->status = IS_DEACTIVATED;
     return true;
 }
+
+REGISTER_OBJECT(O_FLARES_ITEM, M_Setup)
+REGISTER_OBJECT(O_GRENADE_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_GRENADE_ITEM, M_Setup)
+REGISTER_OBJECT(O_HARPOON_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_HARPOON_ITEM, M_Setup)
+REGISTER_OBJECT(O_KEY_ITEM_1, M_Setup)
+REGISTER_OBJECT(O_KEY_ITEM_2, M_Setup)
+REGISTER_OBJECT(O_KEY_ITEM_3, M_Setup)
+REGISTER_OBJECT(O_KEY_ITEM_4, M_Setup)
+REGISTER_OBJECT(O_LARGE_MEDIPACK_ITEM, M_Setup)
+REGISTER_OBJECT(O_M16_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_M16_ITEM, M_Setup)
+REGISTER_OBJECT(O_MAGNUM_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_MAGNUM_ITEM, M_Setup)
+REGISTER_OBJECT(O_PICKUP_ITEM_1, M_Setup)
+REGISTER_OBJECT(O_PICKUP_ITEM_2, M_Setup)
+REGISTER_OBJECT(O_PISTOL_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_PISTOL_ITEM, M_Setup)
+REGISTER_OBJECT(O_PUZZLE_ITEM_1, M_Setup)
+REGISTER_OBJECT(O_PUZZLE_ITEM_2, M_Setup)
+REGISTER_OBJECT(O_PUZZLE_ITEM_3, M_Setup)
+REGISTER_OBJECT(O_PUZZLE_ITEM_4, M_Setup)
+REGISTER_OBJECT(O_SECRET_1, M_Setup)
+REGISTER_OBJECT(O_SECRET_2, M_Setup)
+REGISTER_OBJECT(O_SECRET_3, M_Setup)
+REGISTER_OBJECT(O_SHOTGUN_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_SHOTGUN_ITEM, M_Setup)
+REGISTER_OBJECT(O_SMALL_MEDIPACK_ITEM, M_Setup)
+REGISTER_OBJECT(O_UZI_AMMO_ITEM, M_Setup)
+REGISTER_OBJECT(O_UZI_ITEM, M_Setup)

LostArtefacts/TRX

commit 242e99f7429ecba618cb8f0ae81fa057f22257e0

objects: fix removing activated items
Resolves #2536 - fixes Jade Dragon not disappearing after Lara uses the
detonator box in Bartoli's Hideout. Regression from 9ec44fd85c1a.

src/libtrx/include/libtrx/game/items/types.h
@@ -43,14 +43,13 @@ typedef struct {
     uint16_t flags;
 
     SHADE shade;
-#if TR_VERSION == 1
     void *data;
+#if TR_VERSION == 1
     void *priv;
     CARRIED_ITEM *carried_item;
     bool enable_shadow;
 #elif TR_VERSION == 2
     int16_t carried_item;
-    void *data;
 #endif
 
     XYZ_32 pos;

src/libtrx/include/libtrx/game/objects/types.h
@@ -3,6 +3,7 @@
 #include "../anims/types.h"
 #include "../collision.h"
 #include "../items/types.h"
+#include "../rooms/enum.h"
 #include "../savegame.h"
 #include "../types.h"
 
@@ -60,6 +61,7 @@ typedef struct OBJECT {
         const ITEM *item, int32_t x, int32_t y, int32_t z, int16_t height);
     int16_t (*ceiling_height_func)(
         const ITEM *item, int32_t x, int32_t y, int32_t z, int16_t height);
+    void (*activate_func)(ITEM *item);
     void (*handle_save_func)(ITEM *item, SAVEGAME_STAGE stage);
 #if TR_VERSION == 1
     const OBJECT_BOUNDS *(*bounds_func)(void);

src/tr1/game/objects/general/pickup.c
@@ -66,6 +66,7 @@ static void M_GetAllAtLaraPos(ITEM *item, ITEM *lara_item);
 static void M_Setup(OBJECT *obj);
 static void M_Initialise(int16_t item_num);
 static void M_HandleSave(ITEM *item, SAVEGAME_STAGE stage);
+static void M_Activate(ITEM *item);
 static void M_Control(int16_t item_num);
 static const OBJECT_BOUNDS *M_Bounds(void);
 static void M_Collision(int16_t item_num, ITEM *lara_item, COLL_INFO *coll);
@@ -104,9 +105,12 @@ static void M_GetItem(int16_t item_num, ITEM *item, ITEM *lara_item)
 {
     Overlay_AddPickup(item->object_id);
     Inv_AddItem(item->object_id);
+
     item->status = IS_INVISIBLE;
+    item->flags |= IF_KILLED;
     Item_RemoveDrawn(item_num);
     Item_RemoveActive(item_num);
+
     Savegame_GetCurrentInfo(Game_GetCurrentLevel())->stats.pickup_count++;
     g_Lara.interact_target.is_moving = false;
 }
@@ -133,6 +137,7 @@ static void M_Setup(OBJECT *const obj)
     obj->bounds_func = M_Bounds;
     obj->initialise_func = M_Initialise;
     obj->handle_save_func = M_HandleSave;
+    obj->activate_func = M_Activate;
     obj->control_func = M_Control;
 }
 
@@ -152,6 +157,19 @@ static void M_HandleSave(ITEM *const item, const SAVEGAME_STAGE stage)
             const int16_t item_num = Item_GetIndex(item);
             Item_RemoveDrawn(item_num);
         }
+    }
+}
+
+static void M_Activate(ITEM *const item)
+{
+    if (item->status == IS_INVISIBLE) {
+        item->touch_bits = 0;
+        item->status = IS_ACTIVE;
+        const int16_t item_num = Item_GetIndex(item);
+        Item_AddActive(item_num);
+    } else {
+        item->status = IS_INVISIBLE;
+        item->flags |= IF_KILLED;
     }
 }
 

src/tr1/game/room.c
@@ -628,7 +628,10 @@ void Room_TestSectorTrigger(const ITEM *const item, const SECTOR *const sector)
             }
 
             if (!item->active) {
-                if (Object_Get(item->object_id)->intelligent) {
+                const OBJECT *const obj = Object_Get(item->object_id);
+                if (obj->activate_func != nullptr) {
+                    obj->activate_func(item);
+                } else if (obj->intelligent) {
                     if (item->status == IS_INACTIVE) {
                         item->touch_bits = 0;
                         item->status = IS_ACTIVE;

src/tr2/game/objects/general/pickup.c
@@ -67,6 +67,7 @@ static void M_DoAboveWater(int16_t item, ITEM *lara_item);
 static void M_DoUnderwater(int16_t item, ITEM *lara_item);
 static void M_Setup(OBJECT *obj);
 static void M_HandleSave(ITEM *item, SAVEGAME_STAGE stage);
+static void M_Activate(ITEM *item);
 static void M_Draw(const ITEM *item);
 
 static void M_DoPickup(const int16_t item_num)
@@ -89,7 +90,9 @@ static void M_DoPickup(const int16_t item_num)
     }
 
     item->status = IS_INVISIBLE;
+    item->flags |= IF_KILLED;
     Item_RemoveDrawn(item_num);
+    Item_RemoveActive(item_num);
 }
 
 static void M_DoFlarePickup(const int16_t item_num)
@@ -221,6 +224,7 @@ cleanup:
 static void M_Setup(OBJECT *const obj)
 {
     obj->handle_save_func = M_HandleSave;
+    obj->activate_func = M_Activate;
     obj->collision_func = Pickup_Collision;
     obj->draw_func = M_Draw;
     obj->save_position = 1;
@@ -234,6 +238,19 @@ static void M_HandleSave(ITEM *const item, const SAVEGAME_STAGE stage)
             const int16_t item_num = Item_GetIndex(item);
             Item_RemoveDrawn(item_num);
         }
+    }
+}
+
+static void M_Activate(ITEM *const item)
+{
+    if (item->status == IS_INVISIBLE) {
+        item->touch_bits = 0;
+        item->status = IS_ACTIVE;
+        const int16_t item_num = Item_GetIndex(item);
+        Item_AddActive(item_num);
+    } else {
+        item->status = IS_INVISIBLE;
+        item->flags |= IF_KILLED;
     }
 }
 

src/tr2/game/room.c
@@ -271,7 +271,9 @@ void Room_TestSectorTrigger(const ITEM *const item, const SECTOR *const sector)
             }
 
             const OBJECT *const obj = Object_Get(trig_item->object_id);
-            if (obj->intelligent) {
+            if (obj->activate_func != nullptr) {
+                obj->activate_func(trig_item);
+            } else if (obj->intelligent) {
                 if (trig_item->status == IS_INACTIVE) {
                     trig_item->touch_bits = 0;
                     trig_item->status = IS_ACTIVE;