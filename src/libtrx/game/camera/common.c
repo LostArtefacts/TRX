4909b24255ae1fd398fd132591e3cad73b5daa3d
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-02-28 22:22:24 +0000

    camera: clamp interpolation result
    This performs bounds tests on the interpolated camera result to avoid
    instances where black screens can occur.

--- src/libtrx/game/camera/common.c
+++ src/libtrx/game/camera/common.c
@@ -1,7 +1,9 @@
 #include "game/camera/common.h"
 
 #include "game/camera/vars.h"
-#include "game/rooms/const.h"
+#include "game/pathing.h"
+#include "game/rooms.h"
+#include "utils.h"
 
 static bool m_IsChunky = false;
 
@@ -18,4 +20,32 @@ void Camera_SetChunky(const bool is_chunky)
 void Camera_Reset(void)
 {
     g_Camera.pos.room_num = NO_ROOM;
+}
+
+void Camera_ClampInterpResult(void)
+{
+    XYZ_32 *const pos = &g_Camera.interp.result.pos;
+    const int32_t shift = g_Camera.interp.result.shift;
+    const ROOM *const room = Room_Get(g_Camera.interp.room_num);
+    const SECTOR *sector = Room_GetWorldSector(room, pos->x, pos->z);
+    if (sector->box != NO_BOX) {
+        goto finish;
+    }
+
+    sector = Room_GetWorldSector(room, g_Camera.pos.x, g_Camera.pos.z);
+    if (sector->box == NO_BOX) {
+        goto finish;
+    }
+
+    const BOX_INFO *const box = Box_GetBox(sector->box);
+    CLAMP(pos->x, box->top, box->bottom);
+    CLAMP(pos->z, box->left, box->right);
+
+finish:
+    const int32_t floor = Room_GetHeight(sector, pos->x, pos->y, pos->z);
+    const int32_t ceiling = Room_GetCeiling(sector, pos->x, pos->y, pos->z);
+    if (floor != NO_HEIGHT && ceiling != NO_HEIGHT) {
+        CLAMP(pos->y, ceiling - shift, floor - shift);
+    }
+    Room_GetSector(pos->x, pos->y + shift, pos->z, &g_Camera.interp.room_num);
 }
--- src/libtrx/game/rooms/common.c
+++ src/libtrx/game/rooms/common.c
@@ -480,8 +480,10 @@ BOUNDS_32 Room_GetWorldBounds(void)
 SECTOR *Room_GetWorldSector(
     const ROOM *const room, const int32_t x_pos, const int32_t z_pos)
 {
-    const int32_t x_sector = (x_pos - room->pos.x) >> WALL_SHIFT;
-    const int32_t z_sector = (z_pos - room->pos.z) >> WALL_SHIFT;
+    int32_t x_sector = (x_pos - room->pos.x) >> WALL_SHIFT;
+    int32_t z_sector = (z_pos - room->pos.z) >> WALL_SHIFT;
+    CLAMP(x_sector, 0, room->size.x - 1);
+    CLAMP(z_sector, 0, room->size.z - 1);
     return Room_GetUnitSector(room, x_sector, z_sector);
 }
 
