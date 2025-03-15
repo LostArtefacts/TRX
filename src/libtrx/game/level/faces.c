d392927c6b6f79bc8b42ab0aa75f1970c84ea512
Marcin Kurczewski <dash@wind.garden>
2025-03-15 23:46:00 +0100

    level: sort by texture pages rather than textures

--- src/libtrx/game/level/faces.c
+++ src/libtrx/game/level/faces.c
@@ -1,5 +1,6 @@
 #include "game/level/common.h"
 #include "game/objects.h"
+#include "game/output.h"
 #include "game/rooms.h"
 #include "game/types.h"
 
@@ -15,21 +16,33 @@ static int M_CompareFace3s(const void *const a, const void *const b)
 {
     const FACE3 *const face_a = a;
     const FACE3 *const face_b = b;
-    return face_a->texture_idx - face_b->texture_idx;
+    const OBJECT_TEXTURE *const tex_a =
+        Output_GetObjectTexture(face_a->texture_idx);
+    const OBJECT_TEXTURE *const tex_b =
+        Output_GetObjectTexture(face_b->texture_idx);
+    return tex_a->tex_page - tex_b->tex_page;
 }
 
 static int M_CompareFace4s(const void *const a, const void *const b)
 {
     const FACE4 *const face_a = a;
     const FACE4 *const face_b = b;
-    return face_a->texture_idx - face_b->texture_idx;
+    const OBJECT_TEXTURE *const tex_a =
+        Output_GetObjectTexture(face_a->texture_idx);
+    const OBJECT_TEXTURE *const tex_b =
+        Output_GetObjectTexture(face_b->texture_idx);
+    return tex_a->tex_page - tex_b->tex_page;
 }
 
 static int M_CompareRoomSprites(const void *const a, const void *const b)
 {
     const ROOM_SPRITE *const sprite_a = a;
     const ROOM_SPRITE *const sprite_b = b;
-    return sprite_a->texture - sprite_b->texture;
+    const OBJECT_TEXTURE *const tex_a =
+        Output_GetObjectTexture(sprite_a->texture);
+    const OBJECT_TEXTURE *const tex_b =
+        Output_GetObjectTexture(sprite_b->texture);
+    return tex_a->tex_page - tex_b->tex_page;
 }
 
 static void M_SortRoomFaces(void)
