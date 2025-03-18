8e1599a48e9a67e2cc55358dddac50344a7d6951
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-18 11:07:42 +0000

    tr2/gym: move assault course control to gym module
    This moves the assault course control logic to the gym module.

--- src/tr2/game/gym.h
+++ src/tr2/game/gym.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include "global/types_decomp.h"
+
 bool Gym_IsAccessible(void);
 void Gym_SetInventoryOpenEnabled(bool enabled);
 bool Gym_IsInventoryOpenEnabled(void);
+
+ASSAULT_STATS Gym_GetAssaultStats(void);
+
+void Gym_ResetAssault(void);
+void Gym_StartAssault(void);
+void Gym_StopAssault(void);
+void Gym_FinishAssault(void);
