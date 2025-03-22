1fafa4483cb5551eeebb659331201b9b4a7bda21
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-22 18:51:57 +0000

    objects/bear: move bear to TRX
    This moves the bear module fully to TRX.

--- src/libtrx/game/objects/creatures/bear.c
+++ src/libtrx/game/objects/creatures/bear.c
@@ -1,49 +1,51 @@
+#include "config.h"
 #include "game/creature.h"
-#include "game/items.h"
 #include "game/lara/common.h"
-#include "game/lot.h"
 #include "game/random.h"
 #include "game/spawn.h"
-#include "global/const.h"
-#include "global/types.h"
-#include "global/vars.h"
+#include "utils.h"
 
-#include <libtrx/config.h>
-#include <libtrx/utils.h>
-
+// clang-format off
 #define BEAR_CHARGE_DAMAGE 3
-#define BEAR_SLAM_DAMAGE 200
+#define BEAR_SLAM_DAMAGE   200
 #define BEAR_ATTACK_DAMAGE 200
-#define BEAR_PAT_DAMAGE 400
-#define BEAR_TOUCH 0x2406C
-#define BEAR_ROAR_CHANCE 80
-#define BEAR_REAR_CHANCE 768
-#define BEAR_DROP_CHANCE 1536
-#define BEAR_REAR_RANGE SQUARE(WALL_L * 2) // = 4194304
-#define BEAR_ATTACK_RANGE SQUARE(WALL_L) // = 1048576
-#define BEAR_PAT_RANGE SQUARE(600) // = 360000
+#define BEAR_PAT_DAMAGE    400
+#define BEAR_TOUCH         0x2406C
+#define BEAR_ROAR_CHANCE   80
+#define BEAR_REAR_CHANCE   768
+#define BEAR_DROP_CHANCE   1536
+#define BEAR_REAR_RANGE    SQUARE(WALL_L * 2) // = 4194304
+#define BEAR_ATTACK_RANGE  SQUARE(WALL_L) // = 1048576
+#define BEAR_PAT_RANGE     SQUARE(600) // = 360000
 #define BEAR_FIX_PAT_RANGE SQUARE(300) // = 90000
-#define BEAR_RUN_TURN (5 * DEG_1) // = 910
-#define BEAR_WALK_TURN (2 * DEG_1) // = 364
-#define BEAR_EAT_RANGE SQUARE(WALL_L * 3 / 4) // = 589824
-#define BEAR_HITPOINTS 20
-#define BEAR_RADIUS (WALL_L / 3) // = 341
-#define BEAR_SMARTNESS 0x4000
+#define BEAR_RUN_TURN      (5 * DEG_1) // = 910
+#define BEAR_WALK_TURN     (2 * DEG_1) // = 364
+#define BEAR_EAT_RANGE     SQUARE(WALL_L * 3 / 4) // = 589824
+#define BEAR_HITPOINTS     20
+#define BEAR_RADIUS        (WALL_L / 3) // = 341
+#define BEAR_SMARTNESS     0x4000
+// clang-format on
 
 typedef enum {
-    BEAR_STATE_STROLL = 0,
-    BEAR_STATE_STOP = 1,
-    BEAR_STATE_WALK = 2,
-    BEAR_STATE_RUN = 3,
-    BEAR_STATE_REAR = 4,
-    BEAR_STATE_ROAR = 5,
+    // clang-format off
+    BEAR_STATE_STROLL   = 0,
+    BEAR_STATE_STOP     = 1,
+    BEAR_STATE_WALK     = 2,
+    BEAR_STATE_RUN      = 3,
+    BEAR_STATE_REAR     = 4,
+    BEAR_STATE_ROAR     = 5,
     BEAR_STATE_ATTACK_1 = 6,
     BEAR_STATE_ATTACK_2 = 7,
-    BEAR_STATE_EAT = 8,
-    BEAR_STATE_DEATH = 9,
+    BEAR_STATE_EAT      = 8,
+    BEAR_STATE_DEATH    = 9,
+    // clang-format on
 } BEAR_STATE;
 
+#if TR_VERSION == 1
 static BITE m_BearHeadBite = { 0, 96, 335, 14 };
+#else
+static BITE m_BearHeadBite = { .pos = { 0, 96, 335 }, .mesh_num = 14 };
+#endif
 
 static void M_Setup(OBJECT *obj);
 static void M_Control(int16_t item_num);
@@ -71,20 +73,22 @@ static void M_Setup(OBJECT *const obj)
 
 static void M_Control(const int16_t item_num)
 {
-    ITEM *const item = Item_Get(item_num);
-    OBJECT *const obj = Object_Get(item->object_id);
-    obj->pivot_length = g_Config.gameplay.fix_bear_ai ? 0 : 500;
-
-    if (item->status == IS_INVISIBLE) {
-        if (!LOT_EnableBaddieAI(item_num, 0)) {
-            return;
-        }
-        item->status = IS_ACTIVE;
+    if (!Creature_Activate(item_num)) {
+        return;
     }
 
-    CREATURE *bear = (CREATURE *)item->data;
+    ITEM *const item = Item_Get(item_num);
+    OBJECT *const obj = Object_Get(item->object_id);
+#if TR_VERSION == 1
+    const bool fix_bear_ai = g_Config.gameplay.fix_bear_ai;
+#else
+    const bool fix_bear_ai = false;
+#endif
+    obj->pivot_length = fix_bear_ai ? 0 : 500;
+
+    CREATURE *const bear = (CREATURE *)item->data;
     int16_t head = 0;
-    PHD_ANGLE angle = 0;
+    int16_t angle = 0;
 
     if (item->hit_points <= 0) {
         angle = Creature_Turn(item, DEG_1);
@@ -128,7 +132,7 @@ static void M_Control(const int16_t item_num)
 
         angle = Creature_Turn(item, bear->maximum_turn);
 
-        int dead_enemy = g_LaraItem->hit_points <= 0;
+        const bool dead_enemy = Lara_GetItem()->hit_points <= 0;
         if (item->hit_status) {
             bear->flags = 1;
         }
@@ -194,8 +198,7 @@ static void M_Control(const int16_t item_num)
             } else if (
                 info.bite
                 && info.distance
-                    < (g_Config.gameplay.fix_bear_ai ? BEAR_FIX_PAT_RANGE
-                                                     : BEAR_PAT_RANGE)) {
+                    < (fix_bear_ai ? BEAR_FIX_PAT_RANGE : BEAR_PAT_RANGE)) {
                 item->goal_anim_state = BEAR_STATE_ATTACK_2;
             } else {
                 item->goal_anim_state = BEAR_STATE_WALK;
@@ -245,4 +248,6 @@ static void M_Control(const int16_t item_num)
     Creature_Animate(item_num, angle, 0);
 }
 
+#if TR_VERSION == 1
 REGISTER_OBJECT(O_BEAR, M_Setup)
+#endif
