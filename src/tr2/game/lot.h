58e8def9e5c5f5a057ba0e7e5849474e4fbb557c
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-22 18:18:23 +0000

    lot: move initial LOT declaration to TRX
    This moves the declaration of LOT_EnableBaddieAI to TRX.

--- src/tr2/game/lot.h
+++ src/tr2/game/lot.h
@@ -2,9 +2,10 @@
 
 #include "global/types.h"
 
+#include <libtrx/game/pathing/lot.h>
+
 void LOT_InitialiseArray(void);
 void LOT_DisableBaddieAI(int16_t item_num);
-bool LOT_EnableBaddieAI(int16_t item_num, bool always);
 void LOT_InitialiseSlot(int16_t item_num, int32_t slot);
 void LOT_CreateZone(ITEM *item);
 void LOT_ClearLOT(LOT_INFO *LOT);
