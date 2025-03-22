9e3729702a2c061b525282e63bbe6bc054819d2a
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-22 18:29:31 +0000

    spawn: move initial spawn declaration to TRX
    This moves the declaration of Spawn_Blood to TRX.

--- src/tr1/game/spawn.h
+++ src/tr1/game/spawn.h
@@ -2,13 +2,11 @@
 
 #include "global/types.h"
 
+#include <libtrx/game/spawn.h>
+
 void Spawn_Splash(ITEM *item);
 
 void Spawn_Bubble(const XYZ_32 *pos, int16_t room_num);
-
-int16_t Spawn_Blood(
-    int32_t x, int32_t y, int32_t z, int16_t speed, int16_t direction,
-    int16_t room_num);
 
 int16_t Spawn_ShardGun(
     int32_t x, int32_t y, int32_t z, int16_t speed, int16_t y_rot,
