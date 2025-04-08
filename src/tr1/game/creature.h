5b333e149b3cf828cf1d40b00089f248312072f4
Marcin Kurczewski <dash@wind.garden>
2025-04-08 23:27:52 +0200

    creature: move Creature_IsHostile to trx

--- src/tr1/game/creature.h
+++ src/tr1/game/creature.h
@@ -6,7 +6,7 @@
 #include <libtrx/game/creature.h>
 #include <libtrx/utils.h>
 
+bool Creature_IsBoss(int16_t item_num);
 bool Creature_ShootAtLara(
     ITEM *item, int32_t distance, BITE *gun, int16_t extra_rotation,
     int16_t damage);
-bool Creature_IsBoss(int16_t item_num);
