d4da3f874ec63632ec4a5d28bb17fe4cf62da57b
Marcin Kurczewski <dash@wind.garden>
2025-04-08 23:11:10 +0200

    creature: move Creature_Kill to trx

--- src/tr2/game/objects/creatures/shark.c
+++ src/tr2/game/objects/creatures/shark.c
@@ -159,7 +159,7 @@ static void M_Control(const int16_t item_num)
         }
 
         if (lara_alive && g_LaraItem->hit_points <= 0) {
-            Creature_Kill(
+            Creature_SpecialKill(
                 item, SHARK_ANIM_KILL, SHARK_STATE_KILL, LA_EXTRA_SHARK_KILL);
         } else if (item->current_anim_state == SHARK_STATE_KILL) {
             Item_Animate(item);
