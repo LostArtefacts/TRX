6e0117785920e4d53a10855d16abeeadc51318ea
Marcin Kurczewski <dash@wind.garden>
2025-04-08 23:06:58 +0200

    creature: move Creature_Vault to trx

--- src/tr2/game/creature.h
+++ src/tr2/game/creature.h
@@ -4,8 +4,6 @@
 
 #include <libtrx/game/creature.h>
 
-int32_t Creature_Vault(
-    int16_t item_num, int16_t angle, int32_t vault, int32_t shift);
 void Creature_Kill(
     ITEM *item, int32_t kill_anim, int32_t kill_state, int32_t lara_kill_state);
 void Creature_GetBaddieTarget(int16_t item_num, bool goody);
