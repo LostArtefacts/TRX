49ec394d768f757436915bd38dbdf3b5bae46de3
Marcin Kurczewski <dash@wind.garden>
2025-04-09 14:33:33 +0200

    tr2/savegame/bson: mirror tr1's footer approach

---
+++ src/libtrx/include/libtrx/game/savegame/bson.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+
+#pragma pack(push, 1)
+typedef struct {
+    uint32_t magic;
+    int16_t initial_version;
+    uint16_t version;
+    int32_t compressed_size;
+    int32_t uncompressed_size;
+} SAVEGAME_BSON_HEADER;
+
+typedef struct {
+    uint32_t flags;
+    int32_t counter;
+    int32_t level_num;
+    size_t title_size;
+} SAVEGAME_BSON_EXTENDED_HEADER;
+#pragma pack(pop)
