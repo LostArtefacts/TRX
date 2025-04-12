f3f8b253229a176f9ddede76ff11dabcf0b429ff
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-04-12 09:31:32 +0100

    pathing: raise TR2 creature slot limit
    This brings TR2 into line with TR1 in terms of the maximum number of
    enemies at a time.
    
    Resolves #1624.

--- src/libtrx/include/libtrx/game/pathing/const.h
+++ src/libtrx/include/libtrx/game/pathing/const.h
@@ -4,12 +4,11 @@
 
 #define NO_BOX (-1)
 #define BOX_ZONE(num) (((num) / STEP_L) - 1)
+#define LOT_SLOT_COUNT 32
 #if TR_VERSION == 1
     #define MAX_ZONES 2
-    #define LOT_SLOT_COUNT 32
 #else
     #define MAX_ZONES 4
-    #define LOT_SLOT_COUNT 5
 #endif
 
 #define BOX_BLOCKED 0x4000
