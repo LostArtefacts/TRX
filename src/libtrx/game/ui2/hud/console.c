2e0475e516f20f35859e2d46a9ebef7202fe6c49
Marcin Kurczewski <dash@wind.garden>
2025-04-14 22:34:19 +0200

    ui2: port console widget

---
+++ src/libtrx/game/ui2/hud/console.c
@@ -0,0 +1,168 @@
+#include "game/ui2/hud/console.h"
+
+#include "game/console.h"
+#include "game/text.h"
+#include "game/ui2/elements/modal.h"
+#include "game/ui2/elements/pad.h"
+#include "game/ui2/elements/prompt.h"
+#include "game/ui2/elements/spacer.h"
+#include "game/ui2/elements/stack.h"
+#include "game/ui2/events.h"
+#include "game/ui2/helpers.h"
+#include "game/ui2/hud/console_logs.h"
+#include "utils.h"
+
+static void M_Draw(const UI2_NODE *node);
+
+static const UI2_WIDGET_OPS m_Ops = {
+    .measure = UI2_MeasureWrapper,
+    .layout = UI2_LayoutWrapper,
+    .draw = M_Draw,
+};
+
+static void M_MoveHistoryUp(UI2_CONSOLE_STATE *s);
+static void M_MoveHistoryDown(UI2_CONSOLE_STATE *s);
+static void M_HandleOpen(const EVENT *event, void *user_data);
+static void M_HandleClose(const EVENT *event, void *user_data);
+static void M_HandleCancel(const EVENT *event, void *user_data);
+static void M_HandleConfirm(const EVENT *event, void *user_data);
+
+static void M_MoveHistoryUp(UI2_CONSOLE_STATE *const s)
+{
+    s->history_idx--;
+    CLAMP(s->history_idx, 0, Console_History_GetLength());
+    const char *const new_prompt = Console_History_Get(s->history_idx);
+    UI2_Prompt_ChangeText(&s->prompt, new_prompt == nullptr ? "" : new_prompt);
+}
+
+static void M_MoveHistoryDown(UI2_CONSOLE_STATE *const s)
+{
+    s->history_idx++;
+    CLAMP(s->history_idx, 0, Console_History_GetLength());
+    const char *const new_prompt = Console_History_Get(s->history_idx);
+    UI2_Prompt_ChangeText(&s->prompt, new_prompt == nullptr ? "" : new_prompt);
+}
+
+static void M_HandleKeyDown(const EVENT *const event, void *const user_data)
+{
+    if (!Console_IsOpened()) {
+        return;
+    }
+
+    UI2_CONSOLE_STATE *const s = user_data;
+    const UI2_INPUT key = (UI2_INPUT)(uintptr_t)event->data;
+
+    // clang-format off
+    switch (key) {
+    case UI_KEY_UP:   M_MoveHistoryUp(s); break;
+    case UI_KEY_DOWN: M_MoveHistoryDown(s); break;
+    default:          break;
+    }
+    // clang-format on
+}
+
+static void M_HandleOpen(const EVENT *event, void *user_data)
+{
+    UI2_CONSOLE_STATE *const s = user_data;
+    UI2_Prompt_SetFocus(&s->prompt, true);
+    s->history_idx = Console_History_GetLength();
+}
+
+static void M_HandleClose(const EVENT *event, void *user_data)
+{
+    UI2_CONSOLE_STATE *const s = user_data;
+    UI2_Prompt_SetFocus(&s->prompt, false);
+    UI2_Prompt_Clear(&s->prompt);
+}
+
+static void M_HandleCancel(const EVENT *const event, void *const data)
+{
+    Console_Close();
+}
+
+static void M_HandleConfirm(const EVENT *event, void *user_data)
+{
+    UI2_CONSOLE_STATE *const s = user_data;
+    const char *text = event->data;
+    Console_History_Append(text);
+    Console_Eval(text);
+    Console_Close();
+    s->history_idx = Console_History_GetLength();
+}
+
+static void M_Draw(const UI2_NODE *node)
+{
+    UI2_CONSOLE_STATE *const s = *(UI2_CONSOLE_STATE **)node->data;
+    UI2_DrawWrapper(node);
+    if (Console_IsOpened() || s->logs.vis_lines > 0) {
+        Console_DrawBackdrop();
+    }
+}
+
+void UI2_Console_Init(UI2_CONSOLE_STATE *const s)
+{
+    UI2_Prompt_Init(&s->prompt);
+    UI2_ConsoleLogs_Init(&s->logs);
+
+    struct {
+        const char *event_name;
+        const void *sender;
+        EVENT_LISTENER handler;
+    } listeners[] = {
+        { "console_open", nullptr, M_HandleOpen },
+        { "console_close", nullptr, M_HandleClose },
+        { "cancel", &s->prompt, M_HandleCancel },
+        { "confirm", &s->prompt, M_HandleConfirm },
+        { "key_down", nullptr, M_HandleKeyDown },
+        { 0 },
+    };
+    for (int32_t i = 0; listeners[i].event_name != nullptr; i++) {
+        s->listeners[i] = UI2_Subscribe(
+            listeners[i].event_name, listeners[i].sender, listeners[i].handler,
+            s);
+    }
+
+    s->history_idx = -1;
+}
+
+void UI2_Console_Free(UI2_CONSOLE_STATE *const s)
+{
+    UI2_ConsoleLogs_Free(&s->logs);
+    UI2_Prompt_Free(&s->prompt);
+    for (int32_t i = 0; i < 5; i++) {
+        UI2_Unsubscribe(s->listeners[i]);
+    }
+}
+
+void UI2_Console_Control(UI2_CONSOLE_STATE *const s)
+{
+    UI2_Prompt_Control(&s->prompt);
+}
+
+void UI2_Console(UI2_CONSOLE_STATE *const s)
+{
+    UI2_Prompt_SetFocus(&s->prompt, Console_IsOpened());
+
+    UI2_NODE *const node = UI2_AllocNode(&m_Ops, sizeof(UI2_CONSOLE_STATE *));
+    *(UI2_CONSOLE_STATE **)node->data = s;
+    UI2_AddChild(node);
+    UI2_PushCurrent(node);
+
+    UI2_BeginModal(0.0f, 1.0f);
+    UI2_BeginPad(5.0f, 5.0f);
+    UI2_BeginStack(UI2_STACK_VERTICAL);
+
+    UI2_ConsoleLogs(&s->logs);
+    UI2_Spacer(0.0f, 8.0f);
+    if (Console_IsOpened()) {
+        UI2_Prompt(&s->prompt);
+    } else {
+        UI2_Spacer(0.0f, TEXT_HEIGHT_FIXED);
+    }
+
+    UI2_EndStack();
+    UI2_EndModal();
+    UI2_EndPad();
+
+    UI2_PopCurrent();
+}
---
+++ src/libtrx/include/libtrx/game/console.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "console/common.h"
+#include "console/history.h"
