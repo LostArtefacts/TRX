73da4c294533d6ef4957f160c5d7a6d53c46cc82
Marcin Kurczewski <dash@wind.garden>
2025-04-26 14:07:20 +0200

    shell: do not create logs with --help

--- src/tr1/game/shell.c
+++ src/tr1/game/shell.c
@@ -82,7 +82,6 @@ static SHELL_ARGS m_Args = {
 static const char *m_CurrentGameFlowPath;
 
 static void M_ShowHelp(void);
-static bool M_ParseArgs(SHELL_ARGS *out_args);
 static void M_LoadConfig(void);
 static void M_HandleConfigChange(const EVENT *event, void *data);
 
@@ -94,41 +93,6 @@ static void M_ShowHelp(void)
     puts("   --demo-pc: launch the PC demo level file.");
     puts("-l/--level <PATH>: launch a specific level file.");
     puts("-s/--save <NUM>: launch from a specific save slot (starts at 1).");
-}
-
-static bool M_ParseArgs(SHELL_ARGS *const out_args)
-{
-    const char **args = nullptr;
-    int32_t arg_count = 0;
-    Shell_GetCommandLine(&arg_count, &args);
-
-    out_args->mod = M_MOD_OG;
-
-    for (int32_t i = 0; i < arg_count; i++) {
-        if (!strcmp(args[i], "-h") || !strcmp(args[i], "--help")) {
-            M_ShowHelp();
-            return false;
-        }
-        if (!strcmp(args[i], "-g") || !strcmp(args[i], "--gold")
-            || !strcmp(args[i], "-gold")) {
-            out_args->mod = M_MOD_UB;
-        }
-        if (!strcmp(args[i], "--demo-pc") || !strcmp(args[i], "-demo_pc")) {
-            out_args->mod = M_MOD_DEMO_PC;
-        }
-        if ((!strcmp(args[i], "-l") || !strcmp(args[i], "--level"))
-            && i + 1 < arg_count) {
-            out_args->level_to_play = args[i + 1];
-            out_args->mod = M_MOD_CUSTOM_LEVEL;
-        }
-        if ((!strcmp(args[i], "-s") || !strcmp(args[i], "--save"))
-            && i + 1 < arg_count) {
-            if (String_ParseInteger(args[i + 1], &out_args->save_to_load)) {
-                out_args->save_to_load--;
-            }
-        }
-    }
-    return true;
 }
 
 static void M_HandleConfigChange(const EVENT *const event, void *const data)
@@ -191,12 +155,40 @@ const char *Shell_GetGameFlowPath(void)
     return m_ModPaths[m_Args.mod].game_flow_path;
 }
 
+bool Shell_ParseArgs(const int32_t arg_count, const char **args)
+{
+    SHELL_ARGS *const out_args = &m_Args;
+    out_args->mod = M_MOD_OG;
+
+    for (int32_t i = 0; i < arg_count; i++) {
+        if (!strcmp(args[i], "-h") || !strcmp(args[i], "--help")) {
+            M_ShowHelp();
+            return false;
+        }
+        if (!strcmp(args[i], "-g") || !strcmp(args[i], "--gold")
+            || !strcmp(args[i], "-gold")) {
+            out_args->mod = M_MOD_UB;
+        }
+        if (!strcmp(args[i], "--demo-pc") || !strcmp(args[i], "-demo_pc")) {
+            out_args->mod = M_MOD_DEMO_PC;
+        }
+        if ((!strcmp(args[i], "-l") || !strcmp(args[i], "--level"))
+            && i + 1 < arg_count) {
+            out_args->level_to_play = args[i + 1];
+            out_args->mod = M_MOD_CUSTOM_LEVEL;
+        }
+        if ((!strcmp(args[i], "-s") || !strcmp(args[i], "--save"))
+            && i + 1 < arg_count) {
+            if (String_ParseInteger(args[i + 1], &out_args->save_to_load)) {
+                out_args->save_to_load--;
+            }
+        }
+    }
+    return true;
+}
+
 int32_t Shell_Main(void)
 {
-    if (!M_ParseArgs(&m_Args)) {
-        return 0;
-    }
-
     GameString_Init();
     EnumMap_Init();
     Config_Init();
