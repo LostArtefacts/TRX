83089c8d4c29ff4d57a756cf5ae31acb065a52f5
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-01 12:39:24 +0100

    items: tidy header includes

--- src/tr2/game/items.c
+++ src/tr2/game/items.c
@@ -1,21 +1,12 @@
 #include "game/items.h"
 
-#include "game/effects.h"
 #include "game/game_flow.h"
-#include "game/output.h"
-#include "game/random.h"
 #include "game/room.h"
-#include "game/sound.h"
 #include "global/vars.h"
 
 #include <libtrx/config.h>
-#include <libtrx/debug.h>
 #include <libtrx/game/game.h>
 #include <libtrx/game/interpolation.h>
-#include <libtrx/game/lara/const.h>
-#include <libtrx/game/math.h>
-#include <libtrx/game/matrix.h>
-#include <libtrx/utils.h>
 
 static BOUNDS_16 m_NullBounds = {};
 static BOUNDS_16 m_InterpolatedBounds = {};
