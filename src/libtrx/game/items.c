86f4da66e57197a13edaef2db626cb34db19978d
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-01 11:05:38 +0100

    items: port Item_GetHeight to TRX

--- src/libtrx/game/items.c
+++ src/libtrx/game/items.c
@@ -357,6 +357,17 @@ bool Item_GetAnimChange(ITEM *const item, const ANIM *const anim)
     return false;
 }
 
+int16_t Item_GetHeight(const ITEM *const item)
+{
+    int16_t room_num = item->room_num;
+    const SECTOR *const sector =
+        Room_GetSector(item->pos.x, item->pos.y, item->pos.z, &room_num);
+    const int32_t height =
+        Room_GetHeight(sector, item->pos.x, item->pos.y, item->pos.z);
+
+    return height;
+}
+
 void Item_Translate(
     ITEM *const item, const int32_t x, const int32_t y, const int32_t z)
 {
