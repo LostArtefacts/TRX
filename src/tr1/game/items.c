9a33f05aec9bf940acd8c3db9802eb8f9e28925c
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-01 17:02:14 +0100

    items: tidy header includes

--- src/tr1/game/items.c
+++ src/tr1/game/items.c
@@ -1,12 +1,6 @@
 #include "game/items.h"
 
 #include "game/carrier.h"
-#include "game/room.h"
-#include "global/vars.h"
-
-#include <libtrx/config.h>
-#include <libtrx/game/game.h>
-#include <libtrx/game/interpolation.h>
 
 void Item_Control(void)
 {
