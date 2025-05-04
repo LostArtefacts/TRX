c54fbe789713ad2928a6e219f9ee05f081ad9179
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-04 13:09:30 +0100

    rooms: port Room_GetSector to TRX

--- src/libtrx/include/libtrx/game/rooms/geometry.h
+++ src/libtrx/include/libtrx/game/rooms/geometry.h
@@ -2,8 +2,7 @@
 
 #include "./types.h"
 
-extern SECTOR *Room_GetSector(
-    int32_t x, int32_t y, int32_t z, int16_t *room_num);
+SECTOR *Room_GetSector(int32_t x, int32_t y, int32_t z, int16_t *room_num);
 SECTOR *Room_GetWorldSector(const ROOM *room, int32_t x_pos, int32_t z_pos);
 SECTOR *Room_GetUnitSector(
     const ROOM *room, int32_t x_sector, int32_t z_sector);
