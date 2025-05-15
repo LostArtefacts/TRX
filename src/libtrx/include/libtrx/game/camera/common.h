c172c193b7b48bdb3ab3473367e6e136388a1790
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-15 10:17:12 +0100

    camera: create common mic update function
    This moves duplicated code in TR2 to update the microphone position
    into the common camera module.

--- src/libtrx/include/libtrx/game/camera/common.h
+++ src/libtrx/include/libtrx/game/camera/common.h
@@ -10,6 +10,7 @@
         int32_t bottom
 
 void Camera_Update(void);
+void Camera_UpdateMicPosition(void);
 void Camera_MoveManual(void);
 void Camera_Apply(void);
 void Camera_Move(const GAME_VECTOR *target, int32_t speed);
