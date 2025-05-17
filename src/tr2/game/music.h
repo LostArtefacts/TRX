883b93c655457cf4702a1f1ca6a3e7a6f79f25c8
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-05-17 10:52:43 +0100

    tr2/music: remove duplicated function declarations
    The function declarations removed here are already defined in TRX.

--- src/tr2/game/music.h
+++ src/tr2/game/music.h
@@ -6,12 +6,6 @@
 
 bool Music_Init(void);
 void Music_Shutdown(void);
-void Music_Stop(void);
 double Music_GetTimestamp(void);
 bool Music_SeekTimestamp(double timestamp);
-void Music_SetVolume(int32_t volume);
-MUSIC_TRACK_ID Music_GetCurrentPlayingTrack(void);
-MUSIC_TRACK_ID Music_GetCurrentLoopedTrack(void);
 MUSIC_TRACK_ID Music_GetLastPlayedTrack(void);
-void Music_Pause(void);
-void Music_Unpause(void);
