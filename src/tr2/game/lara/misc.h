7e0a0887c7eb94be54fbd9da64416c76a6c13574
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-07 20:07:47 +0100

    tr2/lara/misc: tidy header whitespace

--- src/tr2/game/lara/misc.h
+++ src/tr2/game/lara/misc.h
@@ -5,39 +5,26 @@
 #include <libtrx/game/lara/misc.h>
 
 int32_t Lara_TestWall(ITEM *item, int32_t front, int32_t right, int32_t down);
-
 int32_t Lara_TestHangOnClimbWall(ITEM *item, COLL_INFO *coll);
-
 int32_t Lara_TestClimbStance(ITEM *item, COLL_INFO *coll);
-
 int32_t Lara_TestEdgeCatch(ITEM *item, COLL_INFO *coll, int32_t *edge);
-
 int32_t Lara_TestHangSwingIn(ITEM *item, int16_t angle);
-
 int32_t Lara_CheckForLetGo(ITEM *item, COLL_INFO *coll);
-
 void Lara_GetJointAbsPosition(XYZ_32 *vec, int32_t joint);
-
 void Lara_GetJointAbsPosition_I(
     ITEM *item, XYZ_32 *vec, ANIM_FRAME *frame1, ANIM_FRAME *frame2,
     int32_t frac, int32_t rate);
-
 void Lara_BaddieCollision(ITEM *lara_item, COLL_INFO *coll);
 void Lara_TakeHit(ITEM *lara_item, const COLL_INFO *coll);
-
 int32_t Lara_TestClimb(
     int32_t x, int32_t y, int32_t z, int32_t x_front, int32_t z_front,
     int32_t item_height, int16_t item_room, int32_t *shift);
-
 int32_t Lara_TestClimbPos(
     const ITEM *item, int32_t front, int32_t right, int32_t origin,
     int32_t height, int32_t *shift);
-
 void Lara_DoClimbLeftRight(
     ITEM *item, const COLL_INFO *coll, int32_t result, int32_t shift);
-
 int32_t Lara_TestClimbUpPos(
     const ITEM *item, int32_t front, int32_t right, int32_t *shift,
     int32_t *ledge);
-
 void Lara_WaterCurrent(COLL_INFO *coll);
