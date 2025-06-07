2b9e03a81c7cd61fe6415786c045266e610731fb
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-07 19:22:32 +0100

    lara/col: port Lara_SwimCollision to TRX

--- src/tr1/game/lara/misc.h
+++ src/tr1/game/lara/misc.h
@@ -8,5 +8,3 @@ bool Lara_Fallen(ITEM *item, COLL_INFO *coll);
 bool Lara_DeflectEdge(ITEM *item, COLL_INFO *coll);
 bool Lara_TestVault(ITEM *item, COLL_INFO *coll);
 bool Lara_TestHangSwingIn(ITEM *item, PHD_ANGLE angle);
-int32_t Lara_GetWaterDepth(int32_t x, int32_t y, int32_t z, int16_t room_num);
-void Lara_TestWaterDepth(ITEM *item, const COLL_INFO *coll);
