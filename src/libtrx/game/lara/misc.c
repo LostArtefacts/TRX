4babca2156adb27961eb390bddb6133fa3ff3082
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-07 19:30:17 +0100

    lara/misc: port Lara_GetWaterDepth to TRX

--- src/libtrx/game/lara/misc.c
+++ src/libtrx/game/lara/misc.c
@@ -93,6 +93,69 @@ void Lara_ShiftCol(COLL_INFO *const coll)
     coll->shift.x = 0;
 }
 
+int32_t Lara_GetWaterDepth(
+    const int32_t x, const int32_t y, const int32_t z, int16_t room_num)
+{
+    const ROOM *room = Room_Get(room_num);
+    const SECTOR *sector;
+
+    while (true) {
+        int32_t z_sector = (z - room->pos.z) >> WALL_SHIFT;
+        int32_t x_sector = (x - room->pos.x) >> WALL_SHIFT;
+
+        if (z_sector <= 0) {
+            z_sector = 0;
+            if (x_sector < 1) {
+                x_sector = 1;
+            } else if (x_sector > room->size.x - 2) {
+                x_sector = room->size.x - 2;
+            }
+        } else if (z_sector >= room->size.z - 1) {
+            z_sector = room->size.z - 1;
+            if (x_sector < 1) {
+                x_sector = 1;
+            } else if (x_sector > room->size.x - 2) {
+                x_sector = room->size.x - 2;
+            }
+        } else if (x_sector < 0) {
+            x_sector = 0;
+        } else if (x_sector >= room->size.x) {
+            x_sector = room->size.x - 1;
+        }
+
+        sector = Room_GetUnitSector(room, x_sector, z_sector);
+        if (sector->portal_room.wall == NO_ROOM) {
+            break;
+        }
+        room_num = sector->portal_room.wall;
+        room = Room_Get(room_num);
+    }
+
+    if ((room->flags & RF_UNDERWATER) != 0) {
+        while (sector->portal_room.sky != NO_ROOM) {
+            room = Room_Get(sector->portal_room.sky);
+            if ((room->flags & RF_UNDERWATER) == 0) {
+                const int32_t water_height = sector->ceiling.height;
+                sector = Room_GetSector(x, y, z, &room_num);
+                return Room_GetHeight(sector, x, y, z) - water_height;
+            }
+            sector = Room_GetWorldSector(room, x, z);
+        }
+        return 0x7FFF;
+    }
+
+    while (sector->portal_room.pit != NO_ROOM) {
+        room = Room_Get(sector->portal_room.pit);
+        if ((room->flags & RF_UNDERWATER) != 0) {
+            const int32_t water_height = sector->floor.height;
+            sector = Room_GetSector(x, y, z, &room_num);
+            return Room_GetHeight(sector, x, y, z) - water_height;
+        }
+        sector = Room_GetWorldSector(room, x, z);
+    }
+    return NO_HEIGHT;
+}
+
 bool Lara_IsM16Active(void)
 {
 #if TR_VERSION == 1
