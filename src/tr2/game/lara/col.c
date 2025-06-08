cdcf358da7d340a29830bb4dd70f74f35e846cae
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-08 12:26:11 +0100

    lara/col: port Lara_Col_Climbing to TRX

--- src/tr2/game/lara/col.c
+++ src/tr2/game/lara/col.c
@@ -14,63 +14,6 @@
 #define LF_CLIMB_L_SHIFT_END 29
 #define LF_CLIMB_R_SHIFT 57
 
-void Lara_Col_Climbing(ITEM *item, COLL_INFO *coll)
-{
-    if (Lara_CheckForLetGo(item, coll)
-        || !Item_TestAnimEqual(item, LA_LADDER_UP)) {
-        return;
-    }
-
-    int32_t yshift;
-    if (Item_TestFrameEqual(item, 0)) {
-        yshift = 0;
-    } else if (Item_TestFrameRange(
-                   item, LF_CLIMB_L_SHIFT_START, LF_CLIMB_L_SHIFT_END)) {
-        yshift = -STEP_L;
-    } else if (Item_TestFrameEqual(item, LF_CLIMB_R_SHIFT)) {
-        yshift = -STEP_L * 2;
-    } else {
-        return;
-    }
-
-    item->pos.y += yshift - STEP_L;
-
-    int32_t shift_r = 0;
-    int32_t ledge_r = 0;
-    int32_t result_r = Lara_TestClimbUpPos(
-        item, coll->radius, coll->radius + LARA_CLIMB_WIDTH_RIGHT, &shift_r,
-        &ledge_r);
-
-    int32_t shift_l = 0;
-    int32_t ledge_l = 0;
-    int32_t result_l = Lara_TestClimbUpPos(
-        item, coll->radius, -(coll->radius + LARA_CLIMB_WIDTH_LEFT), &shift_l,
-        &ledge_l);
-
-    item->pos.y += STEP_L;
-
-    if (!result_r || !result_l || !g_Input.forward) {
-        item->goal_anim_state = LS_CLIMB_STANCE;
-        if (yshift) {
-            Lara_Animate(item);
-        }
-        return;
-    }
-
-    if (result_r < 0 || result_l < 0) {
-        item->goal_anim_state = LS_CLIMB_STANCE;
-        Lara_Animate(item);
-        if (ABS(ledge_l - ledge_r) <= 120) {
-            item->goal_anim_state = LS_PULL_UP;
-            item->pos.y += (ledge_r + ledge_l) / 2 - STEP_L;
-        }
-        return;
-    }
-
-    item->goal_anim_state = LS_CLIMBING;
-    item->pos.y -= yshift;
-}
-
 void Lara_Col_ClimbDown(ITEM *item, COLL_INFO *coll)
 {
     if (Lara_CheckForLetGo(item, coll)
