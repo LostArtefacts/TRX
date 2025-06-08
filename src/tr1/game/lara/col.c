ead6c4f4d53e065f0c0459a21ed227cdc0dc52ba
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-08 09:37:38 +0100

    lara/col: port Lara_Col_FastBack to TRX

--- src/tr1/game/lara/col.c
+++ src/tr1/game/lara/col.c
@@ -13,38 +13,6 @@
 #define LF_BACK_R_START 26
 #define LF_BACK_R_END 55
 
-void Lara_Col_FastBack(ITEM *item, COLL_INFO *coll)
-{
-    g_Lara.move_angle = item->rot.y - DEG_180;
-    item->gravity = false;
-    item->fall_speed = 0;
-    coll->bad_pos = NO_BAD_POS;
-    coll->bad_neg = -STEPUP_HEIGHT;
-    coll->bad_ceiling = 0;
-    coll->slopes_are_pits = 1;
-    coll->slopes_are_walls = 1;
-    Lara_GetCollisionInfo(item, coll);
-
-    if (Lara_HitCeiling(item, coll)) {
-        return;
-    }
-
-    if (coll->side_mid.floor > 200) {
-        item->current_anim_state = LS_FALL_BACK;
-        item->goal_anim_state = LS_FALL_BACK;
-        Item_SwitchToAnim(item, LA_FALL_BACK, 0);
-        item->gravity = true;
-        item->fall_speed = 0;
-        return;
-    }
-
-    if (Lara_DeflectEdge(item, coll)) {
-        Item_SwitchToAnim(item, LA_STAND_STILL, 0);
-    }
-
-    item->pos.y += coll->side_mid.floor;
-}
-
 void Lara_Col_Hang(ITEM *item, COLL_INFO *coll)
 {
     Lara_HangTest(item, coll);
