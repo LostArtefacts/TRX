28fbd8fdaffe714ea52b195fc584814b0a257522
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-13 14:28:21 +0100

    config/priv: move common sanitization calls
    This moves common calls for sanitzing config values to the main config
    priv module to reduce duplication.

--- src/libtrx/config/priv.c
+++ src/libtrx/config/priv.c
@@ -1,3 +1,4 @@
+#include "game/lara/const.h"
 #if TR_VERSION == 1
     #include "priv_tr1.c"
 #elif TR_VERSION == 2
@@ -12,4 +13,20 @@ void Config_SanitizeCommon(void)
     CLAMP(g_Config.audio.inventory_ambient_volume, 0.0f, 1.0f);
     CLAMP(g_Config.audio.underwater_music_volume, 0.0f, 1.0f);
     CLAMP(g_Config.audio.underwater_ambient_volume, 0.0f, 1.0f);
+    CLAMP(g_Config.input.keyboard_layout, 0, INPUT_LAYOUT_NUMBER_OF - 1);
+    CLAMP(g_Config.input.controller_layout, 0, INPUT_LAYOUT_NUMBER_OF - 1);
+    CLAMP(
+        g_Config.gameplay.turbo_speed, CLOCK_TURBO_SPEED_MIN,
+        CLOCK_TURBO_SPEED_MAX);
+    CLAMP(g_Config.gameplay.start_lara_hitpoints, 1, LARA_MAX_HITPOINTS);
+    CLAMP(g_Config.gameplay.camera_speed, 1, 10);
+    CLAMP(g_Config.rendering.wireframe_width, 1.0, 100.0);
+    CLAMP(g_Config.ui.bar_scale, 0.5, 2.0);
+    CLAMP(g_Config.ui.text_scale, 0.5, 2.0);
+    CLAMP(g_Config.visuals.fog_start, 1, 100);
+    CLAMP(g_Config.visuals.fog_end, 1, 100);
+
+    if (g_Config.rendering.fps != 30 && g_Config.rendering.fps != 60) {
+        g_Config.rendering.fps = 30;
+    }
 }
