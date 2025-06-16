519a10b8358b3ba8ea3c2c03a861e0784fc92783
Marcin Kurczewski <dash@wind.garden>
2025-06-16 10:33:47 +0200

    log: include log levels

--- src/libtrx/include/libtrx/log.h
+++ src/libtrx/include/libtrx/log.h
@@ -1,9 +1,20 @@
 #pragma once
 
-#define LOG_INFO(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LOG_WARNING(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LOG_ERROR(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LOG_DEBUG(...) Log_Message(__FILE__, __LINE__, __func__, __VA_ARGS__)
+typedef enum {
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARNING,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_DEBUG,
+} LOG_LEVEL;
+
+#define LOG_INFO(...)                                                          \
+    Log_Message(LOG_LEVEL_INFO, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_WARNING(...)                                                       \
+    Log_Message(LOG_LEVEL_WARNING, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_ERROR(...)                                                         \
+    Log_Message(LOG_LEVEL_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_DEBUG(...)                                                         \
+    Log_Message(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __func__, __VA_ARGS__)
 
 #define LOG_VAR(var)                                                           \
     _Generic(                                                                  \
@@ -22,7 +33,8 @@
 void Log_Init(const char *path);
 void Log_Shutdown(void);
 void Log_Message(
-    const char *file, int line, const char *func, const char *fmt, ...);
+    LOG_LEVEL level, const char *file, int line, const char *func,
+    const char *fmt, ...);
 
 // platform-specific implementations
 void Log_Init_Extra(const char *path);
