LostArtefacts/TRX

commit 545a1013ac0f34260b257f8d95e26a9a22eb390e

camera: ensure environment in the control phase
Resolves #3305.

The underwater hum would play for at most 2 frames before getting reset
on certain devices. The reason for this is because of how
Sound_EndScene() works.

The idea is that looped sound sources need to Sound_Effect() their
samples on every logical frame to achieve continuous playback, otherwise
the samples will stop. Which is actually quite convenient. To achieve
this behavior, Sound_EndScene() checks if the looped samples are silent.
If they aren't, it syncs the parameters to the relevant output stream
and updates the samples volume to be silent – but keeps the other
parameters. In the following frame, the repeated call to Sound_Effect()
will either reset the volume back to some non-silent value, or if there
are no such calls, the second call to Sound_EndScene() will deem the
sample as no longer in use, stop the playback, and fully close the slot.

At the same time the call to Sound_Effect(SFX_UNDERWATER) would happen
inside M_EnsureEnvironment() which is part of the draw phase. Under
normal circumstances this is fine, even if we have 60 FPS. But if we
experience frame drops, this means the calls to Sound_Effect() and the
calls to Sound_EndScene() can go out of order , and the samples will
randomly restart. This is especially visible if the FPS is set to 60 but
the display is only capable of rendering 30 FPS.

The fix ended up being simple and was to just move the camera
environment sync to the control phase.

src/libtrx/game/camera/common.c
@@ -929,6 +929,8 @@ void Camera_RefreshFromTrigger(const TRIGGER *const trigger)
 
 void Camera_Update(void)
 {
+    M_EnsureEnvironment();
+
     if (g_Camera.type == CAM_PHOTO_MODE) {
         Camera_UpdatePhotoMode();
         return;
@@ -1137,7 +1139,6 @@ void Camera_MoveManual(void)
 
 void Camera_Apply(void)
 {
-    M_EnsureEnvironment();
     Matrix_LookAt(
         g_Camera.interp.result.pos.x,
         g_Camera.interp.result.pos.y + g_Camera.interp.result.shift,

commit e053b71b12b842b5780381b8dc952f2a81c53253

camera: fix env sync not working in cutscenes
Partial fix for #3317. Regression from 545a1013ac0f34260b257f8d95e26a9a22eb390e.

src/libtrx/game/camera/cinematic.c
@@ -119,4 +119,5 @@ void Camera_UpdateCutscene(void)
     const ITEM *const lara_item = Lara_GetItem();
     M_UpdateCutscene(lara_item->pos, g_Camera.target_angle);
 #endif
+    Camera_EnsureEnvironment();
 }

src/libtrx/game/camera/common.c
@@ -78,7 +78,6 @@ extern int32_t g_PhdPersp;
 
 static M_SETTINGS M_GetSettings(void);
 static void M_AdjustMusicVolume(bool underwater);
-static void M_EnsureEnvironment(void);
 
 static void M_OffsetAdditionalAngle(int16_t delta);
 static void M_OffsetAdditionalElevation(int16_t delta);
@@ -117,26 +116,6 @@ static void M_AdjustMusicVolume(const bool underwater)
         : is_ambient ? g_Config.audio.underwater_ambient_volume
                      : g_Config.audio.underwater_music_volume;
     Music_SetVolume(g_Config.audio.music_volume * multiplier);
-}
-
-static void M_EnsureEnvironment(void)
-{
-    if (g_Camera.pos.room_num == NO_ROOM) {
-        return;
-    }
-
-    const ROOM *const room = Room_Get(g_Camera.pos.room_num);
-    if ((room->flags & RF_UNDERWATER) != 0) {
-        M_AdjustMusicVolume(true);
-        Sound_Effect(SFX_UNDERWATER, nullptr, SPM_ALWAYS);
-        g_Camera.underwater = true;
-    } else {
-        M_AdjustMusicVolume(false);
-        if (g_Camera.underwater) {
-            Sound_StopEffect(SFX_UNDERWATER);
-            g_Camera.underwater = false;
-        }
-    }
 }
 
 static void M_OffsetAdditionalAngle(const int16_t delta)
@@ -927,9 +906,29 @@ void Camera_RefreshFromTrigger(const TRIGGER *const trigger)
 #endif
 }
 
+void Camera_EnsureEnvironment(void)
+{
+    if (g_Camera.pos.room_num == NO_ROOM) {
+        return;
+    }
+
+    const ROOM *const room = Room_Get(g_Camera.pos.room_num);
+    if ((room->flags & RF_UNDERWATER) != 0) {
+        M_AdjustMusicVolume(true);
+        Sound_Effect(SFX_UNDERWATER, nullptr, SPM_ALWAYS);
+        g_Camera.underwater = true;
+    } else {
+        M_AdjustMusicVolume(false);
+        if (g_Camera.underwater) {
+            Sound_StopEffect(SFX_UNDERWATER);
+            g_Camera.underwater = false;
+        }
+    }
+}
+
 void Camera_Update(void)
 {
-    M_EnsureEnvironment();
+    Camera_EnsureEnvironment();
 
     if (g_Camera.type == CAM_PHOTO_MODE) {
         Camera_UpdatePhotoMode();

src/libtrx/include/libtrx/game/camera/common.h
@@ -4,6 +4,7 @@
 
 void Camera_Update(void);
 void Camera_UpdateMicPosition(void);
+void Camera_EnsureEnvironment(void);
 void Camera_MoveManual(void);
 void Camera_Apply(void);
 bool Camera_IsChunky(void);