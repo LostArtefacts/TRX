213bcff3c1753f5da915d4fbd13f75980c96fb7f
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-28 22:09:04 +0100

    lara/state/jump: port M_FastDive to TRX

--- src/tr2/game/lara/state.c
+++ src/tr2/game/lara/state.c
@@ -45,7 +45,6 @@ static void M_PickupFlare(ITEM *item, COLL_INFO *coll);
 static void M_SwitchOn(ITEM *item, COLL_INFO *coll);
 static void M_UseKey(ITEM *item, COLL_INFO *coll);
 static void M_Special(ITEM *item, COLL_INFO *coll);
-static void M_FastDive(ITEM *item, COLL_INFO *coll);
 static void M_Wade(ITEM *item, COLL_INFO *coll);
 static void M_Zipline(ITEM *item, COLL_INFO *coll);
 
@@ -460,16 +459,6 @@ static void M_Special(ITEM *item, COLL_INFO *coll)
     g_Camera.target_elevation = CAM_SPECIAL_ELEVATION;
 }
 
-static void M_FastDive(ITEM *item, COLL_INFO *coll)
-{
-    if (g_Input.roll && item->goal_anim_state == LS_FAST_DIVE) {
-        item->goal_anim_state = LS_TWIST;
-    }
-    coll->enable_hit = 0;
-    coll->enable_baddie_push = 1;
-    item->speed = item->speed * 95 / 100;
-}
-
 static void M_Wade(ITEM *item, COLL_INFO *coll)
 {
     if (item->hit_points <= 0) {
@@ -541,7 +530,6 @@ REGISTER_LARA_STATE(LS_SWITCH_OFF,   M_SwitchOn)
 REGISTER_LARA_STATE(LS_USE_KEY,      M_UseKey)
 REGISTER_LARA_STATE(LS_USE_PUZZLE,   M_UseKey)
 REGISTER_LARA_STATE(LS_SPECIAL,      M_Special)
-REGISTER_LARA_STATE(LS_FAST_DIVE,    M_FastDive)
 REGISTER_LARA_STATE(LS_GYMNAST,      M_Null)
 REGISTER_LARA_STATE(LS_WADE,         M_Wade)
 REGISTER_LARA_STATE(LS_FLARE_PICKUP, M_PickupFlare)
