d91d5a245a55906f1141528759e21f6881653eab
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-06-29 12:09:18 +0100

    lara/state/land: port M_Walk/M_Run to TRX
    This moves M_Walk and M_Run to TRX, but exposes them temporarily until
    further functions are moved. They are moved together as a result of
    intertwined jump lock behaviour.

--- src/libtrx/include/libtrx/game/lara/state.h
+++ src/libtrx/include/libtrx/game/lara/state.h
@@ -8,3 +8,7 @@ void Lara_State_RegisterExtra(
 void Lara_State_Initialise(void);
 bool Lara_State_IsResponsive(LARA_ANIMATION anim_idx);
 void Lara_State_Update(ITEM *item, COLL_INFO *coll);
+
+// Temporarily public
+void Lara_State_Walk(ITEM *item, COLL_INFO *coll);
+void Lara_State_Run(ITEM *item, COLL_INFO *coll);
