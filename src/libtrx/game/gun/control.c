40ca3ab52b640322dd51380e6115e96c2fc116f1
Marcin Kurczewski <dash@wind.garden>
2025-07-08 18:37:33 +0200

    tr2/gun: fix numeric keys not having effect
    Resolves a part of #828.

--- src/libtrx/game/gun/control.c
+++ src/libtrx/game/gun/control.c
@@ -10,13 +10,32 @@
 #include "game/lara.h"
 #include "game/sound.h"
 
+static struct {
+    LARA_GUN_TYPE gun_type;
+    INPUT_ROLE input_role;
+} m_QuicDrawKeys[] = {
+    { .gun_type = LGT_PISTOLS, .input_role = INPUT_ROLE_EQUIP_PISTOLS },
+    { .gun_type = LGT_SHOTGUN, .input_role = INPUT_ROLE_EQUIP_SHOTGUN },
+    { .gun_type = LGT_MAGNUMS, .input_role = INPUT_ROLE_EQUIP_MAGNUMS },
+    { .gun_type = LGT_UZIS, .input_role = INPUT_ROLE_EQUIP_UZIS },
+#if TR_VERSION >= 2
+    { .gun_type = LGT_HARPOON, .input_role = INPUT_ROLE_EQUIP_HARPOON },
+    { .gun_type = LGT_M16, .input_role = INPUT_ROLE_EQUIP_M16 },
+    { .gun_type = LGT_GRENADE,
+      .input_role = INPUT_ROLE_EQUIP_GRENADE_LAUNCHER },
+#endif
+    { .gun_type = LGT_UNKNOWN, .input_role = (INPUT_ROLE)-1 },
+};
+
 static bool M_IsUsableUnderwater(LARA_GUN_TYPE gun_type);
 static bool M_IsTooSubmerged(LARA_GUN_TYPE gun_type);
+static LARA_GUN_TYPE M_NeedToQuickDraw(void);
+static bool M_QuickDrawWeapon(void);
 static bool M_CanEquip(void);
 static bool M_NeedToDraw(void);
 static bool M_NeedToUndraw(void);
 static void M_DecideRequestedWeapon(void);
-static void M_TryDrawRequestedWeapon(void);
+static void M_DrawRequestedWeapon(void);
 static void M_TryUndrawWeapon(void);
 
 static bool M_IsUsableUnderwater(const LARA_GUN_TYPE gun_type)
@@ -32,6 +51,30 @@ static bool M_IsTooSubmerged(const LARA_GUN_TYPE gun_type)
 {
     const LARA_INFO *const lara = Lara_GetLaraInfo();
     return lara->water_surface_dist > -g_Weapons[gun_type].gun_height;
+}
+
+static LARA_GUN_TYPE M_NeedToQuickDraw(void)
+{
+    const LARA_INFO *const lara = Lara_GetLaraInfo();
+    for (int32_t i = 0; m_QuicDrawKeys[i].gun_type != LGT_UNKNOWN; i++) {
+        if (Input_IsPressedDB(m_QuicDrawKeys[i].input_role)
+            && Inv_RequestItem(Gun_GetGunObject(m_QuicDrawKeys[i].gun_type))
+                > 0) {
+            return m_QuicDrawKeys[i].gun_type;
+        }
+    }
+    return LGT_UNKNOWN;
+}
+
+static bool M_QuickDrawWeapon(void)
+{
+    LARA_INFO *const lara = Lara_GetLaraInfo();
+    const LARA_GUN_TYPE gun_type = M_NeedToQuickDraw();
+    if (gun_type != LGT_UNKNOWN) {
+        lara->request_gun_type = gun_type;
+        return true;
+    }
+    return false;
 }
 
 static bool M_CanEquip(void)
@@ -50,6 +93,7 @@ static bool M_CanEquip(void)
         return false;
     case LWS_ABOVE_WATER:
         return true;
+    case LWS_SURFACE:
     case LWS_UNDERWATER:
         return M_IsUsableUnderwater(lara->request_gun_type);
     case LWS_WADE: {
@@ -62,7 +106,8 @@ static bool M_CanEquip(void)
         }
         return false;
     default:
-        return true;
+        ASSERT_FAIL();
+        return false;
     }
     }
 }
@@ -78,7 +123,7 @@ static bool M_NeedToDraw(void)
 
 static bool M_NeedToUndraw(void)
 {
-    const LARA_INFO *const lara = Lara_GetLaraInfo();
+    LARA_INFO *const lara = Lara_GetLaraInfo();
     if (g_Input.draw || lara->request_gun_type != lara->gun_type) {
         return true;
     }
@@ -118,33 +163,31 @@ static void M_DecideRequestedWeapon(void)
 #endif
 }
 
-static void M_TryDrawRequestedWeapon(void)
+static void M_DrawRequestedWeapon(void)
 {
     LARA_INFO *const lara = Lara_GetLaraInfo();
-    if (M_NeedToDraw()) {
-        if (M_CanEquip()) {
+    if (M_CanEquip()) {
 #if TR_VERSION >= 2
-            if (lara->gun_type == LGT_FLARE) {
-                Lara_Flare_Dispose(false);
-            }
-#endif
-            lara->gun_type = lara->request_gun_type;
-            Gun_InitialiseNewWeapon();
-            lara->gun_status = LGS_DRAW;
-            lara->right_arm.frame_num = 0;
-            lara->left_arm.frame_num = 0;
-        } else {
-            lara->last_gun_type = lara->request_gun_type;
-#if TR_VERSION >= 2
-            if (lara->gun_type == LGT_FLARE) {
-                lara->request_gun_type = LGT_FLARE;
-            } else {
-                lara->gun_type = lara->request_gun_type;
-            }
-#else
-            lara->gun_type = lara->request_gun_type;
-#endif
+        if (lara->gun_type == LGT_FLARE) {
+            Lara_Flare_Dispose(false);
         }
+#endif
+        lara->gun_type = lara->request_gun_type;
+        Gun_InitialiseNewWeapon();
+        lara->gun_status = LGS_DRAW;
+        lara->right_arm.frame_num = 0;
+        lara->left_arm.frame_num = 0;
+    } else {
+        lara->last_gun_type = lara->request_gun_type;
+#if TR_VERSION >= 2
+        if (lara->gun_type == LGT_FLARE) {
+            lara->request_gun_type = LGT_FLARE;
+        } else {
+            lara->gun_type = lara->request_gun_type;
+        }
+#else
+        lara->gun_type = lara->request_gun_type;
+#endif
     }
 }
 
@@ -156,7 +199,8 @@ static void M_TryUndrawWeapon(void)
         lara->request_gun_type = LGT_FLARE;
     }
 #endif
-    if (M_NeedToUndraw()) {
+    if ((M_QuickDrawWeapon() && lara->request_gun_type != lara->gun_type)
+        || M_NeedToUndraw()) {
         lara->gun_status = LGS_UNDRAW;
     }
 }
@@ -168,8 +212,14 @@ void Gun_UpdateGunState(void)
     if (lara_item->hit_points <= 0) {
         lara->gun_status = LGS_ARMLESS;
     } else if (lara->gun_status == LGS_ARMLESS) {
-        M_DecideRequestedWeapon();
-        M_TryDrawRequestedWeapon();
+        if (M_QuickDrawWeapon()) {
+            M_DrawRequestedWeapon();
+        } else {
+            M_DecideRequestedWeapon();
+            if (M_NeedToDraw()) {
+                M_DrawRequestedWeapon();
+            }
+        }
     } else if (lara->gun_status == LGS_READY) {
         M_TryUndrawWeapon();
     }
