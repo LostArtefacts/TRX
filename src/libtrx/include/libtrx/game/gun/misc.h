7b54176a831e6a239e54582afcac78341c1b8476
Marcin Kurczewski <dash@wind.garden>
2025-07-08 02:02:02 +0200

    gun: move Gun_TargetInfo to libtrx

--- src/libtrx/include/libtrx/game/gun/misc.h
+++ src/libtrx/include/libtrx/game/gun/misc.h
@@ -6,4 +6,5 @@
 #include "./types.h"
 
 void Gun_FindTargetPoint(const ITEM *item, GAME_VECTOR *target);
-void Gun_AimWeapon(const WEAPON_INFO *winfo, LARA_ARM *arm);
+void Gun_AimWeapon(const WEAPON_INFO *weapon, LARA_ARM *arm);
+void Gun_TargetInfo(const WEAPON_INFO *weapon);
