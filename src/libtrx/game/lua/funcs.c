9ee5c604769fd6aa378c0e5498ba17f5bc1629cc
Marcin Kurczewski <dash@wind.garden>
2025-07-25 18:10:26 +0200

    lua: add console.log()

--- src/libtrx/game/lua/funcs.c
+++ src/libtrx/game/lua/funcs.c
@@ -1,11 +1,13 @@
 #include "game/lua/funcs.h"
 
+#include "game/console/common.h"
 #include "game/lara.h"
 #include "strings.h"
 
 #include <lauxlib.h>
 
 static int M_L_GetLaraItem(lua_State *l);
+static int M_L_ConsoleLog(lua_State *l);
 
 static int M_L_GetLaraItem(lua_State *l)
 {
@@ -21,10 +23,31 @@ static int M_L_GetLaraItem(lua_State *l)
     return 1;
 }
 
+static int M_L_ConsoleLog(lua_State *l)
+{
+    const int32_t n = lua_gettop(l);
+    const char *log_message;
+    for (int32_t i = 1; i <= n; i++) {
+        const char *const arg = lua_tostring(l, i);
+        if (i > 1) {
+            log_message = String_FormatStatic("%s, %s", log_message, arg);
+        } else {
+            log_message = String_FormatStatic("%s", arg);
+        }
+    }
+    Console_Log("%s", log_message);
+    return 0;
+}
+
 void LUA_CreateFunctions(lua_State *const l)
 {
     lua_newtable(l);
     lua_pushcfunction(l, M_L_GetLaraItem);
     lua_setfield(l, -2, "get_item");
     lua_setglobal(l, "lara");
+
+    lua_newtable(l);
+    lua_pushcfunction(l, M_L_ConsoleLog);
+    lua_setfield(l, -2, "log");
+    lua_setglobal(l, "console");
 }
