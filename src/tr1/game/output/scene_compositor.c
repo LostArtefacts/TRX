LostArtefacts/TRX

commit 64f6529e51be4590c1b38a81dd60373ad4664c2d

shell: fix headless mode not taking screenshots

src/tr1/game/output/scene_compositor.c
@@ -217,7 +217,6 @@ void SceneCompositor_Flush(void)
     M_RenderScenePasses(p);
     M_PROCESS_SOURCES(p, render_end);
     M_PROCESS_SOURCES(p, render_begin);
-    glClear(GL_DEPTH_BUFFER_BIT);
 }
 
 void SceneCompositor_EndScene(void)

src/tr1/game/output.c
@@ -199,10 +199,10 @@ void Output_EndScene(void)
 
 void Output_FlipScreen(void)
 {
-    if (Shell_GetArgs()->headless) {
-        return;
+    if (!Shell_GetArgs()->headless
+        || GFX_Context_GetScheduledScreenshotPath() != nullptr) {
+        GFX_Context_SwapBuffers();
     }
-    GFX_Context_SwapBuffers();
 }
 
 bool Output_LoadBackgroundFromImage(const IMAGE *const image)
@@ -243,9 +243,12 @@ BACKGROUND_TYPE Output_GetBackgroundType(void)
 
 void Output_SwitchViewport(const VIEWPORT_SPACE space)
 {
-    if (space == VIEWPORT_UI) {
+    if (space == VIEWPORT_GAME) {
+        GFX_Renderer_BindGeometryFbo();
+    } else if (space == VIEWPORT_UI) {
         GFX_Renderer_BindUiFbo();
     }
     GFX_Context_SwitchToViewport(space);
     GFX_Context_Clear();
+    glClear(GL_DEPTH_BUFFER_BIT);
 }

src/tr2/game/output.c
@@ -672,10 +672,10 @@ void Output_EndScene(void)
 
 void Output_FlipScreen(void)
 {
-    if (Shell_GetArgs()->headless) {
-        return;
+    if (!Shell_GetArgs()->headless
+        || GFX_Context_GetScheduledScreenshotPath() != nullptr) {
+        GFX_Context_SwapBuffers();
     }
-    GFX_Context_SwapBuffers();
 }
 
 BACKGROUND_TYPE Output_GetBackgroundType(void)
@@ -1104,7 +1104,9 @@ void Output_SetDepthBias(const int32_t bias)
 
 void Output_SwitchViewport(const VIEWPORT_SPACE space)
 {
-    if (space == VIEWPORT_UI) {
+    if (space == VIEWPORT_GAME) {
+        GFX_Renderer_BindGeometryFbo();
+    } else if (space == VIEWPORT_UI) {
         GFX_Renderer_BindUiFbo();
     }
     GFX_Context_SwitchToViewport(space);

commit 1983c1aa50efa7a92b88b0816ec35113ee00bfa1

tr1/output: fix inventory not drawing
Regression from 64f6529e5. Resolves #3687.

src/tr1/game/output/scene_compositor.c
@@ -217,6 +217,7 @@ void SceneCompositor_Flush(void)
     M_RenderScenePasses(p);
     M_PROCESS_SOURCES(p, render_end);
     M_PROCESS_SOURCES(p, render_begin);
+    glClear(GL_DEPTH_BUFFER_BIT);
 }
 
 void SceneCompositor_EndScene(void)