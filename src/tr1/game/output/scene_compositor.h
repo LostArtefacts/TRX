13765c636d19bb701ef806c6d31fca22cef5038b
Marcin Kurczewski <dash@wind.garden>
2025-08-05 21:45:57 +0200

    tr1/output: move scene source to libtrx

--- src/tr1/game/output/scene_compositor.h
+++ src/tr1/game/output/scene_compositor.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "game/output/scene_source.h"
+#include <libtrx/game/output/scene_source.h>
 
 void SceneCompositor_Init(void);
 void SceneCompositor_Shutdown(void);
--- src/tr1/game/output/sources/sprites.h
+++ src/tr1/game/output/sources/sprites.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "game/output/mesh_batcher/batcher.h"
-#include "game/output/scene_source.h"
 
+#include <libtrx/game/output/scene_source.h>
 #include <libtrx/game/rooms/types.h>
 
 void OutputSource_Sprites_Init(MESH_BATCHER *batcher);
