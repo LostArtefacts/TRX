032423e5505ffefbe828eeff48b82d77100dbbbf
Dash <dash@wind.garden>
2025-09-12 11:43:22 +0200

    option/gameplay: fix PDA UI opening too eagerly
    Resolves #3583.

--- docs/tr2/CHANGELOG.md
+++ docs/tr2/CHANGELOG.md
@@ -1,5 +1,6 @@
 ## [Unreleased](https://github.com/LostArtefacts/TRX/compare/tr2-1.4.2...develop) - ××××-××-××
 - fixed trapdoor type 3 (object #116) not functioning (#3895)
+- fixed gameplay settings UI displaying eagerly after the first use (#3583, regression from 1.3)
 
 ## [1.4.2](https://github.com/LostArtefacts/TRX/compare/tr2-1.4.1...tr2-1.4.2) - 2025-09-07
 - fixed broken rendering in MacOS releases (#3880, regression from 1.4)
--- src/libtrx/game/option/gameplay.c
+++ src/libtrx/game/option/gameplay.c
@@ -4,6 +4,7 @@
 #include "game/ui/dialogs/gameplay_settings.h"
 
 typedef struct {
+    bool is_ready;
     UI_SETTINGS_STATE *ui_state;
 } M_PRIV;
 
@@ -14,6 +15,7 @@ static void M_Shutdown(M_PRIV *p);
 
 static void M_Init(M_PRIV *const p)
 {
+    p->is_ready = true;
     if (p->ui_state == nullptr) {
         p->ui_state = UI_GameplaySettings_Init();
     }
@@ -25,6 +27,7 @@ static void M_Shutdown(M_PRIV *const p)
         UI_GameplaySettings_Free(p->ui_state);
         p->ui_state = nullptr;
     }
+    p->is_ready = false;
 }
 
 void Option_Gameplay_Control(INVENTORY_ITEM *const inv_item, const bool is_busy)
@@ -33,7 +36,7 @@ void Option_Gameplay_Control(INVENTORY_ITEM *const inv_item, const bool is_busy)
     if (is_busy) {
         return;
     }
-    if (p->ui_state == nullptr) {
+    if (!p->is_ready) {
         M_Init(p);
     }
     if (UI_GameplaySettings_Control(p->ui_state)) {
@@ -50,13 +53,15 @@ void Option_Gameplay_Control(INVENTORY_ITEM *const inv_item, const bool is_busy)
 void Option_Gameplay_Draw(INVENTORY_ITEM *const inv_item)
 {
     M_PRIV *const p = &m_Priv;
-    if (p->ui_state != nullptr) {
+    if (p->is_ready && p->ui_state != nullptr) {
         UI_GameplaySettings(p->ui_state);
     }
 }
 
 void Option_Gameplay_Close(void)
 {
+    M_PRIV *const p = &m_Priv;
+    p->is_ready = false;
 }
 
 void Option_Gameplay_Shutdown(void)
