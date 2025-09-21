4505992d5f80fe3d6d5e547ea51416739c9665c3
Dash <dash@wind.garden>
2025-09-21 14:16:03 +0200

    sound: split into tabs

--- src/libtrx/game/ui/dialogs/sound_settings.c
+++ src/libtrx/game/ui/dialogs/sound_settings.c
@@ -1,17 +1,30 @@
 #include "game/ui/dialogs/sound_settings.h"
 
 #include "config.h"
+#include "game/lara/const.h"
 #include "game/ui/dialogs/setting_helpers/enums.h"
 #include "game/ui/dialogs/setting_helpers/handlers.h"
 
-static const UI_SETTINGS_OPTION m_SoundOptions[] = {
-#include "setting_tabs/sound_settings.def"
+static const UI_SETTINGS_OPTION m_SoundVolumeOptions[] = {
+#include "setting_tabs/sound_volume.def"
     { .target = nullptr },
+};
+
+static const UI_SETTINGS_OPTION m_SoundMiscOptions[] = {
+#include "setting_tabs/sound_misc.def"
+    { .target = nullptr },
+};
+
+static const UI_SETTINGS_TAB m_Tabs[] = {
+    { GS_ID(SOUND_SETTINGS_VOLUME_TAB), m_SoundVolumeOptions },
+    { GS_ID(SOUND_SETTINGS_MISC_TAB), m_SoundMiscOptions },
 };
 
 UI_SETTINGS_STATE *UI_SoundSettings_Init(void)
 {
-    return UI_Settings_Init(GS_ID(SOUND_SETTINGS_TITLE), m_SoundOptions);
+    return UI_Settings_InitWithTabs(
+        GS_ID(SOUND_SETTINGS_TITLE), sizeof(m_Tabs) / sizeof(m_Tabs[0]),
+        m_Tabs);
 }
 
 void UI_SoundSettings_Free(UI_SETTINGS_STATE *const s)
