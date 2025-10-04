cd6736481ab27b56cace5a597f9ed4c76768e1e1
Dash <dash@wind.garden>
2025-10-05 00:52:28 +0200

    lua: add config module

--- src/libtrx/game/lua/common.c
+++ src/libtrx/game/lua/common.c
@@ -26,6 +26,7 @@ extern void LUA_CreateLara(lua_State *L);
 extern void LUA_CreateLog(lua_State *L);
 extern void LUA_CreateMusic(lua_State *L);
 extern void LUA_CreateSound(lua_State *L);
+extern void LUA_CreateConfig(lua_State *L);
 
 // Shared loader+pcall helper for Eval/EvalFile to capture errors with source
 static LUA_RESULT M_LuaLoadAndRun(
@@ -66,6 +67,7 @@ void LUA_Init(void)
     LUA_CreateLog(L);
     LUA_CreateMusic(L);
     LUA_CreateSound(L);
+    LUA_CreateConfig(L);
 
     M_PRIV *const p = &m_Priv;
     p->state = L;
---
+++ src/libtrx/game/lua/config.c
@@ -0,0 +1,62 @@
+#include "config/common.h"
+#include "game/lua/common.h"
+
+#include <lauxlib.h>
+
+// TRX.Config.Get(key)
+static int M_L_ConfigGet(lua_State *const L)
+{
+    const char *const key = luaL_checkstring(L, 1);
+    const CONFIG_OPTION *const opt = Config_GetOptionByPath(key);
+    if (opt == nullptr) {
+        return luaL_error(L, "Unknown option: %s", key);
+    }
+    const char *const value = Config_GetOptionValueAsString(opt);
+    lua_pushstring(L, value);
+    return 1;
+}
+
+// TRX.Config.Set(key, value)
+static int M_L_ConfigSet(lua_State *const L)
+{
+    const char *const key = luaL_checkstring(L, 1);
+    const char *const new_value = luaL_checkstring(L, 2);
+    const CONFIG_OPTION *const opt = Config_GetOptionByPath(key);
+    if (opt == nullptr) {
+        return luaL_error(L, "Unknown option: %s", key);
+    }
+    const bool ok = Config_SetOptionValueFromString(opt, new_value);
+    if (ok == false) {
+        return luaL_error(L, "Failed to set option %s to %s", key, new_value);
+    }
+    Config_Update();
+    return 0;
+}
+
+// TRX.Config.List()
+static int M_L_ConfigList(lua_State *const L)
+{
+    lua_newtable(L);
+    const CONFIG_OPTION *opt = Config_GetOptionMap();
+    while (opt->name != nullptr) {
+        const char *const value = Config_GetOptionValueAsString(opt);
+        lua_pushstring(L, value);
+        lua_setfield(L, -2, opt->name);
+        opt++;
+    }
+    return 1;
+}
+
+void LUA_CreateConfig(lua_State *const L)
+{
+    lua_getglobal(L, "TRX");
+    lua_newtable(L);
+    lua_pushcfunction(L, M_L_ConfigGet);
+    lua_setfield(L, -2, "Get");
+    lua_pushcfunction(L, M_L_ConfigSet);
+    lua_setfield(L, -2, "Set");
+    lua_pushcfunction(L, M_L_ConfigList);
+    lua_setfield(L, -2, "List");
+    lua_setfield(L, -2, "Config");
+    lua_pop(L, 1);
+}
