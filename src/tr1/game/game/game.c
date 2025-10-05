4a6d83a2aef247c8d502b7e6388773bb90a02159
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-05 13:35:00 +0100

    game: fix starting ambient track
    This ensures that if the savegame logic has started an ambient track,
    that the level's default track is not then started when the game
    itself starts.
    
    Resolves #3997.

--- src/tr1/game/game/game.c
+++ src/tr1/game/game/game.c
@@ -69,10 +69,10 @@ bool Game_Start(const GF_LEVEL *const level, const GF_SEQUENCE_CONTEXT seq_ctx)
     Interpolation_Remember();
 
     Sound_StopAll();
-    if (level->music_track != MX_INACTIVE) {
-        Music_Play(
-            level->music_track,
-            level->type == GFL_CUTSCENE ? MPM_ALWAYS : MPM_LOOPED);
+    const bool is_cutscene = level->type == GFL_CUTSCENE;
+    if (level->music_track != MX_INACTIVE
+        && (is_cutscene || Music_GetCurrentLoopedTrack() == MX_INACTIVE)) {
+        Music_Play(level->music_track, is_cutscene ? MPM_ALWAYS : MPM_LOOPED);
     }
 
     return true;
