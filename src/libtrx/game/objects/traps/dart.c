ea70984ccbe939115a894c4642a42f3c75c3c2e4
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-07 12:30:31 +0100

    objects/dart: merge dart control in TRX

--- src/libtrx/game/objects/traps/dart.c
+++ src/libtrx/game/objects/traps/dart.c
@@ -1,26 +1,29 @@
 #include "game/effects.h"
 #include "game/lara.h"
 #include "game/objects/common.h"
+#include "game/random.h"
+#include "game/rooms.h"
+#include "game/sound.h"
 #include "game/spawn.h"
-#include "global/vars.h"
 
-#include <libtrx/game/lara/common.h>
-#include <libtrx/game/random.h>
-#include <libtrx/game/sound.h>
+#define M_DAMAGE 50
+#define M_PITCH (DEG_45 / 2)
 
-#define DART_DAMAGE 50
-
-static void M_Hit(const int16_t item_num)
+static void M_Hit(const int16_t item_num, const XYZ_32 pos)
 {
     const ITEM *const item = Item_Get(item_num);
     Item_Kill(item_num);
+#if TR_VERSION == 2
     Sound_Effect(SFX_DARTS_HIT, &item->pos, SPM_NORMAL);
+#endif
 
     const int16_t effect_num = Effect_Create(item->room_num);
     if (effect_num != NO_EFFECT) {
         EFFECT *const effect = Effect_Get(effect_num);
         effect->object_id = O_RICOCHET;
-        effect->pos = item->pos;
+        effect->pos.x = pos.x;
+        effect->pos.y = item->pos.y;
+        effect->pos.z = pos.z;
         effect->rot = item->rot;
         effect->room_num = item->room_num;
         effect->speed = 0;
@@ -33,13 +36,14 @@ static void M_Control(const int16_t item_num)
 {
     ITEM *const item = Item_Get(item_num);
     if (item->touch_bits != 0) {
-        Lara_TakeDamage(DART_DAMAGE, true);
+        Lara_TakeDamage(M_DAMAGE, true);
         const ITEM *const lara_item = Lara_GetItem();
         Spawn_Blood(
             item->pos.x, item->pos.y, item->pos.z, lara_item->speed,
             lara_item->rot.y, lara_item->room_num);
     }
 
+    const XYZ_32 old_pos = item->pos;
     Item_Animate(item);
 
     int16_t room_num = item->room_num;
@@ -49,10 +53,12 @@ static void M_Control(const int16_t item_num)
     const int32_t height =
         Room_GetHeight(sector, item->pos.x, item->pos.y, item->pos.z);
 
-    item->rot.x += 4096;
+    if (TR_VERSION == 2) {
+        item->rot.x += M_PITCH;
+    }
     item->floor = height;
     if (item->pos.y >= height) {
-        M_Hit(item_num);
+        M_Hit(item_num, TR_VERSION == 1 ? old_pos : item->pos);
     }
 }
 
@@ -60,7 +66,8 @@ static void M_Setup(OBJECT *const obj)
 {
     obj->control_func = M_Control;
     obj->collision_func = Object_Collision;
-    obj->shadow_size = 128;
+    obj->shadow_size = UNIT_SHADOW / 2;
+    obj->save_flags = true;
 }
 
 REGISTER_OBJECT(O_DART, M_Setup)
