caa45f5d6575a58fb057146b70d3256e7ef98a48
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-11 12:48:39 +0100

    spawn: merge Spawn_Bubble in TRX

--- src/tr1/game/spawn.h
+++ src/tr1/game/spawn.h
@@ -4,8 +4,6 @@
 
 #include <libtrx/game/spawn.h>
 
-void Spawn_Bubble(const XYZ_32 *pos, int16_t room_num);
-
 int16_t Spawn_ShardGun(
     int32_t x, int32_t y, int32_t z, int16_t speed, int16_t y_rot,
     int16_t room_num);
--- src/tr2/game/spawn.h
+++ src/tr2/game/spawn.h
@@ -10,8 +10,6 @@ int16_t Spawn_FireStream(
 
 void Spawn_MysticLight(int16_t item_num);
 
-void Spawn_Bubble(const XYZ_32 *pos, int16_t room_num);
-
 int16_t Spawn_GunShot(
     int32_t x, int32_t y, int32_t z, int16_t speed, int16_t yrot,
     int16_t room_num);
