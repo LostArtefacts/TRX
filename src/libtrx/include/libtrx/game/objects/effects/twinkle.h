7197b286948e3378e3b6963ee38b84549c985c2b
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-11 12:16:16 +0100

    objects/twinkle: merge twinkle control in TRX

---
+++ src/libtrx/include/libtrx/game/objects/effects/twinkle.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../../items.h"
+
+void Twinkle_SparkleItem(const ITEM *item, uint32_t mesh_mask);
--- src/tr1/game/objects/effects/twinkle.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <libtrx/game/items.h>
-
-void Twinkle_SparkleItem(ITEM *item, uint32_t mesh_mask);
