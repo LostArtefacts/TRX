9131579a849b2336b6c174cf950ff0dde6e9a9c3
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-11 11:10:05 +0100

    objects/dart_effect: merge dart effect control in TRX

--- src/tr1/game/objects/common.h
+++ src/tr1/game/objects/common.h
@@ -4,7 +4,6 @@
 
 #include "global/types.h"
 
-void Object_DrawSpriteItem(const ITEM *item);
 void Object_DrawPickupItem(const ITEM *item);
 void Object_SetMeshReflective(
     GAME_OBJECT_ID obj_id, int32_t mesh_idx, bool enabled);
--- src/tr2/game/objects/common.h
+++ src/tr2/game/objects/common.h
@@ -4,7 +4,5 @@
 
 #include <libtrx/game/objects/common.h>
 
-void Object_DrawSpriteItem(const ITEM *item);
-
 BOUNDS_16 Object_GetBoundingBox(
     const OBJECT *obj, const ANIM_FRAME *frame, uint32_t mesh_bits);
