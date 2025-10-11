57d4649ede36f741392d0178dcba3b9ac3dbf2a3
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-11 11:39:51 +0100

    objects/gun_flash: merge gun flash control in TRX

--- src/libtrx/game/objects/effects/gun_flash.c
+++ src/libtrx/game/objects/effects/gun_flash.c
@@ -1,18 +1,19 @@
+#include "config.h"
 #include "game/effects.h"
-#include "game/objects/common.h"
-
-#include <libtrx/config.h>
-#include <libtrx/game/output.h>
-#include <libtrx/game/random.h>
+#include "game/objects.h"
+#include "game/output.h"
+#include "game/random.h"
 
 static void M_Control(const int16_t effect_num)
 {
-    EFFECT *effect = Effect_Get(effect_num);
+    EFFECT *const effect = Effect_Get(effect_num);
+
     effect->counter--;
-    if (!effect->counter) {
+    if (effect->counter == 0) {
         Effect_Kill(effect_num);
         return;
     }
+
     effect->rot.z = Random_GetControl();
     if (g_Config.visuals.enable_gun_lighting) {
         Output_AddDynamicLight(effect->pos, 12, 11);
