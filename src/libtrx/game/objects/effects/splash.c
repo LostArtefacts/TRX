69ef814d3c1b23fc36d600351dc432a196c8bd1e
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-11 11:46:58 +0100

    objects/splash: merge splash control in TRX

--- src/libtrx/game/objects/effects/splash.c
+++ src/libtrx/game/objects/effects/splash.c
@@ -1,11 +1,10 @@
+#include "game/const.h"
 #include "game/effects.h"
-#include "game/objects/common.h"
-
-#include <libtrx/game/math.h>
+#include "game/objects.h"
 
 static void M_Control(const int16_t effect_num)
 {
-    EFFECT *effect = Effect_Get(effect_num);
+    EFFECT *const effect = Effect_Get(effect_num);
     const OBJECT *const obj = Object_Get(effect->object_id);
 
     effect->frame_num--;
@@ -21,6 +20,7 @@ static void M_Control(const int16_t effect_num)
 static void M_Setup(OBJECT *const obj)
 {
     obj->control_func = M_Control;
+    obj->semi_transparent = TR_VERSION == 2;
 }
 
 REGISTER_OBJECT(O_SPLASH_1, M_Setup)
