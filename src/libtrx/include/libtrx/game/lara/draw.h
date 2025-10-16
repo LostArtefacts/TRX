ee57762ee73da2d9f034a8fdfd1e590e387cf27a
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-16 10:44:53 +0100

    lara/draw: privatise interpolated drawing

--- src/libtrx/include/libtrx/game/lara/draw.h
+++ src/libtrx/include/libtrx/game/lara/draw.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "../anims/types.h"
 #include "../items/types.h"
 
 void Lara_Draw(const ITEM *item);
-void Lara_Draw_I(
-    const ITEM *item, const ANIM_FRAME *frame1, const ANIM_FRAME *frame2,
-    int32_t frac, int32_t rate);
