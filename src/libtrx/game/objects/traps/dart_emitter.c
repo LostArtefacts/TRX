e7626d60b3c84de379a3346d09b24d458fa8aff8
Dash <dash@wind.garden>
2025-10-17 16:49:03 +0200

    objects: extract O_DISC from O_DART; same for emitters

--- src/libtrx/game/objects/traps/dart_emitter.c
+++ src/libtrx/game/objects/traps/dart_emitter.c
@@ -2,33 +2,42 @@
 #include "game/effects.h"
 #include "game/objects.h"
 #include "game/sound.h"
+#include "log.h"
 #include "version.h"
 
 typedef enum {
     // clang-format off
-    DART_EMITTER_STATE_IDLE   = 0,
-    DART_EMITTER_STATE_FIRE   = 1,
-    DART_EMITTER_STATE_RELOAD = 2,
+    STATE_IDLE   = 0,
+    STATE_FIRE   = 1,
+    STATE_RELOAD = 2,
     // clang-format on
 } M_STATE;
 
-static void M_CreateDart(ITEM *const item)
+static void M_CreateProjectile(ITEM *const item)
 {
-    const int16_t dart_item_num = Item_Create();
-    if (dart_item_num == NO_ITEM) {
+    const OBJECT_ID projectile_obj_id =
+        item->object_id == O_DART_EMITTER ? O_DART : O_DISC;
+    if (!Object_Get(projectile_obj_id)->loaded) {
+        LOG_ERROR(
+            "Projectile object not loaded for item #%d", Item_GetIndex(item));
         return;
     }
 
-    ITEM *const dart_item = Item_Get(dart_item_num);
-    dart_item->object_id = O_DART;
-    dart_item->room_num = item->room_num;
-    dart_item->shade.value_1 = -1;
-    dart_item->rot.y = item->rot.y;
-    dart_item->pos.y = item->pos.y - 512;
+    const int16_t projectile_item_num = Item_Create();
+    if (projectile_item_num == NO_ITEM) {
+        return;
+    }
+
+    ITEM *const projectile_item = Item_Get(projectile_item_num);
+    projectile_item->object_id = projectile_obj_id;
+    projectile_item->room_num = item->room_num;
+    projectile_item->shade.value_1 = -1;
+    projectile_item->rot.y = item->rot.y;
+    projectile_item->pos.y = item->pos.y - 512;
 
     int32_t x = 0;
     int32_t z = 0;
-    switch (dart_item->rot.y) {
+    switch (projectile_item->rot.y) {
     case 0:
         z = -WALL_L / 2 + 100;
         break;
@@ -43,25 +52,27 @@ static void M_CreateDart(ITEM *const item)
         break;
     }
 
-    dart_item->pos.x = item->pos.x + x;
-    dart_item->pos.z = item->pos.z + z;
-    Item_Initialise(dart_item_num);
-    Item_AddActive(dart_item_num);
-    dart_item->status = IS_ACTIVE;
+    projectile_item->pos.x = item->pos.x + x;
+    projectile_item->pos.z = item->pos.z + z;
+    Item_Initialise(projectile_item_num);
+    Item_AddActive(projectile_item_num);
+    projectile_item->status = IS_ACTIVE;
 
-    if (g_TRVersion == 1) {
-        const int16_t effect_num = Effect_Create(dart_item->room_num);
+    if (item->object_id == O_DART_EMITTER) {
+        const int16_t effect_num = Effect_Create(projectile_item->room_num);
         if (effect_num != NO_EFFECT) {
             EFFECT *const effect = Effect_Get(effect_num);
-            effect->pos = dart_item->pos;
-            effect->rot = dart_item->rot;
+            effect->pos = projectile_item->pos;
+            effect->rot = projectile_item->rot;
             effect->speed = 0;
             effect->frame_num = 0;
             effect->counter = 0;
             effect->object_id = O_DART_EFFECT;
         }
+        Sound_Effect(SFX_DART, &projectile_item->pos, SPM_NORMAL);
+    } else {
+        Sound_Effect(SFX_DISC, &projectile_item->pos, SPM_NORMAL);
     }
-    Sound_Effect(SFX_DARTS, &dart_item->pos, SPM_NORMAL);
 }
 
 static void M_Control(const int16_t item_num)
@@ -69,16 +80,16 @@ static void M_Control(const int16_t item_num)
     ITEM *const item = Item_Get(item_num);
 
     if (Item_IsTriggerActive(item)) {
-        if (item->current_anim_state == DART_EMITTER_STATE_IDLE) {
-            item->goal_anim_state = DART_EMITTER_STATE_FIRE;
+        if (item->current_anim_state == STATE_IDLE) {
+            item->goal_anim_state = STATE_FIRE;
         }
-    } else if (item->current_anim_state == DART_EMITTER_STATE_FIRE) {
-        item->goal_anim_state = DART_EMITTER_STATE_IDLE;
+    } else if (item->current_anim_state == STATE_FIRE) {
+        item->goal_anim_state = STATE_IDLE;
     }
 
-    if (item->current_anim_state == DART_EMITTER_STATE_FIRE
+    if (item->current_anim_state == STATE_FIRE
         && Item_TestFrameEqual(item, 0)) {
-        M_CreateDart(item);
+        M_CreateProjectile(item);
     }
 
     Item_Animate(item);
@@ -91,3 +102,4 @@ static void M_Setup(OBJECT *const obj)
 }
 
 REGISTER_OBJECT(O_DART_EMITTER, M_Setup)
+REGISTER_OBJECT(O_DISC_EMITTER, M_Setup)
