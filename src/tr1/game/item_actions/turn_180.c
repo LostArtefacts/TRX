85f53f172e14249fbdb2116d86779d556f5c35c3
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-19 14:47:03 +0100

    lara: fix twist animations
    Restores missing frames in the jump back roll start and running jump
    roll start animations, and additionally updates the turn 180 command
    to include Lara's move angle.

--- src/tr1/game/item_actions/turn_180.c
+++ src/tr1/game/item_actions/turn_180.c
@@ -1,7 +1,13 @@
 #include "game/item_actions/turn_180.h"
 
+#include "game/lara.h"
+
 void ItemAction_Turn180(ITEM *item)
 {
     item->rot.y += DEG_180;
     item->rot.x *= -1;
+    if (item == Lara_GetItem()) {
+        LARA_INFO *const lara = Lara_GetLaraInfo();
+        lara->move_angle += DEG_180;
+    }
 }
