f3c555702b75fcfd1c39ea01ff792a2c86789669
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 14:04:27 +0100

    items/actions: introduce action registration

--- src/libtrx/game/items/actions/common.c
+++ src/libtrx/game/items/actions/common.c
@@ -1,6 +1,27 @@
 #include "game/items/actions.h"
 #include "game/rooms.h"
 
+static void (*m_Routines[ITEM_ACTION_NUMBER_OF])(ITEM *item) = {};
+
+extern void Item_ActionRunLegacy(ITEM_TRX_ACTION action_id, ITEM *item);
+
+void Item_ActionRegister(
+    const ITEM_TRX_ACTION action, void (*const action_func)(ITEM *item))
+{
+    m_Routines[action] = action_func;
+}
+
+void Item_ActionRun(const ITEM_TRX_ACTION action_id, ITEM *const item)
+{
+    if (action_id >= 0 && action_id < ITEM_ACTION_NUMBER_OF
+        && m_Routines[action_id] != nullptr) {
+        m_Routines[action_id](item);
+    } else {
+        // TODO: remove once all routines merged
+        Item_ActionRunLegacy(action_id, item);
+    }
+}
+
 void Item_ActionRunDirect(const ITEM_ACTION action_id, ITEM *const item)
 {
     const ITEM_TRX_ACTION trx_id = Item_ActionFromGameID(action_id);
--- src/libtrx/include/libtrx/game/items/actions.h
+++ src/libtrx/include/libtrx/game/items/actions.h
@@ -3,6 +3,15 @@
 #include "./types.h"
 #include "actions/ids.h"
 
-extern void Item_ActionRun(ITEM_TRX_ACTION action_id, ITEM *item);
+void Item_ActionRegister(
+    ITEM_TRX_ACTION action, void (*action_func)(ITEM *item));
+void Item_ActionRun(ITEM_TRX_ACTION action_id, ITEM *item);
 void Item_ActionRunDirect(ITEM_ACTION action_id, ITEM *item);
 void Item_ActionRunActive(void);
+
+#define REGISTER_ITEM_ACTION(action, action_func)                              \
+    __attribute__((constructor)) static void M_RegisterActionHandler##action(  \
+        void)                                                                  \
+    {                                                                          \
+        Item_ActionRegister(action, action_func);                              \
+    }
