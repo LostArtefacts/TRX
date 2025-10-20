257a774f5146b31e2067a08408497f2d28fede86
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 15:58:08 +0100

    items/actions/items: move mesh swap handlers

--- src/tr2/game/item_actions.c
+++ src/tr2/game/item_actions.c
@@ -24,33 +24,6 @@ static void M_FlipMap(ITEM *const item)
     Room_FlipMap();
 }
 
-static void M_SwapMeshesWithMeshSwap1(ITEM *const item)
-{
-    const OBJECT *const obj_1 = Object_Get(item->object_id);
-    for (int32_t mesh_idx = 0; mesh_idx < obj_1->mesh_count; mesh_idx++) {
-        Object_SwapMesh(item->object_id, O_MESH_SWAP_1, mesh_idx);
-    }
-}
-
-static void M_SwapMeshesWithMeshSwap2(ITEM *const item)
-{
-    const OBJECT *const obj_1 = Object_Get(item->object_id);
-    for (int32_t mesh_idx = 0; mesh_idx < obj_1->mesh_count; mesh_idx++) {
-        Object_SwapMesh(item->object_id, O_MESH_SWAP_2, mesh_idx);
-    }
-}
-
-static void M_SwapMeshesWithMeshSwap3(ITEM *const item)
-{
-    const OBJECT *const obj_1 = Object_Get(item->object_id);
-    for (int32_t mesh_idx = 0; mesh_idx < obj_1->mesh_count; mesh_idx++) {
-        Object_SwapMesh(item->object_id, O_LARA_SWAP, mesh_idx);
-        if (item == Lara_GetItem()) {
-            Lara_Mesh_SwapSingle(mesh_idx, item->object_id);
-        }
-    }
-}
-
 static void M_InvisibilityOn(ITEM *const item)
 {
     item->status = IS_INVISIBLE;
@@ -101,9 +74,6 @@ void Item_ActionRunLegacy(ITEM_TRX_ACTION action_id, ITEM *item)
         // clang-format off
         [ITEM_ACTION_FINISH_LEVEL]                 = M_FinishLevel,
         [ITEM_ACTION_FLIP_MAP]                     = M_FlipMap,
-        [ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_1] = M_SwapMeshesWithMeshSwap1,
-        [ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_2] = M_SwapMeshesWithMeshSwap2,
-        [ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_3] = M_SwapMeshesWithMeshSwap3,
         [ITEM_ACTION_INVISIBILITY_ON]              = M_InvisibilityOn,
         [ITEM_ACTION_INVISIBILITY_OFF]             = M_InvisibilityOff,
         [ITEM_ACTION_DYNAMIC_LIGHT_ON]             = M_DynamicLightOn,
