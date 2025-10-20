f5998e7e698e026c358a7d1f601a70eb9a0642fd
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 16:05:49 +0100

    items/actions/items: move dynamic light handlers

--- src/libtrx/game/items/actions/items.c
+++ src/libtrx/game/items/actions/items.c
@@ -20,6 +20,16 @@ static void M_InvisibilityOff(ITEM *const item)
     item->status = IS_ACTIVE;
 }
 
+static void M_DynamicLightOn(ITEM *const item)
+{
+    item->dynamic_light = true;
+}
+
+static void M_DynamicLightOff(ITEM *const item)
+{
+    item->dynamic_light = false;
+}
+
 static void M_SwapMeshes(ITEM *const item, const OBJECT_ID swap_id)
 {
     const OBJECT *const obj_1 = Object_Get(item->object_id);
@@ -50,6 +60,8 @@ static void M_SwapMeshesWithMeshSwap3(ITEM *const item)
 REGISTER_ITEM_ACTION(ITEM_ACTION_TURN_180, M_Turn180)
 REGISTER_ITEM_ACTION(ITEM_ACTION_INVISIBILITY_ON, M_InvisibilityOn)
 REGISTER_ITEM_ACTION(ITEM_ACTION_INVISIBILITY_OFF, M_InvisibilityOff)
+REGISTER_ITEM_ACTION(ITEM_ACTION_DYNAMIC_LIGHT_ON, M_DynamicLightOn)
+REGISTER_ITEM_ACTION(ITEM_ACTION_DYNAMIC_LIGHT_OFF, M_DynamicLightOff)
 REGISTER_ITEM_ACTION(
     ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_1, M_SwapMeshesWithMeshSwap1)
 REGISTER_ITEM_ACTION(
