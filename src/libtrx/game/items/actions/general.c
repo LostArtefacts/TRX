81e9986095d287edb5ea5ca9937c8ae03318a0fc
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 16:12:19 +0100

    items/actions/general: merge ITEM_ACTION_FLIP_MAP handler

--- src/libtrx/game/items/actions/general.c
+++ src/libtrx/game/items/actions/general.c
@@ -1,9 +1,16 @@
 #include "game/game.h"
 #include "game/items.h"
+#include "game/rooms.h"
 
 static void M_FinishLevel(ITEM *const item)
 {
     Game_SetIsLevelComplete(true);
 }
 
+static void M_FlipMap(ITEM *const item)
+{
+    Room_FlipMap();
+}
+
 REGISTER_ITEM_ACTION(ITEM_ACTION_FINISH_LEVEL, M_FinishLevel)
+REGISTER_ITEM_ACTION(ITEM_ACTION_FLIP_MAP, M_FlipMap)
--- src/tr1/game/item_actions.c
+++ src/tr1/game/item_actions.c
@@ -1,18 +1,5 @@
-#include "game/item_actions/flipmap.h"
-
 #include <libtrx/game/items.h>
-#include <libtrx/game/rooms.h>
-
-typedef void (*M_FUNC)(ITEM *item);
 
 void Item_ActionRunLegacy(ITEM_TRX_ACTION action_id, ITEM *item)
 {
-    static M_FUNC m_Actions[] = {
-        [ITEM_ACTION_FLIP_MAP] = ItemAction_FlipMap,
-    };
-
-    if (action_id >= 0 && action_id < ITEM_ACTION_NUMBER_OF
-        && m_Actions[action_id] != nullptr) {
-        m_Actions[action_id](item);
-    }
 }
