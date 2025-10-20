7c064354416dd51af013f09fc0f052a36cbb7c83
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 13:55:44 +0100

    items/actions: merge Item_ActionRunActive in TRX

--- src/libtrx/game/items/actions/common.c
+++ src/libtrx/game/items/actions/common.c
@@ -1,7 +1,16 @@
 #include "game/items/actions.h"
+#include "game/rooms.h"
 
 void Item_ActionRunDirect(const ITEM_ACTION action_id, ITEM *const item)
 {
     const ITEM_TRX_ACTION trx_id = Item_ActionFromGameID(action_id);
     Item_ActionRun(trx_id, item);
+}
+
+void Item_ActionRunActive(void)
+{
+    const int32_t flip_effect = Room_GetFlipEffect();
+    if (flip_effect != -1) {
+        Item_ActionRunDirect(flip_effect, nullptr);
+    }
 }
