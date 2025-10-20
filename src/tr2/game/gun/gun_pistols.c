3d0643219e541cf9e4cc8a936a487cae2a95ea32
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 08:54:55 +0100

    lara: adopt arm anim numbers in TR1
    This updates TR1 to store the animation number on each of Lara's arms,
    which is needed ahead of flare implementation. This is only used in
    drawing for pistol type weapons, rifle types will be worked on
    separately.

--- src/tr2/game/gun/gun_pistols.c
+++ src/tr2/game/gun/gun_pistols.c
@@ -6,39 +6,6 @@
 #include <libtrx/game/gun.h>
 #include <libtrx/game/lara.h>
 
-typedef enum {
-    // clang-format off
-    LA_PISTOLS_AIM    = 0,
-    LA_PISTOLS_UNDRAW = 1,
-    LA_PISTOLS_DRAW   = 2,
-    LA_PISTOLS_RECOIL = 3,
-    // clang-format on
-} LARA_PISTOLS_ANIMATION;
-
-void Gun_Pistols_SetArmInfo(LARA_ARM *const arm, const int32_t frame)
-{
-    int16_t anim_idx;
-    if (Anim_TestAbsFrameRange(frame, LF_G_AIM_START, LF_G_AIM_END)) {
-        anim_idx = LA_PISTOLS_AIM;
-    } else if (Anim_TestAbsFrameRange(
-                   frame, LF_G_UNDRAW_START, LF_G_UNDRAW_END)) {
-        anim_idx = LA_PISTOLS_UNDRAW;
-    } else if (Anim_TestAbsFrameRange(frame, LF_G_DRAW_START, LF_G_DRAW_END)) {
-        anim_idx = LA_PISTOLS_DRAW;
-    } else if (Anim_TestAbsFrameRange(
-                   frame, LF_G_RECOIL_START, LF_G_RECOIL_END)) {
-        anim_idx = LA_PISTOLS_RECOIL;
-    } else {
-        return;
-    }
-
-    const OBJECT *const obj = Object_Get(O_LARA_PISTOLS);
-    const ANIM *const anim = Object_GetAnim(obj, anim_idx);
-    arm->anim_num = obj->anim_idx + anim_idx;
-    arm->frame_num = frame;
-    arm->frame_base = anim->frame_ptr;
-}
-
 void Gun_Pistols_Control(const LARA_GUN_TYPE weapon_type)
 {
     const WEAPON_INFO *const weapon = &g_Weapons[weapon_type];
