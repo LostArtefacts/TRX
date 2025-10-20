e5897421b3b896b619c871d8c3add7c2706e00f3
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 16:04:09 +0100

    items/actions/items: move invisibility handlers

--- src/tr2/game/item_actions.c
+++ src/tr2/game/item_actions.c
@@ -24,16 +24,6 @@ static void M_FlipMap(ITEM *const item)
     Room_FlipMap();
 }
 
-static void M_InvisibilityOn(ITEM *const item)
-{
-    item->status = IS_INVISIBLE;
-}
-
-static void M_InvisibilityOff(ITEM *const item)
-{
-    item->status = IS_ACTIVE;
-}
-
 static void M_DynamicLightOn(ITEM *const item)
 {
     item->dynamic_light = true;
@@ -74,8 +64,6 @@ void Item_ActionRunLegacy(ITEM_TRX_ACTION action_id, ITEM *item)
         // clang-format off
         [ITEM_ACTION_FINISH_LEVEL]                 = M_FinishLevel,
         [ITEM_ACTION_FLIP_MAP]                     = M_FlipMap,
-        [ITEM_ACTION_INVISIBILITY_ON]              = M_InvisibilityOn,
-        [ITEM_ACTION_INVISIBILITY_OFF]             = M_InvisibilityOff,
         [ITEM_ACTION_DYNAMIC_LIGHT_ON]             = M_DynamicLightOn,
         [ITEM_ACTION_DYNAMIC_LIGHT_OFF]            = M_DynamicLightOff,
         [ITEM_ACTION_ASSAULT_RESET]                = M_AssaultReset,
