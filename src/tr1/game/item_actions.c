6113f98064aab9612a7166b374cacfe8fbd7d0fb
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 15:29:07 +0100

    items/actions/effects: move ITEM_ACTION_RAISING_BLOCK handler

--- src/tr1/game/item_actions.c
+++ src/tr1/game/item_actions.c
@@ -1,7 +1,6 @@
 #include "game/item_actions/finish_level.h"
 #include "game/item_actions/flipmap.h"
 #include "game/item_actions/powerup.h"
-#include "game/item_actions/raising_block.h"
 #include "game/item_actions/sand.h"
 #include "game/item_actions/stairs2slope.h"
 #include "game/item_actions/turn_180.h"
@@ -16,7 +15,6 @@ void Item_ActionRunLegacy(ITEM_TRX_ACTION action_id, ITEM *item)
     static M_FUNC m_Actions[] = {
         [ITEM_ACTION_TURN_180] = ItemAction_Turn180,
         [ITEM_ACTION_FINISH_LEVEL] = ItemAction_FinishLevel,
-        [ITEM_ACTION_RAISING_BLOCK] = ItemAction_RaisingBlock,
         [ITEM_ACTION_STAIRS_TO_SLOPE] = ItemAction_Stairs2Slope,
         [ITEM_ACTION_DROP_SAND] = ItemAction_DropSand,
         [ITEM_ACTION_POWER_UP] = ItemAction_PowerUp,
