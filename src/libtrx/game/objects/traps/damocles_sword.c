1cec35810d2715189e2508f4eec60504074a1454
Dash <dash@wind.garden>
2025-10-20 21:49:37 +0200

    objects: move damocles sword to trx

--- src/libtrx/game/objects/traps/damocles_sword.c
+++ src/libtrx/game/objects/traps/damocles_sword.c
@@ -1,13 +1,12 @@
 #include "game/lara.h"
+#include "game/random.h"
+#include "game/rooms.h"
+#include "game/sound.h"
+#include "game/spawn.h"
+#include "utils.h"
 
-#include <libtrx/game/random.h>
-#include <libtrx/game/rooms.h>
-#include <libtrx/game/sound.h>
-#include <libtrx/game/spawn.h>
-#include <libtrx/utils.h>
-
-#define DAMOCLES_SWORD_ACTIVATE_DIST ((WALL_L * 3) / 2)
-#define DAMOCLES_SWORD_DAMAGE 100
+#define M_ACTIVATE_DIST ((WALL_L * 3) / 2)
+#define M_DAMAGE 100
 
 static void M_Initialise(const int16_t item_num)
 {
@@ -54,8 +53,7 @@ static void M_Control(const int16_t item_num)
         const int32_t x = lara_item->pos.x - item->pos.x;
         const int32_t y = lara_item->pos.y - item->pos.y;
         const int32_t z = lara_item->pos.z - item->pos.z;
-        if (ABS(x) <= DAMOCLES_SWORD_ACTIVATE_DIST
-            && ABS(z) <= DAMOCLES_SWORD_ACTIVATE_DIST && y > 0
+        if (ABS(x) <= M_ACTIVATE_DIST && ABS(z) <= M_ACTIVATE_DIST && y > 0
             && y < WALL_L * 3) {
             item->current_anim_state = x / 32;
             item->goal_anim_state = z / 32;
@@ -75,7 +73,7 @@ static void M_Collision(
         Lara_Push(item, coll, false, true);
     }
     if (item->gravity) {
-        lara_item->hit_points -= DAMOCLES_SWORD_DAMAGE;
+        lara_item->hit_points -= M_DAMAGE;
         int32_t x = lara_item->pos.x + (Random_GetControl() - 0x4000) / 256;
         int32_t z = lara_item->pos.z + (Random_GetControl() - 0x4000) / 256;
         int32_t y = lara_item->pos.y - Random_GetControl() / 44;
