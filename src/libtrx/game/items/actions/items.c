5b432c10eae644f1712f085be2aa881fe9249f3b
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 15:58:08 +0100

    items/actions/items: move mesh swap handlers

--- src/libtrx/game/items/actions/items.c
+++ src/libtrx/game/items/actions/items.c
@@ -10,4 +10,37 @@ static void M_Turn180(ITEM *const item)
     }
 }
 
+static void M_SwapMeshes(ITEM *const item, const OBJECT_ID swap_id)
+{
+    const OBJECT *const obj_1 = Object_Get(item->object_id);
+    const bool target_lara = swap_id == O_LARA_SWAP && item == Lara_GetItem();
+    for (int32_t mesh_idx = 0; mesh_idx < obj_1->mesh_count; mesh_idx++) {
+        Object_SwapMesh(item->object_id, swap_id, mesh_idx);
+        if (target_lara) {
+            Lara_Mesh_SwapSingle(mesh_idx, item->object_id);
+        }
+    }
+}
+
+static void M_SwapMeshesWithMeshSwap1(ITEM *const item)
+{
+    M_SwapMeshes(item, O_MESH_SWAP_1);
+}
+
+static void M_SwapMeshesWithMeshSwap2(ITEM *const item)
+{
+    M_SwapMeshes(item, O_MESH_SWAP_2);
+}
+
+static void M_SwapMeshesWithMeshSwap3(ITEM *const item)
+{
+    M_SwapMeshes(item, O_LARA_SWAP);
+}
+
 REGISTER_ITEM_ACTION(ITEM_ACTION_TURN_180, M_Turn180)
+REGISTER_ITEM_ACTION(
+    ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_1, M_SwapMeshesWithMeshSwap1)
+REGISTER_ITEM_ACTION(
+    ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_2, M_SwapMeshesWithMeshSwap2)
+REGISTER_ITEM_ACTION(
+    ITEM_ACTION_SWAP_MESHES_WITH_MESH_SWAP_3, M_SwapMeshesWithMeshSwap3)
