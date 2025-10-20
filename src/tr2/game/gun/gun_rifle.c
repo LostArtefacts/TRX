af6911a23d4b4ba7dd4c44f3078dda390606596b
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-20 10:10:19 +0100

    gun: remove local gun misc headers

--- src/tr2/game/gun/gun_rifle.c
+++ src/tr2/game/gun/gun_rifle.c
@@ -1,6 +1,5 @@
 #include "game/gun/gun_rifle.h"
 
-#include "game/gun/gun_misc.h"
 #include "game/lara/misc.h"
 #include "game/stats.h"
 
@@ -19,6 +18,19 @@
 #define GUN_RIFLE_EQUIP_ANIM 1
 #define GUN_RIFLE_DRAW_FRAME 10
 #define GUN_RIFLE_UNDRAW_FRAME 21
+
+typedef enum {
+    LA_G_AIM = 0,
+    LA_G_DRAW = 1,
+    LA_G_RECOIL = 2,
+    LA_G_UNDRAW = 3,
+    LA_G_UNAIM = 4,
+    LA_G_RELOAD = 5,
+    LA_G_UAIM = 6,
+    LA_G_UUNAIM = 7,
+    LA_G_URECOIL = 8,
+    LA_G_SURF_UNDRAW = 9,
+} LARA_GUN_ANIMATION;
 
 static bool m_M16Firing = false;
 bool g_Gun_ReloadHarpoon = false; // TODO: make module-level
