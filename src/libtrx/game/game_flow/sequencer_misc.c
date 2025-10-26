46607b12edee4fd62ef3ba0984371ef2385fa36b
Dash <dash@wind.garden>
2025-10-26 13:05:49 +0100

    game-flow: merge GF_DoLevelSequence to trx

--- src/libtrx/game/game_flow/sequencer_misc.c
+++ src/libtrx/game/game_flow/sequencer_misc.c
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "game/demo.h"
+#include "game/game.h"
 #include "game/game_flow/common.h"
 #include "game/game_flow/sequencer.h"
 #include "game/game_flow/vars.h"
@@ -135,6 +136,30 @@ GF_COMMAND GF_DoFrontendSequence(void)
         return (GF_COMMAND) { .action = GF_NOOP };
     }
     return GF_InterpretSequence(g_GameFlow.title_level, GFSC_NORMAL, nullptr);
+}
+
+GF_COMMAND GF_DoLevelSequence(
+    const GF_LEVEL *const start_level, const GF_SEQUENCE_CONTEXT seq_ctx)
+{
+    const GF_LEVEL *current_level = start_level;
+    const GF_LEVEL_TABLE_TYPE level_table_type =
+        GF_GetLevelTableType(current_level->type);
+    const int32_t level_count = GF_GetLevelTable(level_table_type)->count;
+    while (true) {
+        const GF_COMMAND gf_cmd =
+            GF_InterpretSequence(current_level, seq_ctx, nullptr);
+
+        if (gf_cmd.action != GF_NOOP && gf_cmd.action != GF_LEVEL_COMPLETE) {
+            return gf_cmd;
+        }
+        if (Game_IsInGym()) {
+            return (GF_COMMAND) { .action = GF_EXIT_TO_TITLE };
+        }
+        if (current_level->num + 1 >= level_count) {
+            return (GF_COMMAND) { .action = GF_EXIT_TO_TITLE };
+        }
+        current_level++;
+    }
 }
 
 GF_COMMAND GF_DoDemoSequence(int32_t demo_num)
