8158c8cf2ee74339b0f03afe7497fab86fe0ac57
Dash <dash@wind.garden>
2025-10-26 10:58:06 +0100

    tr1/game: remove unused header

--- src/tr1/game/game.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <libtrx/game/game.h>
-
-GF_COMMAND Game_Stop_Legacy(void);
--- src/tr1/game/game_flow/sequencer_events.c
+++ src/tr1/game/game_flow/sequencer_events.c
@@ -1,10 +1,10 @@
-#include "game/game.h"
 #include "game/savegame.h"
 #include "game/stats.h"
 
 #include <libtrx/config.h>
 #include <libtrx/debug.h>
 #include <libtrx/game/camera.h>
+#include <libtrx/game/game.h>
 #include <libtrx/game/lara.h>
 #include <libtrx/game/lua.h>
 #include <libtrx/game/music.h>
