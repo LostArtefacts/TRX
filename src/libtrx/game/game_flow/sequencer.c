LostArtefacts/TRX

commit 29151f119c8deb4a3f6c2efd3924ad9d9c852387

savegame: reduce warnings about missing RESUME_INFO

src/libtrx/game/game_flow/sequencer.c
@@ -169,13 +169,13 @@ GF_COMMAND GF_InterpretSequence(
     default:
         if (level->type == GFL_GYM) {
             Savegame_ResetCurrentInfo(level);
-        } else if (
-            prev_level != nullptr
-            && (level->type == GFL_NORMAL || level->type == GFL_BONUS)) {
-            Savegame_CarryCurrentInfoToNextLevel(prev_level, level);
-        }
-        Savegame_ApplyLogicToCurrentInfo(level);
-        if (level->type == GFL_NORMAL || level->type == GFL_BONUS) {
+        } else if (level->type == GFL_DEMO) {
+            Savegame_ApplyLogicToCurrentInfo(level);
+        } else if (level->type == GFL_NORMAL || level->type == GFL_BONUS) {
+            if (prev_level != nullptr) {
+                Savegame_CarryCurrentInfoToNextLevel(prev_level, level);
+            }
+            Savegame_ApplyLogicToCurrentInfo(level);
             GF_InventoryModifier_Scan(level);
             GF_InventoryModifier_ApplyToResumeInfo(level);
         }

src/libtrx/game/level/common.c
@@ -1558,21 +1558,6 @@ bool Level_Initialise(
         Random_SeedControl(0xD371F947);
     }
 
-    RESUME_INFO *const resume = Savegame_GetCurrentInfo(level);
-    if (resume != nullptr) {
-        resume->stats.timer = 0;
-        resume->stats.secret_flags = 0;
-        resume->stats.secret_count = 0;
-#if TR_VERSION == 1
-        resume->stats.pickup_count = 0;
-#endif
-        resume->stats.kill_count = 0;
-        resume->stats.ammo_hits = 0;
-        resume->stats.ammo_used = 0;
-        resume->stats.medipacks_used = 0;
-        resume->stats.distance_travelled = 0;
-    }
-
     Game_SetIsLevelComplete(false);
     Game_FadeToBlack(-1);
     if (level->type != GFL_TITLE && level->type != GFL_DEMO) {
@@ -1582,6 +1567,24 @@ bool Level_Initialise(
         Game_SetCurrentLevel(level);
     }
     GF_SetCurrentLevel(level);
+
+    if (level->type != GFL_TITLE) {
+        // TODO: move me elsewhere
+        RESUME_INFO *const resume = Savegame_GetCurrentInfo(level);
+        if (resume != nullptr) {
+            resume->stats.timer = 0;
+            resume->stats.secret_flags = 0;
+            resume->stats.secret_count = 0;
+#if TR_VERSION == 1
+            resume->stats.pickup_count = 0;
+#endif
+            resume->stats.kill_count = 0;
+            resume->stats.ammo_hits = 0;
+            resume->stats.ammo_used = 0;
+            resume->stats.medipacks_used = 0;
+            resume->stats.distance_travelled = 0;
+        }
+    }
 
     if (level == nullptr) {
         return false;

src/libtrx/game/savegame/common.c
@@ -347,6 +347,8 @@ RESUME_INFO *Savegame_GetCurrentInfo(const GF_LEVEL *const level)
         return &m_ResumeInfo[level->num];
     } else if (level->type == GFL_DEMO) {
         return &m_ResumeInfo[GF_GetLevelTable(GFLT_MAIN)->count];
+    } else if (level->type == GFL_CUTSCENE) {
+        return nullptr;
     }
     LOG_WARNING(
         "Warning: unable to get resume info for level %d (type=%s)", level->num,

commit d24524c293b6c721612327fad59d336e40c765da

game-flow: fix gym starting with wrong resume info
Resolves #4182. Regression made in 29151f119c8d.

src/libtrx/game/game_flow/sequencer.c
@@ -169,6 +169,7 @@ GF_COMMAND GF_InterpretSequence(
     default:
         if (level->type == GFL_GYM) {
             Savegame_ResetCurrentInfo(level);
+            Savegame_ApplyLogicToCurrentInfo(level);
         } else if (level->type == GFL_DEMO) {
             Savegame_ApplyLogicToCurrentInfo(level);
         } else if (level->type == GFL_NORMAL || level->type == GFL_BONUS) {