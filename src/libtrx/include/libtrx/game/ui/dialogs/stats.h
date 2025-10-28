e0daa09079333b7ffcbbee26acae34ef1d74a202
Dash <dash@wind.garden>
2025-10-28 09:18:02 +0100

    ui/stats: fix assault course stats

--- src/libtrx/include/libtrx/game/ui/dialogs/stats.h
+++ src/libtrx/include/libtrx/game/ui/dialogs/stats.h
@@ -6,9 +6,7 @@
 typedef enum {
     UI_STATS_DIALOG_MODE_LEVEL,
     UI_STATS_DIALOG_MODE_FINAL,
-#if TR_VERSION == 2
     UI_STATS_DIALOG_MODE_ASSAULT_COURSE,
-#endif
 } UI_STATS_DIALOG_MODE;
 
 typedef enum {
