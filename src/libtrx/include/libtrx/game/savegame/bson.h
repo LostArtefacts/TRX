7c9f979ae74fa7afd20694cd3cadfc3a726a00e0
Dash <dash@wind.garden>
2025-10-31 14:26:06 +0100

    savegame/bson: move dumpflipmaps to trx

--- src/libtrx/include/libtrx/game/savegame/bson.h
+++ src/libtrx/include/libtrx/game/savegame/bson.h
@@ -38,6 +38,7 @@ JSON_OBJECT *Savegame_BSON_GetWriteRoot(SAVEGAME_BSON_WRITE_CONTEXT *ctx);
 void Savegame_BSON_DumpFlares(SAVEGAME_BSON_WRITE_CONTEXT *ctx);
 void Savegame_BSON_DumpEffects(SAVEGAME_BSON_WRITE_CONTEXT *ctx);
 void Savegame_BSON_DumpInventory(SAVEGAME_BSON_WRITE_CONTEXT *ctx);
+void Savegame_BSON_DumpFlipmaps(SAVEGAME_BSON_WRITE_CONTEXT *ctx);
 // End of helper functions =====================================================
 
 #pragma pack(push, 1)
