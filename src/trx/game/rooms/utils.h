166cc3a78b65737f2a00c30eba156619b0252607
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-12-07 11:18:37 +0000

    objects/traps/falling_block: calculate origin dynamically
    This replaces game and object type checks for collapsible tile origins
    with a dynamic frame offset check instead. All three types are as a
    result available to use freely  in all games.
    
    Resolves #4374.

--- src/trx/game/rooms/utils.h
+++ src/trx/game/rooms/utils.h
@@ -6,3 +6,5 @@
 #define ROUND_TO_SECTOR(V) ((V) & ~(WALL_L - 1))
 #define ROUND_TO_CLICK_UP(V) (((V) + STEP_L / 2 - 1) & ~(STEP_L / 2 - 1))
 #define ROUND_TO_HALF_CLICK(V) ((V) & ~(STEP_L / 2 - 1))
+#define ROUND_TO_CLICK_SIGNED(V)                                               \
+    (((V) >= 0) ? (((V) + STEP_L - 1) & ~(STEP_L - 1)) : ((V) & ~(STEP_L - 1)))
