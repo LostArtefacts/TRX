d1555345068bdd9072578d10869689443eff5fea
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-12-13 14:03:56 +0000

    camera: introduce environment module
    This introduces an environment module for the camera to control sound
    and triggers.

---
+++ src/trx/game/camera/environment.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <trx/game/rooms/types.h>
+
+void Camera_RefreshFromTrigger(const TRIGGER *trigger);
+void Camera_EnsureEnvironment(void);
+void Camera_UpdateMicPosition(void);
