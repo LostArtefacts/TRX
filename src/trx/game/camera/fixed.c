cabd2b07f5fef089cee74f4dcf2600ecfd0197fa
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-12-13 16:02:38 +0000

    camera: add support for locked cameras
    This allows TR4 style locked cameras to be used in custom levels.
    
    Resolves #2040.

--- src/trx/game/camera/fixed.c
+++ src/trx/game/camera/fixed.c
@@ -1,5 +1,9 @@
+#include <trx/game/camera/fixed.h>
+
+#include <trx/game/camera/const.h>
 #include <trx/game/game_buf.h>
-#include <trx/game/types.h>
+
+#define M_LOCKED_CAMERA 1
 
 static int32_t m_FixedObjectCount = 0;
 static OBJECT_VECTOR *m_FixedObjects = nullptr;
@@ -23,4 +27,14 @@ OBJECT_VECTOR *Camera_GetFixedObject(const int32_t object_idx)
         return nullptr;
     }
     return &m_FixedObjects[object_idx];
+}
+
+bool Camera_IsLocked(const int32_t camera_num)
+{
+    if (camera_num == NO_CAMERA) {
+        return false;
+    }
+
+    const OBJECT_VECTOR *const fixed_camera = Camera_GetFixedObject(camera_num);
+    return (fixed_camera->flags & M_LOCKED_CAMERA) != 0;
 }
