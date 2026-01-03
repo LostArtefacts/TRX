7af820bf66ed7616a8b3dfbc3fe3716ad2129c9d
Dash <dash@bad.pink>
2026-01-03 16:50:24 +0100

    objects/animating: add

---
+++ src/trx/game/objects/general/animating.c
@@ -0,0 +1,26 @@
+#include <trx/game/objects.h>
+#include <trx/game/rooms.h>
+
+static void M_Control(const int16_t item_num)
+{
+    ITEM *const item = Item_Get(item_num);
+
+    if (Item_IsTriggerActive(item)) {
+        Item_Animate(item);
+    }
+}
+
+static void M_Setup(OBJECT *const obj)
+{
+    obj->control_func = M_Control;
+    obj->collision_func = Object_Collision;
+    obj->save_flags = true;
+    obj->save_anim = true;
+}
+
+REGISTER_OBJECT(O_ANIMATING_1, M_Setup)
+REGISTER_OBJECT(O_ANIMATING_2, M_Setup)
+REGISTER_OBJECT(O_ANIMATING_3, M_Setup)
+REGISTER_OBJECT(O_ANIMATING_4, M_Setup)
+REGISTER_OBJECT(O_ANIMATING_5, M_Setup)
+REGISTER_OBJECT(O_ANIMATING_6, M_Setup)
