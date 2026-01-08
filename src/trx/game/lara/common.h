61b91b538a80123d76e58d36e621e147e3ee317a
Dash <dash@bad.pink>
2026-01-08 10:10:24 +0100

    lara: fix Lara_GetMeshPos once and for all

--- src/trx/game/lara/common.h
+++ src/trx/game/lara/common.h
@@ -30,7 +30,8 @@ void Lara_AnimateUntil(ITEM *lara_item, int32_t goal);
 const ANIM_FRAME *Lara_GetHitFrame(const ITEM *item);
 void Lara_TakeDamage(int16_t damage, bool hit_status);
 
-void Lara_GetMeshPos(LARA_MESH mesh, XYZ_32 *out_pos);
+bool Lara_GetMeshPos(LARA_MESH mesh, XYZ_32 *out_pos);
+void Lara_UpdateMeshPosMatrices(void);
 bool Lara_TestBoundsCollide(const ITEM *item, int32_t radius);
 void Lara_Push(const ITEM *item, COLL_INFO *coll, bool hit_on, bool big_push);
 bool Lara_TestPosition(const ITEM *item, const OBJECT_BOUNDS *bounds);
