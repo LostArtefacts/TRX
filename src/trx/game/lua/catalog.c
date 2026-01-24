d7bd98819b74fdd1908fb8fcd947ecdc49d7307e
Dash <dash@bad.pink>
2026-01-24 22:50:06 +0100

    catalog: support remaining catalogs

--- src/trx/game/lua/catalog.c
+++ src/trx/game/lua/catalog.c
@@ -54,6 +54,54 @@ static void M_PushFlipEffects(lua_State *const L)
     lua_setfield(L, -2, "flip_effects");
 }
 
+static void M_PushLaraStates(lua_State *const L)
+{
+    lua_newtable(L);
+
+    int32_t id = 0;
+#define X_CATALOG_ID(enum_value) M_PushCatalogKey(L, #enum_value, "LS_", id++);
+#include "trx/game/catalog_lara_states.def"
+#undef X_CATALOG_ID
+
+    lua_setfield(L, -2, "lara_states");
+}
+
+static void M_PushLaraAnims(lua_State *const L)
+{
+    lua_newtable(L);
+
+    int32_t id = 0;
+#define X_CATALOG_ID(enum_value) M_PushCatalogKey(L, #enum_value, "LA_", id++);
+#include "trx/game/catalog_lara_anims.def"
+#undef X_CATALOG_ID
+
+    lua_setfield(L, -2, "lara_anims");
+}
+
+static void M_PushMusic(lua_State *const L)
+{
+    lua_newtable(L);
+
+    int32_t id = 0;
+#define X_CATALOG_ID(enum_value) M_PushCatalogKey(L, #enum_value, "MX_", id++);
+#include "trx/game/catalog_music.def"
+#undef X_CATALOG_ID
+
+    lua_setfield(L, -2, "music");
+}
+
+static void M_PushSamples(lua_State *const L)
+{
+    lua_newtable(L);
+
+    int32_t id = 0;
+#define X_CATALOG_ID(enum_value) M_PushCatalogKey(L, #enum_value, "SFX_", id++);
+#include "trx/game/catalog_samples.def"
+#undef X_CATALOG_ID
+
+    lua_setfield(L, -2, "samples");
+}
+
 void LUA_CreateCatalog(lua_State *const L)
 {
     lua_getglobal(L, "trxc");
@@ -61,6 +109,10 @@ void LUA_CreateCatalog(lua_State *const L)
 
     M_PushObjects(L);
     M_PushFlipEffects(L);
+    M_PushLaraStates(L);
+    M_PushLaraAnims(L);
+    M_PushMusic(L);
+    M_PushSamples(L);
 
     lua_setfield(L, -2, "catalog");
     lua_pop(L, 1);
