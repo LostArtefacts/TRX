d83e0898646d1f1d3c20c81ae6bd0471986ab3aa
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-02-01 19:59:28 +0000

    shell: merge config change handlers
    This combines the config change handlers into a single function.

--- src/trx/game/shell/config.h
+++ src/trx/game/shell/config.h
@@ -2,10 +2,8 @@
 
 #include <trx/config/types.h>
 
-void Shell_HandleConfigChange(const CONFIG *old, const CONFIG *new);
-
 void Shell_SyncToWindow(void);
 void Shell_SyncFromWindow(bool update_viewport);
 
 void Shell_RefreshRendererViewport(void);
-void Shell_HandleCommonConfigChange(const CONFIG *old, const CONFIG *new);
+void Shell_HandleConfigChange(const CONFIG *old, const CONFIG *new);
