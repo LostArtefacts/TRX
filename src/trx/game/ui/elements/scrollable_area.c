2b95e2ef4735d90d9608b291c30908abfd22a5fa
Dash <dash@bad.pink>
2026-02-06 20:14:57 +0100

    ui/dialogs/stats: indicate scrolling
    Resolves #3510.

--- src/trx/game/ui/elements/scrollable_area.c
+++ src/trx/game/ui/elements/scrollable_area.c
@@ -10,7 +10,8 @@
 
 static void M_UpArrow(const UI_SCROLLABLE *const s)
 {
-    UI_BeginHide(s->first_item == 0);
+    const bool show_arrow = s->first_item != 0;
+    UI_BeginHide(!show_arrow);
     UI_Spacer(0.0f, g_TRVersion >= 2 ? 6.0f : 4.0f);
     UI_BeginAnchor(0.5f, 0.5f);
     UI_BeginFixed(0.5f, g_TRVersion >= 2 ? 0.7f : 0.9f);
@@ -22,19 +23,20 @@ static void M_UpArrow(const UI_SCROLLABLE *const s)
 
 static void M_DownArrow(const UI_SCROLLABLE *const s)
 {
-    UI_BeginHide(s->first_item + s->vis_items >= s->max_items);
+    const bool show_arrow = s->first_item + s->vis_items < s->max_items;
+    UI_BeginHide(!show_arrow);
     UI_BeginAnchor(0.5f, 0.0f);
     UI_BeginFixed(0.5f, -0.3f);
     UI_LabelEx("\\{arrow down}", (UI_LABEL_SETTINGS) { .scale = 0.7 });
     UI_EndFixed();
     UI_EndAnchor();
-    UI_EndHide();
     UI_Spacer(0.0f, g_TRVersion >= 2 ? 6.0f : 4.0f);
+    UI_EndHide();
 }
 
 void UI_BeginScrollableArea(const UI_SCROLLABLE *const s, const bool force_show)
 {
-    if (s->vis_items < s->max_items || force_show) {
+    if (s->vis_items > 0 || force_show) {
         UI_BeginStackEx((UI_STACK_SETTINGS) {
             .orientation = UI_STACK_VERTICAL,
             .align = { .h = UI_STACK_H_ALIGN_SPAN },
@@ -45,7 +47,7 @@ void UI_BeginScrollableArea(const UI_SCROLLABLE *const s, const bool force_show)
 
 void UI_EndScrollableArea(const UI_SCROLLABLE *const s, const bool force_show)
 {
-    if (s->vis_items < s->max_items || force_show) {
+    if (s->vis_items > 0 || force_show) {
         M_DownArrow(s);
         UI_EndStack();
     }
