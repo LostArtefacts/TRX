fab94ce1dfd2f32cf9ebdf6495115a9b66fd7eda
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-02-07 20:18:22 +0000

    lua/lara: define mesh constants
    This adds Lara mesh and extra mesh constants to Lua in favour of
    magic numbers being used.

--- data/tr3/ship/data/scripts/cut8.lua
+++ data/tr3/ship/data/scripts/cut8.lua
@@ -1,9 +1,10 @@
+local drink_frame_num = 737
 local equipment_set = false
 
 trx.events.after_control(function()
   local lara_item = trx.lara.item
-  if lara_item.frame >= 737 and not equipment_set then
-    trx.lara.set_extra_equipment(10, 10)
+  if lara_item.frame >= drink_frame_num and not equipment_set then
+    trx.lara.set_extra_equipment(trx.lara.mesh.hand_r, trx.lara.extra_mesh.drink_can)
     equipment_set = true
   end
 end)
