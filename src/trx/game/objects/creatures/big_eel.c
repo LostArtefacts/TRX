df6420865563f69f91b2485bb0391f2d37e53a00
Dash <dash@bad.pink>
2026-02-08 01:08:24 +0100

    objects/big-eel: move .data to .priv

--- src/trx/game/objects/creatures/big_eel.c
+++ src/trx/game/objects/creatures/big_eel.c
@@ -30,6 +30,10 @@ typedef enum {
     BIG_EEL_ANIM_DEATH = 2,
 } BIG_EEL_ANIM;
 
+typedef struct {
+    int32_t pos;
+} M_PRIV;
+
 static const BITE m_BigEelBite = {
     .pos = { .x = 7, .y = 157, .z = 333 },
     .mesh_num = 7,
@@ -39,8 +43,9 @@ static void M_Control(const int16_t item_num)
 {
     ITEM *const item = Item_Get(item_num);
     const ITEM *const lara_item = Lara_GetItem();
+    M_PRIV *const p = item->priv;
 
-    int32_t pos = (int32_t)(intptr_t)item->data;
+    int32_t pos = p->pos;
     item->pos.z -= (pos * Math_Cos(item->rot.y)) >> W2V_SHIFT;
     item->pos.x -= ((pos * Math_Sin(item->rot.y)) >> W2V_SHIFT);
 
@@ -84,7 +89,7 @@ static void M_Control(const int16_t item_num)
 
     item->pos.x += (pos * Math_Sin(item->rot.y)) >> W2V_SHIFT;
     item->pos.z += (pos * Math_Cos(item->rot.y)) >> W2V_SHIFT;
-    item->data = (void *)(intptr_t)pos;
+    p->pos = pos;
 
     Item_Animate(item);
 }
@@ -97,6 +102,7 @@ static void M_Setup(OBJECT *const obj)
 
     obj->control_func = M_Control;
     obj->collision_func = Creature_Collision;
+    obj->priv_size = sizeof(M_PRIV);
 
     obj->hit_points = BIG_EEL_HITPOINTS;
 
