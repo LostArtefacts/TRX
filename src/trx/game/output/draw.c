a0e6c4e31299ec4437568e3309d881593fa4661e
Dash <dash@bad.pink>
2026-02-09 04:37:37 +0100

    output: simplify TR3 shadows centering

--- src/trx/game/output/draw.c
+++ src/trx/game/output/draw.c
@@ -108,10 +108,7 @@ static bool M_DrawShadow_Sprite(
                 anchor_floor = height;
             }
         }
-    } else if (
-        g_TRVersion < 3
-        || (g_TRVersion == 3 && item->object_id >= O_PLAYER_1
-            && item->object_id <= O_PLAYER_10)) {
+    } else {
         // TR3 cutscene actors are driven by cutscene data, so their item origin
         // can diverge from the visual mesh center.
         const int32_t x_mid = (bounds->min.x + bounds->max.x) / 2;
