29046fab2f0dcf339fe27d61597172952b7e5b9a
Dash <dash@bad.pink>
2026-02-17 13:25:58 +0100

    ui: trim ui.h

---
+++ src/trx/game/ui/dialogs.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <trx/game/ui/dialogs/color_editor.h>
+#include <trx/game/ui/dialogs/controls.h>
+#include <trx/game/ui/dialogs/controls_backend.h>
+#include <trx/game/ui/dialogs/gameplay_settings.h>
+#include <trx/game/ui/dialogs/graphic_settings.h>
+#include <trx/game/ui/dialogs/new_game.h>
+#include <trx/game/ui/dialogs/pause.h>
+#include <trx/game/ui/dialogs/photo_mode.h>
+#include <trx/game/ui/dialogs/play_any_level.h>
+#include <trx/game/ui/dialogs/save_slot.h>
+#include <trx/game/ui/dialogs/select_level.h>
+#include <trx/game/ui/dialogs/sound_settings.h>
+#include <trx/game/ui/dialogs/stats.h>
+#include <trx/game/ui/dialogs/text.h>
---
+++ src/trx/game/ui/hud.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <trx/game/ui/hud/console.h>
+#include <trx/game/ui/hud/console_logs.h>
+#include <trx/game/ui/hud/overlay.h>
