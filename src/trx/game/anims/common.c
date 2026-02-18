0a155243210c8f77f3c7614ef132bcbbaad46dc6
Dash <dash@bad.pink>
2026-02-18 19:41:28 +0100

    lara/skin: fix invalid bone reads

--- src/trx/game/anims/common.c
+++ src/trx/game/anims/common.c
@@ -1,8 +1,12 @@
 #include <trx/game/anims/common.h>
 
+#include <trx/debug.h>
 #include <trx/game/game_buf.h>
 
 static int32_t m_AnimCount = 0;
+static int32_t m_ChangeCount = 0;
+static int32_t m_RangeCount = 0;
+static int32_t m_BoneCount = 0;
 static ANIM *m_Anims = nullptr;
 static ANIM_CHANGE *m_Changes = nullptr;
 static ANIM_RANGE *m_Ranges = nullptr;
@@ -17,17 +21,20 @@ void Anim_InitialiseAnims(const int32_t num_anims)
 
 void Anim_InitialiseChanges(const int32_t num_changes)
 {
+    m_ChangeCount = num_changes;
     m_Changes =
         GameBuf_Alloc(sizeof(ANIM_CHANGE) * num_changes, GBUF_ANIM_CHANGES);
 }
 
 void Anim_InitialiseRanges(const int32_t num_ranges)
 {
+    m_RangeCount = num_ranges;
     m_Ranges = GameBuf_Alloc(sizeof(ANIM_RANGE) * num_ranges, GBUF_ANIM_RANGES);
 }
 
 void Anim_InitialiseBones(const int32_t num_bones)
 {
+    m_BoneCount = num_bones;
     m_Bones = GameBuf_Alloc(sizeof(ANIM_BONE) * num_bones, GBUF_ANIM_BONES);
 }
 
@@ -38,22 +45,37 @@ int32_t Anim_GetTotalCount(void)
 
 ANIM *Anim_GetAnim(const int32_t anim_idx)
 {
-    return anim_idx == NO_ANIM ? &m_NullAnim : &m_Anims[anim_idx];
+    if (anim_idx < 0 || anim_idx >= m_AnimCount) {
+        return &m_NullAnim;
+    }
+    return &m_Anims[anim_idx];
 }
 
 ANIM_CHANGE *Anim_GetChange(const int32_t change_idx)
 {
+    ASSERT(change_idx >= 0 && change_idx < m_ChangeCount);
     return &m_Changes[change_idx];
 }
 
 ANIM_RANGE *Anim_GetRange(const int32_t range_idx)
 {
+    ASSERT(range_idx >= 0 && range_idx < m_RangeCount);
     return &m_Ranges[range_idx];
 }
 
 ANIM_BONE *Anim_GetBone(const int32_t bone_idx)
 {
-    return &m_Bones[bone_idx];
+    ANIM_BONE *const result = Anim_TryGetBone(bone_idx);
+    ASSERT(result != nullptr);
+    return result;
+}
+
+ANIM_BONE *Anim_TryGetBone(const int32_t bone_idx)
+{
+    if (bone_idx >= 0 && bone_idx < m_BoneCount) {
+        return &m_Bones[bone_idx];
+    }
+    return nullptr;
 }
 
 bool Anim_TestAbsFrameEqual(const int16_t abs_frame, const int16_t frame)
