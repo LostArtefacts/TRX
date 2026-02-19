e4c3fdb8481c947f8984f2413d1850ea03023521
Dash <dash@bad.pink>
2026-02-19 13:32:30 +0100

    lua: expose missing catalogs

--- data/scripting/catalog.lua
+++ data/scripting/catalog.lua
@@ -3,6 +3,10 @@ local raw = trxc.catalog
 local catalog = {
   objects = raw.objects,
   flip_effects = raw.flip_effects,
+  lara_states = raw.lara_states,
+  lara_anims = raw.lara_anims,
+  music = raw.music,
+  samples = raw.samples,
 }
 
 trx.catalog = catalog
