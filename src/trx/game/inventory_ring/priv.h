df75f3c697c3ea570e8d893ef61c56e07e85b13d
Dash <dash@bad.pink>
2026-02-23 00:09:00 +0100

    inv-ring: centralize motion calls

--- src/trx/game/inventory_ring/priv.h
+++ src/trx/game/inventory_ring/priv.h
@@ -32,18 +32,18 @@ void InvRing_DoMotions(INV_RING *ring);
 void InvRing_RotateLeft(INV_RING *ring);
 void InvRing_RotateRight(INV_RING *ring);
 
+void InvRing_SetStatusTransition(
+    INV_RING *ring, RING_STATUS status, RING_STATUS status_target,
+    int16_t frames);
+
 void InvRing_MotionInit(
     INV_RING *ring, RING_STATUS status, RING_STATUS status_target,
     int16_t frames);
 void InvRing_MotionSetup(
     INV_RING *ring, RING_STATUS status, RING_STATUS status_target,
     int16_t frames);
-void InvRing_MotionRadius(INV_RING *ring, int16_t target);
 void InvRing_MotionRotation(INV_RING *ring, int16_t rotation, int16_t target);
-void InvRing_MotionCameraPos(INV_RING *ring, int16_t target);
 void InvRing_MotionCameraPitch(INV_RING *ring, int16_t target);
-void InvRing_MotionItemSelect(INV_RING *ring, const INVENTORY_ITEM *inv_item);
-void InvRing_MotionItemDeselect(INV_RING *ring, const INVENTORY_ITEM *inv_item);
 
 void InvRing_ShowItemName(const INVENTORY_ITEM *inv_item);
 void InvRing_ShowItemQuantity(const char *fmt, int32_t qty);
