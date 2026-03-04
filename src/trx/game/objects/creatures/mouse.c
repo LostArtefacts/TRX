4d20b7558fae98314914315fe3ebd595cd493c31
Dash <dash@bad.pink>
2026-03-04 18:48:55 +0100

    objects/mouse: audit

--- src/trx/game/objects/creatures/mouse.c
+++ src/trx/game/objects/creatures/mouse.c
@@ -6,31 +6,29 @@
 #include <trx/game/spawn.h>
 
 // clang-format off
-#define MOUSE_HITPOINTS     4
-#define MOUSE_TOUCH_BITS    0b01111111 // = 0x7F
-#define MOUSE_RADIUS        (WALL_L / 10) // = 102
-#define MOUSE_RUN_TURN      (DEG_1 * 6) // = 1092
-#define MOUSE_ATTACK_RANGE  SQUARE(WALL_L / 3) // = 116281
-#define MOUSE_BITE_DAMAGE   20
-#define MOUSE_WAIT_1_CHANCE 0x500 // = 1280
-#define MOUSE_WAIT_2_CHANCE (MOUSE_WAIT_1_CHANCE + 0x500) // = 2560
+#define M_HITPOINTS     4
+#define M_TOUCH_BITS    0b01111111 // = 0x7F
+#define M_RADIUS        (WALL_L / 10) // = 102
+#define M_RUN_TURN      (DEG_1 * 6) // = 1092
+#define M_ATTACK_RANGE  SQUARE(WALL_L / 3) // = 116281
+#define M_BITE_DAMAGE   20
+#define M_WAIT_1_CHANCE 0x500 // = 1280
+#define M_WAIT_2_CHANCE (M_WAIT_1_CHANCE + 0x500) // = 2560
 // clang-format on
 
 typedef enum {
-    // clang-format off
-    MOUSE_STATE_EMPTY  = 0,
-    MOUSE_STATE_RUN    = 1,
-    MOUSE_STATE_STOP   = 2,
-    MOUSE_STATE_WAIT_1 = 3,
-    MOUSE_STATE_WAIT_2 = 4,
-    MOUSE_STATE_ATTACK = 5,
-    MOUSE_STATE_DEATH  = 6,
-    // clang-format on
-} MOUSE_STATE;
+    M_STATE_NULL,
+    M_STATE_RUN,
+    M_STATE_STOP,
+    M_STATE_WAIT_1,
+    M_STATE_WAIT_2,
+    M_STATE_ATTACK,
+    M_STATE_DEATH,
+} M_STATE;
 
 typedef enum {
-    MOUSE_ANIM_DEATH = 9,
-} MOUSE_ANIM;
+    M_ANIM_DEATH = 9,
+} M_ANIM;
 
 static const BITE m_MouseBite = {
     .pos = { .x = 0, .y = 0, .z = 57 },
@@ -50,74 +48,74 @@ static void M_Control(const int16_t item_num)
     int16_t angle = 0;
 
     if (item->hit_points <= 0) {
-        if (item->current_anim_state != MOUSE_STATE_DEATH) {
-            Item_SwitchToAnim(item, MOUSE_ANIM_DEATH, 0);
-            item->current_anim_state = MOUSE_STATE_DEATH;
+        if (item->current_anim_state != M_STATE_DEATH) {
+            Item_SwitchToAnim(item, M_ANIM_DEATH, 0);
+            item->current_anim_state = M_STATE_DEATH;
         }
     } else {
         AI_INFO info;
         Creature_AIInfo(item, &info);
-        Creature_Mood(item, &info, MOOD_BORED);
+        Creature_Mood(item, &info, false);
 
         if (info.ahead) {
             head = info.angle;
         }
-        angle = Creature_Turn(item, MOUSE_RUN_TURN);
+        angle = Creature_Turn(item, M_RUN_TURN);
 
         switch (item->current_anim_state) {
-        case MOUSE_STATE_RUN:
-            creature->maximum_turn = MOUSE_RUN_TURN;
+        case M_STATE_RUN:
+            creature->maximum_turn = M_RUN_TURN;
             if (creature->mood == MOOD_BORED || creature->mood == MOOD_STALK) {
                 const int32_t random = Random_GetControl();
-                if (random < MOUSE_WAIT_1_CHANCE) {
-                    item->required_anim_state = MOUSE_STATE_WAIT_1;
-                    item->goal_anim_state = MOUSE_STATE_STOP;
-                } else if (random < MOUSE_WAIT_2_CHANCE) {
-                    item->goal_anim_state = MOUSE_STATE_STOP;
+                if (random < M_WAIT_1_CHANCE) {
+                    item->required_anim_state = M_STATE_WAIT_1;
+                    item->goal_anim_state = M_STATE_STOP;
+                } else if (random < M_WAIT_2_CHANCE) {
+                    item->goal_anim_state = M_STATE_STOP;
                 }
-            } else if (info.ahead && info.distance < MOUSE_ATTACK_RANGE) {
-                item->goal_anim_state = MOUSE_STATE_STOP;
+            } else if (info.ahead && info.distance < M_ATTACK_RANGE) {
+                item->goal_anim_state = M_STATE_STOP;
             }
             break;
 
-        case MOUSE_STATE_STOP:
+        case M_STATE_STOP:
             creature->maximum_turn = 0;
-            if (item->required_anim_state != MOUSE_STATE_EMPTY) {
+            if (item->required_anim_state != M_STATE_NULL) {
                 item->goal_anim_state = item->required_anim_state;
             }
             break;
 
-        case MOUSE_STATE_WAIT_1:
-            if (Random_GetControl() < MOUSE_WAIT_1_CHANCE) {
-                item->goal_anim_state = MOUSE_STATE_STOP;
+        case M_STATE_WAIT_1:
+            if (Random_GetControl() < M_WAIT_1_CHANCE) {
+                item->goal_anim_state = M_STATE_STOP;
             }
             break;
 
-        case MOUSE_STATE_WAIT_2:
+        case M_STATE_WAIT_2:
             if (creature->mood == MOOD_BORED || creature->mood == MOOD_STALK) {
                 const int32_t random = Random_GetControl();
-                if (random < MOUSE_WAIT_1_CHANCE) {
-                    item->required_anim_state = MOUSE_STATE_WAIT_1;
-                } else if (random > MOUSE_WAIT_2_CHANCE) {
-                    item->required_anim_state = MOUSE_STATE_RUN;
+                if (random < M_WAIT_1_CHANCE) {
+                    item->required_anim_state = M_STATE_WAIT_1;
+                } else if (random > M_WAIT_2_CHANCE) {
+                    item->required_anim_state = M_STATE_RUN;
                 }
-            } else if (info.distance < MOUSE_ATTACK_RANGE) {
-                item->required_anim_state = MOUSE_STATE_ATTACK;
+            } else if (info.distance < M_ATTACK_RANGE) {
+                item->required_anim_state = M_STATE_ATTACK;
             } else {
-                item->required_anim_state = MOUSE_STATE_RUN;
+                item->required_anim_state = M_STATE_RUN;
             }
 
-            if (item->required_anim_state != MOUSE_STATE_EMPTY) {
-                item->goal_anim_state = MOUSE_STATE_STOP;
+            if (item->required_anim_state != M_STATE_NULL) {
+                item->goal_anim_state = M_STATE_STOP;
             }
             break;
 
-        case MOUSE_STATE_ATTACK:
-            if (item->required_anim_state == MOUSE_STATE_EMPTY
-                && (item->touch_bits & MOUSE_TOUCH_BITS) != 0) {
-                Lara_TakeDamage(MOUSE_BITE_DAMAGE, true);
+        case M_STATE_ATTACK:
+            if (item->required_anim_state == M_STATE_NULL
+                && (item->touch_bits & M_TOUCH_BITS) != 0) {
+                Lara_TakeDamage(M_BITE_DAMAGE, true);
                 Creature_Effect(item, &m_MouseBite, Spawn_Blood);
-                item->required_anim_state = MOUSE_STATE_STOP;
+                item->required_anim_state = M_STATE_STOP;
             }
             break;
 
@@ -139,8 +137,8 @@ static void M_Setup(OBJECT *const obj)
     obj->control_func = M_Control;
     obj->collision_func = Creature_Collision;
 
-    obj->hit_points = MOUSE_HITPOINTS;
-    obj->radius = MOUSE_RADIUS;
+    obj->hit_points = M_HITPOINTS;
+    obj->radius = M_RADIUS;
     obj->shadow_size = UNIT_SHADOW / 2;
     obj->pivot_length = 50;
 
