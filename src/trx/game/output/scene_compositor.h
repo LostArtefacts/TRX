e113cfb014addd85275618c38fa25f2e0c80202d
Dash <dash@bad.pink>
2026-04-07 08:59:45 +0200

    output: use main texture filter for pickup overlay

--- src/trx/game/output/scene_compositor.h
+++ src/trx/game/output/scene_compositor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <trx/game/output/scene_source.h>
+#include <trx/gl/enum.h>
 
 void SceneCompositor_Init(void);
 void SceneCompositor_Shutdown(void);
@@ -9,3 +10,4 @@ void SceneCompositor_BeginScene(void);
 void SceneCompositor_EndScene(void);
 void SceneCompositor_Flush(void);
 void SceneCompositor_AnimateTextures(void);
+void SceneCompositor_SetSamplerFilter(TEXTURE_FILTER filter);
