2bb22a5e2fa9e1c766fc1d7f0a259d8e67b6971a
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-04-14 11:24:53 +0100

    creature/common: test creature data on activation
    This ensures that a creature is interpreted as being active as long
    as its creature data is set. It avoids issues whereby re-activating
    a dead enemy would allow its control routine to run, which assumes
    item->creature_data is not null.

--- src/trx/game/creature/common.c
+++ src/trx/game/creature/common.c
@@ -281,7 +281,7 @@ bool Creature_Activate(const int16_t item_num)
 {
     ITEM *const item = Item_Get(item_num);
     if (item->status != IS_INVISIBLE) {
-        return true;
+        return item->creature_data != nullptr;
     }
 
     if (!LOT_EnableBaddieAI(item_num, false)) {
