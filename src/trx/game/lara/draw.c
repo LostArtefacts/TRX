LostArtefacts/TRX

commit 2f24b7ca8ffe4a2f2620606894865e45702d2780

output: make Lara lighting more responsive

src/trx/game/lara/draw.c
@@ -17,6 +17,16 @@
 static bool m_CacheMatrices = false;
 static bool m_IsLara = true;
 
+static GAME_VECTOR M_GetLaraLightSample(const ITEM *const item)
+{
+    GAME_VECTOR sample_pos = {
+        .pos = { 0, 0, 0 },
+        .room_num = item->room_num,
+    };
+    Lara_GetJointAbsPosition(&sample_pos.pos, LM_TORSO);
+    return sample_pos;
+}
+
 static void M_CacheMatrix(const LARA_MESH mesh)
 {
     if (!m_CacheMatrices) {
@@ -159,7 +169,7 @@ static bool M_Draw_I(
     }
 
     Matrix_Push();
-    Output_CalculateObjectLighting(item, &frame1->bounds);
+    Output_CalculateObjectLightingAt(item, M_GetLaraLightSample(item));
 
     const ANIM_BONE *const bone =
         m_IsLara ? Lara_Skin_GetBoneBase() : Object_GetBone(obj, 0);
@@ -450,7 +460,7 @@ bool Lara_Draw(const ITEM *const item)
     }
 
     Matrix_Push();
-    Output_CalculateObjectLighting(item, &frame->bounds);
+    Output_CalculateObjectLightingAt(item, M_GetLaraLightSample(item));
 
     const ANIM_BONE *const bone =
         m_IsLara ? Lara_Skin_GetBoneBase() : Object_GetBone(obj, 0);

src/trx/game/output/lights.c
@@ -648,7 +648,6 @@ void Output_CalculateObjectLighting(
     }
 
     Matrix_PushUnit();
-
     Matrix_TranslateSet32((XYZ_32) {});
     Matrix_Rot16(item->rot);
     Matrix_TranslateRel32((XYZ_32) {
@@ -656,17 +655,28 @@ void Output_CalculateObjectLighting(
         .y = (bounds->max.y + bounds->min.y) / 2,
         .z = (bounds->max.z + bounds->min.z) / 2,
     });
-    const XYZ_32 pos = {
-        .x = item->pos.x + (g_MatrixPtr->_03 >> W2V_SHIFT),
-        .y = item->pos.y + (g_MatrixPtr->_13 >> W2V_SHIFT),
-        .z = item->pos.z + (g_MatrixPtr->_23 >> W2V_SHIFT),
+    const GAME_VECTOR sample_pos = {
+        .room_num = item->room_num,
+        .pos = {
+            .x = item->pos.x + (g_MatrixPtr->_03 >> W2V_SHIFT),
+            .y = item->pos.y + (g_MatrixPtr->_13 >> W2V_SHIFT),
+            .z = item->pos.z + (g_MatrixPtr->_23 >> W2V_SHIFT),
+        },
     };
     Matrix_Pop();
 
+    Output_CalculateObjectLightingAt(item, sample_pos);
+}
+
+void Output_CalculateObjectLightingAt(
+    const ITEM *const item, const GAME_VECTOR sample_pos)
+{
+    int16_t room_num = sample_pos.room_num;
+    Room_GetSector(sample_pos.pos, &room_num);
     if (g_TRVersion >= 3) {
-        M_TR3_CalculateLightSmoothed(item, pos, Room_Get(item->room_num));
+        M_TR3_CalculateLightSmoothed(item, sample_pos.pos, Room_Get(room_num));
     } else {
-        Output_CalculateLight(pos, item->room_num);
+        Output_CalculateLight(sample_pos.pos, room_num);
     }
 }
 

src/trx/game/output/lights.h
@@ -4,6 +4,7 @@
 #include <trx/core/vector.h>
 #include <trx/game/objects/types.h>
 #include <trx/game/rooms.h>
+#include <trx/game/types.h>
 #include <trx/game/viewport.h>
 
 void Output_Lights_Init(void);
@@ -16,6 +17,8 @@ void Output_CalculateStaticLightRGB15(int16_t rgb15);
 void Output_CalculateStaticLightRGB_F(RGB_F rgb);
 void Output_CalculateStaticMeshLight(XYZ_32 pos, SHADE shade, const ROOM *room);
 void Output_CalculateObjectLighting(const ITEM *item, const BOUNDS_16 *bounds);
+void Output_CalculateObjectLightingAt(
+    const ITEM *item, const GAME_VECTOR sample_pos);
 int32_t Output_GetRoomLightShade(ROOM_LIGHT_MODE mode);
 
 int32_t Output_GetSunsetDuration(void);

commit fd98573507f59c4787850b8d656e15d0e230950e

lara/draw: fix lighting
Regression from 2f24b7ca8ffe4a2f2620606894865e45702d2780.

src/trx/game/lara/draw.c
@@ -17,14 +17,22 @@
 static bool m_CacheMatrices = false;
 static bool m_IsLara = true;
 
-static GAME_VECTOR M_GetLaraLightSample(const ITEM *const item)
+static void M_CalculateLight(
+    const ITEM *const item, const ANIM_FRAME *const frame)
 {
-    GAME_VECTOR sample_pos = {
-        .pos = { 0, 0, 0 },
-        .room_num = item->room_num,
-    };
-    Lara_GetJointAbsPosition(&sample_pos.pos, LM_TORSO);
-    return sample_pos;
+    if (g_TRVersion >= 3) {
+        GAME_VECTOR sample_pos = {
+            .pos = { 0, 0, 0 },
+            .room_num = item->room_num,
+        };
+        if (!Lara_GetMeshPos(LM_TORSO, &sample_pos.pos)) {
+            Lara_GetJointAbsPosition(&sample_pos.pos, LM_TORSO);
+        }
+        Room_GetSector(sample_pos.pos, &sample_pos.room_num);
+        Output_CalculateObjectLightingAt(item, sample_pos);
+    } else {
+        Output_CalculateObjectLighting(item, &frame->bounds);
+    }
 }
 
 static void M_CacheMatrix(const LARA_MESH mesh)
@@ -164,12 +172,11 @@ static bool M_Draw_I(
     }
 
     m_CacheMatrices = m_IsLara;
+    Matrix_Push();
+    M_CalculateLight(item, frame1);
     if (m_CacheMatrices) {
         lara->mesh_pos_matrices_valid = false;
     }
-
-    Matrix_Push();
-    Output_CalculateObjectLightingAt(item, M_GetLaraLightSample(item));
 
     const ANIM_BONE *const bone =
         m_IsLara ? Lara_Skin_GetBoneBase() : Object_GetBone(obj, 0);
@@ -455,12 +462,11 @@ bool Lara_Draw(const ITEM *const item)
     }
 
     m_CacheMatrices = m_IsLara;
+    Matrix_Push();
+    M_CalculateLight(item, frame);
     if (m_CacheMatrices) {
         lara->mesh_pos_matrices_valid = false;
     }
-
-    Matrix_Push();
-    Output_CalculateObjectLightingAt(item, M_GetLaraLightSample(item));
 
     const ANIM_BONE *const bone =
         m_IsLara ? Lara_Skin_GetBoneBase() : Object_GetBone(obj, 0);