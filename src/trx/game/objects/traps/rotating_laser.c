de720379d8743a2cce0391c1a5ae24746dcb5f36
Dash <dash@bad.pink>
2026-06-01 10:29:57 +0200

    objects/rotating-laser: move damage to Lua

--- src/trx/game/objects/traps/rotating_laser.c
+++ src/trx/game/objects/traps/rotating_laser.c
@@ -4,10 +4,13 @@
 #include <trx/game/items/col.h>
 #include <trx/game/lara.h>
 #include <trx/game/objects.h>
+#include <trx/game/objects/property.h>
 #include <trx/game/output.h>
 #include <trx/game/random.h>
 #include <trx/game/rooms.h>
 #include <trx/game/spawn.h>
+
+#define M_DEFAULT_DAMAGE 25
 
 typedef struct {
     XYZ_32 origin;
@@ -16,6 +19,16 @@ typedef struct {
     int16_t velocity;
     int16_t reverse_timer;
 } M_PRIV;
+
+static int32_t M_GetDamage(const ITEM *const item)
+{
+    OBJECT_PROPERTY_VALUE damage = {};
+    if (ObjectProperty_GetItemValue(item, "damage", &damage)) {
+        return damage.as_int;
+    }
+
+    return M_DEFAULT_DAMAGE;
+}
 
 static void M_LoadPriv(ITEM *const item, JSON_READ_IO *const io)
 {
@@ -123,7 +136,7 @@ static void M_Control(const int16_t item_num)
 
     ITEM *const lara_item = Lara_GetItem();
     if (Item_TestBoundsCollide(item, lara_item, 64)) {
-        Lara_TakeDamage(25, false);
+        Lara_TakeDamage(M_GetDamage(item), false);
         Spawn_BloodBathD(
             lara_item->pos.x, item->pos.y - (Random_GetControl() & 0xFF) - 32,
             lara_item->pos.z, (Random_GetControl() & 0x7F) + 128,
@@ -145,6 +158,11 @@ static void M_Setup(OBJECT *const obj)
     obj->save_position = true;
     obj->save_flags = true;
     obj->save_anim = true;
+    OBJECT_PROPERTIES(
+        obj,
+        OBJECT_PROPERTY_INT(
+            "damage", M_DEFAULT_DAMAGE,
+            "Damage dealt while Lara is touching the rotating laser."));
 }
 
 REGISTER_OBJECT(O_ROTATING_LASER, M_Setup)
