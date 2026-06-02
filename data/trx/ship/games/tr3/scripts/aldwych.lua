05b26d4f6d653c60621150bafb06754af917f5d4
Dash <dash@bad.pink>
2026-06-02 16:51:03 +0200

    lua: use simpler accessors

--- data/trx/ship/games/tr3/scripts/aldwych.lua
+++ data/trx/ship/games/tr3/scripts/aldwych.lua
@@ -1,3 +1,3 @@
 trx.events.before_item_setup(function(level)
-  trx.objects[trx.catalog.objects.ceiling_spikes].properties.speed = 10
+  trx.objects.ceiling_spikes.properties.speed = 10
 end)
--- data/trx/ship/games/tr3/scripts/ganges.lua
+++ data/trx/ship/games/tr3/scripts/ganges.lua
@@ -1,5 +1,5 @@
 trx.events.before_item_setup(function(level)
-  local props = trx.objects[trx.catalog.objects.quad_bike].properties
+  local props = trx.objects.quad_bike.properties
   props.track_1 = 9
   props.track_2 = 12
   props.track_3 = 4
