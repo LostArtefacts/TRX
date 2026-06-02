a3216a33b3d6e3dd0966720d3b2cd85daa7986d2
Checkm8Croft <strategalento@gmail.com>
2026-06-02 17:50:33 +0200

    lua: add trx.assault function

--- data/scripting/assault.lua
+++ data/scripting/assault.lua
@@ -4,9 +4,4 @@ trx.assault = {
   start = raw.start,
   stop = raw.stop,
   reset = raw.reset,
-  quad = {
-    start = raw.quad.start,
-    stop = raw.quad.stop,
-    reset = raw.quad.reset,
-  },
 }
--- src/trx/game/lua/assault.c
+++ src/trx/game/lua/assault.c
@@ -3,97 +3,109 @@
 #include <trx/game/lua/common.h>
 
 #include <lauxlib.h>
+#include <string.h>
+
+/* =========================
+   Timer validation
+   ========================= */
 
 static void M_CheckTimerAvailable(
-    lua_State *const L, const GYM_TRACK_TYPE track, const char *const name)
+    lua_State *const L,
+    const GYM_TRACK_TYPE track,
+    const char *const name)
 {
     if (!Gym_TrackManager_HasStats(track)) {
         luaL_error(L, "%s timer unavailable", name);
     }
+
     if (!Game_IsInGym()) {
         luaL_error(L, "%s timer is only available in gym levels", name);
     }
 }
 
-static void M_CheckAssaultTimerAvailable(lua_State *const L)
+/* =========================
+   Track resolver
+   ========================= */
+
+static GYM_TRACK_TYPE M_GetTrack(lua_State *L)
 {
-    M_CheckTimerAvailable(L, GYM_TRACK_ASSAULT, "Assault");
+    const char *mode = luaL_optstring(L, 1, "course");
+
+    if (strcmp(mode, "quad") == 0) {
+        return GYM_TRACK_QUAD;
+    }
+
+    return GYM_TRACK_ASSAULT;
 }
 
-static void M_CheckQuadTimerAvailable(lua_State *const L)
+static const char *M_GetTrackName(lua_State *L)
 {
-    M_CheckTimerAvailable(L, GYM_TRACK_QUAD, "Quad");
+    const char *mode = luaL_optstring(L, 1, "course");
+
+    if (strcmp(mode, "quad") == 0) {
+        return "Quad";
+    }
+
+    return "Course";
 }
 
-// trxc.assault.start()
-static int M_L_AssaultStart(lua_State *const L)
+/* =========================
+   Lua bindings
+   ========================= */
+
+static int M_L_AssaultStart(lua_State *L)
 {
-    M_CheckAssaultTimerAvailable(L);
-    Gym_TrackManager_Start(GYM_TRACK_ASSAULT);
+    GYM_TRACK_TYPE track = M_GetTrack(L);
+    const char *name = M_GetTrackName(L);
+
+    M_CheckTimerAvailable(L, track, name);
+    Gym_TrackManager_Start(track);
+
     return 0;
 }
 
-// trxc.assault.stop()
-static int M_L_AssaultStop(lua_State *const L)
+static int M_L_AssaultStop(lua_State *L)
 {
-    M_CheckAssaultTimerAvailable(L);
-    Gym_TrackManager_Stop(GYM_TRACK_ASSAULT);
+    GYM_TRACK_TYPE track = M_GetTrack(L);
+    const char *name = M_GetTrackName(L);
+
+    M_CheckTimerAvailable(L, track, name);
+    Gym_TrackManager_Stop(track);
+
     return 0;
 }
 
-// trxc.assault.reset()
-static int M_L_AssaultReset(lua_State *const L)
+static int M_L_AssaultReset(lua_State *L)
 {
-    M_CheckAssaultTimerAvailable(L);
-    Gym_TrackManager_Reset(GYM_TRACK_ASSAULT);
+    GYM_TRACK_TYPE track = M_GetTrack(L);
+    const char *name = M_GetTrackName(L);
+
+    M_CheckTimerAvailable(L, track, name);
+    Gym_TrackManager_Reset(track);
+
     return 0;
 }
 
-// trxc.assault.quad.start()
-static int M_L_AssaultQuadStart(lua_State *const L)
-{
-    M_CheckQuadTimerAvailable(L);
-    Gym_TrackManager_Start(GYM_TRACK_QUAD);
-    return 0;
-}
-
-// trxc.assault.quad.stop()
-static int M_L_AssaultQuadStop(lua_State *const L)
-{
-    M_CheckQuadTimerAvailable(L);
-    Gym_TrackManager_Stop(GYM_TRACK_QUAD);
-    return 0;
-}
-
-// trxc.assault.quad.reset()
-static int M_L_AssaultQuadReset(lua_State *const L)
-{
-    M_CheckQuadTimerAvailable(L);
-    Gym_TrackManager_Reset(GYM_TRACK_QUAD);
-    return 0;
-}
+/* =========================
+   Lua module init
+   ========================= */
 
 void LUA_CreateAssault(lua_State *const L)
 {
     lua_getglobal(L, "trxc");
+
     lua_newtable(L);
 
     lua_pushcfunction(L, M_L_AssaultStart);
     lua_setfield(L, -2, "start");
+
     lua_pushcfunction(L, M_L_AssaultStop);
     lua_setfield(L, -2, "stop");
+
     lua_pushcfunction(L, M_L_AssaultReset);
     lua_setfield(L, -2, "reset");
 
-    lua_newtable(L);
-    lua_pushcfunction(L, M_L_AssaultQuadStart);
-    lua_setfield(L, -2, "start");
-    lua_pushcfunction(L, M_L_AssaultQuadStop);
-    lua_setfield(L, -2, "stop");
-    lua_pushcfunction(L, M_L_AssaultQuadReset);
-    lua_setfield(L, -2, "reset");
-    lua_setfield(L, -2, "quad");
-
     lua_setfield(L, -2, "assault");
+
     lua_pop(L, 1);
 }
