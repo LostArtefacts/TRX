99b98170afb41ff1dab132545a08568144ea624e
Dash <dash@bad.pink>
2026-06-03 08:41:09 +0200

    objects/rx-worker-1: move damage to Lua

--- src/trx/game/objects/creatures/rx_worker_1.c
+++ src/trx/game/objects/creatures/rx_worker_1.c
@@ -1,6 +1,7 @@
 #include <trx/game/creature.h>
 #include <trx/game/lara.h>
 #include <trx/game/objects.h>
+#include <trx/game/objects/property.h>
 #include <trx/game/random.h>
 #include <trx/game/rooms.h>
 #include <trx/game/sound.h>
@@ -66,6 +67,16 @@ static const CREATURE_GUN m_Gun = {
     .tr3_flash_rot_x = -DEG_90,
 };
 
+static int32_t M_GetDamage(const ITEM *const item)
+{
+    OBJECT_PROPERTY_VALUE damage = {};
+    if (ObjectProperty_GetItemValue(item, "damage", &damage)) {
+        return damage.as_int;
+    }
+
+    return M_DAMAGE;
+}
+
 static void M_FireFinalShot(
     ITEM *const item, int16_t *const head, int16_t *const torso_y)
 {
@@ -81,7 +92,7 @@ static void M_FireFinalShot(
 
     *head = info.angle;
     *torso_y = info.angle;
-    Creature_Shoot(item, &info, &m_Gun, info.angle, M_DAMAGE * 3);
+    Creature_Shoot(item, &info, &m_Gun, info.angle, M_GetDamage(item) * 3);
     Sound_Effect(SFX_LONDON_SWAT_FIRE, &item->pos, SPM_NORMAL);
 }
 
@@ -288,7 +299,8 @@ static void M_Control(const int16_t item_num)
 
         if (anim_idx == M_ANIM_SHOOT_1
             || (anim_idx == M_ANIM_AIM_1 && frame_idx == 10)) {
-            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)) {
+            if (!Creature_Shoot(
+                    item, &info, &m_Gun, torso_y, M_GetDamage(item))) {
                 item->required_anim_state = M_STATE_WAIT;
             }
         } else if (M_ShouldDuck(item, near_cover)) {
@@ -315,7 +327,8 @@ static void M_Control(const int16_t item_num)
         }
 
         if (frame_idx == 0) {
-            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)) {
+            if (!Creature_Shoot(
+                    item, &info, &m_Gun, torso_y, M_GetDamage(item))) {
                 item->goal_anim_state = M_STATE_WAIT;
             }
         } else if (M_ShouldDuck(item, near_cover)) {
@@ -332,7 +345,8 @@ static void M_Control(const int16_t item_num)
         }
 
         if (frame_idx == 0 || frame_idx == 11) {
-            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)) {
+            if (!Creature_Shoot(
+                    item, &info, &m_Gun, torso_y, M_GetDamage(item))) {
                 item->goal_anim_state = M_STATE_WAIT;
             }
         } else if (M_ShouldDuck(item, near_cover)) {
@@ -349,7 +363,8 @@ static void M_Control(const int16_t item_num)
 
         if ((anim_idx == M_ANIM_AIM_4A && frame_idx == 16)
             || (anim_idx == M_ANIM_AIM_4B && frame_idx == 6)) {
-            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)) {
+            if (!Creature_Shoot(
+                    item, &info, &m_Gun, torso_y, M_GetDamage(item))) {
                 item->required_anim_state = M_STATE_WALK;
             }
         } else if (M_ShouldDuck(item, near_cover)) {
@@ -374,7 +389,8 @@ static void M_Control(const int16_t item_num)
         }
 
         if (frame_idx == 16
-            && !Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)) {
+            && !Creature_Shoot(
+                item, &info, &m_Gun, torso_y, M_GetDamage(item))) {
             item->goal_anim_state = M_STATE_WALK;
         }
 
@@ -419,7 +435,7 @@ static void M_Control(const int16_t item_num)
         }
 
         if (frame_idx == 0) {
-            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_DAMAGE)
+            if (!Creature_Shoot(item, &info, &m_Gun, torso_y, M_GetDamage(item))
                 || (Random_GetControl() & 7) == 0) {
                 item->goal_anim_state = M_STATE_DUCKED;
             }
@@ -483,7 +499,8 @@ static void M_Setup(OBJECT *const obj)
     OBJECT_PROPERTIES(
         obj,
         OBJECT_PROPERTY_INT(
-            "max_hit_points", M_HIT_POINTS, "Maximum hit points."));
+            "max_hit_points", M_HIT_POINTS, "Maximum hit points."),
+        OBJECT_PROPERTY_INT("damage", M_DAMAGE, "Damage dealt by shots."));
 }
 
 REGISTER_OBJECT(O_RX_WORKER_1, M_Setup)
