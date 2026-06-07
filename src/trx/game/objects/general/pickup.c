LostArtefacts/TRX

commit 945e46d05cc62b48c1286e4aab05b91a2c7eef18

objects/pickup: move quest behaviour to Lua
This changes quest item pickups to no longer finish the level by
default, and instead adds event handlers for those specific levels
that need it in Lua.

src/trx/game/objects/general/pickup.c
@@ -287,19 +287,7 @@ static void M_DoPickup(const int16_t item_num)
     Lua_FireEventInt32(LUA_EVENT_PICKUP, item_num); // LUA uses 1-indexing
 
     item->status = IS_INVISIBLE;
-    item->flags |= IF_KILLED;
-
-    if (Object_IsType(item->object_id, g_QuestObjects)) {
-        if (GF_BadGetLevelNum() == 19
-            || (GF_BadIsMod("tr3-la") && GF_BadGetLevelNum() == 4)) {
-            Item_Kill(item_num);
-        } else {
-            Game_SetIsLevelComplete(true);
-        }
-    } else {
-        Item_RemoveDrawn(item_num);
-        Item_RemoveActive(item_num);
-    }
+    Item_Kill(item_num);
 
     LARA_INFO *const lara = Lara_GetLaraInfo();
     lara->interact_target.is_moving = false;

data/trx/ship/games/tr3/gameflow.json5
@@ -152,6 +152,7 @@
         // 4. Caves of Kaliya
         {
             "path": "tonyboss.tr2",
+            "script": "tonyboss.lua",
             "music_track": 30,
             "lara_outfit": "tr3_classic",
             "sequence": [
@@ -243,6 +244,7 @@
         // 8. Temple of Puna
         {
             "path": "triboss.tr2",
+            "script": "triboss.lua",
             "music_track": 30,
             "lara_outfit": "tr3_south_pacific",
             "sequence": [
@@ -337,6 +339,7 @@
         // 12. City
         {
             "path": "office.tr2",
+            "script": "office.lua",
             "music_track": 78,
             "lara_outfit": "tr3_catsuit",
             "weather_type": "rain",

data/trx/ship/games/tr3/scripts/area51.lua
@@ -1,3 +1,10 @@
 trx.events.before_level_file(function(level)
   trx.creatures.add_ally(trx.catalog.objects.prisoner)
 end)
+
+trx.events.on_pickup(function(pickup_item)
+  local item = trx.items[pickup_item + 1]
+  if item.object_id == trx.catalog.objects.quest_item_2 then
+    trx.rooms.flip_effect(trx.catalog.flip_effects.finish_level)
+  end
+end)

data/trx/ship/games/tr3/scripts/office.lua
@@ -1,0 +1,6 @@
+trx.events.on_pickup(function(pickup_item)
+  local item = trx.items[pickup_item + 1]
+  if item.object_id == trx.catalog.objects.quest_item_3 then
+    trx.rooms.flip_effect(trx.catalog.flip_effects.finish_level)
+  end
+end)

data/trx/ship/games/tr3/scripts/tonyboss.lua
@@ -1,0 +1,6 @@
+trx.events.on_pickup(function(pickup_item)
+  local item = trx.items[pickup_item + 1]
+  if item.object_id == trx.catalog.objects.quest_item_1 then
+    trx.rooms.flip_effect(trx.catalog.flip_effects.finish_level)
+  end
+end)

data/trx/ship/games/tr3/scripts/triboss.lua
@@ -1,0 +1,6 @@
+trx.events.on_pickup(function(pickup_item)
+  local item = trx.items[pickup_item + 1]
+  if item.object_id == trx.catalog.objects.quest_item_4 then
+    trx.rooms.flip_effect(trx.catalog.flip_effects.finish_level)
+  end
+end)

commit 77c3959e49638bb15959c1bb2ce831538edf22e5

objects/pickup: fix pickup list traversal
This fixes the game freezing if animated interactions are enabled
and Lara tries to pick up multiple items on the same tile. Unreleased
regression in 945e46d.

src/trx/game/objects/general/pickup.c
@@ -311,12 +311,13 @@ static void M_GetAllAtLaraPos(const ITEM *const item)
     int16_t pickup_num = Room_Get(item->room_num)->item_num;
     while (pickup_num != NO_ITEM) {
         ITEM *const check_item = Item_Get(pickup_num);
+        const int16_t next_item_num = check_item->next_item;
         if (check_item->pos.x == item->pos.x && check_item->pos.z == item->pos.z
             && Object_Get(check_item->object_id)->collision_func
                 == Pickup_Collision) {
             M_DoPickup(pickup_num);
         }
-        pickup_num = check_item->next_item;
+        pickup_num = next_item_num;
     }
 }
 