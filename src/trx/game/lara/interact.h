1c28fd2c8159c24403d00bb9ed82cef605cb644d
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-06-07 14:36:40 +0100

    lara/interact: introduce common control check

--- src/trx/game/lara/interact.h
+++ src/trx/game/lara/interact.h
@@ -2,4 +2,7 @@
 
 #include <trx/game/lara/enum.h>
 
+#include <stdint.h>
+
 bool Lara_Interact_CanBegin(LARA_INTERACT_MODE mode);
+bool Lara_Interact_CanControl(LARA_INTERACT_MODE mode, int16_t item_num);
