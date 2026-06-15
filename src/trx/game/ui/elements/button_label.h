f91cc694c36249bad0b7b1a9e7cd7ce1bb83357f
Dash <dash@bad.pink>
2026-06-15 13:09:54 +0200

    ui: make examine button hints consistent

--- src/trx/game/ui/elements/button_label.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <trx/game/input.h>
-#include <trx/game/ui/common.h>
-
-void UI_ButtonLabel(INPUT_ROLE input_role, const char *label);
-void UI_ButtonLabelEx(const char *input_label, const char *label);
