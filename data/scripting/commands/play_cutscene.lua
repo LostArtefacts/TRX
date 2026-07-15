6b08634ebcab79c5dda7688e1351cbd7fcc1598a
Dash <dash@bad.pink>
2026-07-15 23:55:49 +0200

    console: port play_cutscene to Lua

---
+++ data/scripting/commands/play_cutscene.lua
@@ -0,0 +1,36 @@
+-- Plays a cutscene by number.
+--
+-- Usages:
+--   /cut 1
+
+local function run(args)
+  if args == "" then
+    return trx.console.Result.BAD_INVOCATION
+  end
+
+  local num = tonumber(args)
+  if num == nil or num % 1 ~= 0 then
+    return trx.console.Result.BAD_INVOCATION
+  end
+
+  local cutscenes = trx.game.cutscenes
+  if num < 1 or num > #cutscenes then
+    return trx.console.Result.FAILURE,
+      trx.locale.get("console/cmd/play_cutscene/invalid")
+  end
+
+  trx.game.play_cutscene(num)
+  return trx.console.Result.OK,
+    trx.locale.format(
+      "console/cmd/play_cutscene/loading",
+      cutscenes[num].num + 1
+    )
+end
+
+for _, name in ipairs({ "cut", "cutscene" }) do
+  trx.console.register({
+    name = name,
+    help = "console/cmd/play_cutscene/help",
+    run = run,
+  })
+end
