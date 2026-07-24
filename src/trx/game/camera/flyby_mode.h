986485498ecb306e9dfd5414c2ecb2c8ffa6345f
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-07-24 17:10:11 +0100

    camera/flyby_mode: perform safe reset
    This moves resetting the current flyby sequence into its own function
    to cater for cases when it's called when unloading a level.

--- src/trx/game/camera/flyby_mode.h
+++ src/trx/game/camera/flyby_mode.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 bool Camera_FlybyMode_Activate(int32_t sequence, bool one_shot);
+void Camera_FlybyMode_Reset(void);
 void Camera_FlybyMode_Deactivate(void);
 bool Camera_FlybyMode_IsActive(void);
 bool Camera_Flybymode_Cancel(void);
