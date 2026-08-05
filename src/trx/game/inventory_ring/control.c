LostArtefacts/TRX

commit 943412f0ffb9a99a13e27868f617c8606d18a835

inv-ring: fix examined item rotations

src/trx/game/inventory_ring/control.c
@@ -874,6 +874,11 @@ INV_RING *InvRing_Open(const INVENTORY_MODE mode)
         Inv_InsertItem(InvRing_GetByObjectID(O_PHOTO_OPTION));
     }
 
+    g_InvRing_Source[RT_KEYS].current = 0;
+    for (int32_t i = 0; i < g_InvRing_Source[RT_KEYS].count; i++) {
+        InvRing_InitInvItem(g_InvRing_Source[RT_KEYS].items[i]);
+    }
+
     g_InvRing_Source[RT_MAIN].current = 0;
     for (int32_t i = 0; i < g_InvRing_Source[RT_MAIN].count; i++) {
         InvRing_InitInvItem(g_InvRing_Source[RT_MAIN].items[i]);

commit f5519b228adf394b3f7c6765b1a5d496bc5637e3

inventory_ring/control: restore item duplication glitch
This fixes being unable to perform the item duplication
glitch. Regression in 943412f.

Resolves TRX937.

src/trx/game/inventory_ring/control.c
@@ -928,7 +928,9 @@ INV_RING *InvRing_Open(const INVENTORY_MODE mode)
         }
     }
 
-    g_InvRing_Source[RT_KEYS].current = 0;
+    if (g_Config.gameplay.fix_item_duplication_glitch) {
+        g_InvRing_Source[RT_KEYS].current = 0;
+    }
     for (int32_t i = 0; i < g_InvRing_Source[RT_KEYS].count; i++) {
         InvRing_InitInvItem(g_InvRing_Source[RT_KEYS].items[i]);
     }